2853c743be82badb8d3f0cdca4be986432ce1de4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:59 +0000

    Add package mohou/0.0.8/mohou-0.0.8.tar.gz

---
+++ packages/mohou/mohou-0.0.8.tar.gz/mohou-0.0.8/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+setup_requires = []
+
+install_requires = [
+    "numpy",
+    "psutil",
+    "torch",
+    "torchvision",
+    "tinyfk",
+    "tqdm",
+    "matplotlib",
+    "albumentations",
+    "opencv-python",
+    "pybullet",
+    'imageio==2.15.0;python_version<"3.7"',  # dependency of moviepy
+    "moviepy",
+]
+
+setup(
+    name='mohou',
+    version='0.0.8',
+    description='Visuomotor imitation learning framework',
+    author='Hirokazu Ishida',
+    author_email='h-ishida@jsk.imi.i.u-tokyo.ac.jp',
+    license='MIT',
+    install_requires=install_requires,
+    packages=find_packages(exclude=('tests', 'docs')),
+    package_data={'mohou': ['py.typed']}
+)
