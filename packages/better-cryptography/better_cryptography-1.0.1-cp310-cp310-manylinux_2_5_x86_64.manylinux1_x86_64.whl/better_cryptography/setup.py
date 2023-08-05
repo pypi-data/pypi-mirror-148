2d35118707ca679759c5cc766911ecc73fdd9a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:27 +0000

    Add package better-cryptography/1.0.1/better_cryptography-1.0.1-cp310-cp310-manylinux_2_5_x86_64.manylinux1_x86_64.whl

---
+++ packages/better-cryptography/better_cryptography-1.0.1-cp310-cp310-manylinux_2_5_x86_64.manylinux1_x86_64.whl/better_cryptography/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
+VERSION = "1.0.1"
+DESCRIPTION = "A module for encryption and information security operations."
+setup(
+    name= "better cryptography",
+    author = 'Wyatt Garrioch',
+    author_email = "w.garrioch456@gmail.com",
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = open("/home/eternal_blue/better_cryptography/README.md").read(),
+    long_description_content_type="text/markdown",
+    packages = find_packages(),
+    install_requires = ['pycryptodome', "rsa", "cryptography"],
+    ext_modules=cythonize(["better_cryptography/file_encryption.pyx"]),
+    keywords=["python", "encryption", "AES", "ciphers", "encryption alogorithms", "information security"],
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ]
+    
+)
