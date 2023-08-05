90568f29587053cf86e26dbb307dfc6dc47fe10c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:10 +0000

    Add package python-pyster/0.1.1/python-pyster-0.1.1.tar.gz

---
+++ packages/python-pyster/python-pyster-0.1.1.tar.gz/python-pyster-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+setup(
+    name = 'python-pyster',
+    version = '0.1.1',
+    license = 'MIT',
+    description = 'Python unit testing made easy with pyster!',
+    author = 'Wrench56',
+    author_email = 'dmarkreg@gmail.com',
+    url = 'https://github.com/Wrench56/pyster',
+    install_requires = ['rich'],
+    scripts = [
+        'pysterminal.py',
+        '__init__.py',
+        'endreport.py',
+        'options.py',
+        'pyster.py',
+        'errors/test_failure.py',
+        'errors/non_overridable_error.py'
+        ],
+    long_description = 'Please find more information on my Github page!',
+    entry_points={
+         "console_scripts": [
+            "pysterminal=pysterminal"
+        ]
+    },
+    packages=['errors']
+)
