ac02948a9615d8be69e1fd2862b115af61d7dab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:49 +0000

    Add package opteryx/0.0.0a71/opteryx-0.0.0a71-cp38-cp38-win_amd64.whl

---
+++ packages/opteryx/opteryx-0.0.0a71-cp38-cp38-win_amd64.whl/opteryx/engine/planner/operations/distinct_node.py
@@ -0,0 +1,49 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Distinct Node
+
+This is a SQL Query Execution Plan Node.
+
+This Node eliminates duplicate records.
+"""
+from typing import Iterable
+from pyarrow import concat_tables, Table
+from opteryx.engine.query_statistics import QueryStatistics
+from opteryx.engine.planner.operations import BasePlanNode
+from opteryx.third_party.pyarrow_ops import drop_duplicates
+
+
+class DistinctNode(BasePlanNode):
+    def __init__(self, statistics: QueryStatistics, **config):
+        self._distinct = config.get("distinct", True)
+
+    @property
+    def config(self):
+        return ""
+
+    def greedy(self):
+        return True
+
+    @property
+    def name(self):
+        return "Distinction"
+
+    def execute(self, data_pages: Iterable) -> Iterable:
+
+        if isinstance(data_pages, Table):
+            data_pages = [data_pages]
+
+        if self._distinct:
+            yield drop_duplicates(concat_tables(data_pages))
+        yield from data_pages
