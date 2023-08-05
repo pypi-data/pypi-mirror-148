d3b062655ee2cb75465a2ee4afa48f7a2c06a4be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:54 +0000

    Add package clime-bus-factor/0.3.2/clime-bus-factor-0.3.2.tar.gz

---
+++ packages/clime-bus-factor/clime-bus-factor-0.3.2.tar.gz/clime-bus-factor-0.3.2/clime_bus_factor/args.py
@@ -0,0 +1,109 @@
+from argparse import ArgumentParser, Namespace
+
+name: str = "CLIME"
+authors: list = [
+    "Nicholas M. Synovic",
+    "Matthew Hyatt",
+    "George K. Thiruvathukal",
+]
+
+
+def mainArgs() -> Namespace:
+    parser: ArgumentParser = ArgumentParser(
+        prog=f"{name} Bus Factor Calculator",
+        description="A tool to calculate the bus factor of a Git repository",
+        epilog=f"Author(s): {', '.join(authors)}",
+    )
+
+    parser.add_argument(
+        "-i",
+        "--input",
+        type=str,
+        help="Commits JSON file. DEFAULT: ./commits_loc.json",
+        default="commits_loc.json",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="Output JSON file. DEFAULT: ./bus_factor.json",
+        type=str,
+        default="bus_factor.json",
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        help="Display version of the tool",
+        action="store_true",
+        default=False,
+    )
+
+    return parser.parse_args()
+
+
+def graphArgs() -> Namespace:
+    parser: ArgumentParser = ArgumentParser(
+        prog=f"{name} Bus Factor Grapher",
+        description="A tool to graph the bus factor of a repository",
+        epilog=f"Author(s): {', '.join(authors)}",
+    )
+
+    parser.add_argument(
+        "-i",
+        "--input",
+        help=f"JSON export from clime-git-bus-factor-compute. DEFAULT: ./bus_factor.json",
+        type=str,
+        required=False,
+        default="bus_factor.json",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="Filename of the graph. DEFAULT: ./bus_factor.pdf",
+        type=str,
+        required=False,
+        default="bus_factor.pdf",
+    )
+    parser.add_argument(
+        "--type",
+        help="Type of figure to plot. DEFAULT: line",
+        type=str,
+        required=False,
+        default="line",
+    )
+    parser.add_argument(
+        "--title",
+        help='Title of the figure. DEFAULT: ""',
+        type=str,
+        required=False,
+        default="",
+    )
+    parser.add_argument(
+        "--x-label",
+        help='X axis label of the figure. DEFAULT: ""',
+        type=str,
+        required=False,
+        default="",
+    )
+    parser.add_argument(
+        "--y-label",
+        help='Y axis label of the figure. DEFAULT: ""',
+        type=str,
+        required=False,
+        default="",
+    )
+    parser.add_argument(
+        "--stylesheet",
+        help='Filepath of matplotlib stylesheet to use. DEFAULT: ""',
+        type=str,
+        required=False,
+        default="",
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        help="Display version of the tool",
+        action="store_true",
+        default=False,
+    )
+
+    return parser.parse_args()
