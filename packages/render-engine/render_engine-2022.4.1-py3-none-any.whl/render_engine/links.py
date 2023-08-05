ab5351e5da10c98cc271f2a07b1bae8aed63be9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:42 +0000

    Add package render-engine/2022.4.1/render_engine-2022.4.1-py3-none-any.whl

---
+++ packages/render-engine/render_engine-2022.4.1-py3-none-any.whl/render_engine/links.py
@@ -0,0 +1,72 @@
+import typing
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Link:
+    """
+    An opinionated format to reference links. Great for creating headers
+
+    Look at the example::
+
+        myImage = Link(
+            name="Render Engine",
+            url="https://render-engine.site"
+            alt="example image"
+            )
+
+
+    In this case the name and url give you enough information that you can supply to your template::
+
+        <a href="{{myImage.url}}">{{myImage.text}}</a>
+
+    Any attribute could be applied but here are some that you can supply on initialization.
+    """
+
+    text: str = field(kw_only=True, default_factory=str)
+    url: str = field(kw_only=True, default="#")
+    meta: dict[str, str] = field(default_factory=dict)
+    
+    def __str__(self):
+        """href is the common """
+
+        if self.meta:
+            attrs = " ".join([f'{k}="{v}"' for k,v in self.meta.items()])
+            starting_path = f'<a href="{self.url}" {attrs}>'
+        
+        else:
+            starting_path = f'<a href="{self.url}">'
+
+        return f'{starting_path}{self.text}</a>'
+        
+
+@dataclass
+class Image(Link):
+    """
+    Link object formatted as an image
+
+    Look at the example::
+
+        myImage = Link(
+            name="Render Engine",
+            url="https://render-engine.site"
+            alt="example image"
+            )
+
+
+    In this case the name and url give you enough information that you can supply to your template::
+
+        <a href="{{myImage.url}}">{{myImage.text}}</a>
+
+    Any attribute could be applied but here are some that you can supply on initialization.
+    """
+
+    def __str__(self):
+        """prints the image point"""
+
+        if self.meta:
+            attrs = " ".join([f'{k}="{v}"' for k,v in self.meta.items()])
+            return f'<img src="{self.url}" alt="{self.text}" {attrs} />'
+        
+        else:
+            return f'<img src="{self.url}" alt="{self.text}" />'
