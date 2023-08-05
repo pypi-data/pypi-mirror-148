f8d067a593b34abe0f3699326907448edc060f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:28 +0000

    Add package MeUtils/2022.4.25.15.27.49/MeUtils-2022.4.25.15.27.49.tar.gz

---
+++ packages/MeUtils/MeUtils-2022.4.25.15.27.49.tar.gz/MeUtils-2022.4.25.15.27.49/meutils/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : X.
+# @File         : __init__.py
+# @Time         : 2020/11/12 10:54 上午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  :
+
+
+import time
+
+__version__ = time.strftime("%Y.%m.%d.%H.%M.%S", time.localtime())
