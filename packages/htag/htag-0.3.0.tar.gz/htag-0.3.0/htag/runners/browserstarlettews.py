b8e118573779734536999f1f484f937249c35ff3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:02 +0000

    Add package htag/0.3.0/htag-0.3.0.tar.gz

---
+++ packages/htag/htag-0.3.0.tar.gz/htag-0.3.0/htag/runners/browserstarlettews.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# #############################################################################
+# Copyright (C) 2022 manatlan manatlan[at]gmail(dot)com
+#
+# MIT licence
+#
+# https://github.com/manatlan/htag
+# #############################################################################
+
+from .. import Tag
+from ..render import HRenderer
+
+
+import webbrowser,os,json
+import uvicorn
+from starlette.applications import Starlette
+from starlette.responses import HTMLResponse
+
+
+class BrowserStarletteWS:
+    """ Simple ASync Web Server (with starlette) with WebSocket interactions with HTag.
+        Open the rendering in a browser tab.
+    """
+    def __init__(self,tag:Tag):
+        js = """
+async function interact( o ) {
+    ws.send( JSON.stringify(o) );
+}
+
+var ws = new WebSocket("ws://localhost:8000/ws");
+ws.onopen = start;
+ws.onmessage = function(e) {
+    action( JSON.parse(e.data) );
+};
+"""
+
+        self.renderer=HRenderer(tag, js, lambda: os._exit(0))
+
+
+    def run(self):
+        app = Starlette()
+
+        @app.route('/')
+        async def homepage(request):
+            return HTMLResponse( str(self.renderer) )
+
+        @app.websocket_route('/ws')
+        async def websocket(websocket):
+            await websocket.accept()
+            while True:
+                mesg = await websocket.receive_text()
+                data = json.loads(mesg)
+                actions = await self.renderer.interact(data["id"],data["method"],data["args"],data["kargs"])
+                await websocket.send_text( json.dumps(actions) )
+            await websocket.close()
+
+        webbrowser.open_new_tab("http://127.0.0.1:8000")
+        uvicorn.run(app)
