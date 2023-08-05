94284372e3c503996736b7fc62cdceb250235c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:20 +0000

    Add package nwon-django-toolbox/0.2.5/nwon_django_toolbox-0.2.5.tar.gz

---
+++ packages/nwon-django-toolbox/nwon_django_toolbox-0.2.5.tar.gz/nwon_django_toolbox-0.2.5/nwon_django_toolbox/typings/__init__.py
@@ -0,0 +1,12 @@
+from nwon_django_toolbox.typings.celery import CeleryFolder, CeleryReturn
+from nwon_django_toolbox.typings.django_ready_enum import DjangoReadyEnum
+from nwon_django_toolbox.typings.pydantic_base_django import PydanticBaseDjango
+from nwon_django_toolbox.typings.test_fixture import TestFixture
+
+__all__ = [
+    "CeleryFolder",
+    "CeleryReturn",
+    "TestFixture",
+    "DjangoReadyEnum",
+    "PydanticBaseDjango",
+]
