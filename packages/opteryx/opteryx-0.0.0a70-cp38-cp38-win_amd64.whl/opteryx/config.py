8ffbafc741edeba66f60f4e3df1869cd233ea6dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:39 +0000

    Add package opteryx/0.0.0a70/opteryx-0.0.0a70-cp38-cp38-win_amd64.whl

---
+++ packages/opteryx/opteryx-0.0.0a70-cp38-cp38-win_amd64.whl/opteryx/config.py
@@ -0,0 +1,14 @@
+import os
+import dotenv
+from pathlib import Path
+
+try:
+    env_path = Path(".") / ".env"
+    dotenv.load_dotenv(dotenv_path=env_path)
+except:
+    pass  # fail quietly
+
+# The maximum input frame size for JOINs
+INTERNAL_BATCH_SIZE: int = int(os.environ.get("INTERNAL_BATCH_SIZE", 500))
+# The maximum number of records to create in a CROSS JOIN frame
+MAX_JOIN_SIZE: int = int(os.environ.get("MAX_JOIN_SIZE", 1000000))
