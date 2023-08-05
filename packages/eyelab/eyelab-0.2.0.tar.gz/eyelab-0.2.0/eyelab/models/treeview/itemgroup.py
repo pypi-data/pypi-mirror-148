c509edf1532ed818ffc2585e4573c93fd516c00c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:10 +0000

    Add package eyelab/0.2.0/eyelab-0.2.0.tar.gz

---
+++ packages/eyelab/eyelab-0.2.0.tar.gz/eyelab-0.2.0/eyelab/models/treeview/itemgroup.py
@@ -0,0 +1,23 @@
+from PySide6 import QtCore, QtWidgets
+
+
+class ItemGroup(QtWidgets.QGraphicsItem):
+    def __init__(self, meta: dict, parent=None):
+        """Provide data to create a new annotation or the id of an existing
+        annotation.
+        """
+        super().__init__(parent=parent)
+
+        self.meta = meta
+        self.setFlag(QtWidgets.QGraphicsItem.ItemHasNoContents, True)
+
+    def sync_with_volume(self):
+        if self.meta:
+            self.setVisible(self.meta["visible"])
+            self.setZValue(self.meta["z_value"])
+
+        for item in self.childItems():
+            item.sync_with_volume()
+
+    def boundingRect(self) -> QtCore.QRectF:
+        return self.childrenBoundingRect()
