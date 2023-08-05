eeb42000dae62eb54c0dd78a5e66a2cff4f91604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:17:07 +0000

    Add package pytorch-accelerated/0.1.25/pytorch_accelerated-0.1.25-py3-none-any.whl

---
+++ packages/pytorch-accelerated/pytorch_accelerated-0.1.25-py3-none-any.whl/pytorch_accelerated/__init__.py
@@ -0,0 +1,15 @@
+import logging
+from . import _version
+
+from accelerate import notebook_launcher as accelerate_notebook_launcher
+from accelerate.utils import set_seed
+from pytorch_accelerated.trainer import Trainer, TrainerPlaceholderValues
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+logger.info("Setting random seeds")
+set_seed(42)
+notebook_launcher = accelerate_notebook_launcher
+
+__version__ = _version.get_versions()["version"]
