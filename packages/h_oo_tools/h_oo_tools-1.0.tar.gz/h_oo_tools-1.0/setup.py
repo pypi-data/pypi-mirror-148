b76ec861fe77d134d20351406d4133b8f9abd8a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:22 +0000

    Add package h_oo_tools/1.0/h_oo_tools-1.0.tar.gz

---
+++ packages/h_oo_tools/h_oo_tools-1.0.tar.gz/h_oo_tools-1.0/setup.py
@@ -0,0 +1,10 @@
+﻿from setuptools import setup
+setup(name='h_oo_tools',
+      version='1.0',
+      description="threading tools and callback tools",
+      url='',
+      author='herming',
+      author_email='an520999@gmail.com',
+      license='JUST',
+      packages=['h_oo_tools'],
+      zip_safe=False)
