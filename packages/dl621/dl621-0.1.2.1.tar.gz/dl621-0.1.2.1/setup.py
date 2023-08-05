a00b02c4f8b378d43bc34ae2edd4cf5b856be4a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:40 +0000

    Add package dl621/0.1.2.1/dl621-0.1.2.1.tar.gz

---
+++ packages/dl621/dl621-0.1.2.1.tar.gz/dl621-0.1.2.1/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+    
+setup(
+    name='dl621',
+    version='0.1.2.1',
+    description='A simple library to download e621 images with embedded tags',
+    long_description=readme,
+    author='Ella Jameson',
+    author_email='ellagjameson@gmail.com',
+    url='https://github.com/nimaid/python-dl621',
+    license=license,
+    install_requires=["imgtag>=0.1.1"],
+    packages=find_packages(exclude=('tests', 'docs')),
+    entry_points={
+        'console_scripts': [
+            'e621-dl = dl621.core:run',
+        ]
+    }
+)
+
