31523d3dd3acc77ecade7c7c68a9c6f24b8a0474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:17:25 +0000

    Add package jina-now/0.0.3.dev71/jina-now-0.0.3.dev71.tar.gz

---
+++ packages/jina-now/jina-now-0.0.3.dev71.tar.gz/jina-now-0.0.3.dev71/now/frontend/deploy-frontend-docker.sh
@@ -0,0 +1,9 @@
+docker build -t now-frontend .
+
+docker tag now-frontend jinaaitmp/now-frontend:0.0.2
+docker push jinaaitmp/now-frontend:0.0.2
+
+#docker run -it --rm \
+#--name jina-now-frontend \
+#-p 80:80 \
+#now-frontend
