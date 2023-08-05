fb45a0f6d6d61aac23af87af228263f68da013c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:48 +0000

    Add package clip-server/0.3.0/clip-server-0.3.0.tar.gz

---
+++ packages/clip-server/clip-server-0.3.0.tar.gz/clip-server-0.3.0/clip_server/helper.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+__resources_path__ = os.path.join(
+    os.path.dirname(
+        sys.modules.get('clip_server').__file__
+        if 'clip_server' in sys.modules
+        else __file__
+    ),
+    'resources',
+)
