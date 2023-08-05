dddfad19a4c5ad8c4591b3889e8af14daf0fcf40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:59 +0000

    Add package ps-typer/0.1.6/ps_typer-0.1.6-py3-none-any.whl

---
+++ packages/ps-typer/ps_typer-0.1.6-py3-none-any.whl/ps_typer/type_test/statistics.py
@@ -0,0 +1,174 @@
+import datetime
+import time
+from calendar import month_abbr
+from typing import List
+
+import pyqtgraph
+from PyQt5 import QtWidgets
+from PyQt5.QtGui import QColor
+
+from ps_typer.ui import stats_window
+
+# CONSTANTS
+AXIS_WIDTH = 1.5
+CURVE_WIDTH = 2.5
+SYMBOL_WIDTH = 7
+GRID_ALPHA = 90
+
+
+class StatsWindow(QtWidgets.QWidget, stats_window.Ui_statsWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.setupUi(self)
+
+    def _get_qcolors(self, colours: dict):
+        """Converts the given dict of colours to a dict of QColor objects."""
+
+        qcolours = dict()
+        for name, colour in colours.items():
+            qcolours[name] = QColor(colour)
+
+        return qcolours
+
+    def update_days_ago(self, days_ago: int) -> None:
+        """Updates labelDaysAgo with a given number of days."""
+
+        self.labelDaysAgo.setText(f"- {str(days_ago)} days ago")
+
+    def set_up_graph(self, data: List[str], colours: dict) -> None:
+        """Sets up the graphView wpm over time graph with the given data."""
+
+        self.colours = self._get_qcolors(colours)
+
+        # Set up axes
+        self.graphView.setLabel("left", "WPM")
+        self.graphView.setAxisItems({"bottom": DateAxisItem(orientation="bottom")})
+
+        # Save axes items to instance variables so they can be easily modified
+        self.left_axis = self.graphView.getAxis("left")
+        self.bottom_axis = self.graphView.getAxis("bottom")
+
+        # Set the colours for the graph
+        self.graphView.setBackground(None)
+        self._set_axes_style(self.colours["axes"])
+
+        # Set up curve of wpm against date
+        self.curve = self.graphView.plot()
+        self._set_data(data)
+        self._update_graph()
+
+    # Private Methods
+    def _set_axes_style(
+        self, colour: tuple, width=AXIS_WIDTH, grid_alpha=GRID_ALPHA
+    ) -> None:
+        """
+        Sets the graph's axes colours to the provided tuple (rgb) and
+        sets their alpha value.
+        """
+
+        self.left_axis.setTextPen(color=colour)
+        self.left_axis.setPen(color=colour, width=width)
+        self.left_axis.setGrid(grid_alpha)
+
+        self.bottom_axis.setTextPen(color=colour)
+        self.bottom_axis.setPen(color=colour, width=width)
+        self.bottom_axis.setGrid(grid_alpha)
+
+    def _get_time_stamp(self, datetime_object: datetime.datetime) -> int:
+        """Returns a timestamp (int) from a given datetime object."""
+
+        return int(time.mktime(datetime_object.timetuple()))
+
+    def _get_datetime_object(self, date_str: str) -> datetime.datetime:
+        """Takes a string in the format yyyy-mm-dd: and returns a datetime object."""
+
+        raw_date: List[str] = date_str.replace(":", "").split("-")
+        date_ints: List[int] = list(map(int, raw_date))
+
+        return datetime.datetime(date_ints[0], date_ints[1], date_ints[2])
+
+    def _clean_date(self, date_str: str) -> int:
+        """
+        Takes a string in the format 'yyyy-mm-dd:' and returns a
+        usable timestamp representation.
+        """
+
+        return self._get_time_stamp(self._get_datetime_object(date_str))
+
+    def _set_data(self, data: List[str]) -> None:
+        """
+        Sets self.dates and self.wpms from the given data for
+        daily highscores.
+
+        Strings in the data list should be in the format 'yyyy-mm-dd: WPM'.
+        """
+
+        self.dates: List[int] = []
+        self.wpms: List[int] = []
+
+        # Add data for each day (data point) to 2 separate lists (2 axes)
+        for day in data:
+            split_day: List[str] = day.split()
+
+            self.dates.append(self._clean_date(split_day[0]))
+            self.wpms.append(int(split_day[1]))
+
+    def _update_graph(self, curve_width: int = CURVE_WIDTH) -> None:
+        """Updates self.curve with the data set in self._set_data."""
+
+        self.curve.setData(
+            x=self.dates,
+            y=self.wpms,
+            pen=pyqtgraph.mkPen(color=self.colours["curve"], width=curve_width),
+            symbolBrush=pyqtgraph.mkBrush(color=self.colours["curve"]),
+            symbolPen=pyqtgraph.mkPen(color=self.colours["curve"]),
+            symbolSize=SYMBOL_WIDTH,
+        )
+
+
+class DateAxisItem(pyqtgraph.AxisItem):
+    """
+    Axis item to replace the x-axis in the above class's graphView item
+    so that strings are used to represent the time stamp values plotted.
+    """
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+        # Set up label for axis
+        self.setLabel(text="Date Set", units=None)
+        self.enableAutoSIPrefix(False)
+
+    def _get_string(self, time_stamp: int) -> str:
+        """Gets the string value which is to replace the given time_stamp value."""
+
+        # types
+        datetime_object: datetime.datetime
+        list_from_time: List[str]
+        month_name: str
+
+        datetime_object = datetime.datetime.fromtimestamp(time_stamp)
+        list_from_time = datetime_object.strftime("%m %d").split()
+
+        month_name = month_abbr[int(list_from_time[0])]
+
+        return " ".join((month_name, list_from_time[1]))
+
+    def tickStrings(self, values, scale, spacing):
+        """
+        Will change the axis tick values to be strings which represent
+        the given time stamp values in the format of Month Day.
+        """
+
+        return [self._get_string(value) for value in values]
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    window = StatsWindow()
+    window.show()
+
+    app.exec_()
---
+++ packages/ps-typer/ps_typer-0.1.6-py3-none-any.whl/ps_typer/type_test/type_test.py
@@ -0,0 +1,271 @@
+from time import perf_counter
+from typing import List
+
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtMultimedia import QSoundEffect
+
+from ps_typer.type_test import highscores, results, texts
+from ps_typer.ui import typing_window
+
+# Constants
+DEFAULT_COLOURS = ["rgb(0, 100, 0)", "rgb(100, 0, 0)"]
+_TRANSLATE_RESULT = {
+    "all-time": "New all-time highscore set! Congratulations!",
+    "daily": "New daily highscore set! Good job!",
+    "none": "No new highscore set. Don't give up!",
+}
+
+
+class TypingWindow(QtWidgets.QWidget, typing_window.Ui_typingWindow):
+    def __init__(self, highscore_obj: highscores.Highscores, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Multiple inheritance allows us to have the ui and window together so
+        # setupui can be given self in for a window
+        self.setupUi(self)
+
+        self.lineInput.textChanged.connect(self._on_input_text_changed)
+        self.buttonNewText.clicked.connect(self.on_clicked_new)
+        self.buttonRestart.clicked.connect(self.on_clicked_restart)
+
+        # time and timer
+        self.timer = QtCore.QTimer(self)
+        self.timer.timeout.connect(self._update_time)
+        self.timer.setInterval(100)
+        self._reset_time()
+
+        # Object to handle saving and updating of highscore values
+        self.highscore = highscore_obj
+
+        # Defaults
+        self.key_sound = QSoundEffect()
+        self.set_colours(DEFAULT_COLOURS)
+
+    # Public Methods
+    def set_mode(self, mode: str) -> None:
+        """
+        Sets the mode for the typing window, by getting a specific generator
+        from texts.py and setting a title for the window.
+        """
+
+        self.mode = mode
+
+        self.labelTitle.setText(mode)
+
+        if mode == "Common Phrases":
+            self.labelMainText.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.text_generator = texts._translate[mode]()
+        self._set_main_text()
+
+    def set_colours(self, colours: List[str]) -> None:
+        """
+        Sets the colours to be used for rich text formatting.
+
+        Dictionary provided should be in the order:
+            [green_colour, red_colour]
+        """
+
+        self.colours = colours
+
+    def set_key_sound(self, key_sound: QSoundEffect) -> None:
+        """
+        Set the sound to be played on a keystroke (should be a QSoundEffect object).
+        """
+
+        self.key_sound = key_sound
+
+    # Private methods
+    def _set_main_text(self) -> None:
+        """
+        Sets the text to be typed out by the user by getting a value
+        from self.text_generator.
+
+        If the list of strings from self.text_generator is already
+        exhausted, a default warning string is set instead.
+        """
+
+        try:
+            self.text = next(self.text_generator)
+        except StopIteration:
+            self.text = "You have typed all the texts in this category!"
+
+        self.labelMainText.setText(self.text)
+
+    def _calculate_score(self, accuracy: int) -> int:
+        """Returns wpm score after calculations including accuracy."""
+        self.start_time: float
+
+        seconds: float = perf_counter() - self.start_time
+
+        return round(((len(self.text) / 5) / (seconds / 60)) * accuracy / 100)
+
+    def _calculate_accuracy(self, input_text: str) -> int:
+        """Returns accuracy as an int between 1-100 representing a percentage."""
+
+        correct: int = 0
+        for i, character in enumerate(input_text):
+            if character == self.text[i]:
+                correct += 1
+
+        return int(correct / len(self.text) * 100)
+
+    def _set_stats(self, input_text: str) -> None:
+        """Sets instance variables for wpm score and accuracy."""
+
+        self.accuracy = self._calculate_accuracy(input_text)
+        self.wpm = self._calculate_score(self.accuracy)
+
+    def _display_highscore_result(self) -> None:
+        """
+        Updates the highscore in the highscore object and displays the result.
+        """
+
+        self.highscore_result = self.highscore.update(self.wpm)
+        self.results_window.labelHighscoreSet.setText(
+            _TRANSLATE_RESULT[self.highscore_result]
+        )
+
+    def _get_rich_text(self, input_text: str) -> str:
+        """
+        Returns the rich text to be displayed so characters typed correctly are
+        highlighted green while incorrect characters are highlighted red.
+        """
+
+        typed_text = []
+        rest_of_text = self.text[len(input_text) :]
+
+        for i, character in enumerate(input_text):
+            if self.text[i] == character:
+                typed_text.append(
+                    f'<span style="background-color:{self.colours[0]};">{self.text[i]}</span>'
+                )
+            else:
+                typed_text.append(
+                    f'<span style="background-color:{self.colours[1]};">{self.text[i]}</span>'
+                )
+
+        rich_text = (
+            "<html><head/><body><p>"
+            f'{"".join(typed_text)}'
+            f"{rest_of_text}</p></body></html>"
+        )
+
+        return rich_text
+
+    def _update_time(self) -> None:
+        """
+        Updates the displayed time on self.labelTime in
+        seconds since self.start_time.
+        """
+
+        self.labelTime.setText(str(int(perf_counter() - self.start_time)))
+
+    def _reset_time(self) -> None:
+        """Resets self.timer, self.start_time and self.labelTime."""
+
+        self.start_time = 0.0
+        self.timer.stop()
+        self.labelTime.setText("0")
+
+    def _make_results_window(self) -> None:
+        """Generates the results window."""
+
+        self.results_window = results.ResultsWindow()
+
+        self.results_window.setWindowIcon(self.windowIcon())
+
+        self.results_window.labelAccuracy.setText(f"{str(self.accuracy)}%")
+        self.results_window.labelSpeed.setText(f"{str(self.wpm)} WPM")
+
+        self._display_highscore_result()
+
+        self.results_window.buttonNext.clicked.connect(self.on_clicked_next)
+
+        # Apply same functionality as for the self.buttonMainMenu, which
+        # is set in main.py
+        self.results_window.buttonMainMenu.clicked.connect(
+            self.on_clicked_results_main_menu
+        )
+
+    def _on_finished(self, input_text: str) -> None:
+        """
+        Opens the results window, and is called when the user has typed
+        out all the given text.
+        """
+
+        self._set_stats(input_text)
+
+        self._make_results_window()
+
+        self.hide()
+        self.results_window.show()
+        if self.isMaximized():
+            self.results_window.setWindowState(QtCore.Qt.WindowMaximized)
+
+        # stylesheet for results window must be set after the window is shown
+        self.results_window.setStyleSheet(self.styleSheet())
+
+    def _on_input_text_changed(self, input_text: str) -> None:
+        """
+        Updates background of each letter as user types and calls a function when
+        the user is finished.
+        """
+
+        # Break out of function if the text characters are exceeded
+        # This is required to avoid an error if the user spams keys
+        # right at the end of the text they are typing
+        if len(input_text) > len(self.text):
+            return None
+
+        # Update displayed time or start timer
+        if not self.start_time:
+            self.timer.start()
+            self.start_time = perf_counter()
+
+        # Try to play key sound effect, if it exists
+        self.key_sound.play()
+
+        # Set label text to rich text so typed characters are highlighted
+        # based on whether they match self.text
+        rich_text = self._get_rich_text(input_text)
+        self.labelMainText.setText(rich_text)
+
+        if len(input_text) >= len(self.text):
+            self._on_finished(input_text)
+
+    # BUTTON METHODS
+    def on_clicked_restart(self) -> None:
+        self.lineInput.clear()
+        self._reset_time()
+
+    def on_clicked_new(self) -> None:
+        self.on_clicked_restart()
+        self._set_main_text()
+
+    def on_clicked_next(self) -> None:
+        self.show()
+        self.on_clicked_new()
+
+        self.results_window.close()
+        del self.results_window
+
+    def on_clicked_results_main_menu(self) -> None:
+        """
+        Clicks the typing window's main menu button and closes the results window.
+
+        This is done because the functionality for the main menu button is given
+        in main.py.
+        """
+
+        self.buttonMainMenu.click()
+        self.results_window.close()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    window = TypingWindow(highscores.Highscores())
+    window.show()
+
+    app.exec_()
