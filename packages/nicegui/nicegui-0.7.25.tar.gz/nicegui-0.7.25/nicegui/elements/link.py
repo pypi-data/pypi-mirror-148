79630ad13ca7954a2a7cc7595db46da746e7c93a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:44 +0000

    Add package nicegui/0.7.25/nicegui-0.7.25.tar.gz

---
+++ packages/nicegui/nicegui-0.7.25.tar.gz/nicegui-0.7.25/nicegui/elements/link.py
@@ -0,0 +1,22 @@
+from typing import Union
+
+import justpy as jp
+
+from .group import Group
+from .page import Page
+
+
+class Link(Group):
+
+    def __init__(self, text: str = '', target: Union[Page, str] = '#'):
+        """Link
+
+        Create a link.
+
+        :param text: link text
+        :param target: link target (either a string or a page object)
+        """
+        href = target if isinstance(target, str) else target.route
+        view = jp.A(text=text, href=href, classes='underline text-blue', temp=False)
+
+        super().__init__(view)
