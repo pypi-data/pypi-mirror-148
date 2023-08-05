2eddd4933b2ce73f5962bc4be7064af47d57bf45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:01 +0000

    Add package ooliver-botbase/1.12.3/ooliver-botbase-1.12.3.tar.gz

---
+++ packages/ooliver-botbase/ooliver-botbase-1.12.3.tar.gz/ooliver-botbase-1.12.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['botbase', 'botbase.coggies', 'botbase.wraps']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['botbase = botbase.cli:main']}
+
+setup_kwargs = {
+    'name': 'ooliver-botbase',
+    'version': '1.12.3',
+    'description': 'A personal nextcord bot base package for bots.',
+    'long_description': '# I\'m Sorry\n\nI wish you luck in trying to use this.\nFuture me or anyone\n\n## Oh yeah the config\n\ndb_enabled: bool default True\n\ndb_url: str either this or name\n\ndb_name: str either this or url\n\ndb_user: str default "ooliver"\n\nversion: str default "0.0.0"\n\naiohttp_enabled: bool default True\n\ncolor: int default 0x9966CC\n\nblacklist_enabled: bool default True\n\nprefix: str\n\nhelpmsg: str default defaulthelpmsg\n\nhelpindex: str default defaulthelpindex\n\nhelptitle: str default "Help Me!"\n\nhelpfields: dict[str, str] default {}\n\nhelpinsert: str default ""\n\nemojiset: Emojis[str, str] default Emojis()\n\nlogchannel: int default 921139782648725515\n\nguild_ids list[int] default None\n',
+    'author': 'ooliver1',
+    'author_email': 'oliverwilkes2006@icloud.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/ooliver1/botbase',
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
