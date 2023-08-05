e6e73e75ae96a97671929c013f4d2b56d7ebd15b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:56 +0000

    Add package pixel-artist/0.0.6/pixel_artist-0.0.6-py3-none-any.whl

---
+++ packages/pixel-artist/pixel_artist-0.0.6-py3-none-any.whl/pixel_artist/__main__.py
@@ -0,0 +1,36 @@
+"""Main entry point for the pixelation tool."""
+
+import sys
+
+from .constants import SUCCESS
+from .core import PixelArt
+from .parser import build_parser, parse_args
+
+
+def main() -> None:
+    """Parses the command line arguments and runs the tool."""
+
+    arg_parser = build_parser()
+    args = parse_args(arg_parser)
+
+    pixel_art = PixelArt(args['filename'],
+                         args['granularity'],
+                         args['ncolors'],
+                         args['nbits'],
+                         args['color_space'],
+                         args['verbose'])
+
+    pixelated_image = pixel_art.pixelate()
+    pixelated_image.show()
+    if args['save']:
+        if args['verbose']:
+            print("Saving to " + args['filename'].split(".")[0] + "_pixelated.png ...")
+            pixelated_image.save(args['filename'].split(".")[0] + "_pixelated.png")
+    if args['verbose']:
+        print('Done')
+
+    return SUCCESS
+
+
+if __name__ == "__main__":
+    sys.exit(main())
