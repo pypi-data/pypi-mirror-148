4505cc03ac891f3191b1844b503cf537fdaaa615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:17 +0000

    Add package blusutils/0.0.2/blusutils-0.0.2.tar.gz

---
+++ packages/blusutils/blusutils-0.0.2.tar.gz/blusutils-0.0.2/blusutils/__init__.py
@@ -0,0 +1,14 @@
+"""Blusutils
+~~~~~~
+Library with random functionality.
+"""
+
+from .collections import *
+from .syntax_constructions import *
+from .crypto import *
+from .other import *
+from .textutils import *
+import errors
+# from browser import Browser
+
+__version__ = '0.0.1.2'
---
+++ packages/blusutils/blusutils-0.0.2.tar.gz/blusutils-0.0.2/blusutils/crypto.py
@@ -0,0 +1,10 @@
+"""Module for encryptions"""
+
+def caesarCrypt(inp: str, pos: int) -> str:
+    pass
+
+# def codingEncrypt(inp: str):
+#     pass
+
+# def codingDecrypt(inp: str):
+#     pass
