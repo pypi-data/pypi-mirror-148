22ecf7adec5f4878a189887db9417145e6e50ebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:32 +0000

    Add package musicvideos/0.8.4/musicvideos-0.8.4.tar.gz

---
+++ packages/musicvideos/musicvideos-0.8.4.tar.gz/musicvideos-0.8.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.8.4'
+DESCRIPTION = 'Old slowedvideos now musicvideos, Various scripts from justcow.'
+
+# Setting up
+setup(
+    name="musicvideos",
+    version=VERSION,
+    author="JustCow",
+    author_email="<justcow@pm.me>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['moviepy', 'pydub', 'pedalboard', 'yt_dlp', 'pillow', 'soundfile', 'requests', 
+                    'google-api-python-client', 'google-auth-oauthlib', 'google-auth-httplib2', 'oauth2client'],
+    keywords=['python', 'video', 'audio'],
+    include_package_data=True ,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+    ]
+)
