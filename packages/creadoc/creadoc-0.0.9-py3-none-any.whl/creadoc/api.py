f46717eb219052997f18990be8f0eff05916f4d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:22 +0000

    Add package creadoc/0.0.9/creadoc-0.0.9-py3-none-any.whl

---
+++ packages/creadoc/creadoc-0.0.9-py3-none-any.whl/creadoc/api.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+from __future__ import absolute_import
+from django.db.models import Q
+from creadoc.models import CreadocReport
+
+
+def get_reports(on_date=None):
+    u"""
+    Список всех доступных отчетов
+    :param on_date: Дата, на которую ищутся актуальные отчеты
+    :return:
+    """
+    q = Q(state=True)
+
+    if on_date is not None:
+        q &= Q(created_at=on_date)
+
+    return CreadocReport.objects.filter(q)
