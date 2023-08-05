b9c03bf49f578b5f42167bdc0dda3a3432fee491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:07 +0000

    Add package strawberry-django-auth/0.2.1/strawberry_django_auth-0.2.1-py2.py3-none-any.whl

---
+++ packages/strawberry-django-auth/strawberry_django_auth-0.2.1-py2.py3-none-any.whl/gqlauth/user/queries.py
@@ -0,0 +1,22 @@
+from typing import Optional
+import strawberry
+from strawberry.django import auth
+from gqlauth.utils import g_user
+
+# project
+from .types import UserType, UserFilter
+from typing import List
+
+
+@strawberry.type
+class UserQueries:
+    user: Optional[UserType] = strawberry.django.field()
+    users: UserType = strawberry.django.field(filters=UserFilter)
+    me: UserType = strawberry.django.field(resolver=lambda info: g_user(info))
+
+    @strawberry.field
+    def public_user(self, info) -> Optional[UserType]:
+        user = g_user(info)
+        if user.is_authenticated:
+            return user
+        return None
