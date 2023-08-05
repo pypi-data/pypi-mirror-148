bad6f7bbf8985e59fdd37cf06e25906eb6579264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:19 +0000

    Add package disutils/1.4.22/disutils-1.4.22.tar.gz

---
+++ packages/disutils/disutils-1.4.22.tar.gz/disutils-1.4.22/disutils/__init__.py
@@ -0,0 +1,8 @@
+from . import Pagination
+from .InviteTracker import InviteTracker
+from .Music import Music, EmptyQueue, NotConnectedToVoice, NotPlaying
+
+__title__ = "disutils"
+__version__ = "1.4.22"
+__author__ = "toxicrecker"
+__license__ = "MIT"
---
+++ packages/disutils/disutils-1.4.22.tar.gz/disutils-1.4.22/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+import disutils
+
+
+with open("README.md", "r", encoding="utf-8", errors="ignore") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="disutils",
+    version=disutils.__version__,
+    author="pintermor9",
+    description="disutils is a very useful library made to be used with discord.py",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pintermor9/disutils/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">= 3.6",
+    include_package_data=True,
+    install_requires=["discord.py"],
+    extras_require={"voice": ["discord.py[voice]", "youtube-dl"]}
+)
