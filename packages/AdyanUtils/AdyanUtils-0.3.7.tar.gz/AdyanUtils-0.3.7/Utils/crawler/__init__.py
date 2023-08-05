7924e7c5d9382ae7ad9b248e4a3b80bbbd575121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:14 +0000

    Add package AdyanUtils/0.3.7/AdyanUtils-0.3.7.tar.gz

---
+++ packages/AdyanUtils/AdyanUtils-0.3.7.tar.gz/AdyanUtils-0.3.7/Utils/crawler/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/2/28 13:39
+# @Author  : Adyan
+# @File    : __init__.py.py
+
+
+from Utils import package
+
+pk_lst = [
+    'requests==2.27.1',
+    'Faker==13.2.0',
+    'Scrapy==2.5.1',
+    'scrapy-redis==0.7.2',
+    'Twisted==22.1.0',
+    'gerapy-pyppeteer==0.2.4',
+    'pytz==2021.3',
+    'xlrd==2.0.1',
+    'xlutils==2.0.0',
+    'xlwt==1.3.0',
+
+]
+package(pk_lst)
