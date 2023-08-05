79f5101acaa559b688ff5988981449b15fb1b68e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:30 +0000

    Add package pyquizAPI/0.0.7/pyquizAPI-0.0.7.tar.gz

---
+++ packages/pyquizAPI/pyquizAPI-0.0.7.tar.gz/pyquizAPI-0.0.7/pyquizAPI/client/utils.py
@@ -0,0 +1,18 @@
+import requests as r
+import json
+
+def make_request(api_key, url, config=None):
+    if config == None:
+        data = {
+            "apiKey": api_key
+        }
+    else:
+        data = config
+        data["apiKey"] = api_key
+    headers = {
+        "X-Api-Key": api_key
+    }
+    
+    response = r.get(url=url, data=data, headers=headers)
+
+    return response, response.json(), response.status_code
