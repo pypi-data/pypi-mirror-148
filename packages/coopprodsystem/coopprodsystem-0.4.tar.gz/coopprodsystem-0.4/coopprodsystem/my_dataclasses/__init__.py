3c13413b670dddc704c67573e39ad6c0881e41cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:28 +0000

    Add package coopprodsystem/0.4/coopprodsystem-0.4.tar.gz

---
+++ packages/coopprodsystem/coopprodsystem-0.4.tar.gz/coopprodsystem-0.4/coopprodsystem/my_dataclasses/__init__.py
@@ -0,0 +1,5 @@
+from .resource import Resource, ResourceType, resource_factory
+from .unitOfMeasure import UoM, UoMType
+from .resourceUom import ResourceUoM, resourceUom_factory
+from .content import Content, ContentFactoryException, content_factory
+from .locations import Location
