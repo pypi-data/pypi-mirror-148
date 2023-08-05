f764fd4d371abaa73ddadd55c6c7b5af76c50ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:35 +0000

    Add package chart-tools/0.0.8/chart_tools-0.0.8.tar.gz

---
+++ packages/chart-tools/chart_tools-0.0.8.tar.gz/chart_tools-0.0.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='chart_tools',
+    version='0.0.8',
+    license='MIT',
+    author="Ryan Young",
+    author_email='ryanyoung99@live.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/ryayoung',
+    keywords='seaborn correlation heatmap',
+    install_requires=[
+          'seaborn',
+          'matplotlib',
+          'pandas',
+          'numpy',
+          'requests',
+      ],
+
+)
---
+++ packages/chart-tools/chart_tools-0.0.8.tar.gz/chart_tools-0.0.8/src/chart_tools/__init__.py
@@ -0,0 +1,9 @@
+from chart_tools.heatmaps.superheat import superheat
+from chart_tools.utils import set_style
+
+# DATASETS
+from chart_tools.datasets.datasource import Source
+from chart_tools.datasets.datasource import DataSource
+from chart_tools.datasets.datasource import load_data
+from chart_tools.datasets.datasource import set_library
+from chart_tools.datasets.datasource import display_sources
