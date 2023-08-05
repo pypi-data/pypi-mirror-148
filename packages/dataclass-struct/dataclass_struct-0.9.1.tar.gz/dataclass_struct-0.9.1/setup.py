f77d856bd9eb351595a9892c5a7813cf47d0a8ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:43:13 +0000

    Add package dataclass-struct/0.9.1/dataclass_struct-0.9.1.tar.gz

---
+++ packages/dataclass-struct/dataclass_struct-0.9.1.tar.gz/dataclass_struct-0.9.1/setup.py
@@ -0,0 +1,25 @@
+"""
+Decorator providing capability to emit and read a dataclass as a binary buffer.
+"""
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    readme_description = fh.read()
+
+setup(
+    name='dataclass_struct',
+    long_description=readme_description,
+    long_description_content_type="text/markdown",
+    version='0.9.1',
+    license='MIT',
+    author="Peter Krahulik",
+    author_email='peter.krahulik@googlemail.com',
+    py_modules=["dataclass_struct"],
+    url='https://https://github.com/Thinkeye/dataclass_struct',
+    keywords='dataclass, struct',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
