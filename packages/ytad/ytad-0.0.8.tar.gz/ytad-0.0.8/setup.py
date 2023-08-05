4737943e14d58e26ac8c010ab1bcf0652871f520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:50 +0000

    Add package ytad/0.0.8/ytad-0.0.8.tar.gz

---
+++ packages/ytad/ytad-0.0.8.tar.gz/ytad-0.0.8/setup.py
@@ -0,0 +1,39 @@
+#!/bin/env python
+"""Packaging setup script."""
+
+from setuptools import setup, find_packages
+import versioneer
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="ytad",
+    long_description=README,
+    long_description_content_type='text/markdown',
+    description="A tool for adding YouTube video like/dislike counts to video descriptions.",
+    url="https://github.com/SpencerPao/YouTube_Automation",
+    author="Spencer Pao",
+    author_email="business.inquiry.spao@gmail.com",
+    license="MIT",
+    packages=['ytad', 'ytad.cli'],
+    entry_points={'console_scripts': [
+        "uvd=ytad.cli.update_notifications:main"]},
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy",
+        "jupyter",
+        "pandas",
+        "python-dotenv",
+        "google-api-python-client",
+        "google-auth",
+        "google-auth-oauthlib",
+        "google-auth-httplib2",
+        "boto3",
+        "pytz",
+    ],
+)
