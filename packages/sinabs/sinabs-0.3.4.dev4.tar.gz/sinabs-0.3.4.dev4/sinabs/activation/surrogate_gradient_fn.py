f9f8e518c46fae9f9e8009e1a53f30b68215fd0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:35 +0000

    Add package sinabs/0.3.4.dev4/sinabs-0.3.4.dev4.tar.gz

---
+++ packages/sinabs/sinabs-0.3.4.dev4.tar.gz/sinabs-0.3.4.dev4/sinabs/activation/surrogate_gradient_fn.py
@@ -0,0 +1,58 @@
+from dataclasses import dataclass
+import math
+import torch
+
+
+@dataclass
+class Heaviside:
+    """
+    Heaviside surrogat gradient with optional shift.
+
+    Parameters:
+        window:
+            Distance between step of Heaviside surrogate gradient and
+            threshold, relative to threshold.
+    """
+
+    window: float = 1.0
+
+    def __call__(self, v_mem, spike_threshold):
+        return ((v_mem >= (spike_threshold - self.window)).float()) / spike_threshold
+
+
+@dataclass
+class MultiGaussian:
+    """
+    Surrogate gradient as defined in Yin et al., 2021.
+    https://www.biorxiv.org/content/10.1101/2021.03.22.436372v2
+    """
+
+    mu: float = 0.0
+    sigma: float = 0.5
+    grad_scale: float = 1.0
+
+    def __call__(self, v_mem, spike_threshold):
+        return (
+            torch.exp(-(((v_mem - spike_threshold) - self.mu) ** 2) / (2 * self.sigma**2))
+            / torch.sqrt(2 * torch.tensor(math.pi))
+            / self.sigma
+        ) * self.grad_scale
+
+
+@dataclass
+class SingleExponential:
+    """
+    Surrogate gradient as defined in Shrestha and Orchard, 2018
+    https://papers.nips.cc/paper/2018/hash/82f2b308c3b01637c607ce05f52a2fed-Abstract.html
+    """
+
+    grad_width: float = 0.5
+    grad_scale: float = 1.0
+
+    def __call__(self, v_mem, spike_threshold):
+        abs_width = spike_threshold * self.grad_width
+        return (
+            self.grad_scale
+            / abs_width
+            * torch.exp(-torch.abs(v_mem - spike_threshold) / abs_width)
+        )
