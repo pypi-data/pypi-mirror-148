601feb03b3a9ccd1a8f5d5c2c11d08e5d7e7c292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:25 +0000

    Add package gitnu/0.0.1a2/gitnu-0.0.1a2.tar.gz

---
+++ packages/gitnu/gitnu-0.0.1a2.tar.gz/gitnu-0.0.1a2/src/gitnu/log.py
@@ -0,0 +1,42 @@
+import warnings
+
+
+class bcolors:
+    GRAY = "\033[90m"
+    RED = "\033[91m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    BLUE = "\033[94m"
+    PURPLE = "\033[95m"
+    CYAN = "\033[96m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+
+def stringify(color):
+    def fn(*args, end="\n"):
+        text = " ".join(map(str, args))
+        print(color + text + bcolors.ENDC, end=end)
+
+    return fn
+
+
+def colored_warn(color):
+    def fn(*args):
+        text = " ".join(map(str, args))
+        gitnu = (color + "\n" + text + bcolors.ENDC, RuntimeWarning)
+        stay_calm_and = warnings.warn
+        stay_calm_and(*gitnu)
+
+    return fn
+
+
+gray = stringify(bcolors.GRAY)
+red = stringify(bcolors.RED)
+yellow = stringify(bcolors.YELLOW)
+green = stringify(bcolors.GREEN)
+blue = stringify(bcolors.BLUE)
+cyan = stringify(bcolors.CYAN)
+purple = stringify(bcolors.PURPLE)
+warn = colored_warn(bcolors.YELLOW)
