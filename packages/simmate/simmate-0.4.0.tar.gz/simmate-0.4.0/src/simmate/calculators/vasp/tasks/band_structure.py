7a9184074f67e5f84cd477526c5f51975cbba347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:49 +0000

    Add package simmate/0.4.0/simmate-0.4.0.tar.gz

---
+++ packages/simmate/simmate-0.4.0.tar.gz/simmate-0.4.0/src/simmate/calculators/vasp/tasks/band_structure.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from pymatgen.io.vasp.inputs import Kpoints
+from pymatgen.symmetry.bandstructure import HighSymmKpath
+from pymatgen.electronic_structure.plotter import BSPlotter
+
+from simmate.calculators.vasp.inputs import Incar, Poscar, Potcar
+from simmate.calculators.vasp.tasks.static_energy.materials_project import (
+    MatProjStaticEnergy,
+)
+
+
+class MatProjBandStructure(MatProjStaticEnergy):
+    """
+    Calculates the band structure using Materials Project settings.
+
+    Your stucture will be converted to the standardized-primitive unitcell so
+    that the high-symmetry K-point path can be used.
+
+    This is also a non self-consistent field (non-SCF) calculation and thus uses
+    the a fixed charge density from a previous static energy calculation.
+
+    We do NOT recommend running this calculation on its own. Instead, you should
+    use the full workflow, which handles the preliminary relaxation and SCF
+    energy calculation for you. This S3Task is only the final step of that workflow.
+
+    See `vasp.workflows.band_structure`.
+    """
+
+    # The default settings to use for this calculation.
+    # The key thing for band structures is that this follows a static energy
+    # calculation. We use the CHGCAR from that previous calculation. We also
+    # hold the charge density static during this calculation so this is a
+    # non-selfconsistent (non-SCF) calculation.
+    incar = MatProjStaticEnergy.incar.copy()
+    incar.update(
+        ICHARG=11,
+        ISMEAR=0,
+        SIGMA=0.01,
+    )
+
+    # set the KptGrid or KptPath object
+    # TODO: in the future, all functionality of this class will be available
+    # by giving a KptPath class here.
+    kpoints = None
+    kpoints_line_density = 20
+
+    # For band-structures, unit cells should be in the standardized format
+    pre_standardize_structure = True
+
+    def setup(self, structure, directory):
+        """
+        Writes input files for this calculation. This differs from the normal
+        VaspTask setup because it converts the structure to the standard primative
+        first and then writes a KPOINT file with using a highsym path.
+        """
+
+        # run cleaning and standardizing on structure (based on class attributes)
+        structure_cleaned = self._get_clean_structure(structure)
+
+        # write the poscar file
+        Poscar.to_file(structure_cleaned, os.path.join(directory, "POSCAR"))
+
+        # Combine our base incar settings with those of our parallelization settings
+        # and then write the incar file
+        incar = Incar(**self.incar) + Incar(**self.incar_parallel_settings)
+        incar.to_file(
+            filename=os.path.join(directory, "INCAR"),
+            structure=structure_cleaned,
+        )
+
+        ##############
+        # we need to find the high-symmetry Kpt path. Note that all of this
+        # functionality will be moved to the KptPath class and then extended to
+        # vasp.inputs.kpoints class. Until those classes are ready, we just use
+        # pymatgen here.
+        sym_prec = self.incar.get("SYMPREC", 1e-5) if self.incar else 1e-5
+        kpath = HighSymmKpath(structure_cleaned, symprec=sym_prec)
+        frac_k_points, k_points_labels = kpath.get_kpoints(
+            line_density=self.kpoints_line_density,
+            coords_are_cartesian=False,
+        )
+        kpoints = Kpoints(
+            comment="Non SCF run along symmetry lines",
+            style=Kpoints.supported_modes.Reciprocal,
+            num_kpts=len(frac_k_points),
+            kpts=frac_k_points,
+            labels=k_points_labels,
+            kpts_weights=[1] * len(frac_k_points),
+        )
+        kpoints.write_file(os.path.join(directory, "KPOINTS"))
+        ##############
+
+        # write the POTCAR file
+        Potcar.to_file_from_type(
+            structure_cleaned.composition.elements,
+            self.functional,
+            os.path.join(directory, "POTCAR"),
+            self.potcar_mappings,
+        )
+
+    def _write_output_summary(self, directory, vasprun):
+        """
+        In addition to writing the normal VASP output summary, this also plots
+        the bandstructure to "band_structure.png"
+        """
+
+        # run the normal output
+        super()._write_output_summary(directory, vasprun)
+
+        bs_plotter = BSPlotter(vasprun.get_band_structure(line_mode=True))
+        plot = bs_plotter.get_plot()
+        plot_filename = os.path.join(directory, "band_structure.png")
+        plot.savefig(plot_filename)
---
+++ packages/simmate/simmate-0.4.0.tar.gz/simmate-0.4.0/src/simmate/calculators/vasp/tasks/relaxation/materials_project.py
@@ -0,0 +1,210 @@
+# -*- coding: utf-8 -*-
+
+from simmate.calculators.vasp.tasks.base import VaspTask
+from simmate.calculators.vasp.inputs.potcar_mappings import (
+    PBE_ELEMENT_MAPPINGS,
+)
+from simmate.calculators.vasp.error_handlers import (
+    TetrahedronMesh,
+    RotationMatrix,
+    Brmix,
+    Zpotrf,
+    SubspaceMatrix,
+    IncorrectShift,
+    RealOptlay,
+    Tetirr,
+    RotationNonIntMatrix,
+    LongVector,
+    TripleProduct,
+    Pricel,
+    Brions,
+    Zbrent,
+    InsufficientBands,
+    Pssyevx,
+    Eddrmm,
+    Edddav,
+    Edwav,
+    Zheev,
+    ElfKpar,
+    Rhosyg,
+    Posmap,
+    PointGroup,
+    SymprecNoise,
+    IncorrectSmearing,
+    MeshSymmetry,
+    Unconverged,
+    NonConverging,
+    Potim,
+    PositiveEnergy,
+    Frozen,
+    LargeSigma,
+)
+
+
+class MatProjRelaxation(VaspTask):
+    """
+    Runs a VASP geometry optimization using Materials Project settings.
+
+    This is currently the highest-quality preset of all of Simmate's relaxations.
+
+    Materials Project settings are often considered the minimum-required
+    quality for publication and is sufficient for most applications. If you are
+    looking at one structure in detail (for electronic, vibrational, and other
+    properties), you should still test for convergence using higher-quality
+    settings.
+    """
+
+    # This uses the PBE functional with POTCARs that have lower electron counts
+    # and convergence criteria.
+    functional = "PBE"
+    potcar_mappings = PBE_ELEMENT_MAPPINGS
+
+    # These are all input settings for this task. Note a lot of settings
+    # depend on the structure/composition being analyzed.
+    incar = dict(
+        # These settings are the same for all structures regardless of composition
+        ALGO="Fast",
+        EDIFF__per_atom=5.0e-05,
+        ENCUT=520,
+        IBRION=2,
+        ISIF=3,
+        ISMEAR=-5,
+        ISPIN=2,
+        KSPACING=0.4,  # !!! This is where we are different from pymatgen right now
+        LASPH=True,
+        LORBIT=11,
+        LREAL="Auto",
+        LWAVE=False,
+        NELM=100,
+        NSW=99,
+        PREC="Accurate",
+        SIGMA=0.05,
+        # The magnetic moments are dependent on what the composition and oxidation
+        # states are. Note our default of 0.6 is different from the VASP default too.
+        MAGMOM__smart_magmom={
+            "default": 0.6,
+            "Ce": 5,
+            "Ce3+": 1,
+            "Co": 0.6,
+            "Co3+": 0.6,
+            "Co4+": 1,
+            "Cr": 5,
+            "Dy3+": 5,
+            "Er3+": 3,
+            "Eu": 10,
+            "Eu2+": 7,
+            "Eu3+": 6,
+            "Fe": 5,
+            "Gd3+": 7,
+            "Ho3+": 4,
+            "La3+": 0.6,
+            "Lu3+": 0.6,
+            "Mn": 5,
+            "Mn3+": 4,
+            "Mn4+": 3,
+            "Mo": 5,
+            "Nd3+": 3,
+            "Ni": 5,
+            "Pm3+": 4,
+            "Pr3+": 2,
+            "Sm3+": 5,
+            "Tb3+": 6,
+            "Tm3+": 2,
+            "V": 5,
+            "W": 5,
+            "Yb3+": 1,
+        },
+        # We run LDA+U for certain compositions. This is a complex configuration
+        # so be sure to read the "__smart_ldau" modifier for more information.
+        # But as an example for how the mappings work...
+        # {"F": {"Ag": 2}} means if the structure is a fluoride, then we set
+        # the MAGMOM for Ag to 2. Any element that isn't listed defaults to 0.
+        multiple_keywords__smart_ldau=dict(
+            LDAU__auto=True,
+            LDAUTYPE=2,
+            LDAUPRINT=1,
+            LMAXMIX__auto=True,
+            LDAUJ={},  # pymatgen sets these, but they're all default values anyways
+            LDAUL={
+                "F": {
+                    "Co": 2,
+                    "Cr": 2,
+                    "Fe": 2,
+                    "Mn": 2,
+                    "Mo": 2,
+                    "Ni": 2,
+                    "V": 2,
+                    "W": 2,
+                },
+                "O": {
+                    "Co": 2,
+                    "Cr": 2,
+                    "Fe": 2,
+                    "Mn": 2,
+                    "Mo": 2,
+                    "Ni": 2,
+                    "V": 2,
+                    "W": 2,
+                },
+            },
+            LDAUU={
+                "F": {
+                    "Co": 3.32,
+                    "Cr": 3.7,
+                    "Fe": 5.3,
+                    "Mn": 3.9,
+                    "Mo": 4.38,
+                    "Ni": 6.2,
+                    "V": 3.25,
+                    "W": 6.2,
+                },
+                "O": {
+                    "Co": 3.32,
+                    "Cr": 3.7,
+                    "Fe": 5.3,
+                    "Mn": 3.9,
+                    "Mo": 4.38,
+                    "Ni": 6.2,
+                    "V": 3.25,
+                    "W": 6.2,
+                },
+            },
+        ),
+    )
+
+    # These are some default error handlers to use
+    error_handlers = [
+        TetrahedronMesh(),
+        RotationMatrix(),
+        Brmix(),
+        Zpotrf(),
+        SubspaceMatrix(),
+        IncorrectShift(),
+        RealOptlay(),
+        Tetirr(),
+        RotationNonIntMatrix(),
+        LongVector(),
+        TripleProduct(),
+        Pricel(),
+        Brions(),
+        Zbrent(),
+        InsufficientBands(),
+        Pssyevx(),
+        Eddrmm(),
+        Edddav(),
+        Edwav(),
+        Zheev(),
+        ElfKpar(),
+        Rhosyg(),
+        Posmap(),
+        PointGroup(),
+        SymprecNoise(),
+        IncorrectSmearing(),
+        MeshSymmetry(),
+        Unconverged(),
+        NonConverging(),
+        Potim(),
+        PositiveEnergy(),
+        Frozen(),
+        LargeSigma(),
+    ]
