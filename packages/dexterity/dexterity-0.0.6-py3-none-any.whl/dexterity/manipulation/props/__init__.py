18a9539c976ef7c9f3bc1206f62ebc2ee647866e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:31 +0000

    Add package dexterity/0.0.6/dexterity-0.0.6-py3-none-any.whl

---
+++ packages/dexterity/dexterity-0.0.6-py3-none-any.whl/dexterity/manipulation/props/__init__.py
@@ -0,0 +1,7 @@
+from dexterity.manipulation.props.openai_cube import OpenAICube
+from dexterity.manipulation.props.target_sphere import TargetSphere
+
+__all__ = [
+    "OpenAICube",
+    "TargetSphere",
+]
