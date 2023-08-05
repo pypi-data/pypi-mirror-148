936ea409340bbdece006eb81376be7ef3cc582c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:10 +0000

    Add package isubrip/2.2.0/isubrip-2.2.0-py3-none-any.whl

---
+++ packages/isubrip/isubrip-2.2.0-py3-none-any.whl/isubrip/exceptions.py
@@ -0,0 +1,42 @@
+# --------------- Scraping --------------- #
+
+class ScrapeError(Exception):
+    """An issue while scraping the page."""
+    pass
+
+
+class InvalidURL(ScrapeError):
+    """An invalid URL has been used."""
+    pass
+
+
+class PageLoadError(ScrapeError):
+    """The Page did not load properly."""
+    pass
+
+
+class PlaylistDownloadError(ScrapeError):
+    """The playlist could not be downloaded."""
+    pass
+
+
+# --------------- Config --------------- #
+
+class ConfigError(Exception):
+    """An issue with a config file."""
+    pass
+
+
+class DefaultConfigNotFound(ConfigError):
+    """Default config file could not be found."""
+    pass
+
+
+class ConfigValueMissing(ConfigError):
+    """A required config value is missing."""
+    pass
+
+
+class InvalidConfigValue(ConfigError):
+    """An invalid value has been used."""
+    pass
