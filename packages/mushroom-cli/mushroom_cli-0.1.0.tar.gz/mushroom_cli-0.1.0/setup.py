5b60204b9581c0fd632b3bce76381e3fc5a79992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:55 +0000

    Add package mushroom-cli/0.1.0/mushroom_cli-0.1.0.tar.gz

---
+++ packages/mushroom-cli/mushroom_cli-0.1.0.tar.gz/mushroom_cli-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name="mushroom_cli",
+    version="0.1.0",
+    author="Asriel",
+    description=("This is a automatically generate the command line interfaces"),
+    license="GPLv3",
+    packages=['mushroom'],
+    author_email="sariellian@icloud.com",
+    platforms="any",
+    zip_safe = False,
+)
