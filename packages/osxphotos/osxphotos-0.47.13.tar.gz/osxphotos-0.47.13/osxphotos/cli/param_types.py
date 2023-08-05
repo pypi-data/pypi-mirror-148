e9ef324a2f48219267f4f82d7492c51e61c6776b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:52 +0000

    Add package osxphotos/0.47.13/osxphotos-0.47.13.tar.gz

---
+++ packages/osxphotos/osxphotos-0.47.13.tar.gz/osxphotos-0.47.13/osxphotos/cli/param_types.py
@@ -0,0 +1,131 @@
+"""Click parameter types for osxphotos CLI"""
+import datetime
+import os
+import pathlib
+
+import bitmath
+import click
+
+from osxphotos.export_db_utils import export_db_get_version
+from osxphotos.photoinfo import PhotoInfoNone
+from osxphotos.phototemplate import PhotoTemplate, RenderOptions
+from osxphotos.utils import expand_and_validate_filepath, load_function
+
+__all__ = [
+    "BitMathSize",
+    "DateTimeISO8601",
+    "ExportDBType",
+    "FunctionCall",
+    "TimeISO8601",
+    "TemplateString",
+]
+
+
+class DateTimeISO8601(click.ParamType):
+
+    name = "DATETIME"
+
+    def convert(self, value, param, ctx):
+        try:
+            return datetime.datetime.fromisoformat(value)
+        except Exception:
+            self.fail(
+                f"Invalid datetime format {value}. "
+                "Valid format: YYYY-MM-DD[*HH[:MM[:SS[.fff[fff]]]][+HH:MM[:SS[.ffffff]]]]"
+            )
+
+
+class BitMathSize(click.ParamType):
+
+    name = "BITMATH"
+
+    def convert(self, value, param, ctx):
+        try:
+            value = bitmath.parse_string(value)
+        except ValueError:
+            # no units specified
+            try:
+                value = int(value)
+                value = bitmath.Byte(value)
+            except ValueError as e:
+                self.fail(
+                    f"{value} must be specified as bytes or using SI/NIST units. "
+                    + "For example, the following are all valid and equivalent sizes: '1048576' '1.048576MB', '1 MiB'."
+                )
+        return value
+
+
+class TimeISO8601(click.ParamType):
+
+    name = "TIME"
+
+    def convert(self, value, param, ctx):
+        try:
+            return datetime.time.fromisoformat(value).replace(tzinfo=None)
+        except Exception:
+            self.fail(
+                f"Invalid time format {value}. "
+                "Valid format: HH[:MM[:SS[.fff[fff]]]][+HH:MM[:SS[.ffffff]]] "
+                "however, note that timezone will be ignored."
+            )
+
+
+class FunctionCall(click.ParamType):
+    name = "FUNCTION"
+
+    def convert(self, value, param, ctx):
+        if "::" not in value:
+            self.fail(
+                f"Could not parse function name from '{value}'. "
+                "Valid format filename.py::function"
+            )
+
+        filename, funcname = value.split("::")
+
+        filename_validated = expand_and_validate_filepath(filename)
+        if not filename_validated:
+            self.fail(f"'{filename}' does not appear to be a file")
+
+        try:
+            function = load_function(filename_validated, funcname)
+        except Exception as e:
+            self.fail(f"Could not load function {funcname} from {filename_validated}")
+
+        return (function, value)
+
+
+class ExportDBType(click.ParamType):
+
+    name = "EXPORTDB"
+
+    def convert(self, value, param, ctx):
+        try:
+            export_db_name = pathlib.Path(value)
+            if export_db_name.is_dir():
+                raise click.BadParameter(f"{value} is a directory")
+            if export_db_name.is_file():
+                # verify it's actually an osxphotos export_db
+                # export_db_get_version will raise an error if it's not valid
+                osxphotos_ver, export_db_ver = export_db_get_version(value)
+            return value
+        except Exception:
+            self.fail(f"{value} exists but is not a valid osxphotos export database. ")
+
+
+class TemplateString(click.ParamType):
+    """Validate an osxphotos template language (OTL) template string"""
+
+    name = "OTL_TEMPLATE"
+
+    def convert(self, value, param, ctx):
+        try:
+            cwd = os.getcwd()
+            _, unmatched = PhotoTemplate(photo=PhotoInfoNone()).render(
+                value,
+                options=RenderOptions(export_dir=cwd, dest_path=cwd, filepath=cwd),
+            )
+            if unmatched:
+                self.fail(f"Template '{value}' contains unknown field(s): {unmatched}")
+            return value
+        except ValueError as e:
+            self.fail(e)
---
+++ packages/osxphotos/osxphotos-0.47.13.tar.gz/osxphotos-0.47.13/osxphotos/photosalbum.py
@@ -0,0 +1,47 @@
+""" PhotosAlbum class to create an album in default Photos library and add photos to it """
+
+from typing import List, Optional
+
+import photoscript
+from more_itertools import chunked
+
+from .photoinfo import PhotoInfo
+from .utils import noop
+
+__all__ = ["PhotosAlbum"]
+
+
+class PhotosAlbum:
+    def __init__(self, name: str, verbose: Optional[callable] = None):
+        self.name = name
+        self.verbose = verbose or noop
+        self.library = photoscript.PhotosLibrary()
+
+        album = self.library.album(name)
+        if album is None:
+            self.verbose(f"Creating Photos album '{self.name}'")
+            album = self.library.create_album(name)
+        self.album = album
+
+    def add(self, photo: PhotoInfo):
+        photo_ = photoscript.Photo(photo.uuid)
+        self.album.add([photo_])
+        self.verbose(
+            f"Added {photo.original_filename} ({photo.uuid}) to album {self.name}"
+        )
+
+    def add_list(self, photo_list: List[PhotoInfo]):
+        photos = []
+        for p in photo_list:
+            try:
+                photos.append(photoscript.Photo(p.uuid))
+            except Exception as e:
+                self.verbose(f"Error creating Photo object for photo {p.uuid}: {e}")
+        for photolist in chunked(photos, 10):
+            self.album.add(photolist)
+        photo_len = len(photos)
+        photo_word = "photos" if photo_len > 1 else "photo"
+        self.verbose(f"Added {photo_len} {photo_word} to album {self.name}")
+
+    def photos(self):
+        return self.album.photos()
