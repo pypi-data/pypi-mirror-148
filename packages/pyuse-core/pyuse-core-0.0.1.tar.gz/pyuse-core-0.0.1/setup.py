d09ab2d28ffbb2089b59550a3283fdef42782e89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:06 +0000

    Add package pyuse-core/0.0.1/pyuse-core-0.0.1.tar.gz

---
+++ packages/pyuse-core/pyuse-core-0.0.1.tar.gz/pyuse-core-0.0.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/4/25 11:10 AM
+# @Author  : Yongchin
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyuse-core",
+    version="0.0.1",
+    author="Yongchin",
+    author_email="yongchin39@qq.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"src": "/pyuse"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
