c4224fc926f3502ac7f18f1570fefeff7db0e96c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:06 +0000

    Add package statworx-theme/0.4.3/statworx_theme-0.4.3-py3-none-any.whl

---
+++ packages/statworx-theme/statworx_theme-0.4.3-py3-none-any.whl/statworx_theme/colors.py
@@ -0,0 +1,21 @@
+# define statoworx design colors
+BLUE = "#0000ff"
+LIGHT_BLUE = "#8781FF"
+PINK = "#FB0059"
+LIGHT_PINK = "#FD88B8"
+GREEN = "#18C205"
+LIGHT_GREEN = "#76FF70"
+YELLOW = "#FEFF09"
+LIGHT_YELLOW = "#FEFF88"
+GREY = "#596A79"
+LIGHT_GREY = "#B7C0C6"
+BLACK = "#000000"
+WHITE = "#ffffff"
+
+# create color dict
+COLOR_DICT = {
+    "Pk": PINK,
+    "Bl": BLUE,
+    "Gn": GREEN,
+    "Yw": YELLOW,
+}
