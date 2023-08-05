1c3ad6dace79f4d9693bcc00277d9c14ecde9eb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:27 +0000

    Add package eden-python/0.1.0/eden_python-0.1.0-py3-none-any.whl

---
+++ packages/eden-python/eden_python-0.1.0-py3-none-any.whl/eden/result_storage.py
@@ -0,0 +1,46 @@
+from redis import Redis
+from .utils import dict_to_bytes, bytes_to_dict
+
+
+class ResultStorage(object):
+    def __init__(self, redis_host, redis_port, redis_db=1):
+        """Wrapper over redis to fetch and store results.
+
+        Args:
+            redis_host (str): url to redis host, generally something like "localhost"
+            redis_port (int): port number
+            db (int, optional): DB number to look at within redis. Defaults to 1.
+        """
+
+        self.redis = self.redis = Redis(
+            host=redis_host, port=str(redis_port), db=redis_db
+        )
+
+        self.redis.ping()
+
+    def add(self, token, encoded_results: dict):
+
+        success = True
+
+        # try:
+        self.redis.set(token, dict_to_bytes(encoded_results))
+        # except:
+        #     success = False
+
+        return success
+
+    def get(self, token):
+
+        results = self.redis.get(token)
+        if results is not None:
+            results = bytes_to_dict(results)
+        return results
+
+    def delete(self, token):
+        """Deletes a result from redis. Useful when we'll have tons of outputs
+        and we won't want to keep them after the user has fetched them
+
+        Args:
+            token (str): identifier token
+        """
+        self.redis.delete(token)
