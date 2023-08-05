fb52390904f364811dd47f712c2f99dbd92aac26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:16 +0000

    Add package echo1-sma/0.1.0/echo1-sma-0.1.0.tar.gz

---
+++ packages/echo1-sma/echo1-sma-0.1.0.tar.gz/echo1-sma-0.1.0/README.md
@@ -0,0 +1,12 @@
+# echo1-sma
+Streaming moving average in python.
+
+## Installation
+```sh
+pip install echo1-sma
+```
+
+## Getting Started
+```python
+from echo1_sma.echo1_sma import StreamingMovingAverage
+```
---
+++ packages/echo1-sma/echo1-sma-0.1.0.tar.gz/echo1-sma-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['echo1_sma']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'echo1-sma',
+    'version': '0.1.0',
+    'description': 'Streaming moving average in python.',
+    'long_description': '# echo1-sma\nStreaming moving average in python.\n\n## Installation\n```sh\npip install echo1-sma\n```\n\n## Getting Started\n```python\nfrom echo1_sma.echo1_sma import StreamingMovingAverage\n```',
+    'author': 'Michael Mohamed',
+    'author_email': 'michael.mohamed@echo1.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/e1-io/echo1-geopix',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6.2',
+}
+
+
+setup(**setup_kwargs)
