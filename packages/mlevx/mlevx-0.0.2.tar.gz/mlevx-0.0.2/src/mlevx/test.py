25c4fa5259239099bae7539db3c0321eb124f9a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:16:25 +0000

    Add package mlevx/0.0.2/mlevx-0.0.2.tar.gz

---
+++ packages/mlevx/mlevx-0.0.2.tar.gz/mlevx-0.0.2/src/mlevx/test.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from regbot import signal
+
+#df = pd.read_csv('../../../jupyter/regbot_v4.csv')
+df = pd.read_csv('../../../crypto/freqtrade/sell.csv')
+#df = pd.read_csv('../../../jupyter/sell.csv')
+#print(df.columns)
+
+
+df = df[df['enter_long'] == 1]
+def getEnterSignal(open,close,thr,sig):
+    return signal(open,close,thr, 'enter')
+def getExitSignal(open,close,thr,sig):
+    return signal(open,close,thr,'exit')
+
+
+#print(df.columns)
+
+#print(df.head())
+
+df['enter_result'] = df.apply(lambda row: getEnterSignal(row['open'], row['close'], 0.7126436781609196,'enter'), axis=1) # optimal 0.7126436781609196
+df['exit_result'] = df.apply(lambda row: getExitSignal(row['open'], row['close'], 0.99,'exit'), axis=1)
+#print(df.head())
+print(len(df[df['enter_result'] == 1]), len(df))
+print(len(df[df['exit_result'] == 1]), len(df))
+
+
