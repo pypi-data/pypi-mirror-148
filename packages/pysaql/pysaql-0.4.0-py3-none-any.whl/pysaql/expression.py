c3396337073a9a8e8c5e872db0c4ddf5d1736b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:08 +0000

    Add package pysaql/0.4.0/pysaql-0.4.0-py3-none-any.whl

---
+++ packages/pysaql/pysaql-0.4.0-py3-none-any.whl/pysaql/expression.py
@@ -0,0 +1,11 @@
+"""Contains base Expression class"""
+
+
+class Expression:
+    """Base expression class
+
+    This is used as the root class for all expressions in the saql subpackage to
+    establish inheritance. It does not currently implement any common functionality.
+    """
+
+    pass
