9b3bca39d2849ccc0fe501a3998ac349f84f3986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:59 +0000

    Add package Ecarx-APITest-2/1.0/Ecarx_APITest_2-1.0.tar.gz

---
+++ packages/Ecarx-APITest-2/Ecarx_APITest_2-1.0.tar.gz/Ecarx_APITest_2-1.0/setup.py
@@ -0,0 +1,33 @@
+"""
+@Time ： 2022/4/24 17:32
+@Auth ： 朱晓东
+@File ：setup.py
+@IDE ：PyCharm
+
+"""
+
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="Ecarx_APITest_2",  # 模块名称
+    version="1.0",  # 当前版本
+    author="xiaodong.zhu",  # 作者
+    author_email="zhuxiaodong45@163.com",  # 作者邮箱
+    description="一个非常NB的包",  # 模块简介
+    long_description=long_description,  # 模块详细介绍
+    long_description_content_type="text/markdown",  # 模块详细介绍格式
+    # url="https://github.com/wupeiqi/fucker",  # 模块github地址
+    packages=setuptools.find_packages(),  # 自动找到项目中导入的模块
+    # 模块相关的元数据
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # 依赖模块
+    install_requires=[
+        'pillow',
+    ],
+    python_requires='>=3',
+)
