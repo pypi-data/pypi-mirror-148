92ab5056f92754a490fc914b8b29996c59c78fcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:25 +0000

    Add package gitnu/0.1.1a3/gitnu-0.1.1a3.tar.gz

---
+++ packages/gitnu/gitnu-0.1.1a3.tar.gz/gitnu-0.1.1a3/src/gitnu/cache.py
@@ -0,0 +1,31 @@
+import json
+
+from . import log
+from . import git
+from .shell import system
+from .data_structure import NumberedStatus, Entry
+
+
+def get_filepath() -> str:
+    cache_directory = system(git.cmd.get_repo)
+    if cache_directory == "":
+        log.warn("get_filepath: cache_directory empty.")
+    return "%s/gitnu.json" % (cache_directory)
+
+
+def write(numbered_status: NumberedStatus):
+    with open(get_filepath(), "w") as f:
+        json.dump(numbered_status.json(), f)
+
+
+def get(path: str = "") -> NumberedStatus:
+    if path == "":
+        path = get_filepath()
+    table = NumberedStatus()
+    try:
+        with open(path, "r") as f:
+            for i in json.load(f):
+                table.push(Entry(i[0], i[1]))
+    except FileNotFoundError:
+        log.warn("Cache doesn't exist yet. Run gitnu first.")
+    return table
---
+++ packages/gitnu/gitnu-0.1.1a3.tar.gz/gitnu-0.1.1a3/src/gitnu/git.py
@@ -0,0 +1,38 @@
+class cmd:
+    status = ["git", "-c", "status.color=always", "status"]
+    get_repo = ["git", "rev-parse", "--git-dir"]
+
+
+commands = ["status", "add", "reset", "diff", "checkout"]
+
+actions = [
+    "deleted:",
+    "new file:",
+    "modified:",
+    "renamed:",
+]
+
+suffix_list = [
+    "(new commits)\n",
+    "(modified content)\n",
+    "(untracked content)\n",
+]
+
+ansi_colors = [
+    "\x1b[31m",
+    "\x1b[32m",
+    "\x1b[33m",
+    "\x1b[34m",
+    "\x1b[35m",
+    "\x1b[36m",
+    "\x1b[37m",
+    "\x1b[m",
+]
+
+
+def get_command(args: list[str]) -> tuple[str, int]:
+    for i in range(len(args)):
+        word = args[i]
+        if word in commands:
+            return (word, i)
+    return ("", 0)
