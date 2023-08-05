59fea5e0cf1e473926e0f47fb3d19e717d407a0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:08 +0000

    Add package nwbinspector/0.4.0/nwbinspector-0.4.0.tar.gz

---
+++ packages/nwbinspector/nwbinspector-0.4.0.tar.gz/nwbinspector-0.4.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+# Get the long description from the README file
+with open("README.md", "r") as f:
+    long_description = f.read()
+setup(
+    name="nwbinspector",
+    version="0.4.0",
+    description="Tool to inspect NWB files for best practices compliance.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Ryan Ly, Ben Dichter, and Cody Baker.",
+    author_email="rly@lbl.gov, ben.dichter@gmail.com, cody.baker@catalystneuro.com",
+    packages=find_packages(),
+    include_package_data=True,
+    url="https://github.com/NeurodataWithoutBorders/nwbinspector",
+    install_requires=["pynwb", "natsort", "click", "PyYAML", "jsonschema", "dandi>=0.39.2", "tqdm"],
+    entry_points={"console_scripts": ["nwbinspector=nwbinspector.nwbinspector:inspect_all_cli"]},
+    license="BSD-3-Clause",
+)
