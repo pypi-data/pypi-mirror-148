057e5c60fafa90ab46f97844ab1f3a50ed96c54e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:02 +0000

    Add package nonebot-plugin-arcaeabot/1.2.1/nonebot_plugin_arcaeabot-1.2.1.tar.gz

---
+++ packages/nonebot-plugin-arcaeabot/nonebot_plugin_arcaeabot-1.2.1.tar.gz/nonebot_plugin_arcaeabot-1.2.1/nonebot_plugin_arcaeabot/handlers/best.py
@@ -0,0 +1,71 @@
+from loguru import logger
+from nonebot.adapters.onebot.v11 import Bot, MessageEvent, Message, MessageSegment
+from nonebot.params import CommandArg
+from nonebot.exception import ActionFailed
+from nonebot.log import logger
+from ..data import UserInfo
+from ..main import arc
+from ..draw_image import UserArcaeaInfo
+from ..database import alias
+
+
+async def best_handler(bot: Bot, event: MessageEvent, args: Message = CommandArg()):
+    args: list = str(args).split()
+    if args[0] == "best":
+        if len(args) == 3:
+            user_info = UserInfo.get_or_none(UserInfo.user_qq == event.user_id)
+            song_alias = (
+                alias.get_or_none(alias.alias == args[1].strip())
+                if alias.get_or_none(alias.alias == args[1].strip())
+                else alias.get_or_none(alias.sid == args[1].strip())
+            )
+            song_id = song_alias.sid if song_alias else args[1].strip()
+            difficulty = args[2].strip()
+            # Exception
+            if not user_info:
+                try:
+                    await arc.finish(
+                        "\n".join(
+                            [
+                                f"> {event.sender.card or event.sender.nickname}",
+                                "你还没绑定哦~",
+                            ]
+                        )
+                    )
+                except ActionFailed as e:
+                    logger.exception(
+                        f'ActionFailed | {e.info["msg"].lower()} | retcode = {e.info["retcode"]} | {e.info["wording"]}'
+                    )
+                    return
+
+            # Query
+            if not UserArcaeaInfo.is_querying(user_info.arcaea_id):
+                result = await UserArcaeaInfo.draw_best(
+                    arcaea_id=user_info.arcaea_id,
+                    song_id=song_id,
+                    difficulty=difficulty,
+                )
+                try:
+                    await arc.finish(MessageSegment.reply(event.message_id) + result)
+                except ActionFailed as e:
+                    logger.exception(
+                        f'ActionFailed | {e.info["msg"].lower()} | retcode = {e.info["retcode"]} | {e.info["wording"]}'
+                    )
+                    return
+            else:
+                try:
+                    await arc.finish(
+                        "\n".join(
+                            [
+                                f"> {event.sender.card or event.sender.nickname}",
+                                "您已在查询队列, 请勿重复发起查询。",
+                            ]
+                        )
+                    )
+                except ActionFailed as e:
+                    logger.exception(
+                        f'ActionFailed | {e.info["msg"].lower()} | retcode = {e.info["retcode"]} | {e.info["wording"]}'
+                    )
+                    return
+        else:
+            await arc.finish("参数输入有误！")
---
+++ packages/nonebot-plugin-arcaeabot/nonebot_plugin_arcaeabot-1.2.1.tar.gz/nonebot_plugin_arcaeabot-1.2.1/nonebot_plugin_arcaeabot/handlers/bind.py
@@ -0,0 +1,56 @@
+from nonebot.adapters.onebot.v11 import Bot, MessageEvent
+from nonebot.exception import ActionFailed
+from nonebot.params import CommandArg
+from nonebot.log import logger
+from ..data import UserInfo
+from ..matcher import arc
+
+try:
+    from ..adapters.utils import fetch_user_info
+except ImportError:
+    logger.error("查分Api填写不规范, 请检查.env中的api_in_use配置")
+from ..adapters.utils import api_in_use
+
+
+async def bind_handler(bot: Bot, event: MessageEvent, args=CommandArg()):
+    args: list = str(args).split()
+    if args[0] == "bind":
+        arc_id = args[1].strip()
+        if len(arc_id) != 9:
+            try:
+                await arc.finish(
+                    "\n".join(
+                        [
+                            f"> {event.sender.card or event.sender.nickname}",
+                            "id 格式错误，请检查。",
+                        ]
+                    )
+                )
+            except ActionFailed as e:
+                logger.exception(
+                    f'ActionFailed | {e.info["msg"].lower()} | retcode = {e.info["retcode"]} | {e.info["wording"]}'
+                )
+        if api_in_use == "AUA":
+            player_name = (await fetch_user_info(arcaea_id=arc_id, recent_only=True))[
+                "content"
+            ]["account_info"]["name"]
+        elif api_in_use == "ESTERTION":
+            player_name = (await fetch_user_info(arcaea_id=arc_id, recent_only=True))[
+                0
+            ]["data"]["name"]
+        UserInfo.replace(
+            user_qq=event.user_id, arcaea_id=arc_id, arcaea_name=player_name
+        ).execute()
+        try:
+            await arc.finish(
+                "\n".join(
+                    [
+                        f"> {event.sender.card or event.sender.nickname}",
+                        f"绑定成功, 用户名: {player_name}, id: {arc_id}",
+                    ]
+                )
+            )
+        except ActionFailed as e:
+            logger.exception(
+                f'ActionFailed | {e.info["msg"].lower()} | retcode = {e.info["retcode"]} | {e.info["wording"]}'
+            )
