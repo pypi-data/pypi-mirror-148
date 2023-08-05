3e7a1efc725fd16e4b1b2f8fba759b30910a90d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:25 +0000

    Add package pyobs-core/0.18.9/pyobs-core-0.18.9.tar.gz

---
+++ packages/pyobs-core/pyobs-core-0.18.9.tar.gz/pyobs-core-0.18.9/pyobs/images/processors/offsets/__init__.py
@@ -0,0 +1,13 @@
+"""
+Offsets
+-------
+"""
+
+from .offsets import Offsets
+from .astrometry import AstrometryOffsets
+from .nstar import NStarOffsets
+from .projected import ProjectedOffsets
+from .fitsheader import FitsHeaderOffsets
+
+
+__all__ = ["Offsets", "AstrometryOffsets", "NStarOffsets", "ProjectedOffsets", "FitsHeaderOffsets"]
