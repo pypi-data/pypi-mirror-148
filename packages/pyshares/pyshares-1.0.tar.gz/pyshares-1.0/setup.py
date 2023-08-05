f91f279e73f794c66df7cf56fda3938a6ecacbc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:43:13 +0000

    Add package pyshares/1.0/pyshares-1.0.tar.gz

---
+++ packages/pyshares/pyshares-1.0.tar.gz/pyshares-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+
+
+setup(
+    name="pyshares",
+    version='1.0',
+    license='Eclipse Public License 2.0',
+    author="Wizz1337",
+    author_email="xin1337dev@protonmail.com",
+    description='by Wizz1337',
+    long_description="https://github.com/Wizz1337/TikTokMassBotting",
+    keywords=['python', 'share', 'tiktok', 'bot', 'devcenter'],
+    packages=['pyshares']
+)
