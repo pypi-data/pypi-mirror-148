2c75f14e81fac1ff44477d2f564c378760f6996b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:05:59 +0000

    Add package junit-report/0.2.7/junit_report-0.2.7-py3-none-any.whl

---
+++ packages/junit-report/junit_report-0.2.7-py3-none-any.whl/junit_report/decorators/_junit_test_case.py
@@ -0,0 +1,107 @@
+import traceback
+from contextlib import suppress
+from typing import Any, Callable, Dict, List, Union
+
+import pytest
+
+from ._junit_decorator import JunitDecorator
+from ._junit_test_suite import JunitTestSuite
+from ..utils import TestCaseCategories, TestCaseData, CaseFailure, Utils, PytestUtils
+
+
+class JunitTestCase(JunitDecorator):
+    """
+    JunitTestCase is a decorator that represents single TestCase.
+    When the decorated function finished its execution, it's registered to the relevant JunitTestSuite.
+    TestCase will fail (TestCase failure) only when exception occurs during execution.
+    """
+
+    _stack_locals: List[Dict[str, Any]]
+    _case_data: Union[TestCaseData, None]
+    _pytest_function: Union[pytest.Function, None]
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._stack_locals = list()
+        self._case_data = None
+        self._pytest_function = None
+
+    @property
+    def name(self):
+        return self._case_data.name if self._case_data.name else self._func.__name__
+
+    def _on_wrapper_start(self, function):
+        super()._on_wrapper_start(function)
+        case = Utils.get_new_test_case(function, self._get_class_name(), TestCaseCategories.FUNCTION)
+        self._case_data = TestCaseData(_start_time=self._start_time, case=case, _func=function)
+        self._pytest_function = [
+            stack_local for stack_local in self._stack_locals
+            if "self" in stack_local and isinstance(stack_local["self"], pytest.Function)][0]["self"]
+
+    def _add_failure(self, e: BaseException, message_prefix: str = ""):
+        message = f"{message_prefix} {str(e)}" if message_prefix else str(e)
+        failure = CaseFailure(message=message, output=traceback.format_exc(), type=e.__class__.__name__)
+        self._case_data.case.failures.append(failure)
+
+    def _on_exception(self, e: BaseException):
+        if Utils.is_case_exception_already_raised(e):
+            raise  # already registered on son test case
+        Utils.mark_case_exception_as_raised(e)
+        self._add_failure(e)
+        raise e
+
+    def _on_wrapper_end(self):
+        self._case_data.set_fin_time()
+        JunitTestSuite.register_case(self._case_data, self.get_suite_key())
+
+    def get_suite_key(self) -> Union[Callable, None]:
+        """
+        Get suite function as unique key.
+        This function handles also on case that the test suite function decorated with pytest.mark.parametrize and
+        collect its parameters and record them into the test case.
+        :return: Wrapped Suite function instance
+        """
+        with suppress(AttributeError):
+            suite_func = self.get_suite()
+            return suite_func
+
+        return None
+
+    def _set_parameterized(self, parameterized: List = None):
+        if not parameterized:
+            parameterized = PytestUtils.get_case_pytest_parameterized(self._pytest_function)
+
+        if parameterized and not self._case_data.parametrize:
+            self._case_data.set_parametrize(parameterized)
+
+    def get_suite(self):
+        stack_locals = [stack_local for stack_local in self._stack_locals if "self" in stack_local]
+        function_locals = [stack_local for stack_local in stack_locals if "function" in stack_local]
+
+        self._set_parameterized()
+        is_inside_fixture = False
+
+        suite_func = self._get_function_suite(function_locals)
+        if suite_func is None:
+            is_inside_fixture = True
+            suite_func = PytestUtils.get_fixture_suite(self._pytest_function)
+            fixture_params = PytestUtils.get_fixture_parameterized(self._pytest_function)
+            if fixture_params:
+                self._set_parameterized(fixture_params)
+
+        self._case_data.is_inside_fixture = is_inside_fixture
+        return suite_func
+
+    def _get_function_suite(self, function_locals: List[Dict[str, Any]]):
+        suite_func = None
+
+        for f_locals in function_locals:
+            suite = f_locals["self"]
+            if isinstance(suite, JunitTestCase) and f_locals["function"].__name__ != self.name:
+                if not self._case_data.has_parent:
+                    self._case_data.set_parent(f_locals["function"].__name__)
+
+            if isinstance(suite, JunitTestSuite):
+                suite_func = f_locals["function"]
+                break
+        return suite_func
---
+++ packages/junit-report/junit_report-0.2.7-py3-none-any.whl/junit_report/decorators/_junit_test_suite.py
@@ -0,0 +1,188 @@
+import datetime
+import os
+import traceback
+from pathlib import Path
+from typing import Callable, ClassVar, Dict, List, Union
+
+from junit_xml import TestCase, TestSuite, to_xml_report_string
+
+from ._junit_decorator import JunitDecorator
+from ..utils import Utils, TestCaseCategories, TestCaseData, CaseFailure, PytestUtils
+
+
+class DuplicateSuiteError(KeyError):
+    """ Test Suite decorator name is already exist in suites poll """
+
+
+class JunitTestSuite(JunitDecorator):
+    """
+    JunitTestSuite is a decorator that waits for JunitTestCases decorators to register their testcases instances.
+    After all test cases has finished their execution, the JunitTestSuite instance collect all registered TestCases
+    and generate junit xml accordingly.
+    JunitTestSuite represents single TestSuite.
+    Generated report format: junit_<Class Name>_<Suite Function Name>_report.xml
+    The usage of this function is regardless of pytest unittest packages.
+    Default report path can be override if DEFAULT_REPORT_PATH_KEY environment variable is set
+    """
+
+    _junit_suites: ClassVar[Dict[Callable, "JunitTestSuite"]] = dict()
+    _report_dir: Path
+    _cases = List[TestCase]
+    _func: Union[Callable, None]
+    suite: Union[TestSuite, None]
+
+    XML_REPORT_FORMAT = "junit_{suite_name}_report{args}.xml"
+
+    def __init__(self, report_dir: Path = None, custom_filename: str = None):
+        """
+        :param report_dir: Target directory, created if not exists
+        :param custom_filename: If set, xml report will set to <exported_filename>.xml
+        """
+        super().__init__()
+        self._report_dir = Utils.get_report_dir(report_dir)
+        self._cases = list()
+        self.suite = None
+        self._timestamp = datetime.datetime.now()
+        self._has_uncollected_fixtures = False
+        self._self_test_case = None
+        self._custom_filename = custom_filename
+
+    def _on_call(self):
+        self._register()
+
+    def _on_wrapper_end(self, force=False):
+        self.suite = TestSuite(
+            name=f"{self._get_class_name()}_{self.name}", test_cases=self._get_cases(), timestamp=self._timestamp
+        )
+        self._export(self.suite, force)
+
+    @classmethod
+    def get_report_file_name(cls, suite_name: str, args: str = None, custom_filename: str = None):
+        if custom_filename:
+            return f"{custom_filename}.xml"
+        if args:
+            return cls.XML_REPORT_FORMAT.format(suite_name=suite_name, args=f"[{args}]")
+        return cls.XML_REPORT_FORMAT.format(suite_name=suite_name, args="")
+
+    @classmethod
+    def get_suite(cls, suite_key: Callable) -> Union["JunitTestSuite", None]:
+        """
+        Return suite if exists, None otherwise
+        :param suite_key: Wrapped function as cases key
+        :return:
+        """
+        if cls.is_suite_exist(suite_key):
+            return cls._junit_suites[suite_key]
+        return None
+
+    @classmethod
+    def collect_all(cls, force=False):
+        """
+        Collect all junit test suites reports from external source
+        :return: None
+        """
+        for junit_suite in cls._junit_suites.values():
+            cls._on_wrapper_end(self=junit_suite, force=force)
+
+    @classmethod
+    def is_suite_exist(cls, suite_func: Callable):
+        return suite_func in cls._junit_suites
+
+    @classmethod
+    def register_case(cls, test_data, suite_func: Callable) -> None:
+        """
+        Register test case to the relevant test suite
+        :param test_data: TestCaseData instance
+        :param suite_func: Wrapped function as cases key
+        :return: None
+        """
+        if cls.is_suite_exist(suite_func):
+            cls._add_case(cls.get_suite(suite_func), test_data)
+
+    def _register(self):
+        if self._func in JunitTestSuite._junit_suites:
+            raise DuplicateSuiteError(f"Suite {self.name} already exist")
+        JunitTestSuite._junit_suites[self._func] = self
+
+    def _get_cases(self):
+        if self._self_test_case:
+            return [data.case for data in self._cases] + [self._self_test_case]
+        return [data.case for data in self._cases]
+
+    def _add_case(self, test_data):
+        if test_data.case.category == "fixture":
+            self._has_uncollected_fixtures = True
+        return self._cases.append(test_data)
+
+    def _get_parametrize_as_str(self) -> str:
+        """
+        In case of pytest parametrize, this function collect and return parametrizes values
+        :return: underscore separated parametrize values
+        """
+        parameterize = PytestUtils.get_suite_pytest_parameterized(self._cases, self._func, self._stack_locals)
+        if parameterize:
+            return "_".join(str(tup[1]) for tup in parameterize)
+        return ""
+
+    def _export(self, suite: TestSuite, force=False) -> None:
+        """
+        Export test suite to JUnit xml file
+        :param suite: TestSuite to export
+        :return: None
+        """
+        if not suite or len(suite.test_cases) == 0:
+            return
+
+        if not self._has_uncollected_fixtures or force:
+            values = self._get_parametrize_as_str()
+            path = self._report_dir.joinpath(
+                self.get_report_file_name(suite_name=suite.name, args=values, custom_filename=self._custom_filename)
+            )
+            xml_string = to_xml_report_string([suite])
+
+            os.makedirs(self._report_dir, exist_ok=True)
+            with open(path, "w") as f:
+                f.write(xml_string)
+
+            self.clear_cases()
+
+    def clear_cases(self):
+        """ Delete all cases from suite """
+        self._cases = list()
+        self.suite.test_cases = list()
+
+    @classmethod
+    def fixture_cleanup(cls, test_data, suite_func: Callable):
+        """
+        Collect junit suite after fixture yield
+        :param test_data: TestCaseData
+        :param suite_func: suite key
+        :return: None
+        """
+        junit_suite = cls.get_suite(suite_func)
+        if junit_suite and junit_suite._cases:
+            for may_be_fixture in junit_suite._cases:
+                if may_be_fixture.case == test_data.case:
+                    return junit_suite._collect_yield()
+
+    def _collect_yield(self):
+        """
+        Export suite if exist
+        :return:
+        """
+        self._has_uncollected_fixtures = False
+        self._export(self.suite)
+
+    def _on_exception(self, e: BaseException):
+        if Utils.is_case_exception_already_raised(e):
+            raise e
+
+        self._handle_in_suite_exception(e)
+
+    def _handle_in_suite_exception(self, exception: BaseException):
+        self._self_test_case = Utils.get_new_test_case(self._func, self._get_class_name(), TestCaseCategories.SUITE)
+
+        case_data = TestCaseData(_start_time=self._start_time, case=self._self_test_case, _func=self._func)
+        failure = CaseFailure(message=str(exception), output=traceback.format_exc(), type=exception.__class__.__name__)
+        case_data.case.failures.append(failure)
+        raise exception
