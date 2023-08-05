1928fb868b83532a63f63def81af38f48d30656e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:16 +0000

    Add package ExplainAI/0.2.9/ExplainAI-0.2.9.tar.gz

---
+++ packages/ExplainAI/ExplainAI-0.2.9.tar.gz/ExplainAI-0.2.9/ExplainAI/flx_data/input.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os
+import sys
+
+def input_dataset(flag=0):
+    '''
+
+    :param flag: index of which data set, see ExplainAI tutorials
+    :return:read data input, pandas.Dataframe
+    '''
+    if flag==0:
+
+
+        path=os.path.dirname(sys.modules['ExplainAI'].__file__)
+        file_name=os.path.join(path,'flx_data/','dataset0.csv')
+        data = pd.read_csv(file_name,header=0)
+
+
+
+    elif flag==1:
+
+        path = os.path.dirname(sys.modules['ExplainAI'].__file__)
+        file_name = os.path.join(path, 'flx_data/', 'dataset1.csv')
+
+        data = pd.read_csv(file_name,header=0)
+
+
+
+    return data
+
+
