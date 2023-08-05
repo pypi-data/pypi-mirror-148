68c3f26d461a96d7773f45fb01d97446cbdacf64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:30 +0000

    Add package zebrazoom/1.32.45/zebrazoom-1.32.45-py3-none-any.whl

---
+++ packages/zebrazoom/zebrazoom-1.32.45-py3-none-any.whl/zebrazoom/code/getImage/headEmbededFrame.py
@@ -0,0 +1,43 @@
+from zebrazoom.code.preprocessImage import preprocessImage
+import numpy as np
+import cv2
+import zebrazoom.videoFormatConversion.zzVideoReading as zzVideoReading
+
+def headEmbededFrame(videoPath, frameNumber, hyperparameters):
+  
+  debug = 0
+  
+  cap = zzVideoReading.VideoCapture(videoPath)
+  
+  cap.set(1, frameNumber)
+  ret, frame = cap.read()
+  
+  while not(ret):
+    print("WARNING: couldn't read the frameNumber", frameNumber, "for the video", hyperparameters["videoName"])
+    frameNumber = frameNumber - 1
+    cap.set(1, frameNumber)
+    ret, frame = cap.read()
+  
+  if ("invertBlackWhiteOnImages" in hyperparameters) and hyperparameters["invertBlackWhiteOnImages"]:
+    frame = 255 - frame
+  
+  if ("imagePreProcessMethod" in hyperparameters) and hyperparameters["imagePreProcessMethod"]:
+    frame = preprocessImage(frame, hyperparameters)
+  
+  kernel = np.ones((8,8),np.float32)/25
+  thres1  = cv2.filter2D(frame,-1,kernel)
+  retval, thres1 = cv2.threshold(thres1, 80, 255, cv2.THRESH_BINARY)
+  thres1 = 255 - thres1
+  
+  frame = 255 - frame
+  
+  if (debug):
+    cv2.imshow('thres1', frame)
+    cv2.waitKey(0)
+    cv2.imshow('thres1', thres1)
+    cv2.waitKey(0)
+    
+  frame  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  thres1 = cv2.cvtColor(thres1, cv2.COLOR_BGR2GRAY)
+  
+  return [frame, thres1]
