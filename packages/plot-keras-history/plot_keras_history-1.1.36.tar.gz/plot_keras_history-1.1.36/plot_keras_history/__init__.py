94c9da046e842e1424c75eed21814dcafc592b88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:32 +0000

    Add package plot-keras-history/1.1.36/plot_keras_history-1.1.36.tar.gz

---
+++ packages/plot-keras-history/plot_keras_history-1.1.36.tar.gz/plot_keras_history-1.1.36/plot_keras_history/__init__.py
@@ -0,0 +1,5 @@
+"""Module providing methods to plot Keras models Histories and some utilities."""
+from .plot_keras_history import plot_history, show_history
+from .utils import chain_histories
+
+__all__ = ["plot_history", "chain_histories", "show_history"]
