1bab3a27b228f61a8b2d4291b7a2b95a1af53557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:56 +0000

    Add package fhir-kindling/0.8.4/fhir_kindling-0.8.4.tar.gz

---
+++ packages/fhir-kindling/fhir_kindling-0.8.4.tar.gz/fhir_kindling-0.8.4/fhir_kindling/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for fhir-kindling."""
+
+__author__ = """Michael Graf"""
+__email__ = 'michael.graf@uni-tuebingen.de'
+__version__ = '0.8.4'
+
+from .fhir_server import FhirServer
+from .fhir_query import FHIRQuery
