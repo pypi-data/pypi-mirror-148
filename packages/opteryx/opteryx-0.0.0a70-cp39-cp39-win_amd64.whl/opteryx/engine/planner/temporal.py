812cf45e78c1aa09c3aa8568aa7a0e28ef2f1e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:39 +0000

    Add package opteryx/0.0.0a70/opteryx-0.0.0a70-cp39-cp39-win_amd64.whl

---
+++ packages/opteryx/opteryx-0.0.0a70-cp39-cp39-win_amd64.whl/opteryx/engine/planner/temporal.py
@@ -0,0 +1,159 @@
+"""
+This compensates for missing temporal table support in the SQL parser.
+
+For information on temporal tables see: 
+https://blog.devgenius.io/a-query-in-time-introduction-to-sql-server-temporal-tables-145ddb1355d9
+
+This supports the following syntaxes
+
+- FOR TODAY
+- FOR YESTERDAY
+- FOR DATE <timestamp>
+- FOR DATES BETWEEN <timestamp> AND <timestamp>
+
+"""
+import re
+from opteryx.utils import dates
+import datetime
+
+SQL_PARTS = [
+    r"SELECT",
+    r"FROM",
+    r"FOR",
+    r"WHERE",
+    r"GROUP\sBY",
+    r"HAVING",
+    r"ORDER\sBY",
+    r"LIMIT",
+    r"OFFSET",
+    r"INNER\sJOIN",
+    r"CROSS\sJOIN",
+    r"JOIN",
+]
+
+
+def clean_statement(string):
+    """
+    Remove carriage returns and all whitespace to single spaces
+    """
+    _RE_COMBINE_WHITESPACE = re.compile(r"\s+")
+    return _RE_COMBINE_WHITESPACE.sub(" ", string).strip().upper()
+
+
+def sql_parts(string):
+    """
+    Split a SQL statement into clauses
+    """
+    reg = re.compile(
+        r"(\(|\)|,|;|"
+        + r"|".join([r"\b" + i.replace(r" ", r"\s") + r"\b" for i in SQL_PARTS])
+        + r")",
+        re.IGNORECASE,
+    )
+    parts = reg.split(string)
+    return [part.strip() for part in parts if part.strip() != ""]
+
+
+def remove_comments(string):
+    """
+    Remove comments from the string
+    """
+    pattern = r"(\".*?\"|\'.*?\')|(/\*.*?\*/|--[^\r\n]*$)"
+    # first group captures quoted strings (double or single)
+    # second group captures comments (//single-line or /* multi-line */)
+    regex = re.compile(pattern, re.MULTILINE | re.DOTALL)
+
+    def _replacer(match):
+        # if the 2nd group (capturing comments) is not None,
+        # it means we have captured a non-quoted (real) comment string.
+        if match.group(2) is not None:
+            return ""  # so we will return empty to remove the comment
+        else:  # otherwise, we will return the 1st group
+            return match.group(1)  # captured quoted-string
+
+    return regex.sub(_replacer, string)
+
+
+def parse_range(range):
+    range = range.upper()
+    TODAY = datetime.date.today()
+
+    if range == "PREVIOUS_MONTH":
+        pass
+    if range == "THIS_MONTH":
+        pass
+    if range == "MONTH(YEAR, MONTH)":
+        pass
+    if range == "PREVIOUS_CYCLE(ROLL_OVER)":
+        pass
+    if range == "CYCLE(YEAR, MONTH, ROLL_OVER)":
+        pass
+
+
+def parse_date(date):
+
+    date = date.upper()
+    TODAY = datetime.date.today()
+
+    if date == "TODAY":
+        return TODAY
+    if date == "YESTERDAY":
+        return TODAY - datetime.timedelta(days=1)
+
+    parsed_date = dates.parse_iso(date[1:-1])
+    if parsed_date:
+        return parsed_date.date()
+
+
+def extract_temporal_filters(sql):
+
+    # prep the statement, by normalizing it
+    clean_sql = remove_comments(sql)
+    clean_sql = clean_statement(clean_sql)
+    parts = sql_parts(clean_sql)
+
+    TODAY = datetime.date.today()
+    clearing_regex = None
+    start_date = TODAY
+    end_date = TODAY
+
+    try:
+        pos = parts.index("FOR")
+        for_date_string = parts[pos + 1]
+        for_date = parse_date(for_date_string)
+
+        if for_date:
+            start_date = for_date
+            end_date = for_date
+            clearing_regex = (
+                r"(\bFOR[\n\r\s]+" + for_date_string.replace("'", r"\'") + r"(?!\S))"
+            )
+        elif for_date_string.startswith("DATES BETWEEN "):
+            parts = for_date_string.split(" ")
+            start_date = parse_date(parts[2])
+            end_date = parse_date(parts[4])
+            clearing_regex = (
+                r"(FOR[\n\r\s]+DATES[\n\r\s]+BETWEEN[\n\r\s]+"
+                + parts[2]
+                + r"[\n\r\s]+AND[\n\r\s]+"
+                + parts[4]
+                + r"(?!\S))"
+            )
+        elif for_date_string.startswith("DATES IN "):
+            raise NotImplementedError("FOR DATES IN not implemented")
+            # PREVIOUS MONTH
+            # PREVIOUS CYCLE
+            # THIS MONTH
+            # THIS CYCLE
+
+        if clearing_regex:
+            regex = re.compile(clearing_regex, re.MULTILINE | re.DOTALL | re.IGNORECASE)
+            sql = regex.sub("\n-- FOR STATEMENT REMOVED\n", sql)
+
+        # swap the order if we need to
+        if start_date > end_date:
+            start_date, end_date = end_date, start_date
+    except:
+        pass
+
+    return start_date, end_date, sql
---
+++ packages/opteryx/opteryx-0.0.0a70-cp39-cp39-win_amd64.whl/opteryx/utils/arrow.py
@@ -0,0 +1,149 @@
+from typing import Iterable, List
+from opteryx.utils.columns import Columns
+
+
+def fetchmany(pages, limit: int = 1000):
+
+    from pyarrow import Table
+
+    if pages is None:
+        return []
+
+    if isinstance(pages, Table):
+        pages = [pages]
+
+    DEFAULT_CHUNK_SIZE = 1000
+    chunk_size = min(limit, DEFAULT_CHUNK_SIZE)
+    if chunk_size < 0:
+        chunk_size = DEFAULT_CHUNK_SIZE
+
+    def _inner_row_reader():
+
+        column_names = None
+
+        for page in pages:
+
+            if column_names is None:
+                columns = Columns(page)
+                preferred_names = columns.preferred_column_names
+                column_names = []
+                for col in page.column_names:
+                    column_names.append([c for a, c in preferred_names if a == col][0])
+
+            page = page.rename_columns(column_names)
+
+            for batch in page.to_batches(max_chunksize=chunk_size):
+                yield from batch.to_pylist()
+
+    index = -1
+    for index, row in enumerate(_inner_row_reader()):
+        if index == limit:
+            return
+        yield row
+
+    if index < 0:
+        yield {}
+
+
+def fetchone(pages: Iterable) -> dict:
+    return fetchmany(pages=pages, limit=1).pop()
+
+
+def fetchall(pages) -> List[dict]:
+    return fetchmany(pages=pages, limit=-1)
+
+
+"""
+Adapted from:
+https://stackoverflow.com/questions/55546027/how-to-assign-arbitrary-metadata-to-pyarrow-table-parquet-columns
+"""
+
+
+def set_metadata(table, table_metadata=None, column_metadata=None):
+    """
+    Store table-level metadata as json-encoded byte strings.
+
+    Table-level metadata is stored in the table's schema.
+
+    parameters:
+        table: pyarrow.Table
+            The table to store metadata in
+        col_meta: dict
+            A json-serializable dictionary with column metadata in the form
+            {
+                'column_1': {'some': 'data', 'value': 1},
+                'column_2': {'more': 'stuff', 'values': [1,2,3]}
+            }
+        tbl_meta: dict
+            A json-serializable dictionary with table-level metadata.
+    """
+    import pyarrow as pa
+    import orjson
+
+    # Create updated column fields with new metadata
+    if table_metadata or column_metadata:
+
+        fields = []
+        for name in table.schema.names:
+            col = table.field(name)
+            if col.name in column_metadata:
+                # Get updated column metadata
+                metadata = col.metadata or {}
+                for k, v in column_metadata[name].items():
+                    if isinstance(k, str):
+                        k = k.encode()
+                    metadata[k] = orjson.dumps(v)
+                # Update field with updated metadata
+                fields.append(col.with_metadata(metadata))
+            else:
+                fields.append(col)
+
+        # Get updated table metadata
+        tbl_metadata = table.schema.metadata or {}
+        if table_metadata:
+            for k, v in table_metadata.items():
+                if isinstance(v, bytes):
+                    tbl_metadata[k] = v
+                else:
+                    tbl_metadata[k] = orjson.dumps(v)
+
+        # Create new schema with updated table metadata
+        schema = pa.schema(fields, metadata=tbl_metadata)
+        # With updated schema build new table (shouldn't copy data)
+        table = table.cast(schema)
+
+    return table
+
+
+def _decode_metadata(metadata):
+    """
+    Arrow stores metadata keys and values as bytes. We store "arbitrary" data as
+    json-encoded strings (utf-8), which are here decoded into normal dict.
+    """
+    import orjson
+
+    if not metadata:
+        # None or {} are not decoded
+        return {}
+
+    decoded = {}
+    for k, v in metadata.items():
+        key = k.decode("utf-8")
+        val = orjson.loads(v)
+        decoded[key] = val
+    return decoded
+
+
+def table_metadata(tbl):
+    """Get table metadata as dict."""
+    return _decode_metadata(tbl.schema.metadata)
+
+
+def column_metadata(tbl):
+    """Get column metadata as dict."""
+    return {col: _decode_metadata(tbl.field(col).metadata) for col in tbl.schema.names}
+
+
+def get_metadata(tbl):
+    """Get column and table metadata as dicts."""
+    return column_metadata(tbl), table_metadata(tbl)
