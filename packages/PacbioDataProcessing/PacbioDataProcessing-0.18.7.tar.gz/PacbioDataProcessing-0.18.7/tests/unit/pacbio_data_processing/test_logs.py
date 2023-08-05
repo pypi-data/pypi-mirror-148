aa620fd6ee54e01edf9c3df291984689b2ed86a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:27 +0000

    Add package PacbioDataProcessing/0.18.7/PacbioDataProcessing-0.18.7.tar.gz

---
+++ packages/PacbioDataProcessing/PacbioDataProcessing-0.18.7.tar.gz/PacbioDataProcessing-0.18.7/tests/unit/pacbio_data_processing/test_logs.py
@@ -0,0 +1,43 @@
+#######################################################################
+#
+# Copyright (C) 2021 David Palao
+#
+# This file is part of PacBio data processing.
+#
+#  PacBioDataProcessing is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  PacBio data processing is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with PacBioDataProcessing. If not, see <http://www.gnu.org/licenses/>.
+#
+#######################################################################
+
+import unittest
+from unittest.mock import patch
+import logging
+
+from pacbio_data_processing.logs import config_logging
+
+
+@patch("pacbio_data_processing.logs.logging.basicConfig")
+class ConfigLoggingTestCase(unittest.TestCase):
+    def test_calls_basic_config_with_no_verbosity(self, pbasicConfig):
+        config_logging(0)
+        pbasicConfig.assert_called_once_with(
+            level=logging.INFO,
+            format="[%(asctime)-15s][%(levelname)s] %(message)s"
+        )
+
+    def test_calls_basic_config_with_verbosity(self, pbasicConfig):
+        config_logging(1)
+        pbasicConfig.assert_called_once_with(
+            level=logging.DEBUG,
+            format="[%(asctime)-15s][%(levelname)s] %(message)s"
+        )
