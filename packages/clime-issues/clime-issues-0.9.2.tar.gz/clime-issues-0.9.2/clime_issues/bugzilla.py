2fea7ddfd990b1f45de00424667e1b0a20030eea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:46 +0000

    Add package clime-issues/0.9.2/clime-issues-0.9.2.tar.gz

---
+++ packages/clime-issues/clime-issues-0.9.2.tar.gz/clime-issues-0.9.2/clime_issues/bugzilla.py
@@ -0,0 +1,41 @@
+from argparse import Namespace
+
+import pandas
+from pandas import DataFrame
+from progress.bar import Bar
+from requests import Response, get
+
+from clime_issues.args import bugzillaArgs
+from clime_issues.version import version
+
+
+def getIssueResponse(url: str, bug: int) -> Response:
+    apiURL: str = f"{url}/rest/bug/{bug}"
+    return get(url=apiURL)
+
+
+def main() -> None:
+    args: Namespace = bugzillaArgs()
+
+    if args.version:
+        print(f"clime-bz-issues-collect version {version()}")
+        quit(0)
+
+    df: DataFrame = pandas.read_csv(args.input)
+    bugIDs: list = df["Bug ID"].tolist()
+
+    json: list = []
+
+    with Bar("Downloading Bugzilla issues...", max=len(bugIDs)) as bar:
+        id: int
+        for id in bugIDs:
+            resp: Response = getIssueResponse(args.url, bug=id)
+            json.append(resp.json())
+            bar.next()
+
+    data: DataFrame = DataFrame(json)
+    data.to_json(args.output, indent=4)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/clime-issues/clime-issues-0.9.2.tar.gz/clime-issues-0.9.2/clime_issues/github.py
@@ -0,0 +1,112 @@
+import logging
+import re
+from argparse import Namespace
+from datetime import datetime
+
+from dateutil.parser import parse as dateParse
+from pandas import DataFrame
+from progress.bar import Bar
+from requests import Response, get
+from requests.models import CaseInsensitiveDict
+
+from clime_issues.args import githubArgs
+from clime_issues.version import version
+
+
+def getIssueResponse(repo: str, token: str, page: int = 1) -> Response:
+    requestHeaders: dict = {
+        "Accept": "application/vnd.github.v3+json",
+        "User-Agent": "gh-all-issues",
+        "Authorization": f"token {token}",
+    }
+
+    apiURL: str = f"https://api.github.com/repos/{repo}/issues?state=all&sort=created&direction=asc&per_page=100&page={page}"
+
+    logging.debug(f"API endpoint URL: {apiURL}")
+    return get(url=apiURL, headers=requestHeaders)
+
+
+def getPageCount(response: Response) -> int:
+    headers: CaseInsensitiveDict = response.headers
+    try:
+        lastPageString: str = headers["Link"].split(",")[-1].split("&")[4]
+        pageCount: int = int(re.search(r"\d+", lastPageString).group())
+    except KeyError:
+        pageCount: int = 1
+    logging.info(f"Number of pages of issues: {pageCount}")
+    return pageCount
+
+
+def iterateAPI(repo: str, token: str) -> list:
+    logging.debug("Starting to iterate through issue pages...\n")
+    with Bar("Determining number of pages of issues...", max=100) as bar:
+        logging.info("Iteration 0")
+        resp: Response = getIssueResponse(repo, token, page=1)
+        pageCount: int = getPageCount(resp)
+        json: list = resp.json()
+        logging.debug(f"JSON data:\n{json}")
+        bar.message = "Downloading GitHub issues..."
+        bar.max = pageCount
+        bar.update()
+        logging.info("Finished iteration 0\n")
+        bar.next()
+
+        for page in range(2, pageCount + 1):
+            logging.info(f"Iteration {page}")
+            resp: Response = getIssueResponse(repo, token, page)
+            json.extend(resp.json())
+            logging.info(f"Finished iteration {page}\n")
+            bar.next()
+
+    return json
+
+
+def computeValues(data: list) -> list | None:
+    try:
+        day0: datetime = dateParse(data[0]["created_at"]).replace(tzinfo=None)
+    except IndexError:
+        logging.error(f"Invalid JSON formatting. Potentially no JSON data.\n{data}")
+        quit(1)
+    x: dict
+    for x in data:
+        x["opened_day_since_0"] = (
+            dateParse(x["created_at"]).replace(tzinfo=None) - day0
+        ).days
+
+        try:
+            x["closed_day_since_0"] = (
+                dateParse(x["closed_at"]).replace(tzinfo=None) - day0
+            ).days
+        except TypeError:
+            x["closed_day_since_0"] = (datetime.now().replace(tzinfo=None) - day0).days
+
+    return data
+
+
+def main() -> None:
+    args: Namespace = githubArgs()
+
+    if args.version:
+        print(f"clime-gh-issues-collect version {version()}")
+        quit(0)
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        filename=args.log,
+        filemode="a",
+        format="%(process)d-%(asctime)s-%(levelname)s: %(message)s",
+    )
+    logging.info("Started logging...")
+
+    raw: list = iterateAPI(repo=args.repository, token=args.token)
+    data: list = computeValues(raw)
+
+    if args.pull_request == False:
+        data = [x for x in data if x.get("pull_request") is None]
+
+    df: DataFrame = DataFrame(data)
+    df.T.to_json(args.output, indent=4)
+
+
+if __name__ == "__main__":
+    main()
