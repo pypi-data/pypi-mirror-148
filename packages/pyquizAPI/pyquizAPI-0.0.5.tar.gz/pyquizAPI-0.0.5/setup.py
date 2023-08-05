7be6a7d98f4bd9eb23806580a4fc0801b3769ec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:06 +0000

    Add package pyquizAPI/0.0.5/pyquizAPI-0.0.5.tar.gz

---
+++ packages/pyquizAPI/pyquizAPI-0.0.5.tar.gz/pyquizAPI-0.0.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as readme:
+    description = readme.read()
+
+setup(
+    name=  "pyquizAPI",
+    version = "0.0.5",
+    author="msherburne",
+    license="MIT",
+    description="API Client Wrapper for QuizAPI.io",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    packages= ['pyquizAPI'],
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    url="https://github.com/msherburne/pyquizAPI"
+)
