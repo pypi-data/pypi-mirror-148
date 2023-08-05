8a62ef2a74513da8517b1798fdb7ca9c984665ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:50 +0000

    Add package fb-sapp/0.5.2/fb-sapp-0.5.2.tar.gz

---
+++ packages/fb-sapp/fb-sapp-0.5.2.tar.gz/fb-sapp-0.5.2/sapp/ui/frontend/build/precache-manifest.a7f7f6abe00bad9560a78f2d91e2b6e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62cb061a6f94f6968c4731a2d6379453",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e1910c183fde53c05a3",
+    "url": "/static/css/2.4f61bb1c.chunk.css"
+  },
+  {
+    "revision": "a07998014b189d3f033b",
+    "url": "/static/css/main.02f8510e.chunk.css"
+  },
+  {
+    "revision": "3e1910c183fde53c05a3",
+    "url": "/static/js/2.0703cbf9.chunk.js"
+  },
+  {
+    "revision": "56d41548479f8d7a081b36fc36026924",
+    "url": "/static/js/2.0703cbf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a07998014b189d3f033b",
+    "url": "/static/js/main.9892bc95.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
