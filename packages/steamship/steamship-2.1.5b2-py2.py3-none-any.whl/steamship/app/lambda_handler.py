81b471779596983c4e852a36d89ca2c768033377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:05 +0000

    Add package steamship/2.1.5b2/steamship-2.1.5b2-py2.py3-none-any.whl

---
+++ packages/steamship/steamship-2.1.5b2-py2.py3-none-any.whl/steamship/app/lambda_handler.py
@@ -0,0 +1,58 @@
+import dataclasses
+import io
+import logging
+from typing import Dict, Type
+
+from steamship.app.app import App
+from steamship.app.request import Request
+from steamship.app.response import Response, Http
+from steamship.client.client import Steamship
+from steamship.base import SteamshipError
+
+
+def create_handler(App: Type[App]):
+    """Wrapper function for an Steamship app within an AWS Lambda function.
+    """
+
+    def _handler(event: Dict, context: Dict = None) -> Response:
+        try:
+            client = Steamship(
+                configDict=event.get("clientConfig", None)
+            )
+        except SteamshipError as se:
+            return Response.from_obj(se)
+        except Exception as ex:
+            return Response.error(code=500, message="Plugin/App handler was unable to create Steamship client.", exception=ex)
+
+        app = None
+
+        try:
+            request = Request.from_dict(event)
+        except SteamshipError as se:
+            return Response.from_obj(se)
+        except Exception as ex:
+            return Response.error(code=500, message="Plugin/App handler was unable to parse inbound request.", exception=ex)
+
+        try:
+            app = App(client=client, config=request.invocation.config)
+        except SteamshipError as se:
+            return Response.from_obj(se)
+        except Exception as ex:
+            return Response.error(code=500, message="Handler was unable to initialize plugin/app.", exception=ex)
+
+        if app is None:
+            return Response.error(code=500, message="Handler was unable to construct app/plugin for invocation")
+
+        try:
+            response = app(request)
+            return Response.from_obj(response)
+        except SteamshipError as se:
+            return Response.from_obj(se)
+        except Exception as ex:
+            return Response.error(code=500, message="Handler was unable to run app/plugin method", exception=ex)
+
+    def handler(event: Dict, context: Dict = None) -> dict:
+        response = _handler(event, context)
+        return response.to_dict()
+
+    return handler
---
+++ packages/steamship/steamship-2.1.5b2-py2.py3-none-any.whl/steamship/data/app_instance.py
@@ -0,0 +1,124 @@
+from dataclasses import dataclass
+from typing import Dict
+
+from steamship.base import Client, Request
+
+
+@dataclass
+class CreateAppInstanceRequest(Request):
+    id: str = None
+    appId: str = None
+    appVersionId: str = None
+    handle: str = None
+    upsert: bool = None
+    config: Dict[str, any] = None
+
+
+@dataclass
+class DeleteAppInstanceRequest(Request):
+    id: str
+
+
+@dataclass
+class AppInstance:
+    client: Client = None
+    id: str = None
+    handle: str = None
+    appId: str = None
+    appHandle: str = None
+    userHandle: str = None
+    appVersionId: str = None
+    userId: str = None
+    invocationURL: str = None
+    config: Dict[str, any] = None
+    spaceId: str = None
+
+    @staticmethod
+    def from_dict(d: any, client: Client = None) -> "AppInstance":
+        if 'appInstance' in d:
+            d = d['appInstance']
+
+        return AppInstance(
+            client=client,
+            id=d.get('id', None),
+            handle=d.get('handle', None),
+            appId=d.get('appId', None),
+            appHandle=d.get('appHandle', None),
+            userHandle=d.get('userHandle', None),
+            appVersionId=d.get('appVersionId', None),
+            userId=d.get('userId', None),
+            invocationURL=d.get('invocationURL', None),
+            config= d.get('config', None),
+            spaceId= d.get('spaceId', None)
+        )
+
+    @staticmethod
+    def create(
+            client: Client,
+            appId: str = None,
+            appVersionId: str = None,
+            handle: str = None,
+            upsert: bool = None,
+            config: Dict[str, any] = None
+    ) -> "AppInstance":
+
+        req = CreateAppInstanceRequest(
+            handle=handle,
+            appId=appId,
+            appVersionId=appVersionId,
+            upsert=upsert,
+            config=config
+        )
+
+        return client.post(
+            'app/instance/create',
+            payload=req,
+            expect=AppInstance
+        )
+
+    def delete(self) -> "AppInstance":
+        req = DeleteAppInstanceRequest(
+            id=self.id
+        )
+        return self.client.post(
+            'app/instance/delete',
+            payload=req,
+            expect=AppInstance
+        )
+
+    def get(self, path: str, **kwargs):
+        if path[0] == '/':
+            path = path[1:]
+        return self.client.get(
+            '/_/_/{}'.format(path),
+            payload=kwargs,
+            appCall=True,
+            appOwner=self.userHandle,
+            appId=self.appId,
+            appInstanceId=self.id,
+            spaceId=self.spaceId
+        )
+
+    def post(self, path: str, **kwargs):
+        if path[0] == '/':
+            path = path[1:]
+        return self.client.post(
+            '/_/_/{}'.format(path),
+            payload=kwargs,
+            appCall=True,
+            appOwner=self.userHandle,
+            appId=self.appId,
+            appInstanceId=self.id,
+            spaceId=self.spaceId
+        )
+
+    def full_url_for(self, path: str):
+        return "{}{}".format(
+            self.invocationURL,
+            path
+        )
+
+
+@dataclass
+class ListPrivateAppInstancesRequest(Request):
+    pass
