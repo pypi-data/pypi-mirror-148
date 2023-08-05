e345c79e069f59eaab6eb2d718f051597783e73b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:12 +0000

    Add package vectorplot/0.0.23/vectorplot-0.0.23.tar.gz

---
+++ packages/vectorplot/vectorplot-0.0.23.tar.gz/vectorplot-0.0.23/README.md
@@ -0,0 +1,166 @@
+# vectorplot
+
+**vectorplot** é um pacote simples e fácil para plotar vetores no espaço bidimensional e tridimensional.
+
+## Dependências
+**Python 3.6** ou posterior
+
+Pacote **numpy**
+Pacote **matplotlib.pyplot**
+
+
+## Começando o uso
+Você vai precisar instalar o pacote **vectorplot**, para isso basta executar:
+```
+pip install vectorplot
+```
+
+## Funções
+
+* `plot2d([<lista de componentes de vetores>],[<lista de cores para cada vetor],[<limites da plotage 2D>])` - Plota vetores a partir de componentes no espaço bidimensional
+```
+Ex: 
+pip install vectorplot
+import numpy as np
+from vectorplot import vp
+
+u_laranja='#FF9A13'
+v_azul='#1190FF'
+r_vermelho='#FF0000'
+
+u=[1,2]
+v=[2,1]
+u=np.array(u)
+v=np.array(v)
+r=u+v
+
+vp.plot2d([u,v,r], [u_laranja,v_azul,r_vermelho], [-3,3,-3,3])
+```
+
+* `plot2d([<Lista de tuplas de coordenadas>],[<lista de cores para cada vetor],[<limites da plotage 2D>])` - Plota vetores a partir de tuplas de coordenadas de pontos no espaço bidimensional
+```
+Ex: 
+pip install vectorplot
+from vectorplot import vp
+
+#As tuplas devem conter coordenadas iniciais (xi,yi) e finais (xf,yf) por onde passa o vetor 
+u=(1,1,4,4)
+v=(-1,2,5,6)
+u_laranja='#FF9A13'
+v_azul='#1190FF'
+vp.plot2d([u,v], [u_laranja,v_azul], [-6,6,-6,6])
+```
+
+
+* `plot3d([<lista de componentes de vetores>],[<lista de cores para cada vetor],[<limites da plotage 3D>])` - Plota vetores a partir de componentes no espaço tridimensional
+```
+Ex: 
+pip install vectorplot
+import numpy as np
+from vectorplot import vp
+
+u_laranja='#FF9A13'
+v_azul='#1190FF'
+r_vermelho='#FF0000'
+
+u=[-1,1,2]
+v=[2,3,2]
+u=np.array(u)
+v=np.array(v)
+r=u+v
+
+vp.plot3d([u,v,r],[u_laranja,v_azul,r_vermelho],[-4,4,-4,4,-4,4])
+```
+
+* `plot3d([<Lista de tuplas de coordenadas>],[<lista de cores para cada vetor],[<limites da plotage 3D>])` - Plota vetores a partir de tuplas de coordenadas de pontos no espaço tridimensional
+```
+Ex: 
+pip install vectorplot
+import numpy as np
+from vectorplot import vp
+
+u_laranja='#FF9A13'
+v_azul='#1190FF'
+
+u=(-1,1,2,2)
+v=(2,3,4,5]
+
+vp.plot3d([u,v],[u_laranja,v_azul],[-5,5,-5,5,-5,5])
+```
+
+* `isequivalent2d([<Lista de componentes de vetores>])` - Verifica se uma lista de vetores 2d gerada a partir de componentes são equivalentes
+```
+Ex: 
+pip install vectorplot
+from vectorplot import vp
+
+u=[2,2]
+v=[2,2]
+print(vp.isequivalent([u,v]))
+
+```
+
+* `isequivalent2d([<Lista de tuplas de coordenadas>])` - Verifica se uma lista de vetores 2d gerada a partir de tuplas de coordenadas de pontos são equivalentes
+```
+Ex: 
+pip install vectorplot
+from vectorplot import vp
+
+u=(1,1,4,4)
+v=(-1,2,5,6)
+print(vp.isequivalent([u,v]))
+
+```
+* `isequivalent3d([<Lista de componentes de vetores>])` - Verifica se uma lista de vetores 3d gerada a partir de componentes são equivalentes
+```
+Ex: 
+pip install vectorplot
+from vectorplot import vp
+
+u=[2,2,2]
+v=[2,2,2]
+print(vp.isequivalent([u,v]))
+
+```
+
+* `isequivalent3d([<Lista de tuplas de coordenadas>])` - Verifica se uma lista de vetores 3d gerada a partir de tuplas de coordenadas de pontos são equivalentes
+```
+Ex: 
+pip install vectorplot
+from vectorplot import vp
+
+u=(1,1,1,4,4)
+v=(0,0,0,3,3,3)
+print(vp.isequivalent([u,v]))
+
+```
+
+* `resultant_module(<módulo do primeiro vetor>,<módulo do segundo vetor>,<ângulo formando entre vetores)` - Calcula o módulo da resultante gerado a partir de um ângulo entre dois vetores
+```
+Ex: 
+pip install vectorplot
+from vectorplot import vp
+
+print(vp.resultant_module_angle(2,2,60))
+
+```
+
+* `vector2d_module(<vetor 2d>)` - Calcula o módulo do vetor 2d a partir dos seus componentes de x e y
+```
+Ex: 
+pip install vectorplot
+from vectorplot import vp
+v=[3,3]
+print(vp.vector2d_module(v))
+
+```
+
+* `vector3d_module(<vetor 3d>)` - Calcula o módulo do vetor 3d a partir dos seus componentes de x, y e z
+```
+Ex: 
+pip install vectorplot
+from vectorplot import vp
+v=[2,2,1]
+print(vp.vector3d_module(v))
+
+```
---
+++ packages/vectorplot/vectorplot-0.0.23.tar.gz/vectorplot-0.0.23/vectorplot/vp.py
@@ -0,0 +1,149 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math as mt
+
+
+def resultant_module(module_vector1, module_vector2, angle):
+    vector1 = float(module_vector1)
+    vector2 =float(module_vector2)
+    angle = mt.radians(float(angle))
+
+    return mt.sqrt((pow(vector1, 2)+pow(vector2, 2)+2*vector1*vector2*mt.cos(angle)))
+
+
+def vector2d_module(vetor):
+    comp_x=vetor[0]
+    comp_y=vetor[1]
+    return mt.sqrt((pow(comp_x, 2)+pow(comp_y, 2)))
+
+
+def vector3d_module(vetor):
+    comp_x=vetor[0]
+    comp_y=vetor[1]
+    comp_z=vetor[2]
+    return mt.sqrt((pow(comp_x, 2)+pow(comp_y, 2))+pow(comp_z, 2))
+
+
+def isequivalent2d(lista_vetores):
+    comp_x_eq = None
+    comp_y_eq = None
+
+    verif = str(type(lista_vetores[0]))
+    if verif == "<class 'tuple'>":
+        for i in range(len(lista_vetores)):
+            comp_x = lista_vetores[i][2] - lista_vetores[i][0]
+            comp_y = lista_vetores[i][3] - lista_vetores[i][1]
+
+            if (comp_x_eq == None) and (comp_y_eq == None):
+                comp_x_eq = comp_x
+                comp_y_eq = comp_y
+            else:
+                if (comp_x_eq != comp_x) or (comp_y_eq != comp_y):
+                    return False
+        return True
+    else:
+        for vector in lista_vetores:
+            comp_x = vector[0]
+            comp_y = vector[1]
+            if (comp_x_eq == None) and (comp_y_eq == None):
+                comp_x_eq = comp_x
+                comp_y_eq = comp_y
+            else:
+                if (comp_x_eq != comp_x) or (comp_y_eq != comp_y):
+                    return False
+        return True
+
+
+def isequivalent3d(lista_vetores):
+    comp_x_eq = None
+    comp_y_eq = None
+    comp_z_eq = None
+
+    verif = str(type(lista_vetores[0]))
+    if verif == "<class 'tuple'>":
+        for i in range(len(lista_vetores)):
+            comp_x = lista_vetores[i][3] - lista_vetores[i][0]
+            comp_y = lista_vetores[i][4] - lista_vetores[i][1]
+            comp_z = lista_vetores[i][5] - lista_vetores[i][2]
+
+            if (comp_x_eq == None) and (comp_y_eq == None) and (comp_y_eq == None):
+                comp_x_eq = comp_x
+                comp_y_eq = comp_y
+                comp_z_eq = comp_z
+            else:
+                if (comp_x_eq != comp_x) or (comp_y_eq != comp_y) or (comp_z_eq != comp_z):
+                    return False
+        return True
+    else:
+        for vector in lista_vetores:
+            comp_x = vector[0]
+            comp_y = vector[1]
+            comp_z = vector[2]
+            if (comp_x_eq == None) and (comp_y_eq == None):
+                comp_x_eq = comp_x
+                comp_y_eq = comp_y
+                comp_z_eq = comp_z
+            else:
+                if (comp_x_eq != comp_x) or (comp_y_eq != comp_y) or (comp_z_eq != comp_z):
+                    return False
+        return True
+
+
+def plot2d(lista_vetores, lista_cores, lista_limites):
+    plt.figure()
+    plt.axvline(x=0, color='#A9A9A9', zorder=0)
+    plt.axhline(y=0, color='#A9A9A9', zorder=0)
+
+    verif = str(type(lista_vetores[0]))
+    if verif == "<class 'tuple'>":
+        for i in range(len(lista_vetores)):
+            plt.quiver([lista_vetores[i][0]],
+                       [lista_vetores[i][1]],
+                       [lista_vetores[i][2] - lista_vetores[i][0]],
+                       [lista_vetores[i][3] - lista_vetores[i][1]],
+                       angles='xy', scale_units='xy', scale=1, color=lista_cores[i],
+                       alpha=1)
+    else:
+        for i in range(len(lista_vetores)):
+            x = np.concatenate([[0, 0], lista_vetores[i]])
+            plt.quiver([x[0]],
+                       [x[1]],
+                       [x[2]],
+                       [x[3]],
+                       angles='xy', scale_units='xy', scale=1, color=lista_cores[i],
+                       alpha=1)
+
+
+
+    plt.grid()
+    plt.axis([lista_limites[0],lista_limites[1],lista_limites[2],lista_limites[3]])
+    plt.show()
+
+
+def plot3d(lista_vetores,lista_cores,lista_limites):
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.set_xlim3d([lista_limites[0], lista_limites[1]])
+    ax.set_ylim3d([lista_limites[2], lista_limites[3]])
+    ax.set_zlim3d([lista_limites[4], lista_limites[5]])
+    verif = str(type(lista_vetores[0]))
+    if verif == "<class 'tuple'>":
+        for i in range(len(lista_vetores)):
+            ax.quiver([lista_vetores[i][0]],[lista_vetores[i][1]],[lista_vetores[i][2]],
+                       [lista_vetores[i][3] - lista_vetores[i][0]],
+                       [lista_vetores[i][4] - lista_vetores[i][1]],
+                       [lista_vetores[i][5] - lista_vetores[i][2]],
+                      length=1, normalize=False, color=lista_cores[i])
+
+    else:
+        for i in range(len(lista_vetores)):
+            x = np.concatenate([[0, 0, 0], lista_vetores[i]])
+            ax.quiver([x[0]],
+                      [x[1]],
+                      [x[2]],
+                      [x[3]],
+                      [x[4]],
+                      [x[5]],
+                      length=1, normalize=False, color=lista_cores[i])
+
+    plt.show()
