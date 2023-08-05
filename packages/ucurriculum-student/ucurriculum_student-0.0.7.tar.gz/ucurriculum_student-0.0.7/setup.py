2f673324915c01531eb50a4076502428d27e991f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:39 +0000

    Add package ucurriculum-student/0.0.7/ucurriculum_student-0.0.7.tar.gz

---
+++ packages/ucurriculum-student/ucurriculum_student-0.0.7.tar.gz/ucurriculum_student-0.0.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ucurriculum_student',
+    version='0.0.7',
+    license='GNU Affero General Public License v3',
+    author="Ignacio Palma",
+    author_email='ignacio.palma@uc.cl',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/IgnacioPalma/UCurriculum-Student',
+    keywords='scraper',
+    install_requires=[
+        'uc_sso',
+        'selenium',
+        'bs4'
+      ],
+)
