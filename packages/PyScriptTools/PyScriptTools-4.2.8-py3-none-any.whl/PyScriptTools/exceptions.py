50bf16bf9343d23998c99ac38c2d8254759243a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:08 +0000

    Add package PyScriptTools/4.2.8/PyScriptTools-4.2.8-py3-none-any.whl

---
+++ packages/PyScriptTools/PyScriptTools-4.2.8-py3-none-any.whl/PyScriptTools/exceptions.py
@@ -0,0 +1,16 @@
+class AdminPermissionRequestDenied(Exception):
+    """ Set the Argument Variable 'show' to 'True' """
+class NoneTypeArgumentBool(Exception):
+    """ The Variable For Argument 'show' Should be 'Boolean' Type """
+class NoneTypeArgumentInt(Exception):
+    """ The Variable For Argument 'timeout' Should be 'Int' Type """
+class NoneTypeArgumentString(Exception):
+    """ The Variable For Argument 'link' Should be 'String' Type """
+class NoneLinuxMethod(Exception):
+    """ This Method is not Used in Linux OS """
+class UndefinedOperatingSystem(Exception):
+    """ The Operating System is Not Defined Yet """
+class UnrecognizeableTypeArgument(Exception):
+    """ The Variable For Arguments is not Recognizeable """
+class InvalidVariableType(Exception):
+    """ The Variable Type Chosen is not The Correct One """
