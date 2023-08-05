50c23cbcb84a778fba505a9c2129aa75c77b4d49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:29 +0000

    Add package NumbersHelper/1.1.0/NumbersHelper-1.1.0.tar.gz

---
+++ packages/NumbersHelper/NumbersHelper-1.1.0.tar.gz/NumbersHelper-1.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="NumbersHelper",
+    version="1.1.0",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    description="Helper functions for working",
+    url="https://github.com/Numbers-Technologies",
+    install_requires=["colorama"],
+    packages=["NumbersHelper"],
+    author_email='blcklptn@gmail.com',
+)
