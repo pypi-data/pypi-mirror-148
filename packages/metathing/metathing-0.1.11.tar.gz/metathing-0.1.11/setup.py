4c2d2583a00e6059b48f38314b427eebe9f309f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:45 +0000

    Add package metathing/0.1.11/metathing-0.1.11.tar.gz

---
+++ packages/metathing/metathing-0.1.11.tar.gz/metathing-0.1.11/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='metathing',
+    version='0.1.11',
+    description='MT-Service Python',
+    license='N/A',
+    packages=find_packages(exclude=['test', 'workdir', 'metathing/__pycache__']),
+)
