3b21aa814fbd3ce7aaada872c7a1ade047f37b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:21 +0000

    Add package influxdblite/0.0.4/influxdblite-0.0.4-py3-none-any.whl

---
+++ packages/influxdblite/influxdblite-0.0.4-py3-none-any.whl/influxdb_lite/influxdb_errors.py
@@ -0,0 +1,9 @@
+class SelectError(ValueError):
+    """ Inappropriate argument selection. """
+    def __init__(self, *args, **kwargs):  # real signature unknown
+        pass
+
+    @staticmethod  # known case of __new__
+    def __new__(*args, **kwargs):  # real signature unknown
+        """ Create and return a new object.  See help(type) for accurate signature. """
+        pass
