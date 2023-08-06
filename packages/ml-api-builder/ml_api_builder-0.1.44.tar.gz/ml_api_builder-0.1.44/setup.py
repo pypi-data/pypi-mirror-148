fc9f7644becf21040a60ea196f76479e58513409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:40 +0000

    Add package ml-api-builder/0.1.44/ml_api_builder-0.1.44.tar.gz

---
+++ packages/ml-api-builder/ml_api_builder-0.1.44.tar.gz/ml_api_builder-0.1.44/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ml_api_builder']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pandas>=1.2.1,<2.0.0',
+ 'python-jenkins>=1.7.0,<2.0.0',
+ 'requests>=2.20.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'ml-api-builder',
+    'version': '0.1.44',
+    'description': '',
+    'long_description': None,
+    'author': 'Michael Henzl',
+    'author_email': 'michael.henzl@seznam.cz',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
