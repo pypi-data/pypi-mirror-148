7eb3dbb534cc27fdc8b6bbd430723d8d53674f6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:28 +0000

    Add package PlayDL/0.0.5/PlayDL-0.0.5.tar.gz

---
+++ packages/PlayDL/PlayDL-0.0.5.tar.gz/PlayDL-0.0.5/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PlayDL",
+    version="0.0.5",
+    author="Xin Zhong",
+    author_email="1140091006@qq.com",
+    description="A package with presentations of various deep learning methods",
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    license="MIT Licence",
+    # packages=setuptools.find_packages("自制库/ZxSci"),
+    # packages=["matplotlib"],
+    install_requires=['torch','torchvision','functools','numpy','PIL','opencv-python'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # include_package_data = True,
+    # platforms = "any",
+    # install_requires = ['chardet']
+
+)
