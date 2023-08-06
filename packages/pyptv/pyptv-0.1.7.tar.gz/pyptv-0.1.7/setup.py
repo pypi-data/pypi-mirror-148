9ef834bf1c419444d5250bcd674beb34a29e98ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:06 +0000

    Add package pyptv/0.1.7/pyptv-0.1.7.tar.gz

---
+++ packages/pyptv/pyptv-0.1.7.tar.gz/pyptv-0.1.7/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+requirements = [
+    'chaco',
+    'enable',
+    'numpy',
+    'optv',
+    'PyQt5',
+    'scikit-image',
+    'Pygments',
+    'six',
+    'imagecodecs'
+]
+
+setup(
+    name='pyptv',
+    version='0.1.7',
+    description='Python GUI for the OpenPTV library `liboptv`',
+    author="Alex Liberzon",
+    author_email='alex.liberzon@gmail.com',
+    url='https://github.com/alexlib/pyptv',
+    packages=['pyptv'],
+    entry_points={
+        'console_scripts': [
+            'pyptv=pyptv.pyptv_gui:main'
+        ]
+    },
+    install_requires=requirements,
+    keywords='pyptv',
+    classifiers=[
+        'Programming Language :: Python :: 3'
+    ]
+)
