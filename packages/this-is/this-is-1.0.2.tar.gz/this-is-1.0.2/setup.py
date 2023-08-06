7469f1ea0d2e9ae66d2dd28fe5fe677caa494699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:39 +0000

    Add package this-is/1.0.2/this-is-1.0.2.tar.gz

---
+++ packages/this-is/this-is-1.0.2.tar.gz/this-is-1.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="this-is",
+    version="1.0.2",
+    author="Fernando Celmer",
+    author_email="email@fernandocelmer.com",
+    description="This is Zen of Python by Tim Peters",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FernandoCelmer/this-is-python",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "this_is"},
+    packages=setuptools.find_packages(where="this_is"),
+    python_requires=">=3.6",
+    zip_safe=False,
+    py_modules=["this_is"]
+)
