3530792c8572911b0be08d866797372809a77d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:05 +0000

    Add package circles-file-iterator/1.0.3/circles_file_iterator-1.0.3-py3-none-any.whl

---
+++ packages/circles-file-iterator/circles_file_iterator-1.0.3-py3-none-any.whl/circles_file_iterator/global_variables/__init__.py
@@ -0,0 +1,6 @@
+__name__ = 'global_variables'
+__package__ = __name__
+
+__all__ = ['global_variables']
+
+from .global_variables import cyverse_path_server_resources_default, cyverse_path_server_resources_user, cyverse_path_server_resources, local_folder, local_long_folder, local_temp_folder, cyverse_path_server_resources_test
