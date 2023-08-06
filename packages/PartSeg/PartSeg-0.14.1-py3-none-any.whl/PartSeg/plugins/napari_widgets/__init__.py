66b75ab2cacc85a3575f753211a895fcc379b636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:06 +0000

    Add package PartSeg/0.14.1/PartSeg-0.14.1-py3-none-any.whl

---
+++ packages/PartSeg/PartSeg-0.14.1-py3-none-any.whl/PartSeg/plugins/napari_widgets/__init__.py
@@ -0,0 +1,45 @@
+from napari_plugin_engine import napari_hook_implementation
+
+from PartSeg._launcher.main_window import PartSegGUILauncher
+from PartSeg.plugins.napari_widgets.mask_create_widget import MaskCreate
+from PartSeg.plugins.napari_widgets.roi_extraction_algorithms import ROIAnalysisExtraction, ROIMaskExtraction
+from PartSeg.plugins.napari_widgets.search_label_widget import SearchLabel
+
+
+@napari_hook_implementation
+def napari_experimental_provide_dock_widget():
+    from PartSeg.plugins.napari_widgets.simple_measurement_widget import SimpleMeasurement
+
+    return SimpleMeasurement
+
+
+@napari_hook_implementation(specname="napari_experimental_provide_dock_widget")
+def napari_experimental_provide_dock_widget1():
+    return ROIAnalysisExtraction
+
+
+@napari_hook_implementation(specname="napari_experimental_provide_dock_widget")
+def napari_experimental_provide_dock_widget2():
+    return ROIMaskExtraction
+
+
+@napari_hook_implementation(specname="napari_experimental_provide_dock_widget")
+def napari_experimental_provide_dock_widget3():
+    return MaskCreate
+
+
+@napari_hook_implementation(specname="napari_experimental_provide_dock_widget")
+def napari_experimental_provide_dock_widget4():
+    from PartSeg.plugins.napari_widgets.measurement_widget import Measurement
+
+    return Measurement
+
+
+@napari_hook_implementation(specname="napari_experimental_provide_dock_widget")
+def napari_experimental_provide_dock_widget5():
+    return SearchLabel
+
+
+@napari_hook_implementation(specname="napari_experimental_provide_dock_widget")
+def napari_experimental_provide_dock_widget6():
+    return PartSegGUILauncher
