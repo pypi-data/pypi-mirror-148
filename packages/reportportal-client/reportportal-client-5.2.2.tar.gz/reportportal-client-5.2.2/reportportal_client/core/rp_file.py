76bfd89a24d2210ca18d3dc253f24a2f275b15be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:06 +0000

    Add package reportportal-client/5.2.2/reportportal-client-5.2.2.tar.gz

---
+++ packages/reportportal-client/reportportal-client-5.2.2.tar.gz/reportportal-client-5.2.2/reportportal_client/core/rp_file.py
@@ -0,0 +1,34 @@
+"""This module contains classes representing RP file object."""
+
+import uuid
+
+
+class RPFile(object):
+    """Class representation for a file that will be attached to the log."""
+
+    def __init__(self,
+                 name=None,
+                 content=None,
+                 content_type=None,
+                 data=None,
+                 mime=None):
+        """Initialize instance attributes.
+
+        :param name:         File name
+        :param content:      File content
+        :param content_type: File content type (i.e. application/pdf)
+        :param data:         File content
+        :param mime:         File content type (i.e. application/pdf)
+        """
+        self.content = content or data
+        self.content_type = content_type or mime
+        self.name = name if name and name.strip() else str(uuid.uuid4())
+
+    @property
+    def payload(self):
+        """Get HTTP payload for the request."""
+        return {
+            'content': self.content,
+            'contentType': self.content_type,
+            'name': self.name
+        }
