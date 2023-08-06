f812be41818eebf74fa6317eb25420ee7c18ef45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:13 +0000

    Add package shopify-prefect-tasks/0.0.6/shopify_prefect_tasks-0.0.6.tar.gz

---
+++ packages/shopify-prefect-tasks/shopify_prefect_tasks-0.0.6.tar.gz/shopify_prefect_tasks-0.0.6/src/shopify_prefect_tasks/__init__.py
@@ -0,0 +1,16 @@
+"""
+Tasks for interacting with Shopify.
+"""
+try:
+    from .customer import FetchCustomer, CreateCustomer, UpdateCustomer, DeleteCustomer
+    from .product import FetchProduct, CreateProduct, UpdateProduct, CountProducts, DeleteProduct
+    from .variant import FetchVariant, CreateVariant, UpdateVariant, CountVariants, DeleteVariant
+    from .locations import FetchLocations
+    from .inventory_items import FetchInventoryItem, UpdateInventoryItem
+    from .inventory_levels import FetchInventoryLevels, LinkInventoryLevel, SetInventoryLevel, AdjustInventoryLevel, \
+        DeleteInventoryLevel
+    from .graphql import ExecuteGraphQL
+except ImportError:
+    raise ImportError(
+        'Using `prefect.tasks.shopify` requires Prefect to be installed with the "shopify" extra.'
+    )
