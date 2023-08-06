3a3e744170870ffee8ae5ca5cf31b840fe144664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:35 +0000

    Add package dotted-notation/0.8.1/dotted-notation-0.8.1.tar.gz

---
+++ packages/dotted-notation/dotted-notation-0.8.1.tar.gz/dotted-notation-0.8.1/dotted/__init__.py
@@ -0,0 +1,14 @@
+"""
+"""
+from .api import \
+    parse, is_pattern, is_inverted, quote, ANY, \
+    register, transform, \
+    assemble, assemble_multi, \
+    build, build_multi, \
+    expand, expand_multi, \
+    match, match_multi, \
+    apply, apply_multi, \
+    get, get_multi, \
+    update, update_multi, \
+    remove, remove_multi, \
+    pluck, pluck_multi
