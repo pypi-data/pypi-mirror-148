ba8329bb4c81ead28c8ce24a7b1363f84f0f5f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:44 +0000

    Add package jupyterlab-myst/0.1.7a0/jupyterlab_myst-0.1.7a0-py3-none-any.whl

---
+++ packages/jupyterlab-myst/jupyterlab_myst-0.1.7a0-py3-none-any.whl/jupyterlab_myst-0.1.7a0.data/data/share/jupyter/labextensions/jupyterlab-myst/static/style_index_js.d42e1454a33d9f63858f.js
@@ -0,0 +1,81 @@
+(self["webpackChunkjupyterlab_myst"] = self["webpackChunkjupyterlab_myst"] || []).push([["style_index_js"],{
+
+/***/ "./node_modules/css-loader/dist/cjs.js!./style/base.css":
+/*!**************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./style/base.css ***!
+  \**************************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/cssWithMappingToString.js */ "./node_modules/css-loader/dist/runtime/cssWithMappingToString.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _node_modules_css_loader_dist_cjs_js_node_modules_markdown_it_docutils_dist_css_style_min_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! -!../node_modules/css-loader/dist/cjs.js!../node_modules/markdown-it-docutils/dist/css/style.min.css */ "./node_modules/css-loader/dist/cjs.js!./node_modules/markdown-it-docutils/dist/css/style.min.css");
+/* harmony import */ var _node_modules_css_loader_dist_cjs_js_node_modules_katex_dist_katex_css__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! -!../node_modules/css-loader/dist/cjs.js!../node_modules/katex/dist/katex.css */ "./node_modules/css-loader/dist/cjs.js!./node_modules/katex/dist/katex.css");
+// Imports
+
+
+
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default()));
+___CSS_LOADER_EXPORT___.i(_node_modules_css_loader_dist_cjs_js_node_modules_markdown_it_docutils_dist_css_style_min_css__WEBPACK_IMPORTED_MODULE_2__.default);
+___CSS_LOADER_EXPORT___.i(_node_modules_css_loader_dist_cjs_js_node_modules_katex_dist_katex_css__WEBPACK_IMPORTED_MODULE_3__.default);
+// Module
+___CSS_LOADER_EXPORT___.push([module.id, "/* Use theme colours */\n:root {\n     --color-foreground-primary: var(--jp-content-font-color0);\n     --color-background-primary: var(--jp-layout-color0);\n     --color-foreground-secondary: var(--jp-content-font-color1);\n     --color-background-secondary: var(--jp-layout-color2);\n     --color-foreground-border: var(--jp-inverse-layout-color3);\n     --color-background-border: var(--jp-layout-color3);\n     --color-directive-unhandled-background: var(--jp-layout-color2);\n}\n\nbody[data-jp-theme-light=\"true\"] {\n     --color-directive-error-background: var(--jp-error-color2);\n     --color-admonition-background: rgba(--jp-layout-color2);\n}\nbody[data-jp-theme-light=\"false\"] {\n     --color-directive-error-background: var(--jp-error-color1);\n     --color-admonition-background: var(--jp-layout-color1);\n}\n \n", "",{"version":3,"sources":["webpack://./style/base.css"],"names":[],"mappings":"AAGA,sBAAsB;AACtB;KACK,yDAAyD;KACzD,mDAAmD;KACnD,2DAA2D;KAC3D,qDAAqD;KACrD,0DAA0D;KAC1D,kDAAkD;KAClD,+DAA+D;AACpE;;AAEA;KACK,0DAA0D;KAC1D,uDAAuD;AAC5D;AACA;KACK,0DAA0D;KAC1D,sDAAsD;AAC3D","sourcesContent":["@import \"markdown-it-docutils\";\n@import '~katex/dist/katex.css';\n\n/* Use theme colours */\n:root {\n     --color-foreground-primary: var(--jp-content-font-color0);\n     --color-background-primary: var(--jp-layout-color0);\n     --color-foreground-secondary: var(--jp-content-font-color1);\n     --color-background-secondary: var(--jp-layout-color2);\n     --color-foreground-border: var(--jp-inverse-layout-color3);\n     --color-background-border: var(--jp-layout-color3);\n     --color-directive-unhandled-background: var(--jp-layout-color2);\n}\n\nbody[data-jp-theme-light=\"true\"] {\n     --color-directive-error-background: var(--jp-error-color2);\n     --color-admonition-background: rgba(--jp-layout-color2);\n}\nbody[data-jp-theme-light=\"false\"] {\n     --color-directive-error-background: var(--jp-error-color1);\n     --color-admonition-background: var(--jp-layout-color1);\n}\n \n"],"sourceRoot":""}]);
+// Exports
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
+
+
+/***/ }),
+
+/***/ "./style/base.css":
+/*!************************!*\
+  !*** ./style/base.css ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ "./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js");
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !!../node_modules/css-loader/dist/cjs.js!./base.css */ "./node_modules/css-loader/dist/cjs.js!./style/base.css");
+
+            
+
+var options = {};
+
+options.insert = "head";
+options.singleton = false;
+
+var update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default()(_node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__.default, options);
+
+
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__.default.locals || {});
+
+/***/ }),
+
+/***/ "./style/index.js":
+/*!************************!*\
+  !*** ./style/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _base_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./base.css */ "./style/base.css");
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=style_index_js.d42e1454a33d9f63858f.js.map
---
+++ packages/jupyterlab-myst/jupyterlab_myst-0.1.7a0-py3-none-any.whl/jupyterlab_myst/labextension/static/lib_index_js.8ee8e9b17871175633c9.js
@@ -0,0 +1,229 @@
+(self["webpackChunkjupyterlab_myst"] = self["webpackChunkjupyterlab_myst"] || []).push([["lib_index_js"],{
+
+/***/ "./lib/builtins/amsmath.js":
+/*!*********************************!*\
+  !*** ./lib/builtins/amsmath.js ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "amsmath": () => (/* binding */ amsmath)
+/* harmony export */ });
+/* harmony import */ var _agoose77_jupyterlab_markup__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @agoose77/jupyterlab-markup */ "webpack/sharing/consume/default/@agoose77/jupyterlab-markup");
+/* harmony import */ var _agoose77_jupyterlab_markup__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_agoose77_jupyterlab_markup__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _tokens__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../tokens */ "./lib/tokens.js");
+
+
+/**
+ * Provides amsmath support
+ */
+const amsmath = (0,_agoose77_jupyterlab_markup__WEBPACK_IMPORTED_MODULE_0__.simpleMarkdownItPlugin)(_tokens__WEBPACK_IMPORTED_MODULE_1__.PACKAGE_NS, {
+    id: 'markdown-it-amsmath',
+    title: 'amsmath',
+    description: 'Plugin for amsmath LaTeX environments',
+    documentationUrls: {
+        Plugin: 'https://github.com/executablebooks/markdown-it-amsmath'
+    },
+    examples: {
+        'Example ': '\\begin{equation}\na = 1\n\\end{equation}'
+    },
+    plugin: async () => {
+        const amsmathPlugin = await __webpack_require__.e(/*! import() | markdown-it-amsmath */ "markdown-it-amsmath").then(__webpack_require__.t.bind(__webpack_require__, /*! markdown-it-amsmath */ "webpack/sharing/consume/default/markdown-it-amsmath/markdown-it-amsmath", 23));
+        return [amsmathPlugin.default];
+    }
+});
+
+
+/***/ }),
+
+/***/ "./lib/builtins/docutils.js":
+/*!**********************************!*\
+  !*** ./lib/builtins/docutils.js ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "docutils": () => (/* binding */ docutils)
+/* harmony export */ });
+/* harmony import */ var _agoose77_jupyterlab_markup__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @agoose77/jupyterlab-markup */ "webpack/sharing/consume/default/@agoose77/jupyterlab-markup");
+/* harmony import */ var _agoose77_jupyterlab_markup__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_agoose77_jupyterlab_markup__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _tokens__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../tokens */ "./lib/tokens.js");
+
+
+/**
+ * Provides docutils roles and directives
+ */
+const docutils = (0,_agoose77_jupyterlab_markup__WEBPACK_IMPORTED_MODULE_0__.simpleMarkdownItPlugin)(_tokens__WEBPACK_IMPORTED_MODULE_1__.PACKAGE_NS, {
+    id: 'markdown-it-docutils',
+    title: 'Docutils',
+    description: 'Plugin for implementing docutils style roles (inline extension point) and directives (block extension point)',
+    documentationUrls: {
+        Plugin: 'https://github.com/executablebooks/markdown-it-docutils'
+    },
+    examples: {
+        'Example ': '```{name} argument\n:option: value\n\ncontent\n```'
+    },
+    plugin: async () => {
+        const docutilsPlugin = await __webpack_require__.e(/*! import() | markdown-it-docutils */ "markdown-it-docutils").then(__webpack_require__.t.bind(__webpack_require__, /*! markdown-it-docutils */ "webpack/sharing/consume/default/markdown-it-docutils/markdown-it-docutils", 23));
+        return [docutilsPlugin.default];
+    }
+});
+
+
+/***/ }),
+
+/***/ "./lib/builtins/front-matter.js":
+/*!**************************************!*\
+  !*** ./lib/builtins/front-matter.js ***!
+  \**************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "frontMatter": () => (/* binding */ frontMatter)
+/* harmony export */ });
+/* harmony import */ var _agoose77_jupyterlab_markup__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @agoose77/jupyterlab-markup */ "webpack/sharing/consume/default/@agoose77/jupyterlab-markup");
+/* harmony import */ var _agoose77_jupyterlab_markup__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_agoose77_jupyterlab_markup__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _tokens__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../tokens */ "./lib/tokens.js");
+
+
+/**
+ * Provides front-matter support
+ */
+const frontMatter = (0,_agoose77_jupyterlab_markup__WEBPACK_IMPORTED_MODULE_0__.simpleMarkdownItPlugin)(_tokens__WEBPACK_IMPORTED_MODULE_1__.PACKAGE_NS, {
+    id: 'markdown-it-front-matter',
+    title: 'Front Matter',
+    description: 'Plugin for processing front matter for markdown-it markdown parser',
+    documentationUrls: {
+        Plugin: 'https://github.com/ParkSB/markdown-it-front-matter'
+    },
+    examples: {
+        'Example ': '---\nvalid-front-matter: true\n---'
+    },
+    plugin: async () => {
+        const frontMatterPlugin = await __webpack_require__.e(/*! import() | markdown-it-front-matter */ "markdown-it-front-matter").then(__webpack_require__.t.bind(__webpack_require__, /*! markdown-it-front-matter */ "webpack/sharing/consume/default/markdown-it-front-matter/markdown-it-front-matter", 23));
+        function handleMarkup(markup) {
+            // Do nothing for now
+        }
+        function plugin(md, options) {
+            frontMatterPlugin.default(md, handleMarkup);
+        }
+        return [plugin];
+    }
+});
+
+
+/***/ }),
+
+/***/ "./lib/builtins/index.js":
+/*!*******************************!*\
+  !*** ./lib/builtins/index.js ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "BUILTINS": () => (/* binding */ BUILTINS)
+/* harmony export */ });
+/* harmony import */ var _front_matter__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./front-matter */ "./lib/builtins/front-matter.js");
+/* harmony import */ var _docutils__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./docutils */ "./lib/builtins/docutils.js");
+/* harmony import */ var _amsmath__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./amsmath */ "./lib/builtins/amsmath.js");
+/* harmony import */ var _myst_extras__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./myst-extras */ "./lib/builtins/myst-extras.js");
+
+
+
+
+/**
+ * Builtin plugins provided by this labextension
+ */
+const BUILTINS = [_front_matter__WEBPACK_IMPORTED_MODULE_0__.frontMatter, _docutils__WEBPACK_IMPORTED_MODULE_1__.docutils, _amsmath__WEBPACK_IMPORTED_MODULE_2__.amsmath, _myst_extras__WEBPACK_IMPORTED_MODULE_3__.mystExtras];
+
+
+/***/ }),
+
+/***/ "./lib/builtins/myst-extras.js":
+/*!*************************************!*\
+  !*** ./lib/builtins/myst-extras.js ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "mystExtras": () => (/* binding */ mystExtras)
+/* harmony export */ });
+/* harmony import */ var _agoose77_jupyterlab_markup__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @agoose77/jupyterlab-markup */ "webpack/sharing/consume/default/@agoose77/jupyterlab-markup");
+/* harmony import */ var _agoose77_jupyterlab_markup__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_agoose77_jupyterlab_markup__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _tokens__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../tokens */ "./lib/tokens.js");
+
+
+/**
+ * Provides extra MyST support
+ */
+const mystExtras = (0,_agoose77_jupyterlab_markup__WEBPACK_IMPORTED_MODULE_0__.simpleMarkdownItPlugin)(_tokens__WEBPACK_IMPORTED_MODULE_1__.PACKAGE_NS, {
+    id: 'markdown-it-myst-extras',
+    title: 'MyST Extras',
+    description: 'Additional markdown-it plugins required for the MyST specification',
+    documentationUrls: {
+        Plugin: 'https://github.com/executablebooks/markdown-it-myst-extras'
+    },
+    examples: {
+        'Blockquotes': '% comment',
+        'Block Breaks': '+++',
+        'MyST Targets': '(name)=',
+        'Colon Fence': ':::name\ncontained text\n :::'
+    },
+    plugin: async () => {
+        const mystExtrasPlugins = await __webpack_require__.e(/*! import() | markdown-it-myst-extras */ "markdown-it-myst-extras").then(__webpack_require__.t.bind(__webpack_require__, /*! markdown-it-myst-extras */ "webpack/sharing/consume/default/markdown-it-myst-extras/markdown-it-myst-extras", 23));
+        return [mystExtrasPlugins.mystBlockPlugin, mystExtrasPlugins.colonFencePlugin];
+    }
+});
+
+
+/***/ }),
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _builtins__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./builtins */ "./lib/builtins/index.js");
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_builtins__WEBPACK_IMPORTED_MODULE_0__.BUILTINS);
+
+
+/***/ }),
+
+/***/ "./lib/tokens.js":
+/*!***********************!*\
+  !*** ./lib/tokens.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "PACKAGE_NS": () => (/* binding */ PACKAGE_NS)
+/* harmony export */ });
+/**
+ * The ID stem for all plugins
+ */
+const PACKAGE_NS = 'executablebooks/jupyterlab-markup';
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_index_js.8ee8e9b17871175633c9.js.map
