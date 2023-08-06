6f524be7bc9950335413b70279392c886766f00a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:14 +0000

    Add package itslive/0.1.0/itslive-0.1.0.tar.gz

---
+++ packages/itslive/itslive-0.1.0.tar.gz/itslive-0.1.0/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['itslive']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Shapely>=1.8.1,<2.0.0',
+ 'matplotlib>=3.5.1,<4.0.0',
+ 'pyproj>=3.3.1,<4.0.0',
+ 's3fs>=2022.3.0,<2023.0.0',
+ 'xarray>=2022.3.0,<2023.0.0',
+ 'zarr>=2.11.3,<3.0.0']
+
+setup_kwargs = {
+    'name': 'itslive',
+    'version': '0.1.0',
+    'description': 'Python client for ITSLIVE gralcier velocity data',
+    'long_description': None,
+    'author': 'betolink',
+    'author_email': 'luis.lopez@nsidc.org',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
