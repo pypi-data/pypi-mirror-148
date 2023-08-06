d5d4fed60f2cd857e7be0f36767a7e1f47c2ef77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:42 +0000

    Add package PyChromecast/12.0.0/PyChromecast-12.0.0-py2.py3-none-any.whl

---
+++ packages/PyChromecast/PyChromecast-12.0.0-py2.py3-none-any.whl/pychromecast/const.py
@@ -0,0 +1,16 @@
+"""
+Chromecast constants
+"""
+# Regular chromecast, supports video/audio
+CAST_TYPE_CHROMECAST = "cast"
+# Cast Audio device, supports only audio
+CAST_TYPE_AUDIO = "audio"
+# Cast Audio group device, supports only audio
+CAST_TYPE_GROUP = "group"
+
+SERVICE_TYPE_HOST = "host"
+SERVICE_TYPE_MDNS = "mdns"
+
+MESSAGE_TYPE = "type"
+REQUEST_ID = "requestId"
+SESSION_ID = "sessionId"
