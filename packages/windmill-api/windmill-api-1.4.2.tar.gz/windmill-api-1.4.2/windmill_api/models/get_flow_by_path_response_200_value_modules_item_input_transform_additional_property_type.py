ae6a906f3caec3af0452502872e45cc37d87d3bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:34 +0000

    Add package windmill-api/1.4.2/windmill-api-1.4.2.tar.gz

---
+++ packages/windmill-api/windmill-api-1.4.2.tar.gz/windmill-api-1.4.2/windmill_api/models/get_flow_by_path_response_200_value_modules_item_input_transform_additional_property_type.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class GetFlowByPathResponse200ValueModulesItemInputTransformAdditionalPropertyType(str, Enum):
+    STATIC = "static"
+    OUTPUT = "output"
+
+    def __str__(self) -> str:
+        return str(self.value)
