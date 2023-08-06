1fd3a284e4777fe9dd6b8fb9e261ced80726699d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:38 +0000

    Add package pygitpub/0.0.14/pygitpub-0.0.14.tar.gz

---
+++ packages/pygitpub/pygitpub-0.0.14.tar.gz/pygitpub-0.0.14/pygitpub/configs.py
@@ -0,0 +1,16 @@
+"""
+All configurations for pygitpub
+"""
+from pytconf import Config, ParamCreator
+
+
+class ConfigGithub(Config):
+    """
+    Paramters for your github account
+    """
+    username = ParamCreator.create_str(
+        help_string="What is your github username?",
+    )
+    token = ParamCreator.create_str(
+        help_string="What is your github token?",
+    )
