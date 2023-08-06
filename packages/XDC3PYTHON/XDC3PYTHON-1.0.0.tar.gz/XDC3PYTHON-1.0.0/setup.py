25983304c8d76afef0ca1af653eb5818d1a3ed7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:18:27 +0000

    Add package XDC3PYTHON/1.0.0/XDC3PYTHON-1.0.0.tar.gz

---
+++ packages/XDC3PYTHON/XDC3PYTHON-1.0.0.tar.gz/XDC3PYTHON-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name = 'XDC3PYTHON',
+    version = '1.0.0',
+    description = 'XDC PYTHON SDK with support for smart contracts, XRC20 & XRC721',
+    long_description=open('README.rst').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='XDC Foundation',
+    author_email=' XFLW@xinfin.org',
+    license='MIT',
+    keywords='',
+    packages=find_packages(),
+)
