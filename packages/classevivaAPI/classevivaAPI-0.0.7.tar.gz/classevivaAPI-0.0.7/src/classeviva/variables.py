a4cd411a34fc60215b7808e34c643b21f6842f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:23:29 +0000

    Add package classevivaAPI/0.0.7/classevivaAPI-0.0.7.tar.gz

---
+++ packages/classevivaAPI/classevivaAPI-0.0.7.tar.gz/classevivaAPI-0.0.7/src/classeviva/variables.py
@@ -0,0 +1,16 @@
+class Roles:
+    STUDENT = 0
+    PARENT = 1
+    TEACHER = 2
+
+
+class NoteSortBy:
+    AUTHOR = 0
+    DATE = 1
+    NOTE = 2
+    TYPE = 3
+
+
+class RegistroStatus:
+    PRESENTE = 0
+    ASSENTE = 1
