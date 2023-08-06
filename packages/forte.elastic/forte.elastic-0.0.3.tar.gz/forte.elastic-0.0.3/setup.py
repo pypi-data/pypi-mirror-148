631c5fe0a6fe79072af54c5410c1ddbaaecd3943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:21 +0000

    Add package forte.elastic/0.0.3/forte.elastic-0.0.3.tar.gz

---
+++ packages/forte.elastic/forte.elastic-0.0.3.tar.gz/forte.elastic-0.0.3/setup.py
@@ -0,0 +1,44 @@
+import sys
+from pathlib import Path
+
+import setuptools
+
+readme = Path("README.md")
+if readme.exists():
+    long_description = (Path("README.md")).read_text()
+else:
+    long_description = (
+        "Provide Forte implementations of a fantastic collection of NLP tools."
+    )
+
+if sys.version_info < (3, 6):
+    sys.exit("Python>=3.6 is required by Forte.")
+
+setuptools.setup(
+    name="forte.elastic",
+    version="0.0.3",
+    url="https://github.com/asyml/forte-wrappers/tree/main/src/elastic",
+    description="Provide Forte implementations of a fantastic collection of "
+    "NLP tools.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="Apache License Version 2.0",
+    packages=["fortex.elastic"],
+    namespace_packages=["fortex"],
+    include_package_data=True,
+    platforms="any",
+    install_requires=[
+        "forte==0.2.0",
+        "elasticsearch>=7.5.1, <8.0.0; python_version<'3.9.0'",
+        "elasticsearch>=7.14.0, <8.0.0; python_version>='3.9.0'",
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
