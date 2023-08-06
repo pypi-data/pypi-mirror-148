20e22d420f89f1d8a5233d7de2da11492a9a387d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:20:42 +0000

    Add package resparser/0.1/resparser-0.1.tar.gz

---
+++ packages/resparser/resparser-0.1.tar.gz/resparser-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='resparser',
+version='0.1',
+description='Extract Resume Entities Using NER',
+author='Yash Sonwane',
+packages=['resparser'],
+zip_safe=False)
