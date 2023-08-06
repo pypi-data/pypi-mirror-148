86977a93766d1a23573676a7d8d5e2386799dd49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:54 +0000

    Add package msds-tdm/1.0.1/msds-tdm-1.0.1.tar.gz

---
+++ packages/msds-tdm/msds-tdm-1.0.1.tar.gz/msds-tdm-1.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="msds-tdm",
+    version="1.0.1",
+    author="Soutick Saha, Chintan Sawla",
+    author_email="soutick2010@gmail.com, sawlachintan@gmail.com",
+    description="MSDS Package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sawlachintan/msds-library",
+    project_urls={
+        "Bug Tracker": "https://github.com/sawlachintan/msds-library/issues",
+    },
+    packages=find_packages(exclude="tests/*"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_data={
+        'msds_tdm': ['*.xlsx']
+    },
+    python_requires=">=3.6",
+    install_requires=[
+        "pandas==1.2.4",
+        "pdfplumber==0.6.0",
+        "openpyxl",
+    ],
+)
