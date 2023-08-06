331790b5a129b2802f8ec4e9ff8b1cf46fc942a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:54 +0000

    Add package midas-store/1.0.0rc4/midas-store-1.0.0rc4.tar.gz

---
+++ packages/midas-store/midas-store-1.0.0rc4.tar.gz/midas-store-1.0.0rc4/midas/modules/store/meta.py
@@ -0,0 +1,11 @@
+META = {
+    "type": "time-based",
+    "models": {
+        "Database": {
+            "public": True,
+            "any_inputs": True,
+            "params": ["filename", "verbose", "buffer_size", "overwrite"],
+            "attrs": [],
+        }
+    },
+}
