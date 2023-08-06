ae9ea3082e17c5a5f0e96ef6a5374859f122fe5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:20:37 +0000

    Add package Todoska/0.2.7/Todoska-0.2.7.tar.gz

---
+++ packages/Todoska/Todoska-0.2.7.tar.gz/Todoska-0.2.7/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import setuptools
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='Todoska',
+  version='0.2.07',
+  description='A Basic Todo Tracker in Cli',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='https://github.com/Abbhiishek/Todoska',  
+  author='Abhishek Kushwaha',
+  author_email='abhishekkushwaha1479@gmail.com',
+  license='MIT', 
+  entry_points={
+    'console_scripts': [
+      'todoska = Todoska.__main__:show',
+      'todoska add = Todoska.__main__:add',
+      'todoska delete = Todoska.__main__:delete',
+      'todoska update = Todoska.__main__:update',
+      'todoska complete = Todoska.__main__:complete'
+
+    ]
+  },
+  classifiers=classifiers,
+  keywords=['Python' , 'cli' , 'Todo' ,'Todotracker' , 'todo-tracker' , 'todo-tracker-cli', 'Todoska'], 
+  packages=['Todoska'],
+  python_requires='>=3.6',
+  install_requires=['typer' , 'rich' ] 
+)
