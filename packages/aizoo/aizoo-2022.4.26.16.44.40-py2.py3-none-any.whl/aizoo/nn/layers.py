8d832d4605ba2949103c8e6d92beff74d77459fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:47 +0000

    Add package aizoo/2022.4.26.16.44.40/aizoo-2022.4.26.16.44.40-py2.py3-none-any.whl

---
+++ packages/aizoo/aizoo-2022.4.26.16.44.40-py2.py3-none-any.whl/aizoo/nn/layers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : aizoo.
+# @File         : layers
+# @Time         : 2022/4/25 下午3:47
+# @Author       : yuanjie
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  :
+
+
