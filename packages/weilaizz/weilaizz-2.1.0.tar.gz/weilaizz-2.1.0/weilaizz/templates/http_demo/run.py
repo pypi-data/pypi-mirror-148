e438e398a8294293f37047447d7bf81fb0b01190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:25:21 +0000

    Add package weilaizz/2.1.0/weilaizz-2.1.0.tar.gz

---
+++ packages/weilaizz/weilaizz-2.1.0.tar.gz/weilaizz-2.1.0/weilaizz/templates/http_demo/run.py
@@ -0,0 +1,15 @@
+"""
+运行启动文件
+"""
+import pytest
+
+from config.run_config import mainSetup as ms, mainTearDown as mt
+
+if __name__ == '__main__':
+    ms()        #测试前置
+    pytest.main()
+    mt()        #测试后置
+
+
+
+
