fe8b73e0e0889ba10c8dcc228a3591a2a6a309a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:13 +0000

    Add package blank-format/0.2.1/blank_format-0.2.1.tar.gz

---
+++ packages/blank-format/blank_format-0.2.1.tar.gz/blank_format-0.2.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['blank_format']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['blank = blank_format:main']}
+
+setup_kwargs = {
+    'name': 'blank-format',
+    'version': '0.2.1',
+    'description': 'The ultimate code formatter',
+    'long_description': None,
+    'author': 'Joni Turunen',
+    'author_email': 'rojun.itu@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/d3rp/blank',
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.5',
+}
+
+
+setup(**setup_kwargs)
