dbf799c5af091ecc095ef64c6027f1bd86b18a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:02 +0000

    Add package types-python-dateutil/2.8.13/types-python-dateutil-2.8.13.tar.gz

---
+++ packages/types-python-dateutil/types-python-dateutil-2.8.13.tar.gz/types-python-dateutil-2.8.13/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+name = "types-python-dateutil"
+description = "Typing stubs for python-dateutil"
+long_description = '''
+## Typing stubs for python-dateutil
+
+This is a PEP 561 type stub package for the `python-dateutil` package.
+It can be used by type-checking tools like mypy, PyCharm, pytype etc. to check code
+that uses `python-dateutil`. The source for this package can be found at
+https://github.com/python/typeshed/tree/master/stubs/python-dateutil. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/master/README.md for more details.
+This package was generated from typeshed commit `f7aa41245e2bc688ae3fe6d5bd1008f0baf8d8e0`.
+'''.lstrip()
+
+setup(name=name,
+      version="2.8.13",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/python-dateutil.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['dateutil-stubs'],
+      package_data={'dateutil-stubs': ['__init__.pyi', '_common.pyi', 'easter.pyi', 'parser/__init__.pyi', 'parser/isoparser.pyi', 'relativedelta.pyi', 'rrule.pyi', 'tz/__init__.pyi', 'tz/_common.pyi', 'tz/tz.pyi', 'utils.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Typing :: Stubs Only",
+      ]
+)
