05463949dedf06b73c3bd03d0c3c72c3917be131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:45 +0000

    Add package pdme/0.7.0/pdme-0.7.0-py3-none-any.whl

---
+++ packages/pdme/pdme-0.7.0-py3-none-any.whl/pdme/model/__init__.py
@@ -0,0 +1,11 @@
+from pdme.model.model import DipoleModel
+from pdme.model.fixed_magnitude_model import SingleDipoleFixedMagnitudeModel
+from pdme.model.multidipole_fixed_magnitude_model import (
+	MultipleDipoleFixedMagnitudeModel,
+)
+
+__all__ = [
+	"DipoleModel",
+	"SingleDipoleFixedMagnitudeModel",
+	"MultipleDipoleFixedMagnitudeModel",
+]
