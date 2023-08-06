cd0f24e7fe05b9ab1a5c2f8feb26d8f064d3517b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:53 +0000

    Add package alfort/0.1.6/alfort-0.1.6-py3-none-any.whl

---
+++ packages/alfort/alfort-0.1.6-py3-none-any.whl/alfort/__init__.py
@@ -0,0 +1,10 @@
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__: str = version(__name__)
+except PackageNotFoundError:
+    __version__: str = "unknown"
+
+from .app import Alfort, Dispatch, Effect, Enqueue, Init, Update, View
+
+__all__ = ["Alfort", "Dispatch", "Effect", "View", "Update", "Init", "Enqueue"]
