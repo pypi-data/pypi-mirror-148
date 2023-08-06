831d6ef1d9ae1f22fcfb2663a5b8c2488d9085e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:10 +0000

    Add package pydude-pyto/0.18.0.5/pydude_pyto-0.18.0.5-py3-none-any.whl

---
+++ packages/pydude-pyto/pydude_pyto-0.18.0.5-py3-none-any.whl/dude_pyto/context.py
@@ -0,0 +1,16 @@
+import logging
+
+from .scraper import Scraper
+
+logger = logging.getLogger(__name__)
+
+_scraper = Scraper()
+group = _scraper.group
+run = _scraper.run
+save = _scraper.save
+select = _scraper.select
+startup = _scraper.startup
+shutdown = _scraper.shutdown
+pre_setup = _scraper.pre_setup
+post_setup = _scraper.post_setup
+start_requests = _scraper.start_requests
