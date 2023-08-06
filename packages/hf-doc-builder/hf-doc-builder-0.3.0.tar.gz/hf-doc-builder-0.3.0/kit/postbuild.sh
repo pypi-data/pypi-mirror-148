3bff5fafff08a7f415a5fa854bc0faa12e3ac4fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:55 +0000

    Add package hf-doc-builder/0.3.0/hf-doc-builder-0.3.0.tar.gz

---
+++ packages/hf-doc-builder/hf-doc-builder-0.3.0.tar.gz/hf-doc-builder-0.3.0/kit/postbuild.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+cp src/routes/_toctree.yml build/_toctree.yml
+
+# To avoid conflict with Hub Tailwind CSS build, 
+# 1. making doc-builder's PostCSS geenrated file an empty one
+# 2. making `rel="stylesheet"` -> `rel="modulepreload"`
+> $(find . -regex '.*build.*.css')
+cd build
+find . -name '*.html' -exec perl -pi -e 's/rel="stylesheet"/rel="modulepreload"/g' {} +
