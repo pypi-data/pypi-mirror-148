e147f1e6ef725360aed3b58cb2f1910773a7bc99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:34 +0000

    Add package hukudo/0.1.8/hukudo-0.1.8.tar.gz

---
+++ packages/hukudo/hukudo-0.1.8.tar.gz/hukudo-0.1.8/hukudo/cli.py
@@ -0,0 +1,34 @@
+import logging
+import os
+
+import click
+
+from hukudo.logging import LOG_LEVELS, configure_structlog_dev
+from .grafana.cli import grafana
+
+
+@click.group()
+@click.option(
+    '-l',
+    '--log-level',
+    type=click.Choice(LOG_LEVELS, case_sensitive=False),
+    default=os.environ.get('LOGLEVEL', 'WARNING'),
+)
+def root(log_level):
+    """
+    For completion, add this to ~/.bashrc:
+
+        eval "$(_HUKUDO_COMPLETE=bash_source hukudo)"
+
+    See also https://click.palletsprojects.com/en/8.1.x/shell-completion/
+    """
+    configure_structlog_dev(log_level)
+    logging.basicConfig(format='%(msg)s')
+
+
+# noinspection PyTypeChecker
+root.add_command(grafana)
+
+
+def main():
+    root()
