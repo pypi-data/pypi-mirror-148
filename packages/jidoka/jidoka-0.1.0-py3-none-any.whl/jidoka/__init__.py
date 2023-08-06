af29050b8abecd22544be8e0bddd578bf8845dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:04 +0000

    Add package jidoka/0.1.0/jidoka-0.1.0-py3-none-any.whl

---
+++ packages/jidoka/jidoka-0.1.0-py3-none-any.whl/jidoka/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# copyright: Copyright (C) [2022] [Jesse P. Johnson]
+# license: AGPL-3.0, see LICENSE.md for more details.
+
+import logging
+from typing import List
+
+__author__ = 'Jesse P. Johnson'
+__title__ = 'jidoka'
+__version__ = '0.1.0'
+__all__: List[str] = []
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
