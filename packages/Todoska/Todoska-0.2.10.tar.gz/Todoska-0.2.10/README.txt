9e11c80075f3407272bfe2d0377e199686f388b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:21:16 +0000

    Add package Todoska/0.2.10/Todoska-0.2.10.tar.gz

---
+++ packages/Todoska/Todoska-0.2.10.tar.gz/Todoska-0.2.10/README.txt
@@ -0,0 +1,49 @@
+Todoska is a simple Cli Todo-Tracker.
+
+
+
+Features :
+
+- You can add new task 
+
+- You can update a task
+
+- You can delete a Task 
+
+
+
+
+
+Usage :
+
+- Add new task :
+
+todoska add <task-name>
+
+- Update a task :
+
+todoska update <task-id> <task-name>
+
+- Delete a task :
+
+todoska delete <task-id>
+
+- View All task :
+
+todoska show
+
+
+
+
+## Installation
+
+```python
+pip install todoska
+```
+
+
+## Usage
+
+```python
+Figuring out dude ,
+```
---
+++ packages/Todoska/Todoska-0.2.10.tar.gz/Todoska-0.2.10/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import setuptools
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='Todoska',
+  version='0.2.10',
+  description='A Basic Todo Tracker in Cli',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='https://github.com/Abbhiishek/Todoska',  
+  author='Abhishek Kushwaha',
+  author_email='abhishekkushwaha1479@gmail.com',
+  license='MIT', 
+  entry_points={
+    'console_scripts': [
+      'Todoska = Todoska.__main__:app',
+
+    ]
+  },
+  classifiers=classifiers,
+  keywords=['Python' , 'cli' , 'Todo' ,'Todotracker' , 'todo-tracker' , 'todo-tracker-cli', 'Todoska'], 
+  packages=find_packages(),
+  python_requires='>=3.6',
+  install_requires=['typer' , 'rich' ] 
+)
