2219a100356ac06ad3a4e93fffecfbb5aebc33b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:03 +0000

    Add package secondstotext/1.0.1b0/secondstotext-1.0.1b0.tar.gz

---
+++ packages/secondstotext/secondstotext-1.0.1b0.tar.gz/secondstotext-1.0.1b0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+"""
+Setup
+"""
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="secondstotext",
+    version="1.0.1-beta",
+    author="Sumiza",
+    author_email="sumiza@gmail.com",
+    description="Converts seconds to human readable text or tuple",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sumiza/secondstotext/",
+    project_urls={
+        "Bug Tracker": "https://github.com/Sumiza/secondstotext/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
