ce43a9ca40cd061bf54b8469997976d70eba1856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:26 +0000

    Add package visual_auditor/0.1.1/visual_auditor-0.1.1-py2.py3-none-any.whl

---
+++ packages/visual_auditor/visual_auditor-0.1.1-py2.py3-none-any.whl/visual_auditor/__init__.py
@@ -0,0 +1,10 @@
+
+"""A Python package for visually auditing machine learning models for fairness through the use of interpretable visualizations summarizing intersectional bias."""
+
+__author__ = """David Munechika"""
+__email__ = 'david.munechika@gatech.edu'
+__version__ = '0.1.1'
+
+import visual_auditor.visual_auditor
+import visual_auditor.risk_control
+import visual_auditor.slice_finder
