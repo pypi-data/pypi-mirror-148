1a7b90211af7d69ac38e5f6fdbe982c312c0e997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:43 +0000

    Add package damo/1.2.9/damo-1.2.9.tar.gz

---
+++ packages/damo/damo-1.2.9.tar.gz/damo-1.2.9/src/damo/damo_stop.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0
+
+"""
+Stop DAMON.
+"""
+
+import argparse
+
+import _damon
+
+def set_argparser(parser):
+    return
+
+def main(args=None):
+    if not args:
+        parser = argparse.ArgumentParser()
+        set_argparser(parser)
+        args = parser.parse_args()
+
+    _damon.ensure_root_permission()
+
+    if not _damon.is_damon_running():
+        print('DAMON is not turned on')
+        exit(1)
+
+    _damon.turn_damon('off')
+
+if __name__ == '__main__':
+    main()
