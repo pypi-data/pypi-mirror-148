51dbce6afea9f75fca9e65224d869675341c1fb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:53 +0000

    Add package paddle-nnUNet/0.0.2/paddle_nnUNet-0.0.2-py3-none-any.whl

---
+++ packages/paddle-nnUNet/paddle_nnUNet-0.0.2-py3-none-any.whl/paddlennunet/__init__.py
@@ -0,0 +1,7 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+# author: jeremy.zhang(szujeremy@gmail.com, Shenzhen University, China)
+
+print('Hello from the paddle-nnUNet. https://paddle-nnunet.readthedocs.io')
+
+
