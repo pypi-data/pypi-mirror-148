bd22f3f2d0936ae0bf5766947395646af51c7c4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:55 +0000

    Add package dbbkp/0.1.3/dbbkp-0.1.3.tar.gz

---
+++ packages/dbbkp/dbbkp-0.1.3.tar.gz/dbbkp-0.1.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dbbkp',
+      version='0.1.3',
+      description='dbbkp Package',
+      packages = ['dbbkp'],
+      install_requires = ["numpy","pandas","matplotlib","utilum","sql_formatter"],
+      zip_safe = False,
+      )
