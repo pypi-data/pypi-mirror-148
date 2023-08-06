d492db03d967e610c20768706ea204dfc6dc3fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:06 +0000

    Add package systembridgebackend/3.0.0.dev14/systembridgebackend-3.0.0.dev14.tar.gz

---
+++ packages/systembridgebackend/systembridgebackend-3.0.0.dev14.tar.gz/systembridgebackend-3.0.0.dev14/systembridgebackend/modules/cpu/__init__.py
@@ -0,0 +1,95 @@
+"""System Bridge: CPU"""
+from __future__ import annotations
+from psutil import (
+    cpu_count,
+    cpu_freq,
+    cpu_percent,
+    cpu_stats,
+    cpu_times,
+    cpu_times_percent,
+    getloadavg,
+)
+from psutil._common import pcputimes, scpufreq, scpustats
+
+from systembridgeshared.database import Database
+
+from systembridgeshared.base import Base
+
+
+class CPU(Base):
+    """CPU"""
+
+    def count(self) -> int:
+        """CPU count"""
+        return cpu_count()
+
+    def freq(self) -> scpufreq:
+        """CPU frequency"""
+        return cpu_freq()
+
+    def freq_per_cpu(self) -> list[scpufreq]:  # pylint: disable=unsubscriptable-object
+        """CPU frequency per CPU"""
+        return cpu_freq(percpu=True)
+
+    def load_average(
+        self,
+    ) -> tuple[float, float, float]:  # pylint: disable=unsubscriptable-object
+        """Get load average"""
+        return getloadavg()
+
+    def stats(self) -> scpustats:
+        """CPU stats"""
+        return cpu_stats()
+
+    def temperature(
+        self,
+        database: Database,
+    ) -> float | None:
+        """CPU temperature"""
+        for key, value in database.table_data_to_ordered_dict("sensors").items():
+            if (
+                "cpu" in key
+                and "temperature" in key
+                and value is not None
+                and value != 0
+            ):
+                return value
+        return None
+
+    def times(self) -> pcputimes:
+        """CPU times"""
+        return cpu_times(percpu=False)
+
+    def times_percent(self) -> pcputimes:
+        """CPU times percent"""
+        return cpu_times_percent(interval=1, percpu=False)
+
+    def times_per_cpu(
+        self,
+    ) -> list[pcputimes]:  # pylint: disable=unsubscriptable-object
+        """CPU times per CPU"""
+        return cpu_times(percpu=True)
+
+    def times_per_cpu_percent(
+        self,
+    ) -> list[pcputimes]:  # pylint: disable=unsubscriptable-object
+        """CPU times per CPU percent"""
+        return cpu_times_percent(interval=1, percpu=True)
+
+    def usage(self) -> float:
+        """CPU usage"""
+        return cpu_percent(interval=1, percpu=False)
+
+    def usage_per_cpu(self) -> list[float]:  # pylint: disable=unsubscriptable-object
+        """CPU usage per CPU"""
+        return cpu_percent(interval=1, percpu=True)
+
+    def voltage(
+        self,
+        database: Database,
+    ) -> float | None:
+        """CPU voltage"""
+        for key, value in database.table_data_to_ordered_dict("sensors").items():
+            if "cpu" in key and "voltage" in key:
+                return value
+        return None
---
+++ packages/systembridgebackend/systembridgebackend-3.0.0.dev14.tar.gz/systembridgebackend-3.0.0.dev14/systembridgebackend/modules/sensors/update.py
@@ -0,0 +1,83 @@
+"""System Bridge: Update Sensors"""
+from systembridgeshared.database import Database
+
+from systembridgebackend.modules.base import ModuleUpdateBase
+from systembridgebackend.modules.sensors import Sensors
+
+
+class SensorsUpdate(ModuleUpdateBase):
+    """Sensors Update"""
+
+    def __init__(
+        self,
+        database: Database,
+    ) -> None:
+        """Initialize"""
+        super().__init__(database, "sensors")
+        self._sensors = Sensors()
+
+    async def update_fans(self) -> None:
+        """Update Fan Sensors"""
+        if data := self._sensors.fans():
+            for key, value in data.items():
+                self._database.write("sensors", f"fans_{key}", value)
+
+    async def update_temperatures(self) -> None:
+        """Update Temperature Sensors"""
+        if data := self._sensors.temperatures():
+            for key, value in data.items():
+                self._database.write("sensors", f"temperatures_{key}", value)
+
+    async def update_windows_sensors(self) -> None:
+        """Update Windows Sensors"""
+        if not (data := self._sensors.windows_sensors()):
+            return
+        if "hardware" in data and data["hardware"] is not None:
+            for item in data["hardware"]:
+                for key in item["sensors"] or []:
+                    sensor_name = (
+                        key["name"]
+                        .replace(" ", "_", -1)
+                        .replace("(", "", -1)
+                        .replace(")", "", -1)
+                        .lower()
+                    )
+                    sensor_type = (
+                        key["type"]
+                        .replace(" ", "_", -1)
+                        .replace("(", "", -1)
+                        .replace(")", "", -1)
+                        .lower()
+                    )
+
+                    self._database.write(
+                        "sensors",
+                        f"windows_hardware_{sensor_name}_{sensor_type}",
+                        key["value"],
+                    )
+
+        if "nvidia" in data and data["nvidia"] is not None:
+            for key, value in data["nvidia"].items():
+                if isinstance(value, list):
+                    counter = 0
+                    for item in value:
+                        for subkey, subvalue in item.items():
+                            self._database.write(
+                                "sensors",
+                                f"windows_nvidia_{key}_{counter}_{subkey}",
+                                subvalue,
+                            )
+                        counter += 1
+                else:
+                    for subkey, subvalue in value.items():
+                        self._database.write(
+                            "sensors",
+                            f"windows_nvidia_{key}_{subkey}",
+                            subvalue,
+                        )
+
+    async def update_all_data(self) -> None:
+        """Update data"""
+        await self.update_fans()
+        await self.update_temperatures()
+        await self.update_windows_sensors()
