0e7797684df9ef81dee9f6222b2d690a63d92c11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:18 +0000

    Add package async-polygon/0.0.0a3/async_polygon-0.0.0a3-py3-none-any.whl

---
+++ packages/async-polygon/async_polygon-0.0.0a3-py3-none-any.whl/async_polygon/rest/dataclasses/__init__.py
@@ -0,0 +1,18 @@
+from .templates import AggregateBars
+from .templates import LastTrade
+from .templates import LastTradeCryptoPair
+from .templates import PreviousClose
+
+import typing
+
+from .templates import Template
+
+
+AnyDefinition = typing.TypeVar("AnyDefinition", bound=Template)
+
+
+name_to_method: typing.Dict[str, typing.Callable[[], typing.Type[AnyDefinition]]] = {
+    'LastTrade':LastTrade,
+    'AggregateBars':AggregateBars,
+    'PreviousClose':PreviousClose,
+}
