55b5be9a76a42f3761584101a5a3afb8fb4a9871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:22:04 +0000

    Add package py-youtube/1.1.7/py_youtube-1.1.7.tar.gz

---
+++ packages/py-youtube/py_youtube-1.1.7.tar.gz/py_youtube-1.1.7/setup.py
@@ -0,0 +1,27 @@
+import pathlib
+import setuptools
+
+file = pathlib.Path(__file__).parent
+
+README = (file / "README.md").read_text()
+
+setuptools.setup(
+    name="py_youtube",
+    version="1.1.7",
+    author="mrlokaman",
+    author_email="ln0technical@gmail.com",
+    long_description = README,
+    long_description_content_type = "text/markdown",
+    description="Python library Get YouTube Video Data & Search Video",
+    license="MIT",
+    url="https://github.com/lntechnical2/PyYouTube",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    install_requires = [""],
+    python_requires=">=3.6",
+    
+)
