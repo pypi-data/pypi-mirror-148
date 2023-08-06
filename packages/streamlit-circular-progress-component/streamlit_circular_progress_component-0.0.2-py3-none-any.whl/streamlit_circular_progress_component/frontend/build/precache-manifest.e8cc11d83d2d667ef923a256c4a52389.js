f0b115e47a15f39afc4fa79d366690a590616ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:09 +0000

    Add package streamlit-circular-progress-component/0.0.2/streamlit_circular_progress_component-0.0.2-py3-none-any.whl

---
+++ packages/streamlit-circular-progress-component/streamlit_circular_progress_component-0.0.2-py3-none-any.whl/streamlit_circular_progress_component/frontend/build/precache-manifest.e8cc11d83d2d667ef923a256c4a52389.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71e636a6759a71ee62bad62558e9ee2e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "51091d5f11fd8d5a8215",
+    "url": "./static/js/2.000e5c0f.chunk.js"
+  },
+  {
+    "revision": "1a313375cf8bad9dfd82b4b26c854a95",
+    "url": "./static/js/2.000e5c0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76f80d19127cf9d1b73f",
+    "url": "./static/js/main.32e2e26f.chunk.js"
+  },
+  {
+    "revision": "829d9cb18352825d0104",
+    "url": "./static/js/runtime-main.af1ee0c6.js"
+  }
+]);
