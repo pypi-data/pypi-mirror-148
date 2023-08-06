38ebad22151ee55b7e1c9a436fc7c0922337f4b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:20 +0000

    Add package Products.MeetingPROVHainaut/4.2b6/Products.MeetingPROVHainaut-4.2b6.tar.gz

---
+++ packages/Products.MeetingPROVHainaut/Products.MeetingPROVHainaut-4.2b6.tar.gz/Products.MeetingPROVHainaut-4.2b6/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = '4.2b6'
+
+setup(name='Products.MeetingPROVHainaut',
+      version=version,
+      description="PloneMeeting profile for Province de Hainaut",
+      long_description=open("README.rst").read() + "\n\n" + open("CHANGES.rst").read(),
+      classifiers=["Programming Language :: Python"],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://www.imio.be/produits/gestion-des-deliberations',
+      license='GPL',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      extras_require=dict(
+          test=['Products.PloneMeeting[test]']),
+      install_requires=[
+          'Products.MeetingCommunes'],
+      entry_points={},
+      )
