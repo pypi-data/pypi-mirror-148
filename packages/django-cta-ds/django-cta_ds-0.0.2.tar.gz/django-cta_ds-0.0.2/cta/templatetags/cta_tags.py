cad46df81b940fdec5b00e32d8f335177dd3c1e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:23:01 +0000

    Add package django-cta-ds/0.0.2/django-cta_ds-0.0.2.tar.gz

---
+++ packages/django-cta-ds/django-cta_ds-0.0.2.tar.gz/django-cta_ds-0.0.2/cta/templatetags/cta_tags.py
@@ -0,0 +1,25 @@
+from django.template import Library, loader
+
+import logging
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter('%(levelname)s: [%(name)s] %(message)s')
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.setLevel(logging.ERROR)
+
+
+register = Library()
+
+# https://localcoder.org/django-inclusion-tag-with-configurable-template
+
+
+@register.simple_tag(takes_context=True)
+def make_cta(context):
+    logger.info(context)
+    t = loader.get_template(f"cta/{context['theme']}.html")
+    c = {
+        'theme': context['theme'],
+        'cta': context['cta']
+    }
+    return t.render(c)
---
+++ packages/django-cta-ds/django-cta_ds-0.0.2.tar.gz/django-cta_ds-0.0.2/mytester/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def home(request):
+    context = {
+        "theme": "bethany_ds",
+        "cta": {
+            "title" : "카카오톡 상담",
+            "desc" : "카카오톡을 통해 상담 및 예약 가능합니다.",
+            "link" : "http://pf.kakao.com/_xexhxgxlV"
+        }
+    }
+    return render(request, f"home.html", context)
