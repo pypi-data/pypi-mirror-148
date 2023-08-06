43d079a723447d30aa8f3014bef2c9be5f6c9580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:24:19 +0000

    Add package django-hero-ds/0.0.4/django_hero_ds-0.0.4-py3-none-any.whl

---
+++ packages/django-hero-ds/django_hero_ds-0.0.4-py3-none-any.whl/hero/templatetags/hero_tags.py
@@ -0,0 +1,25 @@
+from django.template import Library, loader
+
+import logging
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter('%(levelname)s: [%(name)s] %(message)s')
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.setLevel(logging.ERROR)
+
+
+register = Library()
+
+# https://localcoder.org/django-inclusion-tag-with-configurable-template
+
+
+@register.simple_tag(takes_context=True)
+def make_hero(context):
+    logger.info(context)
+    t = loader.get_template(f"hero/{context['theme']}.html")
+    context.update({
+        'theme': context['theme'],
+        'hero': context['hero']
+    })
+    return t.render(context.flatten())
