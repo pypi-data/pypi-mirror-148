6b9ffadb52c9ead59e542d2b43e8b8398d6ea52f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:32 +0000

    Add package zscaler-api-talkers/2.2/zscaler-api-talkers-2.2.tar.gz

---
+++ packages/zscaler-api-talkers/zscaler-api-talkers-2.2.tar.gz/zscaler-api-talkers-2.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='zscaler-api-talkers',
+    version='2.2',
+    author='Sergio Pereira',
+    author_email='sergitopereira@hotmail.com',
+    packages=['/Users/spereira/github/zscaler_api_talkers'],
+    url='https://github.com/sergitopereira/zscaler_api_talkers.git',
+    license='LICENSE.txt',
+    description='Unofficial Zscaler API python sdk for ZIA, ZPA and ZCC',
+    long_description_content_type="text/markdown",
+    long_description=open('README.md').read(),
+    install_requires=[
+        "requests",
+        "appdirs",
+        "ipython"
+    ],
+)
