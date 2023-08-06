f0706f6b054d2a082dfdca68b32c74241833fdc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:44 +0000

    Add package lhub-cli/0.0.7.0/lhub_cli-0.0.7.0.tar.gz

---
+++ packages/lhub-cli/lhub_cli-0.0.7.0.tar.gz/lhub_cli-0.0.7.0/lhub_cli/common/args.py
@@ -0,0 +1,37 @@
+import argparse
+from lhub_cli.common.output import SUPPORTED_OUTPUT_TYPES, SUPPORTED_TABLE_FORMATS
+
+
+def add_script_output_args(parser: argparse.ArgumentParser, include_debug=True, default_output=None):
+    if not default_output:
+        default_output = "table"
+    elif default_output not in SUPPORTED_OUTPUT_TYPES:
+        raise ValueError(f"{default_output} is not a supported output type")
+    output = parser.add_argument_group('output')
+
+    output.add_argument(
+        "-f", "--file",
+        type=str,
+        default=None,
+        help="Also write output to a file")
+
+    output.add_argument(
+        "-o", "--output",
+        type=str,
+        metavar="<OPTION>",
+        default=default_output,
+        choices=SUPPORTED_OUTPUT_TYPES,
+        help=f"Output style (default: {default_output}). Available output types are: {', '.join(SUPPORTED_OUTPUT_TYPES)}")
+
+    # https://github.com/astanin/python-tabulate#table-format
+    output.add_argument(
+        "-t", "--table_format",
+        type=str,
+        metavar="<OPTION>",
+        default=None,
+        choices=SUPPORTED_TABLE_FORMATS,
+        help=f"Table format (ignored if output type is not table). Available formats are: {', '.join(SUPPORTED_TABLE_FORMATS)}"
+    )
+
+    if include_debug:
+        output.add_argument("--debug", action="store_true", help="Enable debug logging")
