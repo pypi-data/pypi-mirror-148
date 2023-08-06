358f7c6cf7a4900dd7da754b391cbdfbf0369714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:31 +0000

    Add package lgg/0.1.1/lgg-0.1.1.tar.gz

---
+++ packages/lgg/lgg-0.1.1.tar.gz/lgg-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['src']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'lgg',
+    'version': '0.1.1',
+    'description': 'A simple yet fancy logger for Python scripts',
+    'long_description': '# python-logger\nA simple yet fancy logger for Python scripts\n',
+    'author': 'Ayoub Assis',
+    'author_email': 'assis.ayoub@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/blurry-mood/python-logger',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
