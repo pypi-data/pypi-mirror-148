b099a4305d341eb45bb88cae7c277c459d32c1e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:32 +0000

    Add package streamlit-alert-component/0.0.1/streamlit_alert_component-0.0.1-py3-none-any.whl

---
+++ packages/streamlit-alert-component/streamlit_alert_component-0.0.1-py3-none-any.whl/streamlit_alert_component/frontend/build/precache-manifest.800ab006b47c07de63af47dd78872338.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0b41ed7b5f5e912556bcfc1eb8690c2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2fc1a14e13f383be3a96",
+    "url": "./static/js/2.12a3a5b8.chunk.js"
+  },
+  {
+    "revision": "0d70bb2dcb2571b1c9d4c7eb24d15ef9",
+    "url": "./static/js/2.12a3a5b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d1629480f99e126a39f",
+    "url": "./static/js/main.cb751afd.chunk.js"
+  },
+  {
+    "revision": "43834f1bc9d1323c3c01",
+    "url": "./static/js/runtime-main.d31d2a75.js"
+  }
+]);
