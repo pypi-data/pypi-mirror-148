8f8ccf97edd15d35feaa64d5fbdb06a7536671f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:27 +0000

    Add package GoldyBot/4.0.dev7/GoldyBot-4.0.dev7.tar.gz

---
+++ packages/GoldyBot/GoldyBot-4.0.dev7.tar.gz/GoldyBot-4.0.dev7/GoldyBot/__init__.py
@@ -0,0 +1,25 @@
+"""
+💙 Goldy Bot V4
+
+Copyright (C) 2022 - Dev Goldy
+"""
+
+import nextcord
+
+from .database import database
+from . import files, paths, logging, goldy, cache, token, settings, config, info, modules, system, errors, assets
+from . import internal_modules, ext, utility, objects
+
+# Functions
+log = logging.print_and_log
+
+# Function Decorators
+command = ext.commands.command
+cmd = command
+
+# Class Inheritors
+Extenstion = ext.extenstions.Extenstion
+
+# Classes
+Goldy = goldy.Goldy
+Database = database.Database
