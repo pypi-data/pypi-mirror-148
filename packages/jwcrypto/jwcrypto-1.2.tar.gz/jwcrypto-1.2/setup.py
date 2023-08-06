be814fdb4f6c828d2836d3af0c9cab6f8538253e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:32 +0000

    Add package jwcrypto/1.2/jwcrypto-1.2.tar.gz

---
+++ packages/jwcrypto/jwcrypto-1.2.tar.gz/jwcrypto-1.2/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2015  JWCrypto Project Contributors, see  LICENSE file
+
+from setuptools import setup
+
+setup(
+    name = 'jwcrypto',
+    version = '1.2',
+    license = 'LGPLv3+',
+    maintainer = 'JWCrypto Project Contributors',
+    maintainer_email = 'simo@redhat.com',
+    url='https://github.com/latchset/jwcrypto',
+    packages = ['jwcrypto'],
+    description = 'Implementation of JOSE Web standards',
+    classifiers = [
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Intended Audience :: Developers',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    data_files = [('share/doc/jwcrypto', ['LICENSE', 'README.md'])],
+    install_requires = [
+        'cryptography >= 2.3',
+        'deprecated',
+    ],
+    python_requires = '>= 3.6',
+)
