c9c48d6b94db1583e16a6d7b885708d57508e2a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:22:49 +0000

    Add package FHIRkit/0.0.10/FHIRkit-0.0.10.tar.gz

---
+++ packages/FHIRkit/FHIRkit-0.0.10.tar.gz/FHIRkit-0.0.10/fhirkit/Bundle.py
@@ -0,0 +1,64 @@
+from importlib.resources import Resource
+
+from typing import List, Optional
+
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal
+
+
+from pydantic import Field, HttpUrl
+from elements import Identifier
+from data_types import Instant
+from fhirkit.elements import BackboneElement
+
+BundleType = Literal[
+    "document",
+    "message",
+    "transaction",
+    "transaction-response",
+    "batch",
+    "batch-response",
+    "history",
+    "searchset",
+    "collection",
+]
+
+
+class BundleLink(BackboneElement):
+    relation: str
+    url: HttpUrl
+
+
+class BundleSearch(BackboneElement):
+    # TODO
+    pass
+
+
+class BundleRequest(BackboneElement):
+    method: Literal["GET", "HEAD", "POST", "PUT", "DELETE", "PATCH"]
+    url: HttpUrl
+
+
+class BundleResponse(BackboneElement):
+    status: str
+    location: Optional[HttpUrl]
+    etag: Optional[str]
+    lastModified: Optional[Instant]
+    outcome: Optional[Resource]
+
+
+class BundleEntry(BackboneElement):
+    fullUrl: Optional[HttpUrl]
+    resource: Optional[Resource]
+    request: Optional[BundleRequest]
+    response: Optional[BundleResponse]
+
+
+class Bundle(Resource):
+    resourceType = Field("Bundle", const=True)
+    identifier: Optional[Identifier]
+    type: BundleType
+    timestamp: Optional[Instant]
+    entry: List[BundleEntry]
