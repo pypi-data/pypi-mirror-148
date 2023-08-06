e02f907ce5c973f02613b7f0049ddf798c6c2b15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:56 +0000

    Add package pretalx-hitalx/0.0.1/pretalx-hitalx-0.0.1.tar.gz

---
+++ packages/pretalx-hitalx/pretalx-hitalx-0.0.1.tar.gz/pretalx-hitalx-0.0.1/pretalx_hitalx/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+
+from .views import MarkExpenseView, SpeakerExpenseDetail, SpeakerExpenseList
+
+urlpatterns = [
+    path(
+        "orga/event/<slug:event>/speakers/<int:speaker_id>/expenses/",
+        view=SpeakerExpenseList.as_view(),
+        name="expenses.view",
+    ),
+    path(
+        "orga/event/<slug:event>/speakers/<int:speaker_id>/expenses/new",
+        view=SpeakerExpenseDetail.as_view(),
+        name="expenses.create",
+    ),
+    path(
+        "orga/event/<slug:event>/speakers/<int:speaker_id>/expense/<int:pk>",
+        view=SpeakerExpenseDetail.as_view(),
+        name="expense.view",
+    ),
+    path(
+        "orga/event/<slug:event>/speakers/<int:speaker_id>/expense/<int:pk>/mark",
+        view=MarkExpenseView.as_view(),
+        name="expense.mark",
+    ),
+]
