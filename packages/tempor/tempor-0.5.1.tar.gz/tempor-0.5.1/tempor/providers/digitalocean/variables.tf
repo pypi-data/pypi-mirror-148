5dfba9ca5fcca4fc8b8e4d4bd7f30fd0076914ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:34 +0000

    Add package tempor/0.5.1/tempor-0.5.1.tar.gz

---
+++ packages/tempor/tempor-0.5.1.tar.gz/tempor-0.5.1/tempor/providers/digitalocean/variables.tf
@@ -0,0 +1,13 @@
+variable "api_token" {}
+variable "image" {
+    default = "ubuntu-20-04-x64"
+}
+variable "region" {
+    default = "nyc1"
+}
+variable "num" {
+	default = 1
+}
+variable "username" {
+	default = "user"
+}
