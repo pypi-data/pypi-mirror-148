c4eed57af4ebc34d99d8aab1567570b7ac558c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:23 +0000

    Add package octue/0.20.0/octue-0.20.0-py3-none-any.whl

---
+++ packages/octue/octue-0.20.0-py3-none-any.whl/octue/mixins/__init__.py
@@ -0,0 +1,22 @@
+from .base import MixinBase
+from .cool_nameable import CoolNameable
+from .filterable import Filterable
+from .hashable import Hashable
+from .identifiable import Identifiable
+from .labelable import Labelable
+from .pathable import Pathable
+from .serialisable import Serialisable
+from .taggable import Taggable
+
+
+__all__ = (
+    "CoolNameable",
+    "Filterable",
+    "Hashable",
+    "Identifiable",
+    "Labelable",
+    "MixinBase",
+    "Pathable",
+    "Serialisable",
+    "Taggable",
+)
