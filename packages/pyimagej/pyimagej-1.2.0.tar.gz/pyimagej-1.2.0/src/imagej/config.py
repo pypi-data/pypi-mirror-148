5bcf67869053647d354ceea9114290cc516c7b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:46 +0000

    Add package pyimagej/1.2.0/pyimagej-1.2.0.tar.gz

---
+++ packages/pyimagej/pyimagej-1.2.0.tar.gz/pyimagej-1.2.0/src/imagej/config.py
@@ -0,0 +1,7 @@
+__author__ = "Curtis Rueden, Ed Evans, Mark Hiner, Michael Pinkert, Gabe Selzer, Yang Liu, and others"
+
+__major__ = 1
+__minor__ = 2
+__patch__ = 0
+__tag__ = ""
+__version__ = f"{__major__}.{__minor__}.{__patch__}.{__tag__}".strip(".")
---
+++ packages/pyimagej/pyimagej-1.2.0.tar.gz/pyimagej-1.2.0/test.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+dir=$(dirname "$0")
+cd "$dir"
+
+echo "-------------------------------------"
+echo "| Testing ImageJ2 + original ImageJ |"
+echo "-------------------------------------"
+python -m pytest -p no:faulthandler test
+
+echo
+echo "-------------------------------------"
+echo "|    Testing ImageJ2 standalone     |"
+echo "-------------------------------------"
+python -m pytest -p no:faulthandler --legacy=false test
+
+echo
+echo "-------------------------------------"
+echo "|  Testing Fiji Is Just ImageJ(2)   |"
+echo "-------------------------------------"
+python -m pytest -p no:faulthandler --ij=sc.fiji:fiji test
