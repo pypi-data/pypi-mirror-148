50248d646e805ecc25b911daa7e45e4fe492a1c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:03 +0000

    Add package pyrocko/2022.4.28/pyrocko-2022.4.28-cp38-cp38-win_amd64.whl

---
+++ packages/pyrocko/pyrocko-2022.4.28-cp38-cp38-win_amd64.whl/pyrocko/info.py
@@ -0,0 +1,6 @@
+# This module is automatically created from setup.py
+git_sha1 = 'b9b27cf767fa47780f182dc039b23fd4c414a541'
+local_modifications = False
+version = '2022.04.28'
+long_version = 'pyrocko-2022.04.28-b9b27cf767fa47780f182dc039b23fd4c414a541-2022-04-28_05:04:35'  # noqa
+installed_date = '2022-04-28_05:04:35'
