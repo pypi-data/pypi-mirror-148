812bee54a1a36c8b97a014ea293eed31f141b90d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:40 +0000

    Add package mocha-time/0.0.8/mocha-time-0.0.8.tar.gz

---
+++ packages/mocha-time/mocha-time-0.0.8.tar.gz/mocha-time-0.0.8/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="mocha-time",
+    version="0.0.8",
+    author="Jared Zhao",
+    description="Python Performance Profiling for Production",
+    long_description=open("../README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mocha-dev/mocha",
+    license="Apache 2.0",
+    python_requires=">=3.6",
+    install_requires=[
+        "requests >= 2.0.0",
+    ],
+    packages=["mocha"],
+)
