8b0965e63ad81e4efcc80fe3a03b29d8ed613844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:05 +0000

    Add package jinjafx/1.11.4/jinjafx-1.11.4.tar.gz

---
+++ packages/jinjafx/jinjafx-1.11.4.tar.gz/jinjafx-1.11.4/setup.py
@@ -0,0 +1,38 @@
+import pathlib, re
+from setuptools import setup
+
+for line in open('jinjafx.py'):
+  if line.startswith('__version__'):
+    exec(line)
+    break
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+README = re.sub(r'^.*\[<img', '[<img', README, flags=re.DOTALL)
+README = re.sub(r'<p.+?</p>', '', README, flags=re.DOTALL)
+
+setup(
+  name="jinjafx",
+  version=__version__,
+  description="JinjaFx - Jinja2 Templating Tool",
+  long_description=README,
+  long_description_content_type="text/markdown",
+  url="https://github.com/cmason3/jinjafx",
+  author="Chris Mason",
+  author_email="chris@netnix.org",
+  license="MIT",
+  classifiers=[
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3"
+  ],
+  packages=["jinjafx"],
+  include_package_data=True,
+  package_data={'': ['extensions/*.py']},
+  install_requires=["jinja2", "pytz", "pyyaml", "cryptography>=2.7", "netaddr"],
+  entry_points={
+    "console_scripts": [
+      "jinjafx=jinjafx:main",
+    ]
+  }
+)
