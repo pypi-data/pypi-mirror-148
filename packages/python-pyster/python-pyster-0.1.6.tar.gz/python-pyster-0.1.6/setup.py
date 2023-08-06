f4352d4d67fdb3651302050f9a51ba07247b26d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:25 +0000

    Add package python-pyster/0.1.6/python-pyster-0.1.6.tar.gz

---
+++ packages/python-pyster/python-pyster-0.1.6.tar.gz/python-pyster-0.1.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+setup(
+    name = 'python-pyster',
+    version = '0.1.6',
+    license = 'MIT',
+    description = 'Python unit testing made easy with pyster!',
+    author = 'Wrench56',
+    author_email = 'dmarkreg@gmail.com',
+    url = 'https://github.com/Wrench56/pyster',
+    install_requires = ['rich'],
+    long_description = 'Please find more information on my Github page!',
+    entry_points={
+         "console_scripts": [
+            "pysterminal=python_pyster.pysterminal:main"
+        ]
+    },
+    packages=['errors']
+)
