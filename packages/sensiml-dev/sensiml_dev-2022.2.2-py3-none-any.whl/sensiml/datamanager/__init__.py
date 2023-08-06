5c23801ca6bdc6aa6bc0b5d363bc89467fdb2f6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:15 +0000

    Add package sensiml-dev/2022.2.2/sensiml_dev-2022.2.2-py3-none-any.whl

---
+++ packages/sensiml-dev/sensiml_dev-2022.2.2-py3-none-any.whl/sensiml/datamanager/__init__.py
@@ -0,0 +1,22 @@
+from sensiml.datamanager.functions import Functions
+from sensiml.datamanager.platformdescriptions import PlatformDescriptions
+from sensiml.datamanager.clientplatformdescriptions import ClientPlatformDescriptions
+from sensiml.datamanager.pipelineseeds import PipelineSeeds
+from sensiml.datamanager.featurefiles import FeatureFiles
+from sensiml.datamanager.projects import Projects
+from sensiml.datamanager.segmenter import SegmenterSet
+from sensiml.datamanager.capture_configurations import CaptureConfigurations
+from sensiml.datamanager.team import Team
+
+
+__all__ = [
+    "Functions",
+    "FeatureFiles",
+    "Projects",
+    "SegmenterSet",
+    "PipelineSeeds",
+    "PlatformDescriptions",
+    "ClientPlatformDescriptions",
+    "CaptureConfigurations",
+    "Team",
+]
