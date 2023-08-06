6e70f935a99d9c62506936d6c1317680489d825c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:24:19 +0000

    Add package ghost-pkg-io/0.0.2/ghost_pkg_io-0.0.2-py3-none-any.whl

---
+++ packages/ghost-pkg-io/ghost_pkg_io-0.0.2-py3-none-any.whl/ghost_pkg_io/say_hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2022/5/1 19:31
+# @Author : lixinpan
+# @Site : 
+# @File : say_hello.py
+# @function:
+
+def hello():
+    print("hello world! ---- ")
