53ec3ba0c364dcc50cc7ca46cdbc3d8e88c091bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:21:57 +0000

    Add package google-api-python-client-stubs/1.10.0/google-api-python-client-stubs-1.10.0.tar.gz

---
+++ packages/google-api-python-client-stubs/google-api-python-client-stubs-1.10.0.tar.gz/google-api-python-client-stubs-1.10.0/googleapiclient-stubs/_apis/networkservices/v1/schemas.pyi
@@ -0,0 +1,155 @@
+import typing
+
+import typing_extensions
+
+_list = list
+
+@typing.type_check_only
+class AuditConfig(typing_extensions.TypedDict, total=False):
+    auditLogConfigs: _list[AuditLogConfig]
+    service: str
+
+@typing.type_check_only
+class AuditLogConfig(typing_extensions.TypedDict, total=False):
+    exemptedMembers: _list[str]
+    logType: typing_extensions.Literal[
+        "LOG_TYPE_UNSPECIFIED", "ADMIN_READ", "DATA_WRITE", "DATA_READ"
+    ]
+
+@typing.type_check_only
+class Binding(typing_extensions.TypedDict, total=False):
+    condition: Expr
+    members: _list[str]
+    role: str
+
+@typing.type_check_only
+class CancelOperationRequest(typing_extensions.TypedDict, total=False): ...
+
+@typing.type_check_only
+class Empty(typing_extensions.TypedDict, total=False): ...
+
+@typing.type_check_only
+class EndpointMatcher(typing_extensions.TypedDict, total=False):
+    metadataLabelMatcher: EndpointMatcherMetadataLabelMatcher
+
+@typing.type_check_only
+class EndpointMatcherMetadataLabelMatcher(typing_extensions.TypedDict, total=False):
+    metadataLabelMatchCriteria: typing_extensions.Literal[
+        "METADATA_LABEL_MATCH_CRITERIA_UNSPECIFIED", "MATCH_ANY", "MATCH_ALL"
+    ]
+    metadataLabels: _list[EndpointMatcherMetadataLabelMatcherMetadataLabels]
+
+@typing.type_check_only
+class EndpointMatcherMetadataLabelMatcherMetadataLabels(
+    typing_extensions.TypedDict, total=False
+):
+    labelName: str
+    labelValue: str
+
+@typing.type_check_only
+class EndpointPolicy(typing_extensions.TypedDict, total=False):
+    authorizationPolicy: str
+    clientTlsPolicy: str
+    createTime: str
+    description: str
+    endpointMatcher: EndpointMatcher
+    labels: dict[str, typing.Any]
+    name: str
+    serverTlsPolicy: str
+    trafficPortSelector: TrafficPortSelector
+    type: typing_extensions.Literal[
+        "ENDPOINT_POLICY_TYPE_UNSPECIFIED", "SIDECAR_PROXY", "GRPC_SERVER"
+    ]
+    updateTime: str
+
+@typing.type_check_only
+class Expr(typing_extensions.TypedDict, total=False):
+    description: str
+    expression: str
+    location: str
+    title: str
+
+@typing.type_check_only
+class ListEndpointPoliciesResponse(typing_extensions.TypedDict, total=False):
+    endpointPolicies: _list[EndpointPolicy]
+    nextPageToken: str
+
+@typing.type_check_only
+class ListLocationsResponse(typing_extensions.TypedDict, total=False):
+    locations: _list[Location]
+    nextPageToken: str
+
+@typing.type_check_only
+class ListOperationsResponse(typing_extensions.TypedDict, total=False):
+    nextPageToken: str
+    operations: _list[Operation]
+
+@typing.type_check_only
+class ListServiceBindingsResponse(typing_extensions.TypedDict, total=False):
+    nextPageToken: str
+    serviceBindings: _list[ServiceBinding]
+
+@typing.type_check_only
+class Location(typing_extensions.TypedDict, total=False):
+    displayName: str
+    labels: dict[str, typing.Any]
+    locationId: str
+    metadata: dict[str, typing.Any]
+    name: str
+
+@typing.type_check_only
+class Operation(typing_extensions.TypedDict, total=False):
+    done: bool
+    error: Status
+    metadata: dict[str, typing.Any]
+    name: str
+    response: dict[str, typing.Any]
+
+@typing.type_check_only
+class OperationMetadata(typing_extensions.TypedDict, total=False):
+    apiVersion: str
+    createTime: str
+    endTime: str
+    requestedCancellation: bool
+    statusMessage: str
+    target: str
+    verb: str
+
+@typing.type_check_only
+class Policy(typing_extensions.TypedDict, total=False):
+    auditConfigs: _list[AuditConfig]
+    bindings: _list[Binding]
+    etag: str
+    version: int
+
+@typing.type_check_only
+class ServiceBinding(typing_extensions.TypedDict, total=False):
+    createTime: str
+    description: str
+    labels: dict[str, typing.Any]
+    name: str
+    service: str
+    updateTime: str
+
+@typing.type_check_only
+class SetIamPolicyRequest(typing_extensions.TypedDict, total=False):
+    policy: Policy
+    updateMask: str
+
+@typing.type_check_only
+class Status(typing_extensions.TypedDict, total=False):
+    code: int
+    details: _list[dict[str, typing.Any]]
+    message: str
+
+@typing.type_check_only
+class TestIamPermissionsRequest(typing_extensions.TypedDict, total=False):
+    permissions: _list[str]
+
+@typing.type_check_only
+class TestIamPermissionsResponse(typing_extensions.TypedDict, total=False):
+    permissions: _list[str]
+
+@typing.type_check_only
+class TrafficPortSelector(typing_extensions.TypedDict, total=False):
+    ports: _list[str]
---
+++ packages/google-api-python-client-stubs/google-api-python-client-stubs-1.10.0.tar.gz/google-api-python-client-stubs-1.10.0/googleapiclient-stubs/_apis/securitycenter/v1beta1/schemas.pyi
@@ -0,0 +1,505 @@
+import typing
+
+import typing_extensions
+
+_list = list
+
+@typing.type_check_only
+class Access(typing_extensions.TypedDict, total=False):
+    callerIp: str
+    callerIpGeo: Geolocation
+    methodName: str
+    principalEmail: str
+    serviceName: str
+    userAgentFamily: str
+
+@typing.type_check_only
+class Asset(typing_extensions.TypedDict, total=False):
+    createTime: str
+    name: str
+    resourceProperties: dict[str, typing.Any]
+    securityCenterProperties: SecurityCenterProperties
+    securityMarks: GoogleCloudSecuritycenterV1beta1SecurityMarks
+    updateTime: str
+
+@typing.type_check_only
+class AssetDiscoveryConfig(typing_extensions.TypedDict, total=False):
+    inclusionMode: typing_extensions.Literal[
+        "INCLUSION_MODE_UNSPECIFIED", "INCLUDE_ONLY", "EXCLUDE"
+    ]
+    projectIds: _list[str]
+
+@typing.type_check_only
+class AuditConfig(typing_extensions.TypedDict, total=False):
+    auditLogConfigs: _list[AuditLogConfig]
+    service: str
+
+@typing.type_check_only
+class AuditLogConfig(typing_extensions.TypedDict, total=False):
+    exemptedMembers: _list[str]
+    logType: typing_extensions.Literal[
+        "LOG_TYPE_UNSPECIFIED", "ADMIN_READ", "DATA_WRITE", "DATA_READ"
+    ]
+
+@typing.type_check_only
+class Binding(typing_extensions.TypedDict, total=False):
+    condition: Expr
+    members: _list[str]
+    role: str
+
+@typing.type_check_only
+class CancelOperationRequest(typing_extensions.TypedDict, total=False): ...
+
+@typing.type_check_only
+class Connection(typing_extensions.TypedDict, total=False):
+    destinationIp: str
+    destinationPort: int
+    protocol: typing_extensions.Literal[
+        "PROTOCOL_UNSPECIFIED", "ICMP", "TCP", "UDP", "GRE", "ESP"
+    ]
+    sourceIp: str
+    sourcePort: int
+
+@typing.type_check_only
+class Cve(typing_extensions.TypedDict, total=False):
+    cvssv3: Cvssv3
+    id: str
+    references: _list[Reference]
+    upstreamFixAvailable: bool
+
+@typing.type_check_only
+class Cvssv3(typing_extensions.TypedDict, total=False):
+    attackComplexity: typing_extensions.Literal[
+        "ATTACK_COMPLEXITY_UNSPECIFIED",
+        "ATTACK_COMPLEXITY_LOW",
+        "ATTACK_COMPLEXITY_HIGH",
+    ]
+    attackVector: typing_extensions.Literal[
+        "ATTACK_VECTOR_UNSPECIFIED",
+        "ATTACK_VECTOR_NETWORK",
+        "ATTACK_VECTOR_ADJACENT",
+        "ATTACK_VECTOR_LOCAL",
+        "ATTACK_VECTOR_PHYSICAL",
+    ]
+    availabilityImpact: typing_extensions.Literal[
+        "IMPACT_UNSPECIFIED", "IMPACT_HIGH", "IMPACT_LOW", "IMPACT_NONE"
+    ]
+    baseScore: float
+    confidentialityImpact: typing_extensions.Literal[
+        "IMPACT_UNSPECIFIED", "IMPACT_HIGH", "IMPACT_LOW", "IMPACT_NONE"
+    ]
+    integrityImpact: typing_extensions.Literal[
+        "IMPACT_UNSPECIFIED", "IMPACT_HIGH", "IMPACT_LOW", "IMPACT_NONE"
+    ]
+    privilegesRequired: typing_extensions.Literal[
+        "PRIVILEGES_REQUIRED_UNSPECIFIED",
+        "PRIVILEGES_REQUIRED_NONE",
+        "PRIVILEGES_REQUIRED_LOW",
+        "PRIVILEGES_REQUIRED_HIGH",
+    ]
+    scope: typing_extensions.Literal[
+        "SCOPE_UNSPECIFIED", "SCOPE_UNCHANGED", "SCOPE_CHANGED"
+    ]
+    userInteraction: typing_extensions.Literal[
+        "USER_INTERACTION_UNSPECIFIED",
+        "USER_INTERACTION_NONE",
+        "USER_INTERACTION_REQUIRED",
+    ]
+
+@typing.type_check_only
+class Empty(typing_extensions.TypedDict, total=False): ...
+
+@typing.type_check_only
+class Expr(typing_extensions.TypedDict, total=False):
+    description: str
+    expression: str
+    location: str
+    title: str
+
+@typing.type_check_only
+class Finding(typing_extensions.TypedDict, total=False):
+    access: Access
+    canonicalName: str
+    category: str
+    connections: _list[Connection]
+    createTime: str
+    description: str
+    eventTime: str
+    externalSystems: dict[str, typing.Any]
+    externalUri: str
+    findingClass: typing_extensions.Literal[
+        "FINDING_CLASS_UNSPECIFIED",
+        "THREAT",
+        "VULNERABILITY",
+        "MISCONFIGURATION",
+        "OBSERVATION",
+        "SCC_ERROR",
+    ]
+    iamBindings: _list[IamBinding]
+    indicator: Indicator
+    mitreAttack: MitreAttack
+    mute: typing_extensions.Literal["MUTE_UNSPECIFIED", "MUTED", "UNMUTED", "UNDEFINED"]
+    muteInitiator: str
+    muteUpdateTime: str
+    name: str
+    nextSteps: str
+    parent: str
+    resourceName: str
+    securityMarks: SecurityMarks
+    severity: typing_extensions.Literal[
+        "SEVERITY_UNSPECIFIED", "CRITICAL", "HIGH", "MEDIUM", "LOW"
+    ]
+    sourceProperties: dict[str, typing.Any]
+    state: typing_extensions.Literal["STATE_UNSPECIFIED", "ACTIVE", "INACTIVE"]
+    vulnerability: Vulnerability
+
+@typing.type_check_only
+class Folder(typing_extensions.TypedDict, total=False):
+    resourceFolder: str
+    resourceFolderDisplayName: str
+
+@typing.type_check_only
+class Geolocation(typing_extensions.TypedDict, total=False):
+    regionCode: str
+
+@typing.type_check_only
+class GetIamPolicyRequest(typing_extensions.TypedDict, total=False):
+    options: GetPolicyOptions
+
+@typing.type_check_only
+class GetPolicyOptions(typing_extensions.TypedDict, total=False):
+    requestedPolicyVersion: int
+
+@typing.type_check_only
+class GoogleCloudSecuritycenterV1BigQueryExport(
+    typing_extensions.TypedDict, total=False
+):
+    createTime: str
+    dataset: str
+    description: str
+    filter: str
+    mostRecentEditor: str
+    name: str
+    principal: str
+    updateTime: str
+
+@typing.type_check_only
+class GoogleCloudSecuritycenterV1BulkMuteFindingsResponse(
+    typing_extensions.TypedDict, total=False
+): ...
+
+@typing.type_check_only
+class GoogleCloudSecuritycenterV1ExternalSystem(
+    typing_extensions.TypedDict, total=False
+):
+    assignees: _list[str]
+    externalSystemUpdateTime: str
+    externalUid: str
+    name: str
+    status: str
+
+@typing.type_check_only
+class GoogleCloudSecuritycenterV1MuteConfig(typing_extensions.TypedDict, total=False):
+    createTime: str
+    description: str
+    displayName: str
+    filter: str
+    mostRecentEditor: str
+    name: str
+    updateTime: str
+
+@typing.type_check_only
+class GoogleCloudSecuritycenterV1NotificationMessage(
+    typing_extensions.TypedDict, total=False
+):
+    finding: Finding
+    notificationConfigName: str
+    resource: GoogleCloudSecuritycenterV1Resource
+
+@typing.type_check_only
+class GoogleCloudSecuritycenterV1Resource(typing_extensions.TypedDict, total=False):
+    displayName: str
+    folders: _list[Folder]
+    name: str
+    parent: str
+    parentDisplayName: str
+    project: str
+    projectDisplayName: str
+    type: str
+
+@typing.type_check_only
+class GoogleCloudSecuritycenterV1RunAssetDiscoveryResponse(
+    typing_extensions.TypedDict, total=False
+):
+    duration: str
+    state: typing_extensions.Literal[
+        "STATE_UNSPECIFIED", "COMPLETED", "SUPERSEDED", "TERMINATED"
+    ]
+
+@typing.type_check_only
+class GoogleCloudSecuritycenterV1beta1Finding(typing_extensions.TypedDict, total=False):
+    category: str
+    createTime: str
+    eventTime: str
+    externalUri: str
+    name: str
+    parent: str
+    resourceName: str
+    securityMarks: GoogleCloudSecuritycenterV1beta1SecurityMarks
+    sourceProperties: dict[str, typing.Any]
+    state: typing_extensions.Literal["STATE_UNSPECIFIED", "ACTIVE", "INACTIVE"]
+
+@typing.type_check_only
+class GoogleCloudSecuritycenterV1beta1RunAssetDiscoveryResponse(
+    typing_extensions.TypedDict, total=False
+):
+    duration: str
+    state: typing_extensions.Literal[
+        "STATE_UNSPECIFIED", "COMPLETED", "SUPERSEDED", "TERMINATED"
+    ]
+
+@typing.type_check_only
+class GoogleCloudSecuritycenterV1beta1SecurityMarks(
+    typing_extensions.TypedDict, total=False
+):
+    marks: dict[str, typing.Any]
+    name: str
+
+@typing.type_check_only
+class GoogleCloudSecuritycenterV1p1beta1Finding(
+    typing_extensions.TypedDict, total=False
+):
+    canonicalName: str
+    category: str
+    createTime: str
+    eventTime: str
+    externalUri: str
+    name: str
+    parent: str
+    resourceName: str
+    securityMarks: GoogleCloudSecuritycenterV1p1beta1SecurityMarks
+    severity: typing_extensions.Literal[
+        "SEVERITY_UNSPECIFIED", "CRITICAL", "HIGH", "MEDIUM", "LOW"
+    ]
+    sourceProperties: dict[str, typing.Any]
+    state: typing_extensions.Literal["STATE_UNSPECIFIED", "ACTIVE", "INACTIVE"]
+
+@typing.type_check_only
+class GoogleCloudSecuritycenterV1p1beta1Folder(
+    typing_extensions.TypedDict, total=False
+):
+    resourceFolder: str
+    resourceFolderDisplayName: str
+
+@typing.type_check_only
+class GoogleCloudSecuritycenterV1p1beta1NotificationMessage(
+    typing_extensions.TypedDict, total=False
+):
+    finding: GoogleCloudSecuritycenterV1p1beta1Finding
+    notificationConfigName: str
+    resource: GoogleCloudSecuritycenterV1p1beta1Resource
+
+@typing.type_check_only
+class GoogleCloudSecuritycenterV1p1beta1Resource(
+    typing_extensions.TypedDict, total=False
+):
+    folders: _list[GoogleCloudSecuritycenterV1p1beta1Folder]
+    name: str
+    parent: str
+    parentDisplayName: str
+    project: str
+    projectDisplayName: str
+
+@typing.type_check_only
+class GoogleCloudSecuritycenterV1p1beta1RunAssetDiscoveryResponse(
+    typing_extensions.TypedDict, total=False
+):
+    duration: str
+    state: typing_extensions.Literal[
+        "STATE_UNSPECIFIED", "COMPLETED", "SUPERSEDED", "TERMINATED"
+    ]
+
+@typing.type_check_only
+class GoogleCloudSecuritycenterV1p1beta1SecurityMarks(
+    typing_extensions.TypedDict, total=False
+):
+    canonicalName: str
+    marks: dict[str, typing.Any]
+    name: str
+
+@typing.type_check_only
+class GroupAssetsRequest(typing_extensions.TypedDict, total=False):
+    compareDuration: str
+    filter: str
+    groupBy: str
+    pageSize: int
+    pageToken: str
+    readTime: str
+
+@typing.type_check_only
+class GroupAssetsResponse(typing_extensions.TypedDict, total=False):
+    groupByResults: _list[GroupResult]
+    nextPageToken: str
+    readTime: str
+
+@typing.type_check_only
+class GroupFindingsRequest(typing_extensions.TypedDict, total=False):
+    filter: str
+    groupBy: str
+    pageSize: int
+    pageToken: str
+    readTime: str
+
+@typing.type_check_only
+class GroupFindingsResponse(typing_extensions.TypedDict, total=False):
+    groupByResults: _list[GroupResult]
+    nextPageToken: str
+    readTime: str
+
+@typing.type_check_only
+class GroupResult(typing_extensions.TypedDict, total=False):
+    count: str
+    properties: dict[str, typing.Any]
+
+@typing.type_check_only
+class IamBinding(typing_extensions.TypedDict, total=False):
+    action: typing_extensions.Literal["ACTION_UNSPECIFIED", "ADD", "REMOVE"]
+    member: str
+    role: str
+
+@typing.type_check_only
+class Indicator(typing_extensions.TypedDict, total=False):
+    domains: _list[str]
+    ipAddresses: _list[str]
+
+@typing.type_check_only
+class ListAssetsResponse(typing_extensions.TypedDict, total=False):
+    listAssetsResults: _list[ListAssetsResult]
+    nextPageToken: str
+    readTime: str
+    totalSize: int
+
+@typing.type_check_only
+class ListAssetsResult(typing_extensions.TypedDict, total=False):
+    asset: Asset
+    state: typing_extensions.Literal[
+        "STATE_UNSPECIFIED", "UNUSED", "ADDED", "REMOVED", "ACTIVE"
+    ]
+
+@typing.type_check_only
+class ListFindingsResponse(typing_extensions.TypedDict, total=False):
+    findings: _list[GoogleCloudSecuritycenterV1beta1Finding]
+    nextPageToken: str
+    readTime: str
+    totalSize: int
+
+@typing.type_check_only
+class ListOperationsResponse(typing_extensions.TypedDict, total=False):
+    nextPageToken: str
+    operations: _list[Operation]
+
+@typing.type_check_only
+class ListSourcesResponse(typing_extensions.TypedDict, total=False):
+    nextPageToken: str
+    sources: _list[Source]
+
+@typing.type_check_only
+class MitreAttack(typing_extensions.TypedDict, total=False):
+    additionalTactics: _list[str]
+    additionalTechniques: _list[str]
+    primaryTactic: typing_extensions.Literal[
+        "TACTIC_UNSPECIFIED",
+        "RECONNAISSANCE",
+        "RESOURCE_DEVELOPMENT",
+        "INITIAL_ACCESS",
+        "EXECUTION",
+        "PERSISTENCE",
+        "PRIVILEGE_ESCALATION",
+        "DEFENSE_EVASION",
+        "CREDENTIAL_ACCESS",
+        "DISCOVERY",
+        "LATERAL_MOVEMENT",
+        "COLLECTION",
+        "COMMAND_AND_CONTROL",
+        "EXFILTRATION",
+        "IMPACT",
+    ]
+    primaryTechniques: _list[str]
+    version: str
+
+@typing.type_check_only
+class Operation(typing_extensions.TypedDict, total=False):
+    done: bool
+    error: Status
+    metadata: dict[str, typing.Any]
+    name: str
+    response: dict[str, typing.Any]
+
+@typing.type_check_only
+class OrganizationSettings(typing_extensions.TypedDict, total=False):
+    assetDiscoveryConfig: AssetDiscoveryConfig
+    enableAssetDiscovery: bool
+    name: str
+
+@typing.type_check_only
+class Policy(typing_extensions.TypedDict, total=False):
+    auditConfigs: _list[AuditConfig]
+    bindings: _list[Binding]
+    etag: str
+    version: int
+
+@typing.type_check_only
+class Reference(typing_extensions.TypedDict, total=False):
+    source: str
+    uri: str
+
+@typing.type_check_only
+class RunAssetDiscoveryRequest(typing_extensions.TypedDict, total=False): ...
+
+@typing.type_check_only
+class SecurityCenterProperties(typing_extensions.TypedDict, total=False):
+    resourceName: str
+    resourceOwners: _list[str]
+    resourceParent: str
+    resourceProject: str
+    resourceType: str
+
+@typing.type_check_only
+class SecurityMarks(typing_extensions.TypedDict, total=False):
+    canonicalName: str
+    marks: dict[str, typing.Any]
+    name: str
+
+@typing.type_check_only
+class SetFindingStateRequest(typing_extensions.TypedDict, total=False):
+    startTime: str
+    state: typing_extensions.Literal["STATE_UNSPECIFIED", "ACTIVE", "INACTIVE"]
+
+@typing.type_check_only
+class SetIamPolicyRequest(typing_extensions.TypedDict, total=False):
+    policy: Policy
+    updateMask: str
+
+@typing.type_check_only
+class Source(typing_extensions.TypedDict, total=False):
+    description: str
+    displayName: str
+    name: str
+
+@typing.type_check_only
+class Status(typing_extensions.TypedDict, total=False):
+    code: int
+    details: _list[dict[str, typing.Any]]
+    message: str
+
+@typing.type_check_only
+class TestIamPermissionsRequest(typing_extensions.TypedDict, total=False):
+    permissions: _list[str]
+
+@typing.type_check_only
+class TestIamPermissionsResponse(typing_extensions.TypedDict, total=False):
+    permissions: _list[str]
+
+@typing.type_check_only
+class Vulnerability(typing_extensions.TypedDict, total=False):
+    cve: Cve
