159f19688bf788da8a25b1fa3699b0ccb3450cf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:36 +0000

    Add package ccc-cli/0.1.1/ccc-cli-0.1.1.tar.gz

---
+++ packages/ccc-cli/ccc-cli-0.1.1.tar.gz/ccc-cli-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ccc_cli']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['tabulate>=0.8.9,<0.9.0']
+
+entry_points = \
+{'console_scripts': ['git-pp = ccc_cli.ccc6_print_task_runs:main']}
+
+setup_kwargs = {
+    'name': 'ccc-cli',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Xinyuan Chen',
+    'author_email': '45612704+tddschn@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
