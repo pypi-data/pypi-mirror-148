ed3a87884fb7f1e1aa77e8363a8ea9a0e1b7f216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:57 +0000

    Add package okta-awscli/0.5.4/okta-awscli-0.5.4.tar.gz

---
+++ packages/okta-awscli/okta-awscli-0.5.4.tar.gz/okta-awscli-0.5.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+exec(open(os.path.join(here, 'oktaawscli/version.py')).read())
+
+setup(
+    name='okta-awscli',
+    version=__version__,
+    description='Provides a wrapper for Okta authentication to awscli',
+    packages=find_packages(),
+    license='Apache License 2.0',
+    author='James Hale',
+    author_email='james@hale.dev',
+    url='https://github.com/okta-awscli/okta-awscli',
+    entry_points={
+        'console_scripts': [
+            'okta-awscli=oktaawscli.okta_awscli:main',
+        ],
+    },
+    install_requires=[
+        'requests',
+        'click',
+        'bs4',
+        'boto3',
+        'ConfigParser',
+        'validators',
+        ],
+    extras_require={
+        'U2F': ['python-u2flib-host']
+    },
+)
