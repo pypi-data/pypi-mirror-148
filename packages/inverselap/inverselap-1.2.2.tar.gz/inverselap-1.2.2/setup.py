96cabc1dac4202db7802150338291ac02f9c6066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:33 +0000

    Add package inverselap/1.2.2/inverselap-1.2.2.tar.gz

---
+++ packages/inverselap/inverselap-1.2.2.tar.gz/inverselap-1.2.2/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import  setup
+import setuptools
+packages = ['inverselap']# 唯一的包名，自己取名
+setup(name='inverselap',
+	version='1.2.2',
+	author='renger',
+    description="Provides a variety of numerical inversion methods for the inverse Laplace transform",
+    packages=packages, 
+    package_dir={'requests': 'requests'},)
