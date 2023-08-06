bf30e3301836ad29da1077d441c61ae3d4298bab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:16:49 +0000

    Add package mirror-up/0.1.3/mirror_up-0.1.3.tar.gz

---
+++ packages/mirror-up/mirror_up-0.1.3.tar.gz/mirror_up-0.1.3/mirror_up/__init__.py
@@ -0,0 +1,7 @@
+"""Upload files to online mirrorring services."""
+from mirror_ace import MirrorAceConnection  # noqa
+
+__author__ = """Mycsina"""
+__email__ = "mycsina@protonmail.com"
+__version__ = "0.1.3"
+APP_NAME = "mirror-up"
