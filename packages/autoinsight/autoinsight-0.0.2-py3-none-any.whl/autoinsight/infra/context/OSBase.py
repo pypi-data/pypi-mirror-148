ef172b1fa3a679c891f0231d1cde5fcd61273041
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:43 +0000

    Add package autoinsight/0.0.2/autoinsight-0.0.2-py3-none-any.whl

---
+++ packages/autoinsight/autoinsight-0.0.2-py3-none-any.whl/autoinsight/infra/context/OSBase.py
@@ -0,0 +1,9 @@
+from abc import abstractmethod, abstractclassmethod, abstractproperty
+
+from autoinsight.context.ContextBase import ContextBase
+
+class OSContextBase(ContextBase):
+    def __init__(self, *args, **kwargs):
+        pass
+
+
