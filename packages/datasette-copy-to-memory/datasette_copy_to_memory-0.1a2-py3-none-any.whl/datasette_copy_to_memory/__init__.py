2a22ca875ec6c8f9ea2ed4df02b56410c52b1edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:03 +0000

    Add package datasette-copy-to-memory/0.1a2/datasette_copy_to_memory-0.1a2-py3-none-any.whl

---
+++ packages/datasette-copy-to-memory/datasette_copy_to_memory-0.1a2-py3-none-any.whl/datasette_copy_to_memory/__init__.py
@@ -0,0 +1,28 @@
+from datasette import hookimpl
+from datasette.utils import sqlite3
+
+
+@hookimpl
+def startup(datasette):
+    plugin_config = datasette.plugin_config("datasette-copy-to-memory") or {}
+    databases = plugin_config.get("databases")
+
+    async def inner():
+        for db in datasette.databases.values():
+            if databases:
+                if db.name not in databases:
+                    continue
+            if db.path:
+                # Ensure the in-memory database is initalized
+                memory_name = "{}_memory".format(db.name)
+                memory_db = datasette.add_memory_database(memory_name)
+                await memory_db.execute("select 1 + 1")
+                # Use a different in-memory database to co-ordinate the VACUUM INTO
+                tmp = sqlite3.connect(":memory:")
+                tmp.execute("ATTACH DATABASE ? AS _copy_from", [db.path])
+                tmp.execute(
+                    "VACUUM _copy_from INTO ?",
+                    ["file:{}?mode=memory&cache=shared".format(memory_name)],
+                )
+
+    return inner
