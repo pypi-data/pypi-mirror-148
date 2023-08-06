8b80378071baa97479b4279c02805febd9d8344c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:02 +0000

    Add package spw/1.7.0/spw-1.7.0-py3-none-any.whl

---
+++ packages/spw/spw-1.7.0-py3-none-any.whl/spw/config/config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#  SPDX-License-Identifier: GPL-3.0-only
+#  Copyright 2022 drad <drader@adercon.com>
+
+import logging
+import sys
+import pkg_resources
+
+_log_level = "INFO"  # [CRITICAL|ERROR|WARNING|INFO|DEBUG] (suggest INFO)
+APP_VERSION = pkg_resources.get_distribution("spw").version
+APP_NAME = pkg_resources.get_distribution("spw").project_name
+
+logging.basicConfig(
+    format="%(message)s",
+    level=logging.getLevelName(_log_level),
+    stream=sys.stdout,
+)
