58cfe08860eb5f96fc566d3fc622ff4b5566a26a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:16 +0000

    Add package chemcharts/0.6.0/chemcharts-0.6.0.tar.gz

---
+++ packages/chemcharts/chemcharts-0.6.0.tar.gz/chemcharts-0.6.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="chemcharts",
+    maintainer="Sophie Margreitter, Christian Margreitter",
+    version="0.6.0",
+    url="https://github.com/SMargreitter/ChemCharts",
+    packages=find_namespace_packages(where="src"),
+    package_dir={"": "src"},
+    description="ChemCharts: Chemical Space Visualizer",
+    python_requires=">=3.7",
+    entry_points={"console_scripts": ["chemcharts_cli = chemcharts.scripts.chemcharts_cli:main",
+                                      "chemcharts = chemcharts.scripts.chemcharts_json:main"]},
+)
