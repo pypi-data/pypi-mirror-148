46db9002ac81425bfec84da9b71954e65deb8e3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:07 +0000

    Add package xenon-gcp-sdk/1.0.1/xenon_gcp_sdk-1.0.1.tar.gz

---
+++ packages/xenon-gcp-sdk/xenon_gcp_sdk-1.0.1.tar.gz/xenon_gcp_sdk-1.0.1/src/xenon_gcp_sdk/apis/user_identifier.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from apis.base_event import BaseEvent
+from utilss.hash_util import string_fingerprint
+
+
+@dataclass
+class UserIdentifier(BaseEvent):
+    organization_id: str
+    email: str
+    auth_token: Optional[dict]
+    admin: bool = False
+
+    def hash_key(self):
+        return string_fingerprint(f'{self.organization_id}-{self.email}')
