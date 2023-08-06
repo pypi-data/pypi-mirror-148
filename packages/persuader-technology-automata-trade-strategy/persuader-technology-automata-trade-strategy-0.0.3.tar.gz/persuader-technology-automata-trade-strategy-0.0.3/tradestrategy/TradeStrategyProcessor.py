7f65d524fe1e800e1bb8ff34be6e73f8bfb08625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:20:21 +0000

    Add package persuader-technology-automata-trade-strategy/0.0.3/persuader-technology-automata-trade-strategy-0.0.3.tar.gz

---
+++ packages/persuader-technology-automata-trade-strategy/persuader-technology-automata-trade-strategy-0.0.3.tar.gz/persuader-technology-automata-trade-strategy-0.0.3/tradestrategy/TradeStrategyProcessor.py
@@ -0,0 +1,8 @@
+from core.oracle.Prediction import Prediction
+from core.position.Position import Position
+
+
+class TradeStrategyProcessor:
+
+    def perform_trade(self, position: Position, prediction: Prediction):
+        pass
