e3635d365435faacb20b642967b934b43c8ee201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:05 +0000

    Add package cg/22.56.0/cg-22.56.0-py3-none-any.whl

---
+++ packages/cg/cg-22.56.0-py3-none-any.whl/cg/cli/workflow/base.py
@@ -0,0 +1,26 @@
+"""Common CLI workflow functions"""
+
+
+import click
+
+from .balsamic.base import balsamic as balsamic_cmd
+from .fluffy.base import fluffy
+from .microsalt.base import microsalt
+from .mip_dna.base import mip_dna
+from .mip_rna.base import mip_rna
+from .mutant.base import mutant
+from .fastq.base import fastq
+
+
+@click.group()
+def workflow():
+    """Workflows commands"""
+
+
+workflow.add_command(balsamic_cmd)
+workflow.add_command(microsalt)
+workflow.add_command(mip_dna)
+workflow.add_command(mip_rna)
+workflow.add_command(fluffy)
+workflow.add_command(mutant)
+workflow.add_command(fastq)
