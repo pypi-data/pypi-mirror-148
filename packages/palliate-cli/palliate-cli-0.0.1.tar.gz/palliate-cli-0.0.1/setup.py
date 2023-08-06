de279781090e3089ae2c4e17c65412768020c1ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:21 +0000

    Add package palliate-cli/0.0.1/palliate-cli-0.0.1.tar.gz

---
+++ packages/palliate-cli/palliate-cli-0.0.1.tar.gz/palliate-cli-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='palliate-cli',
+    version='0.0.1',
+    description='interactive palliate shell',
+    long_description='Interactive shell to control palliate instances',
+    url='https://github.com/palliate/cli',
+    author='Tsche',
+    author_email='contact@palliate.io',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Development Status :: 1 - Planning'
+    ],
+    project_urls={
+        "Bug Tracker": "https://github.com/palliate/cli/issues",
+    },
+    packages=[],
+)
