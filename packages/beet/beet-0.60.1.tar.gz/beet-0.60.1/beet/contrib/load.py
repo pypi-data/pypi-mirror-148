c5091c0d016963214dca393eecee663eaad3a231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:33 +0000

    Add package beet/0.60.1/beet-0.60.1.tar.gz

---
+++ packages/beet/beet-0.60.1.tar.gz/beet-0.60.1/beet/contrib/load.py
@@ -0,0 +1,35 @@
+"""Plugin that loads data packs and resource packs."""
+
+
+__all__ = [
+    "LoadOptions",
+    "load",
+]
+
+
+from glob import glob
+
+from pydantic import BaseModel
+
+from beet import Context, ErrorMessage, ListOption, PackageablePath, configurable
+
+
+class LoadOptions(BaseModel):
+    resource_pack: ListOption[PackageablePath] = ListOption()
+    data_pack: ListOption[PackageablePath] = ListOption()
+
+
+def beet_default(ctx: Context):
+    ctx.require(load)
+
+
+@configurable(validator=LoadOptions)
+def load(ctx: Context, opts: LoadOptions):
+    """Plugin that loads data packs and resource packs."""
+    for load_options, pack in zip([opts.resource_pack, opts.data_pack], ctx.packs):
+        for pattern in load_options.entries():
+            if paths := glob(str(ctx.directory / pattern)):
+                for path in paths:
+                    pack.load(path)
+            else:
+                raise ErrorMessage(f'Couldn\'t load "{pattern}".')
