05f43822e78b20151150fd6653b726a96b515c1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:44 +0000

    Add package rolab-tb/0.0.4/rolab_tb-0.0.4-py3-none-any.whl

---
+++ packages/rolab-tb/rolab_tb-0.0.4-py3-none-any.whl/rolab_tb/log.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+"""
+    SPDX-FileCopyrightText: 2022 Senne Van Baelen
+    SPDX-License-Identifier: Apache-2.0
+"""
+
+import os
+import logging
+import pathlib
+
+GENERAL_FORMATTER = logging.Formatter(fmt="[%(levelname)s \
+- %(module)s (%(asctime)s)] %(message)s")
+
+INFO_FORMATTER = logging.Formatter(fmt="[%(levelname)s \
+- %(module)s] %(message)s")
+
+
+def create_logger(name, log_level="INFO", logfile=None):
+
+    """ initialize logger """
+
+    # remove old handlers
+    log = logging.getLogger(name)    # e.g. root logger
+    for hdlr in log.handlers[:]:     # remove all old handlers
+        log.removeHandler(hdlr)
+
+    logger = logging.getLogger(name)
+    handler = None
+    if not logfile:
+        handler = logging.StreamHandler()
+    else:
+        cwd = os.getcwd()
+        logpath = pathlib.Path(cwd, logfile)
+        print(f"Redirecting all logs to {logpath}")
+        handler = logging.FileHandler(logpath, 'a')
+
+    logger.addHandler(handler)
+
+    if log_level == "DEBUG":
+        logger.setLevel(logging.DEBUG)
+        handler.setFormatter(GENERAL_FORMATTER)
+    elif log_level == "INFO":
+        logger.setLevel(logging.INFO)
+        handler.setFormatter(INFO_FORMATTER)
+    elif log_level == "WARNING":
+        logger.setLevel(logging.WARNING)
+        handler.setFormatter(GENERAL_FORMATTER)
+    elif log_level == "ERROR":
+        logger.setLevel(logging.ERROR)
+        handler.setFormatter(GENERAL_FORMATTER)
+
+    return logger
