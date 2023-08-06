17be6edfab73142a07fe8847be453da8d19b9901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:50:50 +0000

    Add package viadot/0.4.3/viadot-0.4.3.tar.gz

---
+++ packages/viadot/viadot-0.4.3.tar.gz/viadot-0.4.3/viadot/exceptions.py
@@ -0,0 +1,14 @@
+class ValidationError(Exception):
+    pass
+
+
+class APIError(Exception):
+    pass
+
+
+class CredentialError(Exception):
+    pass
+
+
+class DBDataAccessError(Exception):
+    pass
