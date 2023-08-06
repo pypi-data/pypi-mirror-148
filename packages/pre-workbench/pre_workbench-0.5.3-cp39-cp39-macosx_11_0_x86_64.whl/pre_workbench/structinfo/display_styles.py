97655ba9690d8b64d5f35ccbcfbcd4e69b5fbaca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:58 +0000

    Add package pre-workbench/0.5.3/pre_workbench-0.5.3-cp39-cp39-macosx_11_0_x86_64.whl

---
+++ packages/pre-workbench/pre_workbench-0.5.3-cp39-cp39-macosx_11_0_x86_64.whl/pre_workbench/structinfo/display_styles.py
@@ -0,0 +1,17 @@
+
+def dec(i):
+	return str(i)
+
+def hex(i):
+	if isinstance(i, (bytes, bytearray)):
+		return ":".join("%02x" % a for a in i)
+	else:
+		return "0x%x" % i
+
+def dotted_quad(b):
+	return ".".join("%d" % i for i in b)
+
+def ip6(b):
+	return ":".join("%x" % i for i in b)
+
+
