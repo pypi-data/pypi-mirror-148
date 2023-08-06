c80649f5a812df4b82d25831fcc0b21970e08085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:10 +0000

    Add package mqt.qmap/1.7.0/mqt.qmap-1.7.0.tar.gz

---
+++ packages/mqt.qmap/mqt.qmap-1.7.0.tar.gz/mqt.qmap-1.7.0/include/MappingResults.hpp
@@ -0,0 +1,119 @@
+/*
+ * This file is part of the MQT QMAP library which is released under the MIT license.
+ * See file README.md or go to https://iic.jku.at/eda/research/ibm_qx_mapping/ for more information.
+ */
+
+#include "configuration/Configuration.hpp"
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#ifndef QMAP_MAPPINGRESULTS_HPP
+    #define QMAP_MAPPINGRESULTS_HPP
+
+struct MappingResults {
+    struct CircuitInfo {
+        // general info
+        std::string    name{};
+        unsigned short qubits           = 0;
+        unsigned long  gates            = 0;
+        unsigned long  singleQubitGates = 0;
+        unsigned long  cnots            = 0;
+        unsigned long  layers           = 0;
+
+        // info in output circuit
+        unsigned long swaps            = 0;
+        unsigned long directionReverse = 0;
+        unsigned long teleportations   = 0;
+    };
+
+    CircuitInfo input{};
+
+    std::string   architecture{};
+    Configuration config{};
+
+    double time    = 0.0;
+    bool   timeout = true;
+
+    CircuitInfo output{};
+    std::string mappedCircuit{};
+
+    MappingResults()          = default;
+    virtual ~MappingResults() = default;
+
+    virtual void copyInput(const MappingResults& mappingResults) {
+        input        = mappingResults.input;
+        architecture = mappingResults.architecture;
+        config       = mappingResults.config;
+        output       = mappingResults.output;
+    }
+
+    [[nodiscard]] std::string toString() const {
+        return json().dump(2);
+    }
+
+    [[nodiscard]] virtual nlohmann::json json() const {
+        nlohmann::json resultJSON{};
+        auto&          circuit        = resultJSON["circuit"];
+        circuit["name"]               = input.name;
+        circuit["qubits"]             = input.qubits;
+        circuit["gates"]              = input.gates;
+        circuit["single_qubit_gates"] = input.singleQubitGates;
+        circuit["cnots"]              = input.cnots;
+        circuit["layers"]             = input.layers;
+
+        auto& mapped_circuit                 = resultJSON["mapped_circuit"];
+        mapped_circuit["name"]               = output.name;
+        mapped_circuit["qubits"]             = output.qubits;
+        mapped_circuit["gates"]              = output.gates;
+        mapped_circuit["single_qubit_gates"] = output.singleQubitGates;
+        mapped_circuit["cnots"]              = output.cnots;
+        mapped_circuit["swaps"]              = output.swaps;
+        if (!mappedCircuit.empty()) {
+            mapped_circuit["qasm"] = mappedCircuit;
+        }
+        if (config.method == Method::Exact) {
+            mapped_circuit["direction_reverse"] = output.directionReverse;
+        } else if (config.method == Method::Heuristic) {
+            mapped_circuit["teleportations"] = output.teleportations;
+        }
+
+        resultJSON["config"] = config.json();
+
+        auto& stats               = resultJSON["statistics"];
+        stats["timeout"]          = timeout;
+        stats["mapping_time"]     = time;
+        stats["additional_gates"] = output.gates - input.gates;
+        stats["arch"]             = architecture;
+
+        return resultJSON;
+    }
+
+    virtual std::string csv() {
+        std::stringstream ss{};
+        ss << input.name << ";"
+           << input.qubits << ";"
+           << input.gates << ";"
+           << input.singleQubitGates << ";"
+           << input.cnots << ";"
+           << architecture << ";"
+           << output.name << ";"
+           << output.qubits << ";"
+           << output.gates << ";"
+           << output.singleQubitGates << ";"
+           << output.cnots << ";"
+           << output.swaps << ";"
+           << output.directionReverse << ";"
+           << output.teleportations << ";";
+        if (timeout) {
+            ss << "TO";
+        } else {
+            ss << time;
+        }
+        ss << ";";
+        return ss.str();
+    }
+};
+
+#endif //QMAP_MAPPINGRESULTS_HPP
---
+++ packages/mqt.qmap/mqt.qmap-1.7.0.tar.gz/mqt.qmap-1.7.0/src/Mapper.cpp
@@ -0,0 +1,128 @@
+/*
+ * This file is part of the MQT QMAP library which is released under the MIT license.
+ * See file README.md or go to https://iic.jku.at/eda/research/ibm_qx_mapping/ for more information.
+ */
+
+#include "Mapper.hpp"
+
+void Mapper::initResults() {
+    results.input.name    = qc.getName();
+    results.input.qubits  = qc.getNqubits();
+    results.architecture  = architecture.getArchitectureName();
+    results.output.name   = qc.getName() + "_mapped";
+    results.output.qubits = architecture.getNqubits();
+    results.output.gates  = std::numeric_limits<unsigned long>::max();
+    qcMapped.addQubitRegister(architecture.getNqubits());
+}
+
+Mapper::Mapper(qc::QuantumComputation& quantumComputation, Architecture& arch):
+    qc(quantumComputation), architecture(arch) {
+    qubits.fill(DEFAULT_POSITION);
+    locations.fill(DEFAULT_POSITION);
+    fidelities.fill(INITIAL_FIDELITY);
+}
+
+void Mapper::createLayers() {
+    const auto&                          config = results.config;
+    std::array<short, MAX_DEVICE_QUBITS> lastLayer{};
+    lastLayer.fill(DEFAULT_POSITION);
+
+    auto qubitsInLayer = std::set<unsigned short>{};
+
+    for (auto& gate: qc) {
+        // skip over barrier instructions
+        if (gate->getType() == qc::Barrier || gate->getType() == qc::Measure) {
+            continue;
+        }
+
+        if (!gate->isUnitary()) {
+            throw QMAPException("Mapping not possible: circuit contains non-unitary operation: " + std::string(gate->getName()));
+        }
+
+        if (gate->getControls().size() > 1 || gate->getTargets().size() > 1) {
+            throw QMAPException("Circuit contains gates with more than one control. Please make sure that the input circuit's gates are decomposed to the appropriate gate set!");
+        }
+
+        bool  singleQubit = gate->getControls().empty();
+        short control     = -1;
+        if (!singleQubit) {
+            control = static_cast<short>((*gate->getControls().begin()).qubit);
+        }
+        unsigned short target = gate->getTargets().at(0);
+        size_t         layer  = 0;
+
+        switch (config.layering) {
+            case Layering::IndividualGates:
+            case Layering::None:
+                layers.emplace_back();
+                layers.back().emplace_back(control, target, gate.get());
+                break;
+            case Layering::DisjointQubits:
+                if (singleQubit) {
+                    layer                = lastLayer.at(target) + 1;
+                    lastLayer.at(target) = layer;
+                } else {
+                    layer                 = std::max(lastLayer.at(control), lastLayer.at(target)) + 1;
+                    lastLayer.at(control) = lastLayer.at(target) = layer;
+                }
+
+                if (layers.size() <= layer) {
+                    layers.emplace_back();
+                }
+                layers.at(layer).emplace_back(control, target, gate.get());
+                break;
+            case Layering::OddGates:
+                if (results.input.gates % 2 == 0) {
+                    layers.emplace_back();
+                    layers.back().emplace_back(control, target, gate.get());
+                } else {
+                    layers.back().emplace_back(control, target, gate.get());
+                }
+                break;
+            case Layering::QubitTriangle:
+                if (layers.empty()) {
+                    layers.emplace_back();
+                }
+
+                if (singleQubit) {
+                    // single qubit gates can be added in any layer
+                    layers.back().emplace_back(control, target, gate.get());
+                } else {
+                    qubitsInLayer.insert(control);
+                    qubitsInLayer.insert(target);
+
+                    if (qubitsInLayer.size() <= 3) {
+                        layers.back().emplace_back(control, target, gate.get());
+                    } else {
+                        layers.emplace_back();
+                        layers.back().emplace_back(control, target, gate.get());
+                        qubitsInLayer.clear();
+                        qubitsInLayer.insert(control);
+                        qubitsInLayer.insert(target);
+                    }
+                }
+                break;
+        }
+
+        if (singleQubit) {
+            results.input.singleQubitGates++;
+        } else {
+            results.input.cnots++;
+        }
+        results.input.gates++;
+    }
+    results.input.layers = layers.size();
+}
+
+std::size_t Mapper::getNextLayer(std::size_t idx) {
+    auto next = idx + 1;
+    while (next < layers.size()) {
+        for (const auto& gate: layers.at(next)) {
+            if (!gate.singleQubit()) {
+                return next;
+            }
+        }
+        next++;
+    }
+    return -1;
+}
