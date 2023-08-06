f41c6d4eb4478db4824d25f02dee6151f8170bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:03 +0000

    Add package jetfuel/0.0.13/jetfuel-0.0.13.tar.gz

---
+++ packages/jetfuel/jetfuel-0.0.13.tar.gz/jetfuel-0.0.13/server/commons/database/__init__.py
@@ -0,0 +1,4 @@
+"""
+Database backends. Queries are defined in logic/database so that
+we can implement more database backends if desired.
+"""
---
+++ packages/jetfuel/jetfuel-0.0.13.tar.gz/jetfuel-0.0.13/server/config.py
@@ -0,0 +1,8 @@
+import os
+
+PROJECT_ROOT = os.path.dirname(__file__)
+
+SQL_MODE = "sqlite"
+SQL_VERSION = 0
+
+DEFAULT_USER = True
