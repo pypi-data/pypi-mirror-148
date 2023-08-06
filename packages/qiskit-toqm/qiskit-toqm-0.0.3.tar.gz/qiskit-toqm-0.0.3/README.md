ceb9f2a016b47cdd36fe43f21f14ef96af06f979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:19 +0000

    Add package qiskit-toqm/0.0.3/qiskit-toqm-0.0.3.tar.gz

---
+++ packages/qiskit-toqm/qiskit-toqm-0.0.3.tar.gz/qiskit-toqm-0.0.3/README.md
@@ -0,0 +1,33 @@
+Qiskit TOQM
+===========
+
+|      CI              | status |
+|----------------------|--------|
+| pip builds           | [![Pip Actions Status][actions-pip-badge]][actions-pip-link] |
+
+[Qiskit Terra](https://github.com/qiskit/qiskit-terra) transpiler passes for the [Time-Optimal Qubit Mapping](https://doi.org/10.1145/3445814.3446706) (TOQM) algorithm.
+
+[actions-badge]:           https://github.com/kevinhartman/qiskit-toqm/workflows/Tests/badge.svg
+[actions-pip-link]:        https://github.com/kevinhartman/qiskit-toqm/actions?query=workflow%3APip
+[actions-pip-badge]:       https://github.com/kevinhartman/qiskit-toqm/workflows/Pip/badge.svg
+[actions-wheels-link]:     https://github.com/kevinhartman/qiskit-toqm/actions?query=workflow%3AWheels
+[actions-wheels-badge]:    https://github.com/kevinhartman/qiskit-toqm/workflows/Wheels/badge.svg
+
+Installation
+------------
+
+- clone this repository
+- `pip install ./qiskit-toqm`
+
+
+Licenses
+--------
+
+qiskit-toqm is provided under the Apache 2.0 license that can be found in
+the LICENSE file.
+
+pybind11 is provided under a BSD-style license that can be found in the LICENSE_pybind11
+file.
+
+By using, distributing, or contributing to this project, you agree to the
+terms and conditions of both licenses.
---
+++ packages/qiskit-toqm/qiskit-toqm-0.0.3.tar.gz/qiskit-toqm-0.0.3/setup.py
@@ -0,0 +1,49 @@
+import sys
+
+try:
+    from skbuild import setup
+except ImportError:
+    print(
+        "Please update pip, you need pip 10 or greater,\n"
+        " or you need to install the PEP 518 requirements in pyproject.toml yourself",
+        file=sys.stderr,
+    )
+    raise
+
+from setuptools import find_packages
+
+setup(
+    name="qiskit-toqm",
+    version="0.0.3",
+    description="Qiskit transpiler passes for the TOQM algorithm",
+    author="Qiskit Development Team",
+    author_email="hello@qiskit.org",
+    license="Apache 2.0",
+    classifiers=[
+        "Environment :: Console",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering",
+    ],
+    keywords="qiskit sdk quantum",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    cmake_install_dir="src/qiskit_toqm/native",
+    include_package_data=True,
+    extras_require={"test": ["pytest"]},
+    python_requires=">=3.7",
+    project_urls={
+        "Bug Tracker": "https://github.com/kevinhartman/qiskit-toqm/issues",
+        "Documentation": "https://github.com/kevinhartman/qiskit-toqm",
+        "Source Code": "https://github.com/kevinhartman/qiskit-toqm",
+    },
+)
