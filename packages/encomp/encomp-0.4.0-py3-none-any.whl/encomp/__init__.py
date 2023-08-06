10526a75319a05118f10c9984a1d5c8ac15af2a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:50 +0000

    Add package encomp/0.4.0/encomp-0.4.0-py3-none-any.whl

---
+++ packages/encomp/encomp-0.4.0-py3-none-any.whl/encomp/__init__.py
@@ -0,0 +1,12 @@
+
+__version__ = '0.4.0'
+
+
+from encomp.settings import SETTINGS
+
+if SETTINGS.type_checking:
+
+    from typeguard.importhook import install_import_hook
+
+    # installs the @typechecked decorator on all functions imported after this
+    install_import_hook('encomp')
