315080117291b125064756b905b47f9b4a9aca42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:35 +0000

    Add package vinca/2.0.2/vinca-2.0.2.tar.gz

---
+++ packages/vinca/vinca-2.0.2.tar.gz/vinca-2.0.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['vinca', 'vinca._lib', 'vinca._lib.vinput']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fire>=0.4.0,<0.5.0']
+
+entry_points = \
+{'console_scripts': ['vinca = vinca:run']}
+
+setup_kwargs = {
+    'name': 'vinca',
+    'version': '2.0.2',
+    'description': 'Spaced Repetition CLI',
+    'long_description': None,
+    'author': 'Oscar Laird',
+    'author_email': 'olaird25@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
