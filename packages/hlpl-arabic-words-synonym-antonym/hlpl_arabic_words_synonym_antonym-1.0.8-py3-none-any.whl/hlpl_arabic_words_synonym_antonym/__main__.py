c773622c8851f0a9a53b2082c33e7453fe5da603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:47 +0000

    Add package hlpl-arabic-words-synonym-antonym/1.0.8/hlpl_arabic_words_synonym_antonym-1.0.8-py3-none-any.whl

---
+++ packages/hlpl-arabic-words-synonym-antonym/hlpl_arabic_words_synonym_antonym-1.0.8-py3-none-any.whl/hlpl_arabic_words_synonym_antonym/__main__.py
@@ -0,0 +1,23 @@
+﻿
+
+import configparser
+import sys
+import os
+ 
+def get_hlpl_arabic_words_synonym_antonym_version(case):
+    config = configparser.ConfigParser()
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    config_file_path = os.path.join(current_directory, 'setup.cfg')
+    config.read(config_file_path)
+    if case=='version':
+       return config['hlpl_arabic_words_synonym_antonym']['version']
+    if case=='else':
+       return config['hlpl_arabic_words_synonym_antonym']['version']+'\n'+config['hlpl_arabic_words_synonym_antonym']['author']+'\n'+config['hlpl_arabic_words_synonym_antonym']['email']+'\n'+config['hlpl_arabic_words_synonym_antonym']['url']+'\n'
+       
+ 
+    
+def main():
+    if 'version' in sys.argv:
+        print('\n'+get_hlpl_arabic_words_synonym_antonym_version('version'))
+    else:
+        print('\n'+get_hlpl_arabic_words_synonym_antonym_version('else')+'\n'+'hlpl_arabic_words_synonym_antonym lists arabic words synonyms and antonyms')
---
+++ packages/hlpl-arabic-words-synonym-antonym/hlpl_arabic_words_synonym_antonym-1.0.8-py3-none-any.whl/hlpl_arabic_words_synonym_antonym/arabic_words_synonym_antonym.py
@@ -0,0 +1,15 @@
+﻿
+
+import os
+current_directory = os.path.dirname(os.path.abspath(__file__))
+db = os.path.join(current_directory, 'hlpl_arabic_words_synonym_antonym.db')
+
+import sqlite3
+def get():
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+    
+    lst_1=cur.execute("SELECT * FROM noun_sa").fetchall()
+    lst_2=cur.execute("SELECT * FROM verb_sa").fetchall()
+    return lst_1,lst_2,conn
+
