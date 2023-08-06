31ad3477552018e077b4f34b5f3744e02b6a1f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:35:48 +0000

    Add package filoc/0.0.18/filoc-0.0.18.tar.gz

---
+++ packages/filoc/filoc-0.0.18.tar.gz/filoc-0.0.18/filoc/__init__.py
@@ -0,0 +1,19 @@
+"""
+Filoc public API
+"""
+from .contract import Filoc
+from .filoc_io import FilocIO
+from filoc.factories import filoc, filoc_json, filoc_pandas
+
+__version__ = '0.0.7'
+
+
+__all__     = [
+    'filoc',
+    'filoc_json', 
+    'filoc_pandas'
+    'FilocIO',
+    'Filoc',
+    'FilocSingle',
+    'FilocComposite',
+]
