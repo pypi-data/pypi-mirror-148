76af4ddc8592bd671eb55e59cef015c4a65e961a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:17:08 +0000

    Add package steamship/2.1.5b3/steamship-2.1.5b3-py2.py3-none-any.whl

---
+++ packages/steamship/steamship-2.1.5b3-py2.py3-none-any.whl/steamship/data/plugin_instance.py
@@ -0,0 +1,121 @@
+from dataclasses import dataclass
+from typing import Dict
+
+from steamship.base import Client, Request
+from steamship.base.response import Response
+from steamship.plugin.inputs.export_plugin_input import ExportPluginInput
+from steamship.plugin.inputs.training_parameter_plugin_input import TrainingParameterPluginInput
+from steamship.plugin.outputs.raw_data_plugin_output import RawDataPluginOutput
+from steamship.plugin.outputs.training_parameter_plugin_output import TrainingParameterPluginOutput
+
+
+class PluginInstance:
+    pass
+
+
+@dataclass
+class CreatePluginInstanceRequest(Request):
+    id: str = None
+    pluginId: str = None
+    pluginHandle: str = None
+    pluginVersionId: str = None
+    pluginVersionHandle: str = None
+    handle: str = None
+    upsert: bool = None
+    config: Dict[str, any] = None
+
+
+@dataclass
+class DeletePluginInstanceRequest(Request):
+    id: str
+
+
+
+@dataclass
+class PluginInstance:
+    client: Client = None
+    id: str = None
+    handle: str = None
+    pluginId: str = None
+    pluginVersionId: str = None
+    userId: str = None
+    config: Dict[str, any] = None
+    spaceId: str = None
+
+    @staticmethod
+    def from_dict(d: any, client: Client = None) -> "PluginInstance":
+        if 'pluginInstance' in d:
+            d = d['pluginInstance']
+
+        return PluginInstance(
+            client=client,
+            id=d.get('id', None),
+            handle=d.get('handle', None),
+            pluginId=d.get('pluginId', None),
+            pluginVersionId=d.get('pluginVersionId', None),
+            config=d.get('config', None),
+            userId=d.get('userId', None)
+        )
+
+    @staticmethod
+    def create(
+            client: Client,
+            pluginId: str = None,
+            pluginHandle: str = None,
+            pluginVersionId: str = None,
+            pluginVersionHandle: str = None,
+            handle: str = None,
+            upsert: bool = None,
+            config: Dict[str, any] = None
+    ) -> Response[PluginInstance]:
+        req = CreatePluginInstanceRequest(
+            handle=handle,
+            pluginId=pluginId,
+            pluginHandle=pluginHandle,
+            pluginVersionId=pluginVersionId,
+            pluginVersionHandle=pluginVersionHandle,
+            upsert=upsert,
+            config=config
+        )
+
+        return client.post(
+            'plugin/instance/create',
+            payload=req,
+            expect=PluginInstance
+        )
+
+    def delete(self) -> PluginInstance:
+        req = DeletePluginInstanceRequest(
+            id=self.id
+        )
+        return self.client.post(
+            'plugin/instance/delete',
+            payload=req,
+            expect=PluginInstance
+        )
+
+    def export(self, input: ExportPluginInput) -> RawDataPluginOutput:
+        input.pluginInstance = self.handle
+        return self.client.post(
+            'plugin/instance/export',
+            payload=input,
+            expect=RawDataPluginOutput
+        )
+
+    def train(self, trainingRequest: TrainingParameterPluginInput) -> PluginInstance:
+        return self.client.post(
+            'plugin/instance/train',
+            payload=trainingRequest,
+            expect=PluginInstance
+        )
+
+    def getTrainingParameters(self, trainingRequest: TrainingParameterPluginInput) -> PluginInstance:
+        return self.client.post(
+            'plugin/instance/getTrainingParameters',
+            payload=trainingRequest,
+            expect=TrainingParameterPluginOutput
+        )
+
+@dataclass
+class ListPrivatePluginInstancesRequest(Request):
+    pass
