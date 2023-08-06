1b78b7a133d9dd4666a130d20a2e76c50a1be355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:19 +0000

    Add package machine-stats-alpha/1.0.3/machine_stats_alpha-1.0.3.tar.gz

---
+++ packages/machine-stats-alpha/machine_stats_alpha-1.0.3.tar.gz/machine_stats_alpha-1.0.3/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+with open("alpha-description.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="machine_stats_alpha",
+    version="1.0.3",
+    author="Tidal Migrations",
+    author_email="support@tidalmigrations.com",
+    description="A simple and effective way to gather machine statistics (RAM, Storage, CPU, etc.) from server environment",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["machine_stats"],
+    package_dir={"": "src"},
+    package_data={"": ["modules/*", "plugins/*"]},
+    url="https://github.com/tidalmigrations/machine_stats",
+    install_requires=[
+        "ansible==5.6.0",
+        "pluginbase==1.0.1",
+    ],
+    entry_points={
+        "console_scripts": [
+            "machine_stats_alpha=machine_stats:main",
+            "machine-stats-alpha=machine_stats:main",
+        ]
+    },
+)
