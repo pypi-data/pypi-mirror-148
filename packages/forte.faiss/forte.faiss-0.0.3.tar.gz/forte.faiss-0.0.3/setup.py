2d287efa5393db4d90d0e85a39367e9742b1f7ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:20 +0000

    Add package forte.faiss/0.0.3/forte.faiss-0.0.3.tar.gz

---
+++ packages/forte.faiss/forte.faiss-0.0.3.tar.gz/forte.faiss-0.0.3/setup.py
@@ -0,0 +1,43 @@
+import sys
+from pathlib import Path
+
+import setuptools
+
+readme = Path("README.md")
+if readme.exists():
+    long_description = (Path("README.md")).read_text()
+else:
+    long_description = (
+        "Provide Forte implementations of a fantastic collection of NLP tools."
+    )
+
+if sys.version_info < (3, 6):
+    sys.exit("Python>=3.6 is required by Forte.")
+
+setuptools.setup(
+    name="forte.faiss",
+    version="0.0.3",
+    url="https://github.com/asyml/forte-wrappers/tree/main/src/faiss",
+    description="Provide Forte implementations of a fantastic collection of "
+    "NLP tools.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="Apache License Version 2.0",
+    packages=["fortex.faiss"],
+    namespace_packages=["fortex"],
+    include_package_data=True,
+    platforms="any",
+    install_requires=[
+        "forte==0.2.0",
+        "faiss-cpu>=1.6.1",
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
