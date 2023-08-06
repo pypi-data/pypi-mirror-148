b1525753824f6645647f85dcb479412595696079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:24:17 +0000

    Add package realcwpmat/1.0.4/realcwpmat-1.0.4.tar.gz

---
+++ packages/realcwpmat/realcwpmat-1.0.4.tar.gz/realcwpmat-1.0.4/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+  name="realcwpmat",
+  version="1.0.4",
+  author="auth_cwpccna",
+  author_email="623365663@qq.com",
+  description="一个数学计算包",
+  long_description=" --------------简单的包----------------",
+  long_description_content_type="text/markdown",
+  url="https://github.com/pypa/sampleproject",
+  packages=setuptools.find_packages(),
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+)
