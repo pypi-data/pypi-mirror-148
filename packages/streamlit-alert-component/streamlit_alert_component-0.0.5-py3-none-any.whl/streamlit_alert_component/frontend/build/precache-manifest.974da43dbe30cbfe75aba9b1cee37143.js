59f0e2bfd66483c769b3f1105f13f0e0449b29aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:46 +0000

    Add package streamlit-alert-component/0.0.5/streamlit_alert_component-0.0.5-py3-none-any.whl

---
+++ packages/streamlit-alert-component/streamlit_alert_component-0.0.5-py3-none-any.whl/streamlit_alert_component/frontend/build/precache-manifest.974da43dbe30cbfe75aba9b1cee37143.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "944a3fff286c500113fa371c7a218f0c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2fc1a14e13f383be3a96",
+    "url": "./static/js/2.12a3a5b8.chunk.js"
+  },
+  {
+    "revision": "0d70bb2dcb2571b1c9d4c7eb24d15ef9",
+    "url": "./static/js/2.12a3a5b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92ed3af52dc384205f9b",
+    "url": "./static/js/main.27039064.chunk.js"
+  },
+  {
+    "revision": "43834f1bc9d1323c3c01",
+    "url": "./static/js/runtime-main.d31d2a75.js"
+  }
+]);
---
+++ packages/streamlit-alert-component/streamlit_alert_component-0.0.5-py3-none-any.whl/streamlit_alert_component/frontend/build/precache-manifest.b4a40df728cd714edf1a25ae11638462.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6509d5dc76d882d5cf6f43f916a8b77f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2fc1a14e13f383be3a96",
+    "url": "./static/js/2.12a3a5b8.chunk.js"
+  },
+  {
+    "revision": "0d70bb2dcb2571b1c9d4c7eb24d15ef9",
+    "url": "./static/js/2.12a3a5b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81b347ef0fa4d8da691b",
+    "url": "./static/js/main.b1a99691.chunk.js"
+  },
+  {
+    "revision": "43834f1bc9d1323c3c01",
+    "url": "./static/js/runtime-main.d31d2a75.js"
+  }
+]);
