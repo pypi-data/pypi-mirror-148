e580f91461dc15726f0435dc6623f74160efbc53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:45 +0000

    Add package darkwood/1.0.6/darkwood-1.0.6.tar.gz

---
+++ packages/darkwood/darkwood-1.0.6.tar.gz/darkwood-1.0.6/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="darkwood",
+    version="1.0.6",
+    description="Darkwood python client",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Darkwood",
+    author_email="angelo@darkwood.tech",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["darkwood"] + find_packages(),
+    include_package_data=True,
+    install_requires=["requests"],
+    entry_points={
+        "console_scripts": [
+            "darkwood-data=darkwood.__main__:main",
+        ]
+    },
+)
