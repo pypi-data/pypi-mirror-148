b4a9e29fbc76d2439b7e29635fa16c1e433bfd09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:17:09 +0000

    Add package steamship/2.1.5b3/steamship-2.1.5b3.tar.gz

---
+++ packages/steamship/steamship-2.1.5b3.tar.gz/steamship-2.1.5b3/src/steamship/base/error.py
@@ -0,0 +1,63 @@
+from typing import Any
+from dataclasses import dataclass
+import logging
+from typing import Union
+
+@dataclass
+class SteamshipError(Exception):
+    message: str = None
+    internalMessage: str = None
+    suggestion: str = None
+    code: str = None
+    error: str = None
+
+    def __init__(
+            self,
+            message: str = "Undefined remote error",
+            internalMessage : str = None,
+            suggestion: str = None,
+            code: str = None,
+            error: Union[Exception, str] = None):
+        self.message = message
+        self.suggestion = suggestion
+        self.internalMessage = internalMessage
+        self.statusCode = code
+        if error is not None:
+            self.error = "{}".format(error)
+
+        parts = []
+        if code is not None:
+            parts.append("[{}]".format(code))
+        if message is not None:
+            parts.append(message)
+        if internalMessage is not None:
+            parts.append("Internal Message: {}".format(internalMessage))
+        if suggestion is not None:
+            parts.append("Suggestion: {}".format(suggestion))
+
+        super().__init__("\n".join(parts))
+
+    def log(self):
+        logging.error("[{}] {}. [Internal: {}] [Suggestion: {}]".format(self.code, self.message, self.internalMessage, self.suggestion))
+        if self.error:
+            logging.error(self.error)
+
+    def to_dict(self) -> dict:
+        return dict(
+            message=self.message,
+            internalMessage=self.internalMessage,
+            suggestion=self.suggestion,
+            code=self.code,
+            error=self.error
+        )
+
+    @staticmethod
+    def from_dict(d: any, client: Any = None) -> "SteamshipError":
+        """Last resort if subclass doesn't override: pass through."""
+        return SteamshipError(
+            message=d.get('statusMessage', d.get('message', None)),
+            internalMessage=d.get('internalMessage', None),
+            suggestion=d.get('statusSuggestion', d.get('suggestion', None)),
+            code=d.get('statusCode', d.get('code', None)),
+            error=d.get('error', d.get('error', None))
+        )
---
+++ packages/steamship/steamship-2.1.5b3.tar.gz/steamship-2.1.5b3/src/steamship/data/app_version.py
@@ -0,0 +1,89 @@
+from dataclasses import dataclass
+from typing import Dict
+
+from steamship.base import Client, Request
+
+
+@dataclass
+class CreateAppVersionRequest(Request):
+    appId: str = None
+    handle: str = None
+    upsert: bool = None
+    type: str = 'file'
+    configTemplate: Dict[str, any] = None
+
+
+@dataclass
+class DeleteAppVersionRequest(Request):
+    id: str
+
+
+@dataclass
+class ListPrivateAppVersionsRequest(Request):
+    pass
+
+
+@dataclass
+class AppVersion:
+    client: Client = None
+    id: str = None
+    appId: str = None
+    handle: str = None
+    configTemplate: Dict[str, any] = None
+
+    @staticmethod
+    def from_dict(d: any, client: Client = None) -> "AppVersion":
+        if 'appVersion' in d:
+            d = d['appVersion']
+
+        return AppVersion(
+            client=client,
+            id=d.get('id', None),
+            handle=d.get('handle', None),
+            configTemplate=d.get('configTemplate', None)
+        )
+
+    @staticmethod
+    def create(
+            client: Client,
+            appId: str = None,
+            handle: str = None,
+            filename: str = None,
+            filebytes: bytes = None,
+            upsert: bool = None,
+            configTemplate: Dict[str, any] = None
+    ) -> "AppVersion":
+
+
+        if filename is None and filebytes is None:
+            raise Exception("Either filename or filebytes must be provided.")
+        if filename is not None and filebytes is not None:
+            raise Exception("Only either filename or filebytes should be provided.")
+
+        if filename is not None:
+            with open(filename, 'rb') as f:
+                filebytes = f.read()
+
+        req = CreateAppVersionRequest(
+            handle=handle,
+            appId=appId,
+            upsert=upsert,
+            configTemplate=configTemplate
+        )
+
+        return client.post(
+            'app/version/create',
+            payload=req,
+            file=('app.zip', filebytes, "multipart/form-data"),
+            expect=AppVersion
+        )
+
+    def delete(self) -> "AppVersion":
+        req = DeleteAppVersionRequest(
+            id=self.id
+        )
+        return self.client.post(
+            'app/version/delete',
+            payload=req,
+            expect=AppVersion
+        )
