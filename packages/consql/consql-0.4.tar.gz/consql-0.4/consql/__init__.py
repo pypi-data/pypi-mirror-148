bba5781d9a580506507d8ed30ac79b5a80c782b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:11 +0000

    Add package consql/0.4/consql-0.4.tar.gz

---
+++ packages/consql/consql-0.4.tar.gz/consql-0.4/consql/__init__.py
@@ -0,0 +1,19 @@
+"""
+Initializing the Python package
+"""
+
+from .model import Attribute
+from .main import make_base
+from .table import Table
+from . import coerces
+
+
+__version__ = '0.4'
+
+__all__ = (
+    '__version__',
+    'Attribute',
+    'Table',
+    'make_base',
+    'coerces',
+)
