fc20d43f1fede2f0d01b79e1db7612885d80bad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:25:22 +0000

    Add package tinkoff-investments/0.2.0b24/tinkoff-investments-0.2.0b24.tar.gz

---
+++ packages/tinkoff-investments/tinkoff-investments-0.2.0b24.tar.gz/tinkoff-investments-0.2.0b24/tinkoff/invest/clients.py
@@ -0,0 +1,37 @@
+from contextlib import asynccontextmanager, contextmanager
+from typing import AsyncGenerator, Generator, Optional
+
+from .async_services import AsyncServices
+from .channels import create_channel
+from .services import Services
+from .typedefs import ChannelArgumentType
+
+__all__ = ("Client", "AsyncClient")
+
+
+@contextmanager
+def Client(
+    token: str,
+    *,
+    sandbox_token: Optional[str] = None,
+    options: Optional[ChannelArgumentType] = None,
+    app_name: Optional[str] = None,
+) -> Generator[Services, None, None]:
+    with create_channel(options=options) as channel:
+        yield Services(
+            channel, token=token, sandbox_token=sandbox_token, app_name=app_name
+        )
+
+
+@asynccontextmanager
+async def AsyncClient(
+    token: str,
+    *,
+    sandbox_token: Optional[str] = None,
+    options: Optional[ChannelArgumentType] = None,
+    app_name: Optional[str] = None,
+) -> AsyncGenerator[AsyncServices, None]:
+    async with create_channel(force_async=True, options=options) as channel:
+        yield AsyncServices(
+            channel, token=token, sandbox_token=sandbox_token, app_name=app_name
+        )
