1ba09b3ab8359eef1df2dd8175566ad4f37a1799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:20:26 +0000

    Add package django-hero-ds/0.0.1/django-hero_ds-0.0.1.tar.gz

---
+++ packages/django-hero-ds/django-hero_ds-0.0.1.tar.gz/django-hero_ds-0.0.1/mytester/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def home(request):
+    context = {
+        "theme": "mentor_ds",
+        "hero": {
+            "bg": ["bg1"],
+            "title": "Say Tell Speak Talk",
+            "slogan": "Global connection starts with improving your speaking skills<br>Let’s get to talking!",
+            "btn": {"title": "Classes", "link": "#classes"}
+        }
+    }
+    return render(request, f"home.html", context)
