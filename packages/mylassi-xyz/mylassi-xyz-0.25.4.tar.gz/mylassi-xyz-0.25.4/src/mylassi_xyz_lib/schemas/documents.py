fb36e714bd6ada709c3757875777963956c188f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:26 +0000

    Add package mylassi-xyz/0.25.4/mylassi-xyz-0.25.4.tar.gz

---
+++ packages/mylassi-xyz/mylassi-xyz-0.25.4.tar.gz/mylassi-xyz-0.25.4/src/mylassi_xyz_lib/schemas/documents.py
@@ -0,0 +1,73 @@
+__all__ = [
+    'CreateProviderOptionData', 'CreateProviderOptionSchema',
+    'ProviderData', 'ProviderSchema', 'ProverListData', 'ProviderListSchema',
+    'ProviderCredentialData', 'ProviderCredentialSchema',
+    'CreateDocumentOptionData', 'CreateDocumentOptionSchema',
+    'UpdateDocumentOptionData', 'UpdateDocumentOptionSchema',
+    'DocumentData', 'DocumentSchema'
+]
+
+from dataclasses import dataclass, field
+from typing import Optional, List
+
+import marshmallow_dataclass
+
+from .users import UserData
+
+
+@dataclass
+class CreateProviderOptionData:
+    name: str = field()
+    type: str = field()
+    credentials: str = field()
+
+
+@dataclass
+class ProviderData:
+    id: str = field()
+    name: str = field()
+    type: str = field()
+
+
+@dataclass
+class ProverListData:
+    length: int = field()
+    page: int = field()
+    pages: int = field()
+    items: List[ProviderData] = field(default_factory=list)
+
+
+@dataclass
+class ProviderCredentialData:
+    id: str = field()
+    name: str = field()
+    type: str = field()
+    credentials: str = field()
+
+
+@dataclass
+class CreateDocumentOptionData:
+    name: str = field()
+
+
+@dataclass
+class UpdateDocumentOptionData:
+    name: str = field()
+
+
+@dataclass
+class DocumentData(UpdateDocumentOptionData):
+    id: str = field()
+    name: str = field()
+
+    owner: Optional[UserData] = field()
+
+
+CreateProviderOptionSchema = marshmallow_dataclass.class_schema(CreateProviderOptionData)()
+ProviderSchema = marshmallow_dataclass.class_schema(ProviderData)()
+ProviderListSchema = marshmallow_dataclass.class_schema(ProverListData)()
+ProviderCredentialSchema = marshmallow_dataclass.class_schema(ProviderCredentialData)()
+
+CreateDocumentOptionSchema = marshmallow_dataclass.class_schema(CreateDocumentOptionData)()
+UpdateDocumentOptionSchema = marshmallow_dataclass.class_schema(UpdateDocumentOptionData)()
+DocumentSchema = marshmallow_dataclass.class_schema(DocumentData)()
