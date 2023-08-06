d1193d1455f3048f5a913864822b99f2dc8f0c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:29 +0000

    Add package notebooker/0.4.2/notebooker-0.4.2.tar.gz

---
+++ packages/notebooker/notebooker-0.4.2.tar.gz/notebooker-0.4.2/notebooker/web/static/notebooker/header.js
@@ -0,0 +1,81 @@
+$(document).ready(() => {
+    $.ajax({
+        url: '/core/version',
+        success: (result) => {
+            $('#versionTitle').text("v" + result.version);
+            $('#versionTitle').show();
+        },
+        error: () => {
+            $('#versionTitle').hide();
+        },
+    });
+    // Check auth is enabled on our host
+    $.ajax({
+        url: '/oauth/health',
+        success: () => {
+            // If enabled, then fetch our login status
+            $.ajax({
+                url: '/core/user_profile',
+                dataType: 'json',
+                success: (result) => {
+                    console.log(result);
+                    const user = result;
+                    if (result.username) {
+                        $('#usernameInfo').text(result.username);
+                        $('.loggedIn').fadeIn();
+                    } else {
+                        $('.loggedOut').fadeIn();
+                    }
+                },
+                error: (jqXHR, textStatus, errorThrown) => {
+                    $('.loggedOut').fadeIn();
+                },
+            });
+        },
+        error: () => {
+            $('#authArea').hide();
+        },
+    });
+    $.ajax({
+        url: '/scheduler/health',
+        success: () => {
+            // Show the status at some point
+        },
+        error: () => {
+            $('#schedulerButton').hide();
+        },
+    });
+
+    const sb = $('.ui.left.sidebar');
+    sb.sidebar({
+        transition: 'overlay',
+    });
+    sb.sidebar('attach events', '#runReport');
+    $('.ui .dropdown').dropdown();
+
+    $('.message .close')
+        .on('click', function () {
+            $(this)
+                .closest('.message')
+                .hide();
+        });
+});
+
+function rerunReport(jobId, rerunUrl) {
+    $.ajax({
+        type: 'POST',
+        url: rerunUrl,
+        dataType: 'json',
+        success(data, status, request) {
+            window.location.href = data.results_url;
+        },
+        error(xhr, textStatus, errorThrown) {
+            $('#errorMsg').text(`${xhr.status} ${textStatus} ${errorThrown}`);
+            $('#errorPopup').show();
+        },
+    });
+}
+
+function cloneReport(cloneUrl) {
+    window.location.href = cloneUrl;
+}
