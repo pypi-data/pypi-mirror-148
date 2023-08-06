790c0c76ae68dfdb77f5088ebe853bc12ce4c876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:18:43 +0000

    Add package espnet-onnx/0.1.4/espnet_onnx-0.1.4.tar.gz

---
+++ packages/espnet-onnx/espnet_onnx-0.1.4.tar.gz/espnet_onnx-0.1.4/espnet_onnx/asr/beam_search/hyps.py
@@ -0,0 +1,60 @@
+from typing import Union
+from typing import Dict
+from typing import List
+from typing import NamedTuple
+from typing import Tuple
+from typing import Any
+from typing import Optional
+
+import numpy as np
+
+
+class Hypothesis(NamedTuple):
+    """Hypothesis data type."""
+
+    yseq: np.ndarray
+    score: Union[float, np.ndarray] = 0
+    scores: Dict[str, Union[float, np.ndarray]] = dict()
+    states: Dict[str, Any] = dict()
+
+    def asdict(self) -> dict:
+        """Convert data to JSON-friendly dict."""
+        return self._replace(
+            yseq=self.yseq.tolist(),
+            score=float(self.score),
+            scores={k: float(v) for k, v in self.scores.items()},
+        )._asdict()
+
+
+class BatchHypothesis(NamedTuple):
+    """Batchfied/Vectorized hypothesis data type."""
+
+    yseq: np.ndarray = np.ndarray([])  # (batch, maxlen)
+    score: np.ndarray = np.ndarray([])  # (batch,)
+    length: np.ndarray = np.ndarray([])  # (batch,)
+    scores: Dict[str, np.ndarray] = dict()  # values: (batch,)
+    states: Dict[str, Dict] = dict()
+
+    def __len__(self) -> int:
+        """Return a batch size."""
+        return len(self.length)
+
+
+class TransducerHypothesis(NamedTuple):
+    """Default hypothesis definition for Transducer search algorithms."""
+
+    score: float
+    yseq: List[int]
+    dec_state: Union[
+        Tuple[np.ndarray, Optional[np.ndarray]],
+        List[Optional[np.ndarray]],
+        np.ndarray,
+    ]
+    lm_state: Union[Dict[str, Any], List[Any]] = None
+
+
+class ExtendedHypothesis(TransducerHypothesis):
+    """Extended hypothesis definition for NSC beam search and mAES."""
+
+    dec_out: List[np.ndarray] = None
+    lm_scores: np.ndarray = None
