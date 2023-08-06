6418e0c09cac5f80b286ee8954255166d2b322af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:26 +0000

    Add package fastapi-azure-auth/3.4.0/fastapi_azure_auth-3.4.0-py3-none-any.whl

---
+++ packages/fastapi-azure-auth/fastapi_azure_auth-3.4.0-py3-none-any.whl/fastapi_azure_auth/__init__.py
@@ -0,0 +1,6 @@
+from fastapi_azure_auth.auth import (  # noqa: F401
+    MultiTenantAzureAuthorizationCodeBearer as MultiTenantAzureAuthorizationCodeBearer,
+    SingleTenantAzureAuthorizationCodeBearer as SingleTenantAzureAuthorizationCodeBearer,
+)
+
+__version__ = '3.3.0'
