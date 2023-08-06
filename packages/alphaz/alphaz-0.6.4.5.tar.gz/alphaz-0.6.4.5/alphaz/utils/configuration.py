a40191fdf672153994f367a5038323da58a59f02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:04 +0000

    Add package alphaz/0.6.4.5/alphaz-0.6.4.5.tar.gz

---
+++ packages/alphaz/alphaz-0.6.4.5.tar.gz/alphaz-0.6.4.5/alphaz/utils/configuration.py
@@ -0,0 +1,17 @@
+import argparse, os
+os.environ["ALPHA_LOG_CMD_OUTPUT"] = "N"
+
+from ..models.config import AlphaConfig
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Load configuration file")
+
+    parser.add_argument("--log", "-l", help="log file path")
+    parser.add_argument("--file", "-f", help="Input configuration file", required=True)
+    parser.add_argument("--configuration", "-c", help="Configuration", default=None)
+    parser.add_argument("--user", "-u", help="User", default=None)
+    parser.add_argument("--path", "-p", help="Input parameter path", required=True)
+
+    args = parser.parse_args()
+
+    config = AlphaConfig(filepath=args.file, configuration=args.configuration, user=args.user)
