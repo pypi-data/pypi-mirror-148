d441987055a0f46aeb43050b27db9ad597940ff3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:28:30 +0000

    Add package brazilian-register/0.0.3/brazilian_register-0.0.3.tar.gz

---
+++ packages/brazilian-register/brazilian_register-0.0.3.tar.gz/brazilian_register-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="brazilian_register",
+    version="0.0.3",
+    author="Phelipe Mathias",
+    author_email="silvamathias@gmail.com",
+    description="Tools to test brazilian register numbers (CPF or CNPJ)",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/silvamathias/dio_desafio_ciacao_pacote_python",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.7',
+)
