3d4367e8aead5373e787138b0ba38998ed72d1da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:17:33 +0000

    Add package lzlla/0.0.3/lzlla-0.0.3.tar.gz

---
+++ packages/lzlla/lzlla-0.0.3.tar.gz/lzlla-0.0.3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='lzlla',
+    version='0.0.3',
+    author='龙·玛卡巴卡的小推车·卓·奥儿’s_ruddy·李/Long·Makabaka`s_bogey_dolly_trolley·Zhuo·Aoer`s_ruddy·Zhuo·Makabaka'
+           '`s_bogey_dolly_trolley·Zhuo·Aoer`s_ruddy·Li',
+    author_email='liuniandexiaohuo@qq.com',
+    description='一个很会玩的第三方库库',
+    packages=setuptools.find_packages(),
+    long_description='text/markdown',
+    url='',
+    classifiers=['Programming Language :: Python :: 3',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent',
+                 ],
+)
