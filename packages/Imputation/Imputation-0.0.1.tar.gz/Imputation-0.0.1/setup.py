379d4a87bfb17fe40ab053bdb6f062a8039c7ce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:26 +0000

    Add package Imputation/0.0.1/Imputation-0.0.1.tar.gz

---
+++ packages/Imputation/Imputation-0.0.1.tar.gz/Imputation-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'This package is used for performing exploratory data analysis'
+LONG_DESCRIPTION = 'This package is used for performing exploratory data analysis. It contains functions to perform imputation, statistical tests'
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="Imputation",
+    version=VERSION,
+    author="Saisakul Chernbumroong",
+    author_email="s.chernbumroong@bham.ac.uk",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[], # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+
+    keywords=['python', 'first package'],
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
