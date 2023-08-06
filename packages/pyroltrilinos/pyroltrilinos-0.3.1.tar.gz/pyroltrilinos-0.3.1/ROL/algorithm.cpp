1a6b47af84753aa660279005cb8417287f66ea5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:35 +0000

    Add package pyroltrilinos/0.3.1/pyroltrilinos-0.3.1.tar.gz

---
+++ packages/pyroltrilinos/pyroltrilinos-0.3.1.tar.gz/pyroltrilinos-0.3.1/ROL/algorithm.cpp
@@ -0,0 +1,17 @@
+#include <pybind11/pybind11.h>
+namespace py = pybind11;
+
+#include <ROL_TypeB_LinMoreAlgorithm.hpp>
+#include <ROL_Bounds.hpp>
+
+void init_algorithm(py::module &m) {
+  py::class_<ROL::TypeB::LinMoreAlgorithm<double>>(m, "LinMoreAlgorithm")
+    .def(py::init<ROL::ParameterList&>())
+    .def(py::init<ROL::ParameterList&, const std::shared_ptr<ROL::Secant<double>>&>())
+    .def("setStatusTest", &ROL::TypeB::LinMoreAlgorithm<double>::setStatusTest)
+    .def("run",
+	 [](ROL::TypeB::LinMoreAlgorithm<double> &instance, ROL::Vector<double> &x,
+	    ROL::Objective<double> &obj, ROL::Bounds<double> &bnd) {
+	   instance.run(x, obj, bnd, std::cout);
+	 });
+}
