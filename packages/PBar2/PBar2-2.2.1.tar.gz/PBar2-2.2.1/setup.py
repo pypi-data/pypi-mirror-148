b79ee7314c72c22bcf58a8d501e20f503dacd813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:47:28 +0000

    Add package PBar2/2.2.1/PBar2-2.2.1.tar.gz

---
+++ packages/PBar2/PBar2-2.2.1.tar.gz/PBar2-2.2.1/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+try:
+	from src.pbar import __version__ as prj_version	# get project version
+except RuntimeError:
+	prj_version = "dev"
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="PBar2",
+	version=prj_version,
+	author="David Losantos (DarviL82)",
+	author_email="davidlosantos89@gmail.com",
+	description="Display customizable progress bars on the terminal easily.",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://darvil82.github.io/PBar",
+	project_urls={
+		"Tracker": "https://github.com/DarviL82/PBar/issues",
+		"Documentation": "https://github.com/DarviL82/PBar/wiki"
+	},
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.9",
+		"Programming Language :: Python :: 3.10",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	package_dir={"": "src"},
+	packages=setuptools.find_packages(where="src"),
+	python_requires=">=3.9",
+)
