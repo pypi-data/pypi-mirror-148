f793f081930bfe8413ef2b445497e1d7129c39f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:23 +0000

    Add package algora-sdk/1.1.18/algora-sdk-1.1.18.tar.gz

---
+++ packages/algora-sdk/algora-sdk-1.1.18.tar.gz/algora-sdk-1.1.18/algoralabs/research/resource.py
@@ -0,0 +1,16 @@
+from algoralabs.common.requests import __get_request, __delete_request
+from algoralabs.decorators.data import data_request
+
+
+@data_request(
+    transformer=lambda data: data,
+    process_response=lambda response: response.content
+)
+def get_resource(id: str):
+    endpoint = f"config/research/resource/{id}/code"
+    return __get_request(endpoint)
+
+
+def delete_resource(id: str):
+    endpoint = f"config/research/resource/{id}"
+    return __delete_request(endpoint)
