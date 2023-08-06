775a049d9422b859ab5bbce067e52286a40ad6ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:16 +0000

    Add package random-quote-generator-93618/0.1.0/random-quote-generator-93618-0.1.0.tar.gz

---
+++ packages/random-quote-generator-93618/random-quote-generator-93618-0.1.0.tar.gz/random-quote-generator-93618-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['random_quote_generator']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'random-quote-generator-93618',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Amir Tadrisi',
+    'author_email': 'amirtds@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
