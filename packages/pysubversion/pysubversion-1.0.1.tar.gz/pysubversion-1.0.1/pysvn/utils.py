6e72cad2d2a312839263459bc7270309b302a722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:41 +0000

    Add package pysubversion/1.0.1/pysubversion-1.0.1.tar.gz

---
+++ packages/pysubversion/pysubversion-1.0.1.tar.gz/pysubversion-1.0.1/pysvn/utils.py
@@ -0,0 +1,39 @@
+'''pysvn utilities module.
+'''
+import subprocess
+from typing import List, Tuple
+from pathlib import Path
+
+def check_svn_installed() -> bool:
+    """Check if the current system has the svn cli client installed.
+
+    Returns:
+        bool: True if svn is installed, otherwise False
+    """
+    try:
+        subprocess.run('svn', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return True
+    except FileNotFoundError:
+        return False
+
+
+def get_longest_line_len(lines: List[str]) -> int:
+    longest_len = 0
+    for line in lines:
+        if len(line) > longest_len:
+            longest_len = len(line)
+    return longest_len
+
+
+def get_output(cmd: subprocess.Popen) -> Tuple[str, str]:
+    """Get stdout and stderr from a finished process.
+
+    Args:
+        cmd (subprocess.Popen): process
+
+    Returns:
+        Tuple[str, str]: stdout, stderr
+    """
+    stdout = cmd.stdout.read().decode('utf-8').strip()
+    stderr = cmd.stderr.read().decode('utf-8').strip()
+    return stdout, stderr
---
+++ packages/pysubversion/pysubversion-1.0.1.tar.gz/pysubversion-1.0.1/setup.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pysubversion",
+    version="1.0.1",
+    keywords=["svn", "pysvn", "subversion"],
+    description="Svn for python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="BSD License",
+    url="https://github.com/ryanbender2/pysvn",
+    author="Ryan Bender",
+    author_email="ryan.bender@cfacorp.com",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    include_package_data=True,
+    platforms="any",
+    classifiers={
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+    },
+    # install_requires = ["codecs"]
+)
