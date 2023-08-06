6ef988a053cf24a197833ae2dd166e94943449aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:35 +0000

    Add package cdk-tweet-queue/1.0.238/cdk_tweet_queue-1.0.238-py3-none-any.whl

---
+++ packages/cdk-tweet-queue/cdk_tweet_queue-1.0.238-py3-none-any.whl/cdk_tweet_queue/_jsii/__init__.py
@@ -0,0 +1,29 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_dynamodb._jsii
+import aws_cdk.aws_events._jsii
+import aws_cdk.aws_events_targets._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_lambda_nodejs._jsii
+import aws_cdk.aws_sqs._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-tweet-queue", "1.0.238", __name__[0:-6], "cdk-tweet-queue@1.0.238.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
