19debf4a66abf0e019f91dea8c25dea2170f1f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:19 +0000

    Add package three-py/0.0.2/three_py-0.0.2-py3-none-any.whl

---
+++ packages/three-py/three_py-0.0.2-py3-none-any.whl/three/__init__.py
@@ -0,0 +1,16 @@
+#This module is derived from three.js, please check the source code of three.js for the specific algorithm
+
+from .core import *
+from .structure import *
+from .math import *
+from .objects import *
+from .camera import *
+from .renderer import *
+from .constants import *
+from .materials import *
+from .geometries import *
+from .lights import *
+from .controls import *
+from .helpers import *
+
+__version__ = "0.0.2"
---
+++ packages/three-py/three_py-0.0.2-py3-none-any.whl/three/structure/none_attribute.py
@@ -0,0 +1,31 @@
+def _isSubType(cls_name:str, cls: type):
+    if cls_name == cls.__name__:
+        return True
+    
+    for base in cls.__bases__: 
+        if _isSubType(cls_name, base):
+            return True
+
+    return False
+
+class NoneAttribute:
+    def __getattr__(self, name:str):
+        # TODO someway better?
+        if name.startswith('is'):
+            cls_name = name[2:]
+            return _isSubType(cls_name, self.__class__)
+        return None
+
+    @property
+    def type(self):
+        if hasattr(self, '_type'):
+            return self._type
+        return self.__class__.__name__
+
+    @type.setter
+    def type(self, type):
+        self._type = type
+
+
+    def __hash__(self) -> int:
+        return id(self)
