a00541e56cb1e878b47d3c5b97782185473dd17a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:09 +0000

    Add package alphaz/0.6.2.1/alphaz-0.6.2.1.tar.gz

---
+++ packages/alphaz/alphaz-0.6.2.1.tar.gz/alphaz-0.6.2.1/alphaz/models/database/operators.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+class Operators(Enum):
+    EQUAL = "=="
+    ASIGN = "="
+    DIFFERENT = "!="
+    NOT = "!"
+    LIKE = "like"
+    NOT_LIKE = "notlike"
+    ILIKE = "ilike"
+    NOT_ILIKE = "notlike"
+    SUPERIOR = ">"
+    INFERIOR = "<"
+    SUPERIOR_OR_EQUAL = ">="
+    INFERIOR_OR_EQUAL = "<="
+    IN = "in"
+    NOT_IN = "notin"
+    HAS = "has"
+    ANY = "any"
+
+    def equals(self, val):
+        if type(val) == str:
+            return self.value == val
+        elif type(val) == Operators:
+            return self.value == val.value
+        return False
