f21b9d10fdde2313c486489a19338b31fcc3679e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:18 +0000

    Add package palliate/0.0.1/palliate-0.0.1.tar.gz

---
+++ packages/palliate/palliate-0.0.1.tar.gz/palliate-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='palliate',
+    version='0.0.1',
+    description='palliate library',
+    long_description='Library to control a palliate instance',
+    url='https://github.com/palliate/palliate-python',
+    author='Tsche',
+    author_email='contact@palliate.io',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Development Status :: 1 - Planning'
+    ],
+    project_urls={
+        "Bug Tracker": "https://github.com/palliate/palliate-python/issues",
+    },
+    packages=[],
+)
