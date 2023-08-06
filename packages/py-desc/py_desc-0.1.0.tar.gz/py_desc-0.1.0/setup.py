fcdba16b0f34f26524fd2df23e928406347c865a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:22:22 +0000

    Add package py-desc/0.1.0/py_desc-0.1.0.tar.gz

---
+++ packages/py-desc/py_desc-0.1.0.tar.gz/py_desc-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='py_desc',
+    version='0.1.0',
+    packages=['py_desc'],
+    url='https://github.com/syth0le/py_desc',
+    license='Apache 2.0',
+    author='Cherednichenko Daniil',
+    author_email='syth0le565@gmail.com',
+    description='A simple fields descriptors tool'
+)
