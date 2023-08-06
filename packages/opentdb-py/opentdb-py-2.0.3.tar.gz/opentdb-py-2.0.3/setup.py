45e647d8ea7f7790fcd365da7fd713cb5ff45b71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:54 +0000

    Add package opentdb-py/2.0.3/opentdb-py-2.0.3.tar.gz

---
+++ packages/opentdb-py/opentdb-py-2.0.3.tar.gz/opentdb-py-2.0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", 'r', encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="opentdb-py",
+    version="2.0.3",
+    author="Marseel Eeso",
+    author_email="marseeleeso@gmail.com",
+    description="Python wrapper for the open-trivia-database API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Marseel-E/opentdb-py",
+    project_urls={
+        "Bug Tracker": "https://github.com/Marseel-E/opentdb-py/issues"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "trivia"},
+    packages=setuptools.find_packages(where="trivia"),
+    python_requires=">=3.8",
+)
