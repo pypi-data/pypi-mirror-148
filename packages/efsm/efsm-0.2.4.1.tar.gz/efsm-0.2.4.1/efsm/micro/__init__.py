ea79045a2c09c02c3951b2a2b745958f93fd3c5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:22 +0000

    Add package efsm/0.2.4.1/efsm-0.2.4.1.tar.gz

---
+++ packages/efsm/efsm-0.2.4.1.tar.gz/efsm-0.2.4.1/efsm/micro/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from efsm.micro.core import *
+
+"""
+a branch version specific for micropython or other streamlined case.
+
+Only provide a single statemachine with the same usage.
+This single statemachine is made up of variables and functions.
+
+I adjust you using 'import efsm.micro as msm', wich mean you can use this module as a StateMachine object.
+
+
+"""
+
+
+
