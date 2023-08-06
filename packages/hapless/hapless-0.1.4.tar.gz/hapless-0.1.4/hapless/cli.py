50783ec6bb0d32c73da32baa979555d2d754bcc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:54 +0000

    Add package hapless/0.1.4/hapless-0.1.4.tar.gz

---
+++ packages/hapless/hapless-0.1.4.tar.gz/hapless-0.1.4/hapless/cli.py
@@ -0,0 +1,100 @@
+import sys
+from typing import Optional
+
+import click
+from rich.console import Console
+
+from hapless import config
+from hapless.main import Hapless
+
+try:
+    from shlex import join as shlex_join
+except ImportError:
+    # Fallback for Python 3.7
+    from hapless.utils import shlex_join_backport as shlex_join
+
+console = Console(highlight=False)
+hapless = Hapless()
+
+
+def get_or_exit(hap_alias: str):
+    hap = hapless.get_hap(hap_alias)
+    if hap is None:
+        console.print(
+            f"{config.ICON_INFO} No such hap: {hap_alias}",
+            style=f"{config.COLOR_ERROR} bold",
+        )
+        sys.exit(1)
+    return hap
+
+
+@click.group(invoke_without_command=True)
+@click.version_option(message="hapless, version %(version)s")
+@click.option("-v", "--verbose", is_flag=True, default=False)
+@click.pass_context
+def cli(ctx, verbose):
+    if ctx.invoked_subcommand is None:
+        _status(None, verbose=verbose)
+
+
+@cli.command()
+@click.argument("hap_alias", metavar="hap", required=False)
+@click.option("-v", "--verbose", is_flag=True, default=False)
+def status(hap_alias, verbose):
+    _status(hap_alias, verbose)
+
+
+@cli.command()
+@click.argument("hap_alias", metavar="hap", required=False)
+@click.option("-v", "--verbose", is_flag=True, default=False)
+def show(hap_alias, verbose):
+    _status(hap_alias, verbose)
+
+
+def _status(hap_alias: Optional[str] = None, verbose: bool = False):
+    if hap_alias is not None:
+        hap = get_or_exit(hap_alias)
+        hapless.show(hap, verbose=verbose)
+    else:
+        haps = hapless.get_haps()
+        hapless.stats(haps, verbose=verbose)
+
+
+@cli.command()
+@click.argument("hap_alias", metavar="hap")
+@click.option("-f", "--follow", is_flag=True, default=False)
+def logs(hap_alias, follow):
+    hap = get_or_exit(hap_alias)
+    hapless.logs(hap, follow=follow)
+
+
+@cli.command()
+@click.option("--skip-failed", is_flag=True, default=False)
+def clean(skip_failed):
+    hapless.clean(skip_failed)
+
+
+@cli.command()
+@click.argument("cmd", nargs=-1)
+@click.option("-n", "--name")
+@click.option("--check", is_flag=True, default=False)
+def run(cmd, name, check):
+    hapless.run(shlex_join(cmd), name=name, check=check)
+
+
+@cli.command()
+@click.argument("hap_alias", metavar="hap")
+def pause(hap_alias):
+    hap = get_or_exit(hap_alias)
+    hapless.pause_hap(hap)
+
+
+@cli.command()
+@click.argument("hap_alias", metavar="hap")
+def resume(hap_alias):
+    hap = get_or_exit(hap_alias)
+    hapless.resume_hap(hap)
+
+
+if __name__ == "__main__":
+    cli()
---
+++ packages/hapless/hapless-0.1.4.tar.gz/hapless-0.1.4/hapless/utils.py
@@ -0,0 +1,59 @@
+import logging
+import shlex
+import time
+from functools import wraps
+from pathlib import Path
+
+from hapless import config
+
+logging.disable(level=logging.CRITICAL)
+
+if config.DEBUG:
+    logging.disable(logging.NOTSET)
+    logging.basicConfig(level=logging.DEBUG)
+
+
+logger = logging.getLogger(__package__)
+
+
+def allow_missing(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except FileNotFoundError:
+            pass
+
+    return wrapper
+
+
+def shlex_join_backport(split_command):
+    """Return a shell-escaped string"""
+    return " ".join(shlex.quote(arg) for arg in split_command)
+
+
+class timed(object):
+    def __init__(self):
+        self._start = time.time()
+        self._end = None
+
+    @property
+    def elapsed(self):
+        if self._end is not None:
+            return self._end - self._start
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self._end = time.time()
+        return self
+
+
+def wait_created(
+    path: Path, interval: float = 0.1, timeout: float = config.FAILFAST_DELAY
+):
+    start = time.time()
+    while not path.exists() and time.time() - start < timeout:
+        time.sleep(interval)
+    return path.exists()
