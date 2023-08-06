bcf1454d6b3de768f60ca18ad0d3bf9de00f1373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:46 +0000

    Add package toolgui/0.0.10/toolgui-0.0.10.tar.gz

---
+++ packages/toolgui/toolgui-0.0.10.tar.gz/toolgui-0.0.10/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="toolgui",
+    version="0.0.10",
+    description="Modular event-driven GUI system for quickly building tools with Python and pyimgui.",
+    packages=find_packages(),
+    package_data={'': ['Roboto-Regular.ttf']},
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rempelj/toolgui",
+    author="Justin Rempel",
+    classifiers=[
+    ],
+    install_requires=[
+        "imgui[glfw]",
+    ],
+    extras_require={
+        "dev": [
+            "twine",
+            "wheel",
+        ],
+    },
+)
