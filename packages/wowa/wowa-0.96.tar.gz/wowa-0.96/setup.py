21da83d3d10bfc26b0d22521359dea9ac6bb6405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:49 +0000

    Add package wowa/0.96/wowa-0.96.tar.gz

---
+++ packages/wowa/wowa-0.96.tar.gz/wowa-0.96/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: GNU LESSER GENERAL PUBLIC LICENSE Version 3, 29 June 2007"
+]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup( 
+    name="wowa",
+    version="0.96",
+    description="Calculate weighted OWA functions and extending bivariate means",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="",
+    author='Gleb Beliakov, Norbert Henseler',
+    author_email='gleb.beliakov@deakin.edu.au, norbert.henseler@deakin.edu.au',
+    include_license_file=True,
+    license_file='LICENSE',
+    py_modules=['wowa'],
+    package_dir={'': 'src'},
+    install_requires=['cffi>=1.0.0'],
+    setup_requires=['cffi>=1.0.0'],
+    cffi_modules=['./src/buildWowa.py:ffibuilder'],
+    include_package_data=True,
+    package_data={'':['tests/test.py']},
+)
+
+    
+    
