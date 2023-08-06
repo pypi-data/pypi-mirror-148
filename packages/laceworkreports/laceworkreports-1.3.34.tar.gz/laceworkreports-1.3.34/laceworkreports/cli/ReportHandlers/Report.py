92589581938beca3d2b1c55fe9d06b81e2f1d64a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:16 +0000

    Add package laceworkreports/1.3.34/laceworkreports-1.3.34.tar.gz

---
+++ packages/laceworkreports/laceworkreports-1.3.34.tar.gz/laceworkreports-1.3.34/laceworkreports/cli/ReportHandlers/Report.py
@@ -0,0 +1,43 @@
+import typer
+
+from laceworkreports import common
+
+from .AgentCoverageHandler import AgentCoverageHandler
+from .ComplianceCoverageHandler import ComplianceCoverageHandler
+from .ContainerVulnerabilityCoverageHandler import ContainerVulnerabilityCoverageHandler
+from .VpcChartHandler import VpcChartHandler
+from .VulnerabilityCoverageHandler import VulnerabilityCoverageHandler
+
+app = typer.Typer(no_args_is_help=True)
+
+commands = [
+    {"command_name": "agent-coverage", "command_type": AgentCoverageHandler.app},
+    {
+        "command_name": "compliance-coverage",
+        "command_type": ComplianceCoverageHandler.app,
+    },
+    {
+        "command_name": "vpc-chart",
+        "command_type": VpcChartHandler.app,
+    },
+    {
+        "command_name": "vulnerability-coverage",
+        "command_type": VulnerabilityCoverageHandler.app,
+    },
+    {
+        "command_name": "container-vulnerability-coverage",
+        "command_type": ContainerVulnerabilityCoverageHandler.app,
+    },
+]
+
+for command in commands:
+    app.add_typer(
+        command["command_type"],
+        name=command["command_name"],
+        help=f"Generate {command['command_name']} report",
+        no_args_is_help=True,
+        epilog=f"{common.config.name} export {command['command_name']} <exporttype> [OPTIONS]",
+    )
+
+if __name__ == "__main__":
+    app()
