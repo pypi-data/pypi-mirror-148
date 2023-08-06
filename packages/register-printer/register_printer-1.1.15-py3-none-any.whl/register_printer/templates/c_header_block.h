af9076b86c0fa67e97f1706416b7120c6e370273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:17:14 +0000

    Add package register-printer/1.1.15/register_printer-1.1.15-py3-none-any.whl

---
+++ packages/register-printer/register_printer-1.1.15-py3-none-any.whl/register_printer/templates/c_header_block.h
@@ -0,0 +1,26 @@
+#ifndef __REGS_{{ block_type | upper }}_H__
+#define __REGS_{{ block_type | upper }}_H__
+
+{% for c_struct in c_structs %}
+typedef struct {
+{% for struct_field in c_struct.struct_fields %}
+    {{ "%-24s\t%-24s\t;" | format(struct_field.type, struct_field.name) }}
+{% endfor %}
+} {{ c_struct.name | upper}};
+
+{% endfor %}
+typedef struct
+{
+{% for struct_field in struct_fields %}
+    {{ "%-24s\t%-24s\t;" | format(struct_field.type, struct_field.name) }}
+{% endfor %}
+} {{ block_type | upper }}_TypeDef;
+
+
+{% for pos_mask_macro in pos_mask_macros %}
+#define    {{ "%-64s" | format(pos_mask_macro.prefix + "_Pos") }} {{ pos_mask_macro.pos_value }}
+#define    {{ "%-64s" | format(pos_mask_macro.prefix + "_Msk") }} ({{ "0x%xU" | format(pos_mask_macro.mask_value) }} << {{ pos_mask_macro.prefix }}_Pos)
+
+{% endfor %}
+
+#endif
