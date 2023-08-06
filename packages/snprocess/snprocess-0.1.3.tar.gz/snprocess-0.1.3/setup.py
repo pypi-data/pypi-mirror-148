76a908ca5d91fb90836910a308f3d08b8cb90958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:41 +0000

    Add package snprocess/0.1.3/snprocess-0.1.3.tar.gz

---
+++ packages/snprocess/snprocess-0.1.3.tar.gz/snprocess-0.1.3/setup.py
@@ -0,0 +1,31 @@
+"""
+Python package configuration for scripts.
+
+Thomas Dokas <dokastho@umich.edu>
+"""
+
+from setuptools import find_packages, setup
+
+setup(
+    name='snprocess',
+    version='0.1.3',
+    packages=find_packages(include=['snprocess', 'qc', 'templates'], exclude=['testout']),
+    include_package_data=True,
+    install_requires=[
+        'pycodestyle',
+        'pydocstyle',
+        'pylint',
+        'pytest',
+        'pytest-mock',
+        'click',
+        'matplotlib',
+        'pandas',
+        'jinja2',
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'snprocess = snprocess.__main__:main'
+        ]
+    },
+)
