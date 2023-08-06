94575c52a179a8af0cf715cbf9886ea165a73402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:05 +0000

    Add package basicreadme/1.0.0/basicreadme-1.0.0.tar.gz

---
+++ packages/basicreadme/basicreadme-1.0.0.tar.gz/basicreadme-1.0.0/README.txt
@@ -0,0 +1,9 @@
+# basicreadme
+`
+XDC3PYTHON SDK with support for smart contracts, XRC20 & XRC721.
+`
+
+### Usage
+`
+This example returns symbol of the specified address.
+
---
+++ packages/basicreadme/basicreadme-1.0.0.tar.gz/basicreadme-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name = 'basicreadme',
+    version = '1.0.0',
+    description = 'basic',
+    long_description=open('README.txt').read(),
+    url='',
+    author='saurabh',
+    license='MIT',
+    keywords='',
+    packages=find_packages(),
+)
