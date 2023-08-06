171d9745fd8fadf078718f61525d00b5ff2b49cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:21:42 +0000

    Add package samplics/0.3.18/samplics-0.3.18.tar.gz

---
+++ packages/samplics/samplics-0.3.18.tar.gz/samplics-0.3.18/src/samplics/sampling/__init__.py
@@ -0,0 +1,29 @@
+from samplics.sampling.selection import SampleSelection
+from samplics.sampling.size import (
+    SampleSize,
+    SampleSizeMeanOneSample,
+    SampleSizePropOneSample,
+    allocate,
+    calculate_power,
+    power_for_proportion,
+    sample_size_for_mean_wald,
+    sample_size_for_proportion_fleiss,
+    sample_size_for_proportion_wald,
+)
+from samplics.sampling.size_and_power import power_for_one_mean, power_for_one_proportion
+
+
+__all__ = [
+    "allocate",
+    "calculate_power",
+    "power_for_proportion",
+    "power_for_one_proportion",
+    "power_for_one_mean",
+    "SampleSelection",
+    "SampleSize",
+    "SampleSizeMeanOneSample",
+    "SampleSizePropOneSample",
+    "sample_size_for_mean_wald",
+    "sample_size_for_proportion_fleiss",
+    "sample_size_for_proportion_wald",
+]
