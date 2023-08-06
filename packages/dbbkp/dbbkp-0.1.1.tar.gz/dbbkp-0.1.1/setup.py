eba838b40b7e2f3147c92c5577549fbad797c92a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:36 +0000

    Add package dbbkp/0.1.1/dbbkp-0.1.1.tar.gz

---
+++ packages/dbbkp/dbbkp-0.1.1.tar.gz/dbbkp-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dbbkp',
+      version='0.1.1',
+      description='dbbkp Package',
+      packages = ['dbbkp'],
+      install_requires = ["numpy","pandas","matplotlib"],
+      zip_safe = False,
+      )
