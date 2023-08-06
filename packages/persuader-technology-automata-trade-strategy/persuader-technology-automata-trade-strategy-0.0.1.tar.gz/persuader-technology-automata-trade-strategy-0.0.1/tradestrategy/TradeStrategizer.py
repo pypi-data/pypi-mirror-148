a775d85ccb3675349444251fa119d6480d61a673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:50:48 +0000

    Add package persuader-technology-automata-trade-strategy/0.0.1/persuader-technology-automata-trade-strategy-0.0.1.tar.gz

---
+++ packages/persuader-technology-automata-trade-strategy/persuader-technology-automata-trade-strategy-0.0.1.tar.gz/persuader-technology-automata-trade-strategy-0.0.1/tradestrategy/TradeStrategizer.py
@@ -0,0 +1,8 @@
+from core.oracle.Prediction import Prediction
+from core.position.Position import Position
+
+
+class TradeStrategizer:
+
+    def perform_trade_strategy(self, position: Position, prediction: Prediction):
+        pass
