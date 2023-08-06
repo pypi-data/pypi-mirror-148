55448c9f8ece0596662f48a95d83d7834b915640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:16:54 +0000

    Add package mayan-edms/4.2.4/mayan_edms-4.2.4-py2.py3-none-any.whl

---
+++ packages/mayan-edms/mayan_edms-4.2.4-py2.py3-none-any.whl/mayan/apps/dynamic_search/queues.py
@@ -0,0 +1,32 @@
+from django.utils.translation import ugettext_lazy as _
+
+from mayan.apps.task_manager.classes import CeleryQueue
+from mayan.apps.task_manager.workers import worker_b
+
+queue_search = CeleryQueue(
+    label=_('Search'), name='search', worker=worker_b
+)
+queue_search_slow = CeleryQueue(
+    label=_('Search slow'), name='search_slow', worker=worker_b
+)
+
+queue_search.add_task_type(
+    dotted_path='mayan.apps.dynamic_search.tasks.task_deindex_instance',
+    label=_('Remove a model instance from the search engine.'),
+    name='task_deindex_instance',
+)
+queue_search.add_task_type(
+    dotted_path='mayan.apps.dynamic_search.tasks.task_index_instance',
+    label=_('Index a model instance to the search engine.'),
+    name='task_index_instance',
+)
+queue_search.add_task_type(
+    dotted_path='mayan.apps.dynamic_search.tasks.task_index_search_model',
+    label=_('Index all instances of a search model to the search engine.'),
+    name='task_index_search_model',
+)
+queue_search_slow.add_task_type(
+    dotted_path='mayan.apps.dynamic_search.tasks.task_reindex_backend',
+    label=_('Reset the search backend indices and index all instances again.'),
+    name='task_reindex_backend',
+)
