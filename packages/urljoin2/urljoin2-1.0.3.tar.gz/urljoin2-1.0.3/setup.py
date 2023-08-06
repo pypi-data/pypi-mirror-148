d0c42d76396c6e423a33775ed0e2c436456831b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:46 +0000

    Add package urljoin2/1.0.3/urljoin2-1.0.3.tar.gz

---
+++ packages/urljoin2/urljoin2-1.0.3.tar.gz/urljoin2-1.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+      name="urljoin2",
+      version="1.0.3",
+      description="Module for joining strings to form urls.",
+      author="Nagy Zoltán",
+      author_email="zolika3400@gmail.com",
+      packages=find_packages('.'),
+      package_dir={'': '.'}
+      )
