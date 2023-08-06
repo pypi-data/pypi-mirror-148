f2a649766c3b5e4561f26956dc0d3c2dd7d218d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:57 +0000

    Add package urlrecode/0.0.1/urlrecode-0.0.1.tar.gz

---
+++ packages/urlrecode/urlrecode-0.0.1.tar.gz/urlrecode-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='urlrecode',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/MKaterbarg/urlencode',
+    license='MIT',
+    author='Martijn Katerbarg',
+    author_email='martijnkaterbarg@gmail.com',
+    description='Simple URLEncode and URLDecode'
+)
