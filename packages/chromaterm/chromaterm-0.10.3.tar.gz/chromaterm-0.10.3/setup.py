87c250078cabc5e230fdeff13ec0ce580d146f91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:41 +0000

    Add package chromaterm/0.10.3/chromaterm-0.10.3.tar.gz

---
+++ packages/chromaterm/chromaterm-0.10.3.tar.gz/chromaterm-0.10.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name='chromaterm',
+    author='hSaria',
+    author_email='sariahajjar@gmail.com',
+    classifiers=[
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3', 'Topic :: Terminals',
+        'Topic :: Utilities'
+    ],
+    description='Color your Terminal with Regex!',
+    entry_points={'console_scripts': ['ct = chromaterm.__main__:main']},
+    install_requires=['psutil', 'PyYAML>=5.1'],
+    license='MIT',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    packages=['chromaterm'],
+    python_requires='>=3.6.0',
+    url='https://github.com/hSaria/ChromaTerm',
+    version='0.10.3',
+)
