e574f201aa6568c241414b25aa8ff3c2eb080c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:21 +0000

    Add package forte.vader/0.0.3/forte.vader-0.0.3.tar.gz

---
+++ packages/forte.vader/forte.vader-0.0.3.tar.gz/forte.vader-0.0.3/setup.py
@@ -0,0 +1,40 @@
+import sys
+from pathlib import Path
+
+import setuptools
+
+readme = Path("README.md")
+if readme.exists():
+    long_description = (Path("README.md")).read_text()
+else:
+    long_description = (
+        "Provide Forte implementations of a fantastic collection of NLP tools."
+    )
+
+if sys.version_info < (3, 6):
+    sys.exit("Python>=3.6 is required by Forte.")
+
+setuptools.setup(
+    name="forte.vader",
+    version="0.0.3",
+    url="https://github.com/asyml/forte-wrappers/tree/main/src/vader",
+    description="Provide Forte implementations of a fantastic collection of "
+    "NLP tools.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="Apache License Version 2.0",
+    packages=["fortex.vader"],
+    namespace_packages=["fortex"],
+    include_package_data=True,
+    platforms="any",
+    install_requires=["forte==0.2.0", "vaderSentiment==3.2.1", "requests"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
