1d65ffffec487c624e170f8779790776a423324e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:54 +0000

    Add package bpyutils/0.5.0/bpyutils-0.5.0.tar.gz

---
+++ packages/bpyutils/bpyutils-0.5.0.tar.gz/bpyutils-0.5.0/src/bpyutils/util/_dict.py
@@ -0,0 +1,69 @@
+# imports - standard imports
+import collections
+
+from bpyutils._compat import iteritems
+
+def merge_dict(*args):
+    """
+    Merge Dictionaries.
+    
+    :param args: arguments of dictionaries to be merged. `merge_dict` will override keys from right to left.
+
+    :returns: dict
+
+    Example::
+    
+        >>> bpy.merge_dict({ 'foo': 'bar' }, { 'bar': 'baz' }, { 'baz': 'boo' })
+        {'foo': 'bar', 'bar': 'baz', 'baz': 'boo'}
+        >>> bpy.merge_dict({ 'foo': 'bar' }, { 'foo': 'baz', 'bar': 'boo' })
+        {'foo': 'baz', 'bar': 'boo'}
+    """
+    merged = dict()
+
+    for arg in args:
+        copy = arg.copy()
+        merged.update(copy)
+
+    return merged
+
+def dict_from_list(keys, values):
+    """
+    Generate a dictionary from a list of keys and values.
+
+    :param keys: A list of keys.
+    :param values: A list of values.
+
+    :returns: dict
+
+    Example::
+
+        >>> bpy.dict_from_list(['a', 'b', 'c'], [1, 2, 3])
+        {'a': 1, 'b': 2, 'c': 3}
+    """
+    return dict(zip(keys, values))
+
+class AutoDict(collections.defaultdict):
+    __repr__ = dict.__repr__
+
+def autodict(*args, **kwargs):
+    """
+    Automatically adds a key to a dictionary.
+
+    Example::
+
+        >>> d = bpy.autodict()
+        >>> d['foo']['bar']['baz'] = 'boo'
+        {'foo': {'bar': {'baz': 'boo'}}}
+    """
+    _autodict = AutoDict(autodict)
+    update    = dict(*args, **kwargs)
+
+    for key, value in iteritems(update):
+        if isinstance(value, collections.Mapping):
+            value = autodict(value)
+        
+        _autodict.update({
+            key: value
+        })
+    
+    return _autodict
