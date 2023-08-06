f9d3b44fa4e54ce8366fab316de7c9383992356f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:20 +0000

    Add package xsprofile/0.0.2/xsprofile-0.0.2.tar.gz

---
+++ packages/xsprofile/xsprofile-0.0.2.tar.gz/xsprofile-0.0.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name="xsprofile",
+    version="0.0.2",
+    author="Will Conley",
+    author_email="EcoRioGeo@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: Microsoft :: Windows :: Windows 10"
+        ],
+    py_modules=[],
+    package_dir={'':'src'},
+    url="https://gitlab.com/Geomorph/xsprofile",
+    python_requires=">=3.9",
+    install_requires=[
+        "geopandas",
+        "rasterio",
+        "matplotlib"
+        ]
+)
