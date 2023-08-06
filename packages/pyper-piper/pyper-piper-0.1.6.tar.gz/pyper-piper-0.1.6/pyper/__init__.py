2d7eb911441c00188df981bc21e50a350c59ea40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:42 +0000

    Add package pyper-piper/0.1.6/pyper-piper-0.1.6.tar.gz

---
+++ packages/pyper-piper/pyper-piper-0.1.6.tar.gz/pyper-piper-0.1.6/pyper/__init__.py
@@ -0,0 +1,6 @@
+"""Pyper is a python implementation of the piper networking protocol.
+Piper: https://github.com/Luminoso-256/piper
+Pyper: https://github.com/Bigjango13/pyper"""
+from pyper import client, common, server
+
+__all__ = ("client", "server", "commom")
