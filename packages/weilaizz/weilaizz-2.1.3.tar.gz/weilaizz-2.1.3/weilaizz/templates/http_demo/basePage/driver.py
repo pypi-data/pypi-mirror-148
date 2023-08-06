95365ccd631bfcb43207d796efbf33bfc3bb858f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:58 +0000

    Add package weilaizz/2.1.3/weilaizz-2.1.3.tar.gz

---
+++ packages/weilaizz/weilaizz-2.1.3.tar.gz/weilaizz-2.1.3/weilaizz/templates/http_demo/basePage/driver.py
@@ -0,0 +1,51 @@
+# _*_ coding: utf-8 _*_
+# @Author : 魏来
+# @Version：基础版本(初级UI自动化框架)
+# -------**---**-------
+
+
+
+
+from selenium.webdriver import Remote
+from selenium import webdriver
+from loguru import logger
+from common.base_path import _dir
+from common import _read_yaml
+from weilaizz.templates.http_demo.downloadChromeDriver.downloadBrowser import get_chrome_driver
+
+def browser_init():
+    """定义浏览器类型并返回driver对象"""
+    web_cfg_data = _read_yaml.read_yaml()['browser']
+    if web_cfg_data['type'] == 'chrome':
+        if web_cfg_data['env'] == 'localhost':
+            driver = get_chrome_driver()
+        elif web_cfg_data['env'] == 'grid':
+            driver = Remote(command_executor=web_cfg_data['gridUrl'],
+                            desired_capabilities={
+                                "browserName": "chrome",
+                            })
+        else:
+            raise ValueError('env类型定义错误！')
+    #
+    # elif web_cfg_data['type'] == 'firefox':
+    #     if web_cfg_data['env'] == 'localhost':
+    #         driver = webdriver.Firefox()
+    #     else:
+    #         raise ValueError('env类型定义错误！')
+    # elif web_cfg_data['type'] == 'ie':
+    #     if web_cfg_data['env'] == 'localhost':
+    #         driver = webdriver.Ie()
+    #     elif web_cfg_data['env'] == 'grid':
+    #         driver = Remote(command_executor=web_cfg_data['gridUrl'],
+    #                         desired_capabilities={
+    #                             "browserName": "internet explorer",
+    #                         })
+    #     else:
+    #         raise ValueError('env类型定义错误！')
+    # else:
+    #     raise ValueError('driver驱动类型定义错误！')
+    # logger.info(f'在{web_cfg_data["env"]}使用{web_cfg_data["type"]}执行')
+    # driver.maximize_window()
+
+
+    return driver
---
+++ packages/weilaizz/weilaizz-2.1.3.tar.gz/weilaizz-2.1.3/weilaizz/templates/http_demo/common/readYaml.py
@@ -0,0 +1,42 @@
+# _*_ coding: utf-8 _*_
+# @Author : 魏来
+# @Version：基础版本(初级UI自动化框架)
+# -------**---**-------
+
+
+
+import yaml
+import os
+from common.base_path import _dir
+from loguru import logger
+
+def read_yaml():
+    """读取yaml文件并return文件"""
+    file_dir = _dir + '/config/cfg.yaml'.replace(r'\/'.replace(os.sep, ''), os.sep)
+    with open(file_dir, mode='r', encoding='utf-8') as file:
+        msg = yaml.load(file, Loader=yaml.FullLoader)
+    return msg
+
+
+def load_yaml(file_path):
+    """读取yaml并return"""
+    logger.info("加载 {} 文件......".format(file_path))
+    with open(file_path, mode='r', encoding='utf-8') as file:
+        data = yaml.load(file, Loader=yaml.FullLoader)
+    return data
+
+
+def load(file_name, path=_dir):
+    """在项目内读取指定文件"""
+    ls = os.listdir(path)
+    for i in ls:
+        c_path = os.path.join(path, i)
+        if os.path.isdir(c_path):
+            data = load(file_name, c_path)
+            if data is not None:
+                return data
+        elif file_name == i:
+            data = load_yaml(c_path)
+            return data
+
+
