3c82720981d1fd10aa0cd6fe0712a51fc618da2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:16 +0000

    Add package docarray/0.13.0/docarray-0.13.0.tar.gz

---
+++ packages/docarray/docarray-0.13.0.tar.gz/docarray-0.13.0/docarray/__init__.py
@@ -0,0 +1,17 @@
+__version__ = '0.13.0'
+
+import os
+
+from .document import Document
+from .array import DocumentArray
+from .dataclasses import dataclass, field
+
+if 'DA_NO_RICH_HANDLER' not in os.environ:
+    from rich.traceback import install
+
+    install()
+
+if 'NO_VERSION_CHECK' not in os.environ:
+    from .helper import is_latest_version
+
+    is_latest_version()
