c7cca28bb66913de174fc9aa5da06bf8eca62d16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:21:34 +0000

    Add package fairlib/0.0.3/fairlib-0.0.3-py3-none-any.whl

---
+++ packages/fairlib/fairlib-0.0.3-py3-none-any.whl/fairlib/__init__.py
@@ -0,0 +1,17 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+try:
+    from .src.base_options import BaseOptions
+    from .src import analysis
+    from .src import dataloaders
+    from .src import evaluators
+    from .src import networks
+    from .src import utils
+except:
+    from src.base_options import BaseOptions
+    from src import analysis
+    from src import dataloaders
+    from src import evaluators
+    from src import networks
+    from src import utils
