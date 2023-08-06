8052591798bafc42bce3ac6e8fd515188482b9fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:16:08 +0000

    Add package rssbot/102/rssbot-102.tar.gz

---
+++ packages/rssbot/rssbot-102.tar.gz/rssbot-102/rssbot/rpt.py
@@ -0,0 +1,24 @@
+# This file is placed in the Public Domain.
+
+
+"repeater"
+
+
+from thr import launch
+
+
+from rssbot.tmr import Timer
+
+
+def __dir__():
+    return (
+        "Repeater",
+    )
+
+
+class Repeater(Timer):
+
+    def run(self):
+        thr = launch(self.start)
+        super().run()
+        return thr
