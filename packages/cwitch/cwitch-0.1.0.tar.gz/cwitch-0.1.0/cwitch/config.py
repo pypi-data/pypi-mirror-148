d24e28d71bd46e2e27857bcb2ca84ec6ce6db9a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:28 +0000

    Add package cwitch/0.1.0/cwitch-0.1.0.tar.gz

---
+++ packages/cwitch/cwitch-0.1.0.tar.gz/cwitch-0.1.0/cwitch/config.py
@@ -0,0 +1,57 @@
+"""Parse the config and the channels list."""
+from typing import Optional, TextIO
+from configparser import ConfigParser, NoOptionError, NoSectionError
+from pathlib import Path
+
+from .__init__ import prog_name
+
+
+def get_config(config_file: Optional[TextIO] = None) -> dict:
+    """Parse a config file."""
+    config = ConfigParser()
+
+    try:
+        if config_file is None:
+            config_file = open(
+                Path.joinpath(Path.home(), f".config/{prog_name}/config.ini"), "r"
+            )
+
+        config.read_file(config_file)
+    except FileNotFoundError:
+        pass
+
+    options = {"playlist_fetching": {"max_videos_count": 5}}
+
+    try:
+        options["playlist_fetching"]["max_videos_count"] = config.getint(
+            "playlist_fetching", "max_videos_count"
+        )
+    except (NoOptionError, NoSectionError):
+        pass
+
+    return options
+
+
+def get_following_channels(channels_file: Optional[TextIO] = None) -> tuple:
+    """Parse the following channels' list from a file."""
+    channels = ConfigParser()
+
+    try:
+        if channels_file is None:
+            channels_file = open(
+                Path.joinpath(Path.home(), ".config/cwitch/channels.ini"), "r"
+            )
+
+        channels.read_file(channels_file)
+    except FileNotFoundError:
+        pass
+
+    channels_list = []
+
+    for channel in channels.sections():
+        try:
+            channels_list.append({"name": channel, "id": channels.get(channel, "id")})
+        except NoOptionError:
+            continue
+
+    return tuple(channels_list)
