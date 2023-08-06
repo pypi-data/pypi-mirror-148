581cae96788e1dcbe1b87eda6213f22340b57eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:25:49 +0000

    Add package cellacdc/1.2.4rc17/cellacdc-1.2.4rc17-py3-none-any.whl

---
+++ packages/cellacdc/cellacdc-1.2.4rc17-py3-none-any.whl/cellacdc/html_utils.py
@@ -0,0 +1,50 @@
+from . import is_mac
+
+def css_head(txt):
+    if is_mac:
+        txt = txt.replace(',', ',&nbsp;')
+    s = (f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <style type="text/css">
+            {txt}
+    </style>
+    </head>
+    """)
+    return s
+
+def html_body(txt):
+    if is_mac:
+        txt = txt.replace(',', ',&nbsp;')
+    s = (f"""
+    <body>
+        {txt}
+    </body>
+    </html>
+    """)
+    return s
+
+def paragraph(txt, font_size='13px', font_color=None):
+    if is_mac:
+        # Qt < 5.15.3 has a bug on macOS and the space after comma and perdiod
+        # are super small. Force a non-breaking space (except for 'e.g.,').
+        txt = txt.replace(',', ',&nbsp;')
+        txt = txt.replace('.', '.&nbsp;')
+        txt = txt.replace('e.&nbsp;g.&nbsp;', 'e.g.')
+        txt = txt.replace('.&nbsp;.&nbsp;.&nbsp;', '...')
+        txt = txt.replace('i.&nbsp;e.&nbsp;', 'i.e.')
+        txt = txt.replace('etc.&nbsp;)', 'etc.)')
+    if font_color is None:
+        s = (f"""
+        <p style="font-size:{font_size};">
+            {txt}
+        </p>
+        """)
+    else:
+        s = (f"""
+        <p style="font-size:{font_size}; color:{font_color}">
+            {txt}
+        </p>
+        """)
+    return s
