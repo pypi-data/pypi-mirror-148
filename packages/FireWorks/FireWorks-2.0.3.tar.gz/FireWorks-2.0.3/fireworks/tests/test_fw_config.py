46f1e9bbdf9aa103ea39fa7fa50a5317b4706762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:01 +0000

    Add package FireWorks/2.0.3/FireWorks-2.0.3.tar.gz

---
+++ packages/FireWorks/FireWorks-2.0.3.tar.gz/FireWorks-2.0.3/fireworks/tests/test_fw_config.py
@@ -0,0 +1,19 @@
+__author__ = "Shyue Ping Ong"
+__copyright__ = "Copyright 2013, The Materials Project"
+__maintainer__ = "Shyue Ping Ong"
+__email__ = "ongsp@ucsd.edu"
+__date__ = "2/3/14"
+
+import unittest
+
+from fireworks.fw_config import *
+
+
+class ConfigTest(unittest.TestCase):
+    def test_config(self):
+        d = config_to_dict()
+        self.assertNotIn("NEGATIVE_FWID_CTR", d)
+
+
+if __name__ == "__main__":
+    unittest.main()
