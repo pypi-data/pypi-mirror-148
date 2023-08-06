b3e7e815578ca118f3b4b54882a9029fe4f78202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:01 +0000

    Add package django-generic-tasks/0.2.3/django-generic-tasks-0.2.3.tar.gz

---
+++ packages/django-generic-tasks/django-generic-tasks-0.2.3.tar.gz/django-generic-tasks-0.2.3/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['django_generic_tasks', 'django_generic_tasks.migrations']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['CacheControl>=0.12.11,<0.13.0',
+ 'Django>=4.0.4,<5.0.0',
+ 'django-ninja>=0.17.0,<0.18.0',
+ 'google-auth>=2.6.6,<3.0.0',
+ 'google-cloud-tasks>=2.8.1,<3.0.0',
+ 'requests>=2.27.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'django-generic-tasks',
+    'version': '0.2.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Jiayu Yi',
+    'author_email': 'yijiayu@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
