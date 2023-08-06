590394aeb22e7d1cccbab81611ed8761142e25c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:30 +0000

    Add package myBasics/0.0.1/myBasics-0.0.1.tar.gz

---
+++ packages/myBasics/myBasics-0.0.1.tar.gz/myBasics-0.0.1/myBasics/__init__.py
@@ -0,0 +1,15 @@
+from .main import *
+
+__all__=[
+
+]
+
+
+__version__='0.0.1'
+__license__='GPL-2.0 License'
+__author__='Tiancheng Jiao'
+__url__='https://github.com/jtc1246/myBasics'
+__author_email__='jtc1246@outlook.com'
+__description__='Basic tools.'
+
+
---
+++ packages/myBasics/myBasics-0.0.1.tar.gz/myBasics-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='myBasics',
+    version='0.0.1',
+    author='Tiancheng Jiao',
+    author_email='jtc1246@outlook.com',
+    url='https://github.com/jtc1246/myBasics',
+    description='Basic tools',
+    packages=['myBasics'],
+    install_requires=[],
+    python_requires='>=3',
+    platforms=["all"],
+    license='GPL-2.0 License'
+)
+
