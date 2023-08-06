43a0478aa2662fd0f5fdef911857f5ba62b81b05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:24 +0000

    Add package botoolkit/0.0.103/botoolkit-0.0.103.tar.gz

---
+++ packages/botoolkit/botoolkit-0.0.103.tar.gz/botoolkit-0.0.103/botoolkit/bo_git/settings.py
@@ -0,0 +1,20 @@
+from pathlib import (
+    Path,
+)
+
+
+TOOL_NAME = NAMESPACE = 'bogit'
+
+TEMPLATE_CONF_FILE_PATH = (
+    Path(__file__).parent.absolute() / 'templates' / f'{TOOL_NAME}.conf'
+)
+
+CONSOLE_SCRIPTS = [
+    f'{TOOL_NAME} = botoolkit.bo_git.main:main',
+]
+
+NAMESPACES = {
+    f'{NAMESPACE}': [
+        'configure = botoolkit.bo_git.commands:ConfigureBOWebBBCommand',
+    ],
+}
