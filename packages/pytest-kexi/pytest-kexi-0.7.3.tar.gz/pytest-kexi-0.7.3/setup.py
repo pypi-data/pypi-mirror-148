6b9bc754c5aa936ccbc1ec20b17e5c30a8f25cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:49 +0000

    Add package pytest-kexi/0.7.3/pytest-kexi-0.7.3.tar.gz

---
+++ packages/pytest-kexi/pytest-kexi-0.7.3.tar.gz/pytest-kexi-0.7.3/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['pytest_kexi']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['logzero>=1.7.0,<2.0.0', 'setuptools>=62.1.0,<63.0.0']
+
+entry_points = \
+{'pytest11': ['pytest-kexi = pytest_kexi']}
+
+setup_kwargs = {
+    'name': 'pytest-kexi',
+    'version': '0.7.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Kei Nakayama',
+    'author_email': 'kei.of.nakayama@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
