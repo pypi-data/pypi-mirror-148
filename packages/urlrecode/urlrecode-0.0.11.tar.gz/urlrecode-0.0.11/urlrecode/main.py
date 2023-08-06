ac1c7a6fb90d86248ec14098882c70ad0a10358e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:19 +0000

    Add package urlrecode/0.0.11/urlrecode-0.0.11.tar.gz

---
+++ packages/urlrecode/urlrecode-0.0.11.tar.gz/urlrecode-0.0.11/urlrecode/main.py
@@ -0,0 +1,14 @@
+import urllib.parse
+import sys
+
+def main():
+
+    if(sys.argv[1] == "-e"):
+        print(urllib.parse.quote(str(sys.argv[2])))
+
+    if (sys.argv[1] == "-d"):
+        print(urllib.parse.unquote(str(sys.argv[2])))
+
+if __name__ == '__main__':
+    print("hello")
+
