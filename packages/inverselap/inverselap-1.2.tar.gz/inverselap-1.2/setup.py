4f7cca9c133a1aff6203b02dca282b205ad2adb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:20 +0000

    Add package inverselap/1.2/inverselap-1.2.tar.gz

---
+++ packages/inverselap/inverselap-1.2.tar.gz/inverselap-1.2/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import  setup
+import setuptools
+packages = ['inverselap']# 唯一的包名，自己取名
+setup(name='inverselap',
+	version='1.2',
+	author='renger',
+    packages=packages, 
+    package_dir={'requests': 'requests'},)
