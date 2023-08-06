6211c6d2168e07176d3095da13df0ae85d8ce17a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:52 +0000

    Add package ReprDynamics/0.1.0/ReprDynamics-0.1.0.tar.gz

---
+++ packages/ReprDynamics/ReprDynamics-0.1.0.tar.gz/ReprDynamics-0.1.0/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+setup(
+    name="ReprDynamics",
+    version="0.1.0",
+    description="Visualization of representation dynamics",
+    author="Noah",
+    author_email="Noah.Barrett@dal.ca",
+    url = 'https://pypi.python.org/pypi/ReprDynamics/',
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'click==7.1.2',
+        'numpy',
+        'opencv-python==4.5.2.52',
+        "Pillow",
+        'scikit-image',
+        'scikit-learn==0.24.2',
+        "scipy",
+        'torch==1.8.1',
+        "torchvision",
+        'flask'
+    ],
+    classifiers=[
+            'Intended Audience :: Science/Research',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python :: 3.9',
+    ],
+    entry_points={
+        "console_scripts": ["ReprDynamics = app:run"]
+    },
+)
