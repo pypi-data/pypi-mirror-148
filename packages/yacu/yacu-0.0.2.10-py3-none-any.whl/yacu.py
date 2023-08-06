f60ab9e359afe85869bbb29a23596426318815af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:02 +0000

    Add package yacu/0.0.2.10/yacu-0.0.2.10-py3-none-any.whl

---
+++ packages/yacu/yacu-0.0.2.10-py3-none-any.whl/yacu.py
@@ -0,0 +1,31 @@
+"""
+Top level file for the application.
+"""
+import os
+import sys
+import src.crypto_util
+import src.gui
+from dataclasses import dataclass
+
+def gui():
+    """
+    Start the gui.
+    """
+    src.gui.start_gui()
+
+def cli():
+    """
+    Start the cli
+    """
+    src.crypto_util.main()
+
+@dataclass()
+class Coin():
+    """
+    Use this to make you crypto bot!
+    """
+    name: str
+    source: str
+    days: int = 7
+    model: str = "all"
+    lags: int = 300
