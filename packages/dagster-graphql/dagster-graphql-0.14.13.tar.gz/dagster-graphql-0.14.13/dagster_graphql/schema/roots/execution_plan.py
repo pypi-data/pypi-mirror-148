6b2c48c113967aabfe86532af0c971b460cf7e22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:34 +0000

    Add package dagster-graphql/0.14.13/dagster-graphql-0.14.13.tar.gz

---
+++ packages/dagster-graphql/dagster-graphql-0.14.13.tar.gz/dagster-graphql-0.14.13/dagster_graphql/schema/roots/execution_plan.py
@@ -0,0 +1,18 @@
+import graphene
+
+from ..errors import GraphenePipelineNotFoundError, GraphenePythonError
+from ..execution import GrapheneExecutionPlan
+from ..pipelines.config import GrapheneRunConfigValidationInvalid
+from ..pipelines.pipeline_errors import GrapheneInvalidSubsetError
+
+
+class GrapheneExecutionPlanOrError(graphene.Union):
+    class Meta:
+        types = (
+            GrapheneExecutionPlan,
+            GrapheneRunConfigValidationInvalid,
+            GraphenePipelineNotFoundError,
+            GrapheneInvalidSubsetError,
+            GraphenePythonError,
+        )
+        name = "ExecutionPlanOrError"
---
+++ packages/dagster-graphql/dagster-graphql-0.14.13.tar.gz/dagster-graphql-0.14.13/dagster_graphql/schema/roots/pipeline.py
@@ -0,0 +1,26 @@
+import graphene
+
+from ..errors import GrapheneGraphNotFoundError, GraphenePipelineNotFoundError, GraphenePythonError
+from ..pipelines.pipeline import GrapheneGraph, GraphenePipeline
+from ..pipelines.pipeline_errors import GrapheneInvalidSubsetError
+
+
+class GraphenePipelineOrError(graphene.Union):
+    class Meta:
+        types = (
+            GraphenePipeline,
+            GraphenePipelineNotFoundError,
+            GrapheneInvalidSubsetError,
+            GraphenePythonError,
+        )
+        name = "PipelineOrError"
+
+
+class GrapheneGraphOrError(graphene.Union):
+    class Meta:
+        types = (
+            GrapheneGraph,
+            GrapheneGraphNotFoundError,
+            GraphenePythonError,
+        )
+        name = "GraphOrError"
