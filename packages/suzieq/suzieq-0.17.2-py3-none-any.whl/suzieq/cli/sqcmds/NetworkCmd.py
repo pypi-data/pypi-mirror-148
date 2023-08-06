ec4a4d719dd15b5b1de139999e51b3607f5c12f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:30 +0000

    Add package suzieq/0.17.2/suzieq-0.17.2-py3-none-any.whl

---
+++ packages/suzieq/suzieq-0.17.2-py3-none-any.whl/suzieq/cli/sqcmds/NetworkCmd.py
@@ -0,0 +1,85 @@
+import time
+from nubia import command
+from suzieq.cli.nubia_patch import argument
+
+from suzieq.cli.sqcmds.command import SqCommand
+from suzieq.sqobjects.network import NetworkObj
+
+
+@command("network", help="Act on network-wide data")
+class NetworkCmd(SqCommand):
+    """Overall network information such as namespaces present etc."""
+
+    def __init__(
+            self,
+            engine: str = "",
+            hostname: str = "",
+            start_time: str = "",
+            end_time: str = "",
+            view: str = "",
+            namespace: str = "",
+            format: str = "",  # pylint: disable=redefined-builtin
+            query_str: str = " ",
+            columns: str = "default",
+    ) -> None:
+        super().__init__(
+            engine=engine,
+            hostname=hostname,
+            start_time=start_time,
+            end_time=end_time,
+            view=view,
+            namespace=namespace,
+            columns=columns,
+            format=format,
+            query_str=query_str,
+            sqobj=NetworkObj,
+        )
+
+    @command("show", help="Show device information")
+    @argument("model", description="model(s), space separated")
+    @argument("os", description='NOS(es), space separated')
+    @argument('vendor', description='Vendor(s), space separated')
+    @argument('version', description='NOS version(s), space separated')
+    # pylint: disable=arguments-differ
+    def show(self, os: str = "", vendor: str = "", model: str = "",
+             version: str = "") -> int:
+        """Show network info
+        """
+
+        now = time.time()
+
+        df = self._invoke_sqobj(self.sqobj.get,
+                                namespace=self.namespace, os=os.split(),
+                                vendor=vendor.split(), model=model.split(),
+                                version=version, query_str=self.query_str,
+                                hostname=self.hostname)
+
+        self.ctxt.exec_time = "{:5.4f}s".format(time.time() - now)
+
+        return self._gen_output(df)
+
+    @command("find", help="find where an IP/MAC address is attached")
+    @argument("address", type=str,
+              description="IP/MAC addresses, in quotes, space separated")
+    @argument("vrf", type=str,
+              description="Find within this VRF, used for IP addr")
+    @argument("vlan", type=str,
+              description="Find MAC within this VLAN")
+    def find(self, address: str = '', vrf: str = '',
+             vlan: str = ''):
+        """Find the network attach point of a given IP or MAC address.
+        """
+        now = time.time()
+
+        df = self._invoke_sqobj(self.sqobj.find,
+                                namespace=self.namespace,
+                                hostname=self.hostname,
+                                address=address.split(),
+                                vlan=vlan,
+                                vrf=vrf,
+                                query_str=self.query_str,
+                                )
+
+        self.ctxt.exec_time = "{:5.4f}s".format(time.time() - now)
+
+        return self._gen_output(df)
