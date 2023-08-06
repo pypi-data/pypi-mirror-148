b0c9663501d65a47da81fdefb6ad3cb90e77770e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:27 +0000

    Add package pi2143oop/0.5.6/pi2143oop-0.5.6.tar.gz

---
+++ packages/pi2143oop/pi2143oop-0.5.6.tar.gz/pi2143oop-0.5.6/pi2143oop/__init__.py
@@ -0,0 +1,4 @@
+from .ErrorClass import Error
+from .CanvasClass import Canvas
+from .DotClass import Dot
+from .Settings import default_font_color, default_background_color, font_color, background_color
