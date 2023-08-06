924376a21dc8e37e8cbc104118f500b670ea986c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:15 +0000

    Add package kabbes-the-garden/0.3.0/kabbes_the_garden-0.3.0-py3-none-any.whl

---
+++ packages/kabbes-the-garden/kabbes_the_garden-0.3.0-py3-none-any.whl/the_garden/garden_params.py
@@ -0,0 +1,29 @@
+git_clone_suffix = '.git'
+
+github_accounts = {
+
+    'jameskabbes':{
+
+        'url_base': 'https://github.com/jameskabbes',
+        'add_to_pythonpath': True,
+        'repos': {
+            'analytics_packages',
+            'aws_connections',
+            'aws_credentials',
+            'database_connections',
+            'dir_ops',
+            'ml_pipeline',
+            'nanoid',
+            'parent_class',
+            'py_starter',
+            'pypi_builder',
+            'real_time_input',
+            'repository_generator',
+            's3synchrony',
+            'smart_documentation',
+            'user_profile'
+        },
+    }
+
+}#end github accounts
+
