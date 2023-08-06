845fc1af1faa35f57a59133d529081e7ffcd1e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:21:44 +0000

    Add package sqllite3tools1/0.0.5/sqllite3tools1-0.0.5.tar.gz

---
+++ packages/sqllite3tools1/sqllite3tools1-0.0.5.tar.gz/sqllite3tools1-0.0.5/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+import sys
+
+VERSION = '0.0.5'
+DESCRIPTION = 'SQLITE tools'
+LONG_DESCRIPTION = 'this package is designed to manage the database'
+
+about = {}
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "sqllite3tools1", "__version__.py"), "r") as f:
+    exec(f.read(), about)
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+# Setting up
+setup(
+    name="sqllite3tools1",
+    version=VERSION,
+    author="SanjarbekDev",
+    author_email="<sanjarbeksodiqov0302@gmail.com>",
+    description=DESCRIPTION,
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["sqlite3"],
+    keywords=['eexecute','commit','fetchone','fetchall','create_table','info','select'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
