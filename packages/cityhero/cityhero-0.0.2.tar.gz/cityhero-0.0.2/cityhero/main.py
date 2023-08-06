f95d4a2baf8b1ec88d0010b80ee8778c06275f9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:38 +0000

    Add package cityhero/0.0.2/cityhero-0.0.2.tar.gz

---
+++ packages/cityhero/cityhero-0.0.2.tar.gz/cityhero-0.0.2/cityhero/main.py
@@ -0,0 +1,9 @@
+from cityhero import Hero
+
+
+WechatId = "osnijv3u51YTY4DvX1rX90Z-gy8M"
+PublicOpenID = "oGwGAv2zCrR2Vay-QgYagl6FPhIk"
+zw = "osnijvy-3CV5q4cvE1nmrBANf1kY"
+zp = "oGwGAv79IBfaF6b8RU5x-teeSIuc"
+Hero(WechatId=WechatId,PublicOpenID=PublicOpenID)
+Hero(WechatId=zw, PublicOpenID=zp)
