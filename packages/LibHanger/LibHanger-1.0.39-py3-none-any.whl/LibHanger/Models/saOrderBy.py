9a04535f37930f975730c3e0af1b3d0f047ddd0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:06 +0000

    Add package LibHanger/1.0.39/LibHanger-1.0.39-py3-none-any.whl

---
+++ packages/LibHanger/LibHanger-1.0.39-py3-none-any.whl/LibHanger/Models/saOrderBy.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+#from enum import Enum
+from sqlalchemy import asc,desc
+
+class saOrderBy():
+    
+    __orderBy = dict()
+    """ Orderby格納用ディクショナリ """
+
+    def __init__(self) -> None:
+        
+        """
+        コンストラクタ
+        """
+        
+        self.__sortLevel = 0
+        
+    @property
+    def orderBy(self):
+        
+        """
+        orderBy句dict
+        """
+        
+        return self.__orderBy
+
+    def asc_(self, s) -> saOrderBy:
+
+        """
+        asc(昇順)追加
+        
+        Parameters
+        ----------
+        s : Any
+            ソート条件
+
+        """
+
+        # sortLevel++
+        self.__sortLevel += 1
+
+        # dict追加
+        self.__orderBy[self.__sortLevel] = asc(s)
+
+        # 戻り値を返す
+        return self
+    
+    def desc_(self, s) -> saOrderBy:
+
+        """
+        desc(降順)追加
+        
+        Parameters
+        ----------
+        s : Any
+            ソート条件
+
+        """
+
+        # sortLevel++
+        self.__sortLevel += 1
+        
+        # dict追加
+        self.__orderBy[self.__sortLevel] = desc(s)
+
+        # 戻り値を返す
+        return self
