6408ce69873c3ed2c485ab94e93eba7e21f20eb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:58 +0000

    Add package opteryx/0.0.0a72/opteryx-0.0.0a72.tar.gz

---
+++ packages/opteryx/opteryx-0.0.0a72.tar.gz/opteryx-0.0.0a72/setup.py
@@ -0,0 +1,48 @@
+import numpy as np
+from setuptools import setup
+from setuptools import find_packages
+from setuptools import Extension
+from Cython.Build import cythonize
+
+
+with open("opteryx/version.py", "r") as v:
+    vers = v.read()
+exec(vers)  # nosec
+
+with open("README.md", "r") as rm:
+    long_description = rm.read()
+
+try:
+    with open("requirements.txt") as f:
+        required = f.read().splitlines()
+except FileNotFoundError:
+    # this sometimes fails - so put them here, but this needs to be maintained manually
+    required = ["cython", "numpy", "orjson", "cityhash", "sqloxide", "pyarrow"]
+
+extensions = [
+    Extension(
+        name="cjoin",
+        sources=["opteryx/third_party/pyarrow_ops/cjoin.pyx"],
+        include_dirs=[np.get_include()],
+    ),
+    Extension(
+        name="cythonize",
+        sources=["opteryx/third_party/accumulation_tree/accumulation_tree.pyx"],
+    )
+    #    "mabel/data/internals/group_by.py",
+]
+
+setup(
+    name="opteryx",
+    version=__version__,
+    description="Distributed SQL Engine",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    maintainer="Joocer",
+    author="joocer",
+    author_email="justin.joyce@joocer.com",
+    packages=find_packages(include=["opteryx", "opteryx.*"]),
+    url="https://github.com/mabel-dev/opteryx/",
+    install_requires=required,
+    ext_modules=cythonize(extensions),
+)
