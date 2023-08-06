975e0123fd2bdc3b8f7a43dd1bb89f5e37b522b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:09 +0000

    Add package stl-in-mc/0.1.0/stl-in-mc-0.1.0.tar.gz

---
+++ packages/stl-in-mc/stl-in-mc-0.1.0.tar.gz/stl-in-mc-0.1.0/stlinmc/voxel.py
@@ -0,0 +1,27 @@
+"""
+# voxel.py
+# this module creates a voxel array from an stl
+"""
+
+from stl import mesh
+from stltovoxel import convert_meshes
+import numpy as np
+
+
+def import_stl_as_voxels(input_file_path, parallel):
+    """imports an stl file into a voxel array
+
+    Args:
+        input_file_path (str): path to stl file
+
+    Returns:
+        np.nDArray[int8]: a set of voxels in a 3D array,
+                          0 and 1 indicate air and block respectively
+    """
+    meshes = []
+    mesh_obj = mesh.Mesh.from_file(input_file_path)
+    org_mesh = np.hstack(
+        (mesh_obj.v0[:, np.newaxis], mesh_obj.v1[:, np.newaxis], mesh_obj.v2[:, np.newaxis]))
+    meshes.append(org_mesh)
+    vol = convert_meshes(meshes, resolution=100, parallel=parallel)[0]
+    return vol
