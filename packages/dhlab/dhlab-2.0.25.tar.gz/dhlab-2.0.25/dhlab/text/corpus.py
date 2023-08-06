e8f5de5817374626012da3ec07c99919cc1ead23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:25 +0000

    Add package dhlab/2.0.25/dhlab-2.0.25.tar.gz

---
+++ packages/dhlab/dhlab-2.0.25.tar.gz/dhlab-2.0.25/dhlab/text/corpus.py
@@ -0,0 +1,60 @@
+from pandas import DataFrame
+
+from dhlab.api.dhlab_api import document_corpus, get_metadata
+
+
+class Corpus:
+    def __init__(
+            self,
+            doctype=None,
+            author=None,
+            freetext=None,
+            fulltext=None,
+            from_year=None,
+            to_year=None,
+            from_timestamp=None,
+            to_timestamp=None,
+            title=None,
+            ddk=None,
+            subject=None,
+            lang=None,
+            limit=None):
+
+        self.corpus = document_corpus(
+            doctype,
+            author,
+            freetext,
+            fulltext,
+            from_year,
+            to_year,
+            from_timestamp,
+            to_timestamp,
+            title,
+            ddk,
+            subject,
+            lang,
+            limit
+        )
+
+        self.size = len(self.corpus)
+
+        return
+
+
+class CorpusFromIdentifiers(Corpus):
+    def __init__(self, identifiers=None):
+        self.corpus = get_metadata(urnlist(identifiers))
+
+
+def urnlist(corpus):
+    """Try to pull out a list of URNs from corpus"""
+
+    if isinstance(corpus, Corpus):
+        urnlist = list(corpus.corpus.urn)
+    elif isinstance(corpus, DataFrame):
+        urnlist = list(corpus.urn)
+    elif isinstance(corpus, list):
+        urnlist = corpus
+    else:
+        urnlist = []
+    return urnlist
