2266578d37b7785418f286e8cf8fadd60f4190a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:44 +0000

    Add package numerous-engine/0.2.3/numerous_engine-0.2.3-py3-none-any.whl

---
+++ packages/numerous-engine/numerous_engine-0.2.3-py3-none-any.whl/numerous/engine/model/external_mappings/__init__.py
@@ -0,0 +1,4 @@
+from .external_mappings import ExternalMapping, ExternalMappingElement, EmptyMapping
+from .interpolation_type import InterpolationType
+
+__all__ = ['ExternalMapping', 'ExternalMappingElement', 'EmptyMapping', 'InterpolationType']
