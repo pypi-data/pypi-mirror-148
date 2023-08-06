3becc89ff3a5f71cecb9e28a14ba6ab853f25b73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:29 +0000

    Add package akerbp.mlpet/2.0.4/akerbp.mlpet-2.0.4.tar.gz

---
+++ packages/akerbp.mlpet/akerbp.mlpet-2.0.4.tar.gz/akerbp.mlpet-2.0.4/src/akerbp/mlpet/__init__.py
@@ -0,0 +1,16 @@
+import importlib.metadata
+
+from akerbp.mlpet import feature_engineering, imputers, preprocessors, utilities
+from akerbp.mlpet.dataset import Dataset
+from akerbp.mlpet.transformer import MLPetTransformer
+
+__version__ = importlib.metadata.version(__name__)
+
+__all__ = [
+    "Dataset",
+    "feature_engineering",
+    "imputers",
+    "utilities",
+    "preprocessors",
+    "MLPetTransformer",
+]
