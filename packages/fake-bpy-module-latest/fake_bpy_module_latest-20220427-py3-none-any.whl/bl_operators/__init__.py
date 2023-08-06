64f258345d0d379b9d5f6a0ba247a2c48cda4f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:50 +0000

    Add package fake-bpy-module-latest/20220427/fake_bpy_module_latest-20220427-py3-none-any.whl

---
+++ packages/fake-bpy-module-latest/fake_bpy_module_latest-20220427-py3-none-any.whl/bl_operators/__init__.py
@@ -0,0 +1,46 @@
+import sys
+import typing
+from . import object_quick_effects
+from . import geometry_nodes
+from . import vertexpaint_dirt
+from . import mesh
+from . import console
+from . import object_align
+from . import uvcalc_follow_active
+from . import sequencer
+from . import screen_play_rendered_anim
+from . import assets
+from . import image
+from . import add_mesh_torus
+from . import bmesh
+from . import userpref
+from . import file
+from . import wm
+from . import spreadsheet
+from . import rigidbody
+from . import view3d
+from . import clip
+from . import object_randomize_transform
+from . import constraint
+from . import node
+from . import uvcalc_lightmap
+from . import presets
+from . import object
+from . import anim
+from . import freestyle
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
---
+++ packages/fake-bpy-module-latest/fake_bpy_module_latest-20220427-py3-none-any.whl/gpu/__init__.py
@@ -0,0 +1,10 @@
+import sys
+import typing
+from . import select
+from . import matrix
+from . import shader
+from . import capabilities
+from . import types
+from . import state
+from . import platform
+from . import texture
