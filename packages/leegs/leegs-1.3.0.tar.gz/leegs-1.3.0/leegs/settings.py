e27de956ce4e9bb61e09527e2342c4f82d2e7f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:49 +0000

    Add package leegs/1.3.0/leegs-1.3.0.tar.gz

---
+++ packages/leegs/leegs-1.3.0.tar.gz/leegs-1.3.0/leegs/settings.py
@@ -0,0 +1,28 @@
+import os
+
+'''
+Settings for leegs.
+'''
+
+# burner accts
+emails = ['millburns496@gmail.com', 'millburns487@gmail.com', ]
+passwords = ['KROD.thob1zesm9tauy', 'BUG9squ@scut@sqah']
+
+# emails = [
+# 'millburns486@gmail.com',
+# 'millburns487@gmail.com',
+# 'millburns484@gmail.com',
+# 'millburns485@gmail.com',
+# ]
+
+# passwords=[
+# 'KROD.thob1zesm9tauy',
+# 'BUG9squ@scut@sqah'
+# 'FAUH*yush5phen*bauf',
+# 'NOUN6ret.smit.shap',
+# ]
+
+total_employees = 100
+company_url = 'https://www.linkedin.com/company/hubspot/'
+
+num_threads = 1
