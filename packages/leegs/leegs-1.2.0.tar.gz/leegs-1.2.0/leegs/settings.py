a2e4750cf08f23d75f1092c23fd114224e42ef02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:40 +0000

    Add package leegs/1.2.0/leegs-1.2.0.tar.gz

---
+++ packages/leegs/leegs-1.2.0.tar.gz/leegs-1.2.0/leegs/settings.py
@@ -0,0 +1,27 @@
+import os
+
+'''
+Settings for leegs.
+'''
+
+# emails = [os.environ.get(f'LINKEDIN_EMAIL{i}') for i in range(7, 9)] + [os.environ.get('LINKEDIN_EMAIL10')]
+# passwords = [os.environ.get(f'LINKEDIN_PASSWORD{i}') for i in range(7, 9)] + [os.environ.get('LINKEDIN_PASSWORD10')]
+
+emails = [
+'millburns485@gmail.com',
+'millburns486@gmail.com',
+'millburns487@gmail.com',
+'millburns484@gmail.com',
+]
+
+passwords=[
+'NOUN6ret.smit.shap',
+'KROD.thob1zesm9tauy',
+'BUG9squ@scut@sqah'
+'FAUH*yush5phen*bauf',
+]
+
+total_employees = 100
+company_url = 'https://www.linkedin.com/company/hubspot/'
+
+num_threads = 3
