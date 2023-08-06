59ad0616d3178d1d124e0c75ec3df96a2dc6b14b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:19:06 +0000

    Add package gpforecaster/0.2.1/gpforecaster-0.2.1.tar.gz

---
+++ packages/gpforecaster/gpforecaster-0.2.1.tar.gz/gpforecaster-0.2.1/gpforecaster/tests/test_calculate_results.py
@@ -0,0 +1,24 @@
+import unittest
+import tsaugmentation as tsag
+from gpforecaster.model.gpf import GPF
+import shutil
+
+
+class TestModel(unittest.TestCase):
+
+    def setUp(self):
+        self.data = tsag.preprocessing.PreprocessDatasets('prison').apply_preprocess()
+        self.n = self.data['predict']['n']
+        self.s = self.data['train']['s']
+        shutil.rmtree("./data/original_datasets")
+        self.gpf = GPF('prison', self.data)
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree("./results")
+
+    def test_calculate_metrics_dict(self):
+        model, like = self.gpf.train(n_iterations=100)
+        mean, lower, upper = self.gpf.predict(model, like)
+        res = self.gpf.metrics(mean, lower, upper)
+        self.assertLess(res['mase']['bottom'], 2.5)
---
+++ packages/gpforecaster/gpforecaster-0.2.1.tar.gz/gpforecaster-0.2.1/gpforecaster/tests/test_model_results_prison.py
@@ -0,0 +1,39 @@
+import unittest
+from gpforecaster.model.gpf import GPF
+import tsaugmentation as tsag
+import shutil
+
+
+class TestModel(unittest.TestCase):
+
+    def setUp(self):
+        self.data = tsag.preprocessing.PreprocessDatasets('prison').apply_preprocess()
+        self.n = self.data['predict']['n']
+        self.s = self.data['train']['s']
+        shutil.rmtree("./data/original_datasets")
+        self.gpf = GPF('prison', self.data)
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree("./results")
+
+    def test_correct_train(self):
+        model, like = self.gpf.train(n_iterations=10)
+        self.assertIsNotNone(model)
+
+    def test_predict_shape(self):
+        model, like = self.gpf.train(n_iterations=10)
+        mean, lower, upper = self.gpf.predict(model, like)
+        self.assertTrue(mean.shape == (1, self.n, self.s))
+
+    def test_results_interval(self):
+        model, like = self.gpf.train(n_iterations=100)
+        mean, lower, upper = self.gpf.predict(model, like)
+        res = self.gpf.metrics(mean, lower, upper)
+        self.assertLess(res['mase']['bottom'], 2.5)
+
+    def test_wall_time(self):
+        model, like = self.gpf.train(n_iterations=10)
+        mean, lower, upper = self.gpf.predict(model, like)
+        res = self.gpf.metrics(mean)
+        self.assertLess(res['wall_time']['wall_time_total'], 100)
