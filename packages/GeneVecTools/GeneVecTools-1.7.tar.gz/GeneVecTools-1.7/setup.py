83846074aa839dda54eb95388b51e4d11276be6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:54 +0000

    Add package GeneVecTools/1.7/GeneVecTools-1.7.tar.gz

---
+++ packages/GeneVecTools/GeneVecTools-1.7.tar.gz/GeneVecTools-1.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="GeneVecTools",
+    version="1.7",
+    license="danielum16license",
+    author="Daniel Um",
+    author_email="danielum.16@gmail.com",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    url="https://github.com/gmyrianthous/example-publish-pypi",
+    keywords="example project",
+    install_requires=[
+        # "scikit-learn",
+        "pandas",
+        "faiss-cpu",
+        "numpy",
+        "requests",
+        # "io",
+        "tensorflow",
+        # "datetime",
+        # "os",
+        # "sys",
+        "biopython",
+        "pysam",
+    ],
+)
