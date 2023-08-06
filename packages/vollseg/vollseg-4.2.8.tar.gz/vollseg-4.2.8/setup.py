42b6d69dba2641077dd283dc0a562cdd565d19d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:30 +0000

    Add package vollseg/4.2.8/vollseg-4.2.8.tar.gz

---
+++ packages/vollseg/vollseg-4.2.8.tar.gz/vollseg-4.2.8/setup.py
@@ -0,0 +1,50 @@
+import setuptools
+from setuptools import find_packages, setup
+
+from os import path
+
+_dir = path.dirname(__file__)
+
+
+with open('README.md') as f:
+    long_description = f.read()
+with open(path.join(_dir,'vollseg','version.py'), encoding="utf-8") as f:
+    exec(f.read())
+
+setup(
+    name="vollseg",
+
+    version=__version__,
+
+    author='Varun Kapoor,Claudia Carabana Garcia,Mari Tolonen',
+    author_email='randomaccessiblekapoor@gmail.com',
+    url='https://github.com/kapoorlab/vollseg/',
+    description='Segmentation tool for biological cells of irregular size and shape in 3D and 2D.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        
+        "pandas",
+
+        "stardist",
+
+        "scipy",
+        "tifffile",
+        "matplotlib",
+        "imagecodecs",
+        "napari",
+        "diplib",
+        "opencv-python" 
+       
+    ],
+    dependeny_links = ['https://github.com/bhoeckendorf/pyklb.git@skbuild'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
