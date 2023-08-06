cf5183e3559bf23c6ed68726e1a4b950d315f47c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:43 +0000

    Add package damo/1.2.9/damo-1.2.9-py3-none-any.whl

---
+++ packages/damo/damo-1.2.9-py3-none-any.whl/damo/damo_start.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0
+
+"""
+Start DAMON with given parameters.
+"""
+
+import argparse
+
+import _damon
+
+def set_argparser(parser):
+    _damon.set_monitoring_argparser(parser)
+    parser.add_argument('--ops', choices=['vaddr', 'paddr', 'fvaddr'],
+            default='vaddr',
+            help='monitoring operations set')
+    parser.add_argument('--target_pid', type=int, help='target pid')
+
+def main(args=None):
+    if not args:
+        parser = argparse.ArgumentParser()
+        set_argparser(parser)
+        args = parser.parse_args()
+
+    print(args)
+    print('to be implemented...')
+
+if __name__ == '__main__':
+    main()
