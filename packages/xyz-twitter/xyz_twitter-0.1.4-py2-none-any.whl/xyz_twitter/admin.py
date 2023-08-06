0261f15fe235ec419114aaa8c8eaf0a7dbd1f110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:34 +0000

    Add package xyz-twitter/0.1.4/xyz_twitter-0.1.4-py2-none-any.whl

---
+++ packages/xyz-twitter/xyz_twitter-0.1.4-py2-none-any.whl/xyz_twitter/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+from . import models
+
+
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'screen_name', 'description', 'is_active', 'created_at', 'create_time')
+    raw_id_fields = ('user',)
+    search_fields = ("name", 'screen_name')
+    date_hierarchy = 'create_time'
+
+@admin.register(models.Tweet)
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ('user', 'full_text', 'created_at', 'create_time')
+    raw_id_fields = ('user',)
+    list_filter = ('user', )
+    search_fields = ("full_text", )
+    date_hierarchy = 'create_time'
