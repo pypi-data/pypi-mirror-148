414e316e059b1d3b688c6b6114e28054acfc9bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:03 +0000

    Add package h5mapper/0.2.2/h5mapper-0.2.2.tar.gz

---
+++ packages/h5mapper/h5mapper-0.2.2.tar.gz/h5mapper-0.2.2/h5mapper/__init__.py
@@ -0,0 +1,10 @@
+__version__ = '0.2.2'
+
+from .core import *
+from .create import *
+from .crud import *
+from .features import *
+from .file_walker import *
+from .serve import *
+from .typed_files import *
+from .utils import *
