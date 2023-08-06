f98be2637cca95059387545612fc0605d56114e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:06 +0000

    Add package kaniko-remote/0.2.1/kaniko-remote-0.2.1.tar.gz

---
+++ packages/kaniko-remote/kaniko-remote-0.2.1.tar.gz/kaniko-remote-0.2.1/src/kaniko_remote/builder.py
@@ -0,0 +1,123 @@
+import base64
+import json
+from contextlib import AbstractContextManager
+from signal import SIGINT
+from tempfile import TemporaryDirectory
+from typing import Callable, List, Optional
+from urllib.parse import urlparse
+
+from kaniko_remote.authorisers import KanikoAuthoriser, get_matching_authorisers
+from kaniko_remote.config import Config
+from kaniko_remote.k8s.k8s import K8sWrapper
+from kaniko_remote.k8s.specs import K8sSpecs
+from kaniko_remote.logging import getLogger
+
+logger = getLogger(__name__)
+
+
+class Builder(AbstractContextManager):
+    def __init__(
+        self,
+        k8s_wrapper: K8sWrapper,
+        config: Config,
+        **kaniko_kwargs,
+    ) -> None:
+        self.k8s = k8s_wrapper
+
+        self.pod_name = None
+        self.stopped = False
+
+        local_context = self._parse_local_context(kaniko_kwargs["context"])
+        pod_spec = K8sSpecs.generate_pod_spec(**config.get_builder_options())
+        urls_to_auth = [kaniko_kwargs["destination"]]
+
+        if local_context:
+            pod_spec = K8sSpecs.mount_context_for_exec_transfer(pod_spec)
+        else:
+            urls_to_auth.append(kaniko_kwargs["context"])
+
+        authorisers: List[KanikoAuthoriser] = get_matching_authorisers(urls=urls_to_auth, config=config)
+        docker_config = {}
+        pod_spec = K8sSpecs.add_kaniko_args(pod_spec, **kaniko_kwargs)
+
+        for auth in authorisers:
+            docker_config = auth.append_auth_to_docker_config(docker_config=docker_config)
+            pod_spec = auth.append_auth_to_pod(pod_spec=pod_spec)
+
+        logger.info(f"Configuring builder with auth for {[a.url for a in authorisers]}")
+        logger.debug(f"Generated docker config for builder: {docker_config}")
+        logger.debug(f"Generated pod spec for builder: {pod_spec}")
+
+        self._local_context = local_context
+        self._docker_config = docker_config
+        self._pod_spec = pod_spec
+
+    @classmethod
+    def _parse_local_context(cls, context: str) -> Optional[str]:
+        _urlparse = urlparse(context)
+        if not _urlparse.scheme:
+            logger.info("Local context detected, the context will be transferred directly to the builder pod.")
+            return context
+        else:
+            logger.info("Remote context detected, builder pod will be authorised to access configured remote storage.")
+            return None
+
+    def __enter__(self) -> "Builder":
+        # self._loop = asyncio.get_event_loop()
+        self._create()
+        # self._loop.add_signal_handler(SIGINT, self.__exit__)
+        return self
+
+    def __exit__(self, *exc) -> bool:
+        self._destroy()
+        # self._loop.remove_signal_handler(SIGINT)
+        return False
+
+    def _create(self) -> None:
+        logger.info(f"Creating builder pod in '{self.k8s.namespace}' namespace.")
+        pod_spec = self.k8s.create_pod(body=self._pod_spec)
+        self.pod_name = pod_spec.metadata.name
+        logger.info(f"Created builder pod '{self.k8s.namespace}/{self.pod_name}'.")
+        logger.debug(f"Created builder pod with spec: {pod_spec}")
+
+    def _destroy(self):
+        if self.pod_name:
+            logger.warn(f"Deleting pod {self.pod_name}")
+            self.k8s.delete_pod(self.pod_name)
+            logger.warn(f"Deleted pod {self.pod_name}")
+        self.stopped = True
+
+    async def setup(self) -> None:
+        await self.k8s.wait_for_container(pod=self.pod_name, container="setup")
+
+        if self._local_context:
+            await self.k8s.upload_local_dir_to_container(
+                pod=self.pod_name,
+                container="setup",
+                local_path=self._local_context,
+                remote_path="/workspace",
+                progress_bar_description="Sending context dir: ",
+            )
+        else:
+            logger.info("Using remote storage for context dir, skipping upload")
+
+        with TemporaryDirectory() as config_dir:
+            with open(config_dir + "/config.json", "w") as f:
+                json.dump(self._docker_config, f)
+
+            await self.k8s.upload_local_dir_to_container(
+                pod=self.pod_name,
+                container="setup",
+                local_path=config_dir,
+                remote_path="/kaniko/.docker",
+            )
+
+        logger.info(f"Setup builder pod {self.pod_name}.")
+
+    async def build(self, log_callback: Callable[[str], None]) -> None:
+        logger.info("Connecting to logs...")
+        await self.k8s.wait_for_container(pod=self.pod_name, container="builder", timeout=10)
+        async for line in self.k8s.tail_container(pod=self.pod_name, container="builder"):
+            log_callback(line)
+            if self.stopped:
+                return
---
+++ packages/kaniko-remote/kaniko-remote-0.2.1.tar.gz/kaniko-remote-0.2.1/src/kaniko_remote/main.py
@@ -0,0 +1,97 @@
+from pathlib import Path
+
+import asyncclick as click
+
+from kaniko_remote.builder import Builder
+from kaniko_remote.config import Config
+from kaniko_remote.k8s.k8s import K8sWrapper
+from kaniko_remote.logging import init_logging
+
+
+@click.group()
+@click.option(
+    "-v",
+    "--verbosity",
+    type=click.Choice(
+        ["trace", "debug", "info", "warn", "error", "fatal", "panic"],
+        case_sensitive=False,
+    ),
+    default="info",
+    # help='Log level (trace, debug, info, warn, error, fatal, panic) (default "info")',
+)
+@click.pass_context
+def cli(ctx: click.Context, verbosity: str):
+    """
+    Build and push images without Docker, using using kaniko on a configured k8s cluster
+
+    This tool can be explicitly invoked as 'kaniko-remote'.
+    If optionally installed, this tool can additionally be invoked as 'docker'.
+    """
+    init_logging(level=verbosity)
+
+
+@cli.command()
+@click.option(
+    "-f",
+    "--file",
+    "dockerfile",
+    help='Name of the Dockerfile (Default is "PATH/Dockerfile")',
+    type=click.Path(),
+    default="Dockerfile",
+)
+@click.option(
+    "-t",
+    "--tag",
+    "destination",
+    required=True,
+    help='Name and tag in the "name:tag" format',
+    type=str,
+)
+@click.argument(
+    "path",
+    type=click.Path(exists=True),
+)
+async def build(path: Path, **kwargs):
+    """
+    Build an image from a Dockerfile on a k8s cluster using kaniko
+
+    This tool can be explicitly invoked as 'kaniko-remote'.
+    If optionally installed, this tool can additionally be invoked as 'docker'.
+    """
+    config = Config()
+    with K8sWrapper(
+        kubeconfig=config.get_kubeconfig(),
+        namespace=config.get_namespace(),
+    ) as k8s:
+        with Builder(
+            k8s_wrapper=k8s,
+            config=config,
+            context=path,
+            **kwargs,
+        ) as builder:
+            await builder.setup()
+            await builder.build(click.echo)
+
+    # Options:
+    #     --add-host list           Add a custom host-to-IP mapping (host:ip)
+    #     --build-arg list          Set build-time variables
+    #     --cache-from strings      Images to consider as cache sources
+    #     --disable-content-trust   Skip image verification (default true)
+    # -f, --file string             Name of the Dockerfile (Default is 'PATH/Dockerfile')
+    #     --iidfile string          Write the image ID to the file
+    #     --isolation string        Container isolation technology
+    #     --label list              Set metadata for an image
+    #     --network string          Set the networking mode for the RUN instructions during build (default "default")
+    #     --no-cache                Do not use cache when building the image
+    # -o, --output stringArray      Output destination (format: type=local,dest=path)
+    #     --platform string         Set platform if server is multi-platform capable
+    #     --progress string         Set type of progress output (auto, plain, tty). Use plain to show container
+    #                                 output (default "auto")
+    #     --pull                    Always attempt to pull a newer version of the image
+    # -q, --quiet                   Suppress the build output and print image ID on success
+    #     --secret stringArray      Secret file to expose to the build (only if BuildKit enabled):
+    #                                 id=mysecret,src=/local/secret
+    #     --ssh stringArray         SSH agent socket or keys to expose to the build (only if BuildKit enabled)
+    #                                 (format: default|<id>[=<socket>|<key>[,<key>]])
+    # -t, --tag list                Name and optionally a tag in the 'name:tag' format
+    #     --target string           Set the target build stage to build.
