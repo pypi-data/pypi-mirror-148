6ca5354467bb19aa5b6ddc93bf1a07f5465f6f40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:53 +0000

    Add package lauetoolsnn/3.0.43/lauetoolsnn-3.0.43.tar.gz

---
+++ packages/lauetoolsnn/lauetoolsnn-3.0.43.tar.gz/lauetoolsnn-3.0.43/lauetoolsnn/util_scripts/template_matching.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 28 21:40:11 2022
+
+@author: PURUSHOT
+
+Template matching with open CV
+
+COllection of useful opencv function for object detection and template matching
+"""
+
