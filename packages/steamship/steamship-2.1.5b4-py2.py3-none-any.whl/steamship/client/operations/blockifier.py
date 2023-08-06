61e377d1e2c521ed3eadbd1cc1135aea1f6d2a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:17:31 +0000

    Add package steamship/2.1.5b4/steamship-2.1.5b4-py2.py3-none-any.whl

---
+++ packages/steamship/steamship-2.1.5b4-py2.py3-none-any.whl/steamship/client/operations/blockifier.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from steamship.base import Client
+
+@dataclass
+class BlockifyRequest:
+    type: str = None
+    pluginInstance: str = None
+    id: str = None
+    handle: str = None
+    name: str = None
+
+    @staticmethod
+    def from_dict(d: any, client: Client = None) -> "BlockifyRequest":
+        return BlockifyRequest(
+            type=d.get('type', None),
+            pluginInstance=d.get('pluginInstance', None),
+            id=d.get('id', None),
+            handle=d.get('handle', None),
+            name=d.get('name', None)
+        )
