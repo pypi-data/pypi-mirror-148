764d9af596a43e4c54c55499cf27703cb9f8ef57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:05 +0000

    Add package xenon-gcp-sdk/1.0.0/xenon_gcp_sdk-1.0.0.tar.gz

---
+++ packages/xenon-gcp-sdk/xenon_gcp_sdk-1.0.0.tar.gz/xenon_gcp_sdk-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xenon_gcp_sdk',
+    version='1.0.0',
+    license='MIT',
+    author="support",
+    author_email='support@xenon.work',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/xenon-work/xenon',
+    keywords='xenon cloud platform sdk',
+    install_requires=[
+        'google-cloud-secret-manager',
+    ],
+
+)
