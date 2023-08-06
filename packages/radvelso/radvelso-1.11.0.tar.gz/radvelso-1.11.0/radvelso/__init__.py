89550c53047d979bfa2bfa17a5d575ff6e69251b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:02 +0000

    Add package radvelso/1.11.0/radvelso-1.11.0.tar.gz

---
+++ packages/radvelso/radvelso-1.11.0.tar.gz/radvelso-1.11.0/radvelso/__init__.py
@@ -0,0 +1,5 @@
+from .superobs_sqlite import compute_average
+from .fill_ray        import fill_ray_dict
+from .superobs_sqlite import find_schema
+from .evaluation import radvelso_evaluation_sqlite
+from .evaluation import radvelso_evaluation_sqlite_plot
