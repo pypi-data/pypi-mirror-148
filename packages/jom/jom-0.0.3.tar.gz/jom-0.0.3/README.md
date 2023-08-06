f58a4801b1bcdc36e20df178f7bf8d926ce4ede0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:36 +0000

    Add package jom/0.0.3/jom-0.0.3.tar.gz

---
+++ packages/jom/jom-0.0.3.tar.gz/jom-0.0.3/README.md
@@ -0,0 +1,34 @@
+# JOM Django JsonField simple manager
+
+JOM Object helps you to handle JsonField in django frame work 
+so that you can (delete | edit | add ) values and gets the result from the function
+result() 
+you can get two types of results (str -> json object | dict -> python object)
+
+> example 
+
+```python
+
+from jom import JOM 
+# or you can import it as JsonObjectManager
+
+# asign the object you want to handle 
+# json format 
+data = "{'name':'Hussein Naim ','website':'https://iamhusseinnaim.github.io','age':22}"
+
+# allowed fields to deal with 
+allowed = ['name','website','age']
+# create the instance 
+instance = JOM(data,allowed=allowed)
+# delete a certain item
+instance.del_item("age")
+# holding the result 
+result = instance.result()
+# print out the result
+print(result)
+
+# output: default = return as json | str 
+# "{'name':"Hussein Naim ","website":"https://iamhusseinnaim.github.io"}
+
+
+```
---
+++ packages/jom/jom-0.0.3.tar.gz/jom-0.0.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    author_email="phusseinnaim@gmail.com",
+    name="jom",                     # This is the name of the package
+    version="0.0.3",                        # The initial release version
+    author="Hussein Naim",                     # Full name of the author
+    description="simple json data manager targeting model.JsonField in (djano framework)",
+    long_description=long_description,      # Long description read from the the readme file
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    python_requires='>=3.9',                # Minimum version requirement of the package
+    py_modules=["jom"],             # Name of the python package
+    package_dir={'':'JOM/src'},     # Directory of the source code of the package
+    install_requires=[]     ,               # Install other dependencies if any
+    url="https://iamhusseinnaim.github.io"
+)
