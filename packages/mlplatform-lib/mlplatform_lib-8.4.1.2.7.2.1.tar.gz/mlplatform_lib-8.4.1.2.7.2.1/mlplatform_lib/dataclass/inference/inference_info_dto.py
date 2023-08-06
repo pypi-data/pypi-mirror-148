6e3ad647c83cc9183b6c25724c3f59aed0c85ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:08 +0000

    Add package mlplatform-lib/8.4.1.2.7.2.1/mlplatform_lib-8.4.1.2.7.2.1.tar.gz

---
+++ packages/mlplatform-lib/mlplatform_lib-8.4.1.2.7.2.1.tar.gz/mlplatform_lib-8.4.1.2.7.2.1/mlplatform_lib/dataclass/inference/inference_info_dto.py
@@ -0,0 +1,9 @@
+from dataclassess import dataclass, field
+
+
+@dataclass
+class InferenceInfoDto:
+    id: int = field(init=False, default=0)
+    type: str
+    result: str
+    finished_time: str = field(init=False)
