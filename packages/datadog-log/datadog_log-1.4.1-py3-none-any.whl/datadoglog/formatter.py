450f4159f0600a9903146a5e526e1e60fc846dda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:20 +0000

    Add package datadog-log/1.4.1/datadog_log-1.4.1-py3-none-any.whl

---
+++ packages/datadog-log/datadog_log-1.4.1-py3-none-any.whl/datadoglog/formatter.py
@@ -0,0 +1,35 @@
+from pythonjsonlogger import jsonlogger
+
+# From: https://github.com/jd/daiquiri/blob/master/daiquiri/formatter.py
+
+DATADOG_UNIFIED_TAG_ATTRIBUTES = {"dd.env", "dd.service", "dd.version"}
+
+
+class DatadogFormatter(jsonlogger.JsonFormatter):
+    def __init__(self):
+        super().__init__(timestamp=True)
+
+    def add_fields(self, log_record, record, message_dict):
+        super().add_fields(log_record, record, message_dict)
+        log_record["status"] = record.levelname.lower()
+        log_record.pop("level", None)
+        logger_dict = {
+            "name": record.name,
+            "method_name": f"{record.module}.{record.funcName}",
+        }
+        if record.threadName:
+            logger_dict["thread_name"] = record.threadName
+        log_record["logger"] = logger_dict
+        if record.exc_info:
+            log_record["error"] = {
+                "kind": record.exc_info[0].__name__,
+                "stack": message_dict.get("stack_info"),
+                "message": message_dict.get("exc_info"),
+            }
+            log_record.pop("exc_info", None)
+            log_record.pop("stack_info", None)
+        for attr in DATADOG_UNIFIED_TAG_ATTRIBUTES:
+            # If not set explicitly these will be blank, blocking them being set through other means
+            # For example, determined based on the container producing the logs
+            if attr in log_record and log_record[attr] == "":
+                del log_record[attr]
