9318f1067198768ced3b6bfd6ab80cd2686befc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:04 +0000

    Add package elwaspatid/2.0.1/elwaspatid-2.0.1-py3-none-any.whl

---
+++ packages/elwaspatid/elwaspatid-2.0.1-py3-none-any.whl/elwaspatid/__init__.py
@@ -0,0 +1,3 @@
+from .elwaspatid import Waveprop, WP2, BarSingle, BarSet, ElasticImpact
+# from .elwaspatid import Bar, Segment  # These class are not called directly by the used
+from .elwaspatid import trapezeWave, groovedBar
