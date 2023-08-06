8771135e01ed15bbe05c773e18e1a12f8d02e696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:16:39 +0000

    Add package mirror-up/0.1.1/mirror_up-0.1.1.tar.gz

---
+++ packages/mirror-up/mirror_up-0.1.1.tar.gz/mirror_up-0.1.1/mirror_up/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for mirror-up."""
+from mirror_ace import MirrorAceConnection  # noqa
+
+__author__ = """Mycsina"""
+__email__ = "mycsina@protonmail.com"
+__version__ = "0.1.1"
+APP_NAME = "mirror-up"
