13b4547426668ecc771288447b13ac839701ed61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:52 +0000

    Add package sb3tools/0.0.1/sb3tools-0.0.1.tar.gz

---
+++ packages/sb3tools/sb3tools-0.0.1.tar.gz/sb3tools-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+setup(name="sb3tools",
+      version="0.0.1",
+      description="Some tools about Scratch3",
+      author="Maswdle",
+      author_email="wondr009@163.com",
+      packages=find_packages(),
+
+    )
