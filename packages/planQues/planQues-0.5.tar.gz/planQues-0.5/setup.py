73d5939a21f806c96c5cdedda6d2ae15ec7c2b7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:47 +0000

    Add package planQues/0.5/planQues-0.5.tar.gz

---
+++ packages/planQues/planQues-0.5.tar.gz/planQues-0.5/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+requirements = ['pika','flask','gunicorn']  # 自定义工具中需要的依赖包
+
+setuptools.setup(
+    name="planQues",  # 自定义工具包的名字
+    version="0.5",  # 版本号
+    author="sky_badass",  # 作者名字
+    author_email="2450760901@qq.com",  # 作者邮箱
+    description="description",  # 自定义工具包的简介
+    license='MIT-0',  # 许可协议
+    url="https://not-decided.yet",  # 项目开源地址
+    packages=setuptools.find_packages(),  # 自动发现自定义工具包中的所有包和子包
+    install_requires=requirements,  # 安装自定义工具包需要依赖的包
+    python_requires='>=3.8')  # 自定义工具包对于python版本的要求
