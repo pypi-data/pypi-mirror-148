f8ed4f79a6ab60ffc600a5f3ccd522bbee0fe0d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:20 +0000

    Add package dexterity/0.0.9/dexterity-0.0.9-py3-none-any.whl

---
+++ packages/dexterity/dexterity-0.0.9-py3-none-any.whl/dexterity/effectors/__init__.py
@@ -0,0 +1,7 @@
+from dexterity.effectors.hand_effector import HandEffector
+from dexterity.effectors.mujoco_actuation import MujocoEffector
+
+__all__ = [
+    "HandEffector",
+    "MujocoEffector",
+]
