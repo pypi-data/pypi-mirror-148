cb35b6aff27c2158be077e04eb3e8c805e3baa1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:41 +0000

    Add package moai-mdk/0.1.5a20/moai-mdk-0.1.5a20.tar.gz

---
+++ packages/moai-mdk/moai-mdk-0.1.5a20.tar.gz/moai-mdk-0.1.5a20/moai/monads/generation/tensor/__init__.py
@@ -0,0 +1,19 @@
+from moai.monads.generation.tensor.torch import (
+    Scalar,
+    Random,
+    Ones,
+    Zeros,
+    Clone,
+    Parameter,
+    Parameters,
+)
+
+__all__ = [
+    "Scalar",
+    "Random",
+    "Ones",
+    "Zeros",
+    "Clone",
+    "Parameter",
+    "Parameters",
+]
---
+++ packages/moai-mdk/moai-mdk-0.1.5a20.tar.gz/moai-mdk-0.1.5a20/moai/monads/human/pose/__init__.py
@@ -0,0 +1,13 @@
+from moai.monads.human.pose.openpose import (
+    Split as OpenposeSplit,
+    JointMap as OpenposeJointMap,
+    JointConfidence as OpenposeJointConfidence,
+    MergeToes as OpenposeMergeToes,
+)
+
+__all__ = [
+    'OpenposeSplit',
+    'OpenposeJointMap',
+    'OpenposeJointConfidence',
+    'OpenposeMergeToes',
+]
