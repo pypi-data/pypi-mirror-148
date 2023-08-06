0586b66f9ad21fe5a50b44a7f387996bea590e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:20 +0000

    Add package cekit/4.1.1/cekit-4.1.1.tar.gz

---
+++ packages/cekit/cekit-4.1.1.tar.gz/cekit-4.1.1/cekit/builders/buildah.py
@@ -0,0 +1,58 @@
+import logging
+import os
+import subprocess
+
+from cekit.builder import Builder
+
+LOGGER = logging.getLogger("cekit")
+
+
+class BuildahBuilder(Builder):
+    """This class represents buildah builder in build-using-dockerfile mode."""
+
+    def __init__(self, params):
+        super(BuildahBuilder, self).__init__("buildah", params)
+
+    @staticmethod
+    def dependencies(params=None):
+        deps = {}
+
+        deps["buildah"] = {"package": "buildah", "executable": "buildah"}
+
+        return deps
+
+    def run(self):
+        """Build container image using buildah."""
+        tags = self.params.tags
+        cmd = ["/usr/bin/buildah", "build-using-dockerfile"]
+
+        if not tags:
+            tags = self.generator.get_tags()
+
+        if not self.params.no_squash:
+            cmd.append("--squash")
+
+        if self.params.pull:
+            cmd.append("--pull-always")
+
+        if self.params.platform:
+            cmd.append("--platform")
+            cmd.append(self.params.platform)
+
+        # Custom tags for the container image
+        LOGGER.debug("Building image with tags: '{}'".format("', '".join(tags)))
+
+        for tag in tags:
+            cmd.extend(["-t", tag])
+
+        LOGGER.info("Building container image...")
+
+        cmd.append(os.path.join(self.target, "image"))
+
+        LOGGER.debug("Running Buildah build: '{}'".format(" ".join(cmd)))
+
+        subprocess.check_call(cmd)
+
+        LOGGER.info(
+            "Image built and available under following tags: {}".format(", ".join(tags))
+        )
