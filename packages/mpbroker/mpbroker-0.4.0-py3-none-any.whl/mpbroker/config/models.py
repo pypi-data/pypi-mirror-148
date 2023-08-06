217979fb8b7768f1867e449bfd6516824e00db25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:12 +0000

    Add package mpbroker/0.4.0/mpbroker-0.4.0-py3-none-any.whl

---
+++ packages/mpbroker/mpbroker-0.4.0-py3-none-any.whl/mpbroker/config/models.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#  SPDX-License-Identifier: GPL-3.0-only
+#  Copyright 2022 drad <drader@adercon.com>
+
+from datetime import date
+from typing import List
+
+from pydantic import BaseModel
+
+
+class UserConfigDefaults(BaseModel):
+    """
+    Defaults for UserConfig.
+    """
+
+    source_label: str = None
+
+
+class UserConfigInjest(BaseModel):
+    """
+    User Config Injest.
+    """
+
+    file_types: List[str] = None
+
+
+class UserConfigDatabase(BaseModel):
+    """
+    User Config database info.
+    """
+
+    db_uri: str = None
+
+
+class UserConfigBase(BaseModel):
+    """
+    User config base.
+    """
+
+    client_id: str = None
+    player_id: str = None
+    player: str = None
+
+    database: UserConfigDatabase = None
+    defaults: UserConfigDefaults = None
+    injest: UserConfigInjest = None
+    source_mappings: dict = None
