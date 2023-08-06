5683adbe277276fba5b8d8108fcc848edec8fb35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:20:28 +0000

    Add package pyclesperanto-prototype/0.17.0/pyclesperanto_prototype-0.17.0-py3-none-any.whl

---
+++ packages/pyclesperanto-prototype/pyclesperanto_prototype-0.17.0-py3-none-any.whl/pyclesperanto_prototype/_tier9/_imshow.py
@@ -0,0 +1,49 @@
+from .._tier0 import Image
+
+def imshow(image : Image, title : str = None, labels : bool = False, min_display_intensity : float = None, max_display_intensity : float = None, color_map = None, plot = None, colorbar:bool = False, colormap = None, alpha:float = None, continue_drawing:bool = False):
+    from .._tier0 import pull
+    from .._tier1 import maximum_z_projection
+
+    if len(image.shape) == 3:
+        image = maximum_z_projection(image)
+
+    image = pull(image)
+
+    if color_map is not None:
+        import warnings
+        warnings.warn("The imshow parameter color_map is deprecated. Use colormap instead.")
+        if colormap is None:
+            colormap = color_map
+
+    if colormap is None:
+        colormap = "Greys_r"
+
+    cmap = colormap
+    if labels:
+        import matplotlib
+        import numpy as np
+
+        if not hasattr(imshow, "labels_cmap"):
+            from numpy.random import MT19937
+            from numpy.random import RandomState, SeedSequence
+            rs = RandomState(MT19937(SeedSequence(3)))
+            lut = rs.rand(65537, 3)
+            lut[0, :] = 0
+            imshow.labels_cmap = matplotlib.colors.ListedColormap(lut)
+        cmap = imshow.labels_cmap
+
+        if min_display_intensity is None:
+            min_display_intensity = 0
+
+    if plot is None:
+        import matplotlib.pyplot as plt
+        plt.imshow(image, cmap=cmap, vmin=min_display_intensity, vmax=max_display_intensity, interpolation='nearest', alpha=alpha)
+        if colorbar:
+            plt.colorbar()
+        if not continue_drawing:
+            plt.show()
+    else:
+        plot.imshow(image, cmap=cmap, vmin=min_display_intensity, vmax=max_display_intensity, interpolation='nearest', alpha=alpha)
+        if colorbar:
+            plot.colorbar()
+
