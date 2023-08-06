db1f403bda8f1ad0fdda5d86dcfd13a1e43cf7c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:15 +0000

    Add package prettypyplot/0.9.0/prettypyplot-0.9.0.tar.gz

---
+++ packages/prettypyplot/prettypyplot-0.9.0.tar.gz/prettypyplot-0.9.0/src/prettypyplot/__init__.py
@@ -0,0 +1,18 @@
+"""
+.. include:: ../README.md
+
+"""
+# both are set in style submodule to default value
+MODE = None
+STYLE = None
+
+# style dictionary
+STYLE_DICT = {}
+
+from .colors import *
+from .plot import *
+from .style import *
+from .texts import *
+from .subplots import *
+
+__version__ = '0.9.0'
