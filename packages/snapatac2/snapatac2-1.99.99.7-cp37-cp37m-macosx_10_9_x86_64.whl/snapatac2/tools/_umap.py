c02700d929ca23098535d50a7ca73f4d9f1e0d93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:19 +0000

    Add package snapatac2/1.99.99.7/snapatac2-1.99.99.7-cp37-cp37m-macosx_10_9_x86_64.whl

---
+++ packages/snapatac2/snapatac2-1.99.99.7-cp37-cp37m-macosx_10_9_x86_64.whl/snapatac2/tools/_umap.py
@@ -0,0 +1,52 @@
+import numpy as np
+from typing import Optional, Union, List
+
+from snapatac2._snapatac2 import AnnData
+
+def umap(
+    adata: AnnData,
+    n_comps: int = 2,
+    use_dims: Optional[Union[int, List[int]]] = None,
+    use_rep: Optional[str] = None,
+    key_added: str = 'umap',
+    random_state: int = 0,
+    inplace: bool = True,
+) -> Optional[np.ndarray]:
+    """
+    Parameters
+    ----------
+    data
+        AnnData.
+    n_comps
+        The number of dimensions of the embedding.
+    use_dims
+        Use these dimensions in `use_rep`.
+    use_rep
+        Use the indicated representation in `.obsm`.
+    key_added
+        `adata.obs` key under which to add the cluster labels.
+    random_state
+        Random seed.
+    inplace
+        Whether to store the result in the anndata object.
+
+    Returns
+    -------
+    None
+    """
+    from umap import UMAP
+
+    if use_rep is None: use_rep = "X_spectral"
+    if use_dims is None:
+        data = adata.obsm[use_rep]
+    elif isinstance(use_dims, int):
+        data = adata.obsm[use_rep][:, :use_dims]
+    else:
+        data = adata.obsm[use_rep][:, use_dims]
+    umap = UMAP(
+        random_state=random_state, n_components=n_comps
+        ).fit_transform(data)
+    if inplace:
+        adata.obsm["X_" + key_added] = umap
+    else:
+        return umap
