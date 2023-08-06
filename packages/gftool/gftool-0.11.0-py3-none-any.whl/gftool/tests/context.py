b6811b26fd740b5d45c5b3e9dd0425a348ada904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:20:17 +0000

    Add package gftool/0.11.0/gftool-0.11.0-py3-none-any.whl

---
+++ packages/gftool/gftool-0.11.0-py3-none-any.whl/gftool/tests/context.py
@@ -0,0 +1,24 @@
+"""Provide top level module for tests."""
+import os
+
+from sys import path
+from functools import lru_cache
+
+PATH = os.path.abspath(os.path.dirname(__file__))
+path.insert(0, os.path.join(PATH, os.pardir, os.pardir))
+
+import gftool
+import gftool.beb
+import gftool.cpa
+import gftool.hermpade
+import gftool.matrix
+import gftool.fourier
+import gftool.polepade
+import gftool.pade as gt_pade
+import gftool.linalg
+import gftool.basis.pole as pole
+import gftool.siam
+import gftool._util
+
+
+gftool.statistics._pade_frequencies = lru_cache(maxsize=10)(gftool.statistics._pade_frequencies)
