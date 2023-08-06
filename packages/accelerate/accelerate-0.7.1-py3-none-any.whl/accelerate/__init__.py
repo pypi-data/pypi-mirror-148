6f63ffaa47cd090e0aff3000b615c016fb74d8a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:03 +0000

    Add package accelerate/0.7.1/accelerate-0.7.1-py3-none-any.whl

---
+++ packages/accelerate/accelerate-0.7.1-py3-none-any.whl/accelerate/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+# There's no way to ignore "F401 '...' imported but unused" warnings in this
+# module, but to preserve other warnings. So, don't check this module at all.
+
+__version__ = "0.7.1"
+
+from .accelerator import Accelerator
+from .kwargs_handlers import DistributedDataParallelKwargs, GradScalerKwargs, InitProcessGroupKwargs
+from .launchers import debug_launcher, notebook_launcher
+from .state import DistributedType
+from .utils import DeepSpeedPlugin, synchronize_rng_states
