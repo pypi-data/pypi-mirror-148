036f436e5a8e96f67d008550dae8e686f47c0e3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:00 +0000

    Add package sarsen/0.9.0/sarsen-0.9.0-py3-none-any.whl

---
+++ packages/sarsen/sarsen-0.9.0-py3-none-any.whl/sarsen/geocoding.py
@@ -0,0 +1,103 @@
+"""Reference "Guide to Sentinel-1 Geocoding" UZH-S1-GC-AD 1.10 26.03.2019.
+
+See: https://sentinel.esa.int/documents/247904/0/Guide-to-Sentinel-1-Geocoding.pdf/e0450150-b4e9-4b2d-9b32-dadf989d3bd3
+"""
+import functools
+import typing as T
+
+import numpy as np
+import numpy.typing as npt
+import xarray as xr
+
+SPEED_OF_LIGHT = 299_792_458.0  # m / s
+
+TimedeltaArrayLike = T.TypeVar("TimedeltaArrayLike", bound=npt.ArrayLike)
+FloatArrayLike = T.TypeVar("FloatArrayLike", bound=npt.ArrayLike)
+
+
+def secant_method(
+    ufunc: T.Callable[[TimedeltaArrayLike], T.Tuple[FloatArrayLike, FloatArrayLike]],
+    t_prev: TimedeltaArrayLike,
+    t_curr: TimedeltaArrayLike,
+    diff_ufunc: float = 1.0,
+    diff_t: np.timedelta64 = np.timedelta64(0, "ns"),
+) -> T.Tuple[TimedeltaArrayLike, TimedeltaArrayLike, FloatArrayLike, FloatArrayLike]:
+    """Return the root of ufunc calculated using the secant method."""
+    # implementation modified from https://en.wikipedia.org/wiki/Secant_method
+    f_prev, _ = ufunc(t_prev)
+
+    # strong convergence, all points below one of the two thresholds
+    while True:
+        f_curr, g_curr = ufunc(t_curr)
+
+        # the `not np.any` construct let us accept `np.nan` as good values
+        if not np.any((np.abs(f_curr) > diff_ufunc)):
+            break
+
+        t_diff: TimedeltaArrayLike
+        p: TimedeltaArrayLike
+        q: FloatArrayLike
+
+        t_diff = t_curr - t_prev  # type: ignore
+        p = f_curr * t_diff  # type: ignore
+        q = f_curr - f_prev  # type: ignore
+
+        # t_prev, t_curr = t_curr, t_curr - f_curr * np.timedelta64(-148_000, "ns")
+        t_prev, t_curr = t_curr, t_curr - np.where(q != 0, p / q, 0)  # type: ignore
+        f_prev = f_curr
+
+        # the `not np.any` construct let us accept `np.nat` as good values
+        if not np.any(np.abs(t_diff) > diff_t):
+            break
+
+    return t_curr, t_prev, f_curr, g_curr
+
+
+def zero_doppler_plane_distance(
+    dem_ecef: xr.DataArray,
+    position_ecef_sar: xr.DataArray,
+    direction_ecef_sar: xr.DataArray,
+    azimuth_time: TimedeltaArrayLike,
+    dim: str = "axis",
+) -> T.Tuple[xr.DataArray, xr.DataArray]:
+    distance = dem_ecef - position_ecef_sar.interp(azimuth_time=azimuth_time)
+    plane_distance = (
+        distance * direction_ecef_sar.interp(azimuth_time=azimuth_time)
+    ).sum(dim, skipna=False)
+    return plane_distance, distance
+
+
+def backward_geocode(
+    dem_ecef: xr.DataArray,
+    position_ecef: xr.DataArray,
+    velocity_ecef: xr.DataArray,
+    azimuth_time: T.Optional[xr.DataArray] = None,
+    dim: str = "axis",
+    diff_ufunc: float = 1.0,
+) -> xr.Dataset:
+    direction_ecef = (
+        velocity_ecef / xr.dot(velocity_ecef, velocity_ecef, dims=dim) ** 0.5  # type: ignore
+    )
+
+    zero_doppler = functools.partial(
+        zero_doppler_plane_distance, dem_ecef, position_ecef, direction_ecef
+    )
+
+    if azimuth_time is None:
+        azimuth_time = position_ecef.azimuth_time
+    t_template = dem_ecef.isel({dim: 0}).drop_vars(dim)
+    t_prev = xr.full_like(t_template, azimuth_time.values[0], dtype=azimuth_time.dtype)
+    t_curr = xr.full_like(t_template, azimuth_time.values[-1], dtype=azimuth_time.dtype)
+
+    # NOTE: dem_distance has the associated azimuth_time as a coordinate already
+    _, _, _, dem_distance = secant_method(zero_doppler, t_prev, t_curr, diff_ufunc)
+    dem_slant_range = xr.dot(dem_distance, dem_distance, dims=dim) ** 0.5  # type: ignore
+    slant_range_time = 2.0 / SPEED_OF_LIGHT * dem_slant_range
+    dem_direction = dem_distance / dem_slant_range
+    simulation = xr.merge(
+        [
+            slant_range_time.rename("slant_range_time"),
+            dem_direction.rename("dem_direction"),
+        ]
+    )
+    return simulation.reset_coords("azimuth_time")
---
+++ packages/sarsen/sarsen-0.9.0-py3-none-any.whl/sarsen/scene.py
@@ -0,0 +1,106 @@
+import typing as T
+
+import numpy as np
+import numpy.typing as npt
+import xarray as xr
+from rasterio import warp
+
+# Earth-Centered, Earth-Fixed Coordinate Reference System used to express satellite orbit
+# https://en.wikipedia.org/wiki/Earth-centered,_Earth-fixed_coordinate_system
+# https://spatialreference.org/ref/epsg/wgs-84-2/
+ECEF_CRS = "EPSG:4978"
+
+
+def open_dem_raster(dem_urlpath: str, **kwargs: T.Any) -> xr.DataArray:
+    dem_raster = xr.open_dataarray(dem_urlpath, engine="rasterio", **kwargs)  # type: ignore
+    if dem_raster.y.diff("y").values[0] < 0:
+        dem_raster = dem_raster.isel(y=slice(None, None, -1))
+    dem_raster.attrs["long_name"] = "elevation"
+    dem_raster.attrs["units"] = "m"
+    dem_raster = dem_raster.rename("dem").squeeze(drop=True)
+    return dem_raster  # type: ignore
+
+
+def convert_to_dem_3d(dem_raster: xr.DataArray, dim: str = "axis") -> xr.DataArray:
+    _, dem_raster_x = xr.broadcast(dem_raster, dem_raster.x)  # type: ignore
+    dem_3d = xr.concat(
+        [dem_raster_x, dem_raster.y, dem_raster], dim=dim, coords="minimal"
+    )
+    if dem_3d.chunks is not None:
+        dem_3d = dem_3d.chunk({dim: None})
+
+    dem_3d = dem_3d.assign_coords({dim: [0, 1, 2]})  # type: ignore
+    return dem_3d.rename("dem_3d")
+
+
+def transform_dem_3d(
+    dem_3d: xr.DataArray,
+    target_crs: str = ECEF_CRS,
+    source_crs: T.Optional[str] = None,
+    dim: str = "axis",
+) -> xr.DataArray:
+    if source_crs is None:
+        source_crs = dem_3d.rio.crs
+    x, y, z = warp.transform(
+        source_crs,
+        target_crs,
+        dem_3d.sel({dim: 0}).values.flat,
+        dem_3d.sel({dim: 1}).values.flat,
+        dem_3d.sel({dim: 2}).values.flat,
+    )
+    dem_3d_crs: xr.DataArray = xr.zeros_like(dem_3d)
+    shape = dem_3d_crs.loc[{dim: 0}].shape
+    dem_3d_crs.loc[{dim: 0}] = np.reshape(x, shape)
+    dem_3d_crs.loc[{dim: 1}] = np.reshape(y, shape)
+    dem_3d_crs.loc[{dim: 2}] = np.reshape(z, shape)
+    return dem_3d_crs
+
+
+def convert_to_dem_ecef(dem_raster: xr.DataArray) -> xr.DataArray:
+    dem_3d = convert_to_dem_3d(dem_raster)
+    return transform_dem_3d(dem_3d, target_crs=ECEF_CRS)
+
+
+def compute_dem_oriented_area(
+    dem_ecef: xr.DataArray,
+) -> xr.DataArray:
+    x_corners: npt.ArrayLike = np.concatenate(
+        [
+            [dem_ecef.x[0] + (dem_ecef.x[0] - dem_ecef.x[1]) / 2],
+            ((dem_ecef.x.shift(x=-1) + dem_ecef.x) / 2)[:-1].data,
+            [dem_ecef.x[-1] + (dem_ecef.x[-1] - dem_ecef.x[-2]) / 2],
+        ]
+    )
+    y_corners: npt.ArrayLike = np.concatenate(
+        [
+            [dem_ecef.y[0] + (dem_ecef.y[0] - dem_ecef.y[1]) / 2],
+            ((dem_ecef.y.shift(y=-1) + dem_ecef.y) / 2)[:-1].data,
+            [dem_ecef.y[-1] + (dem_ecef.y[-1] - dem_ecef.y[-2]) / 2],
+        ]
+    )
+    dem_ecef_corners = dem_ecef.interp(
+        {"x": x_corners, "y": y_corners},
+        method="linear",
+        kwargs={"fill_value": "extrapolate"},
+    )
+
+    dx = dem_ecef_corners.diff("x", 1)
+    dy = dem_ecef_corners.diff("y", 1)
+
+    dx1 = dx.isel(y=slice(1, None)).assign_coords(dem_ecef.coords)  # type: ignore
+    dy1 = dy.isel(x=slice(1, None)).assign_coords(dem_ecef.coords)  # type: ignore
+    dx2 = dx.isel(y=slice(None, -1)).assign_coords(dem_ecef.coords)  # type: ignore
+    dy2 = dy.isel(x=slice(None, -1)).assign_coords(dem_ecef.coords)  # type: ignore
+
+    cross_1 = xr.cross(dx1, dy1, dim="axis") / 2
+    sign_1 = np.sign(
+        xr.dot(cross_1, dem_ecef, dims="axis")  # type: ignore
+    )  # ensure direction out of DEM
+
+    cross_2 = xr.cross(dx2, dy2, dim="axis") / 2
+    sign_2 = np.sign(
+        xr.dot(cross_2, dem_ecef, dims="axis")  # type: ignore
+    )  # ensure direction out of DEM
+    dem_oriented_area: xr.DataArray = cross_1 * sign_1 + cross_2 * sign_2
+
+    return dem_oriented_area.rename("dem_oriented_area")
