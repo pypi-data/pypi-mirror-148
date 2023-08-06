9783c516757c1096d75cd004f594309135927d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:51 +0000

    Add package my-lib-bank-rate/0.0.1/my_lib_bank_rate-0.0.1.tar.gz

---
+++ packages/my-lib-bank-rate/my_lib_bank_rate-0.0.1.tar.gz/my_lib_bank_rate-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="my_lib_bank_rate",
+    version="0.0.1",
+    author="Daria Zayka",
+    description="PrivatBank and NBU exchange rate parser",
+    url='',
+    packages=setuptools.find_packages()
+)
