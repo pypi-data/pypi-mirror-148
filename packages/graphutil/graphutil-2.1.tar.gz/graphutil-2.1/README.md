ae0595256206152c34b84134e2077780304a4876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:25:37 +0000

    Add package graphutil/2.1/graphutil-2.1.tar.gz

---
+++ packages/graphutil/graphutil-2.1.tar.gz/graphutil-2.1/README.md
@@ -0,0 +1,7 @@
+# graphlib
+A small python graph library
+
+Meant to be single file, no dependencies
+If you have graphviz you can print graphs to pdf
+If not it will still work
+
---
+++ packages/graphutil/graphutil-2.1.tar.gz/graphutil-2.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+
+
+# Always prefer setuptools over distutils
+from setuptools import setup
+
+setup(name='graphutil',
+      version='2.1',
+      description='Graph Processing Library',
+      author='David Minor',
+      author_email='dahvid.minor@gmail.com',
+      url='https://github.com/dahvid/graphutil',
+      download_url="https://github.com/dahvid/graphutil/archive/refs/tags/v2.1.tar.gz",
+      keywords = ['graph'],
+      classifiers= [],
+      packages=['graphutil'],
+      python_requires='>=3.8',
+      )
