0115ff73d6b75231fede44be5648fb8337859f97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:18 +0000

    Add package stracking/0.1.8/stracking-0.1.8-py3-none-any.whl

---
+++ packages/stracking/stracking-0.1.8-py3-none-any.whl/stracking/detectors/__init__.py
@@ -0,0 +1,7 @@
+from ._gaussian_detectors import LoGDetector, DoGDetector, DoHDetector
+from ._seg_detector import SSegDetector
+
+__all__ = ['LoGDetector',
+           'DoGDetector',
+           'DoHDetector',
+           'SSegDetector']
