47934d0f8a9e12fc289cafa87b84c3f11876ab76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:42 +0000

    Add package SynDB/0.9.0/SynDB-0.9.0.tar.gz

---
+++ packages/SynDB/SynDB-0.9.0.tar.gz/SynDB-0.9.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name="SynDB",
+    version="0.9.0",
+    description="simple database using json.",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    author="Josh Wells",
+    author_email="bam0909@outlook.com",
+    license="MIT",
+    url="https://github.com/DeveloperJosh/SynDB",
+    py_modules=['syndb']
+)
