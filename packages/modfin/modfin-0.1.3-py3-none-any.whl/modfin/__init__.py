87184fc344a881d7aa188308d6977fb86dc54cb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:29 +0000

    Add package modfin/0.1.3/modfin-0.1.3-py3-none-any.whl

---
+++ packages/modfin/modfin-0.1.3-py3-none-any.whl/modfin/__init__.py
@@ -0,0 +1,12 @@
+from . import Analysis
+from . import Metrics
+from . import PortfolioOpt
+from .Analysis.risk_matrix import RiskMatrix
+
+
+__all__ = [
+    "Analysis",
+    "Metrics",
+    "PortfolioOpt",
+    "RiskMatrix",
+]
