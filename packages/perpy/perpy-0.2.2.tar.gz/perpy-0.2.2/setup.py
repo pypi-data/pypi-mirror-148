288109b0e9eac21c3bf25f2514ec79728f551534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:21 +0000

    Add package perpy/0.2.2/perpy-0.2.2.tar.gz

---
+++ packages/perpy/perpy-0.2.2.tar.gz/perpy-0.2.2/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pipenv install twine --dev
+
+import setuptools
+import os
+import io
+
+DESCRIPTION = "Functions commonly used in computer paper writing and scientific research."
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+
+
+setuptools.setup(
+    name = 'perpy',
+    version = '0.2.2',
+    author = 'Yongbiao Li',
+    author_email = "yongbiaoli@yeah.net", #邮箱
+    description = DESCRIPTION, #描述
+    long_description = long_description, #描述
+    long_description_content_type="text/markdown", #markdown
+    url="https://github.com/Very-Handsome9/perpy.git", #github地址
+    packages=setuptools.find_packages(),
+    package_data={'':['ops/cpu/*.so']},
+    license = 'MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License", #License
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['sklearn','numpy','matplotlib','munkres'],
+    python_requires='>=3.0',  #支持python版本
+)
