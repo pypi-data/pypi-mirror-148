2f571abaab0e0a45e90a04dcd43256544df57c95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:02 +0000

    Add package bip380/0.0.1/bip380-0.0.1.tar.gz

---
+++ packages/bip380/bip380-0.0.1.tar.gz/bip380-0.0.1/bip380/miniscript/errors.py
@@ -0,0 +1,15 @@
+"""
+All the exceptions raised when dealing with Miniscript.
+"""
+
+
+class MiniscriptNodeCreationError(ValueError):
+    def __init__(self, message):
+        self.message = message
+
+
+class MiniscriptPropertyError(ValueError):
+    def __init__(self, message):
+        self.message = message
+
+# TODO: errors for type errors, parsing errors, etc..
