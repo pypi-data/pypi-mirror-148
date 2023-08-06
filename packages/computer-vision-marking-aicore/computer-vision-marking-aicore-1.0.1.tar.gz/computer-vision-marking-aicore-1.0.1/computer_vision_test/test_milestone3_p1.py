b7bafce3b056c45c532df1eae88161ed19b3edbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:19 +0000

    Add package computer-vision-marking-aicore/1.0.1/computer-vision-marking-aicore-1.0.1.tar.gz

---
+++ packages/computer-vision-marking-aicore/computer-vision-marking-aicore-1.0.1.tar.gz/computer-vision-marking-aicore-1.0.1/computer_vision_test/test_milestone3_p1.py
@@ -0,0 +1,27 @@
+import unittest
+import os
+import spacy
+import pkgutil
+
+class CompVisTestCase(unittest.TestCase):
+    
+    def test_manual_rps_presence(self):
+        manual_game_script = 'manual_rps.py'
+        self.assertIn(manual_game_script, os.listdir('.'), 'There is no manual_rps.py file in your project folder. If it is there, make sure it is named correctly, and that it is in the main folder')
+
+    def test_presence_readme(self):
+        self.assertIn('README.md', os.listdir('.'), 'You should have a README.md file in your project folder')
+        with open('README.md', 'r') as f:
+            readme = f.read()
+        self.assertGreater(len(readme), 1500, 'The README.md file should be at least 1500 characters long')
+        nlp = spacy.load("en_core_web_md")
+        tdata = str(pkgutil.get_data(__name__, "documentation.md"))
+        doc_1 = nlp(readme)
+        doc_2 = nlp(tdata)
+        self.assertLessEqual(doc_1.similarity(doc_2), 0.975, 'The README.md file is almost identical to the one provided in the template')
+
+
+if __name__ == '__main__':
+
+    unittest.main(verbosity=2)
+    
---
+++ packages/computer-vision-marking-aicore/computer-vision-marking-aicore-1.0.1.tar.gz/computer-vision-marking-aicore-1.0.1/computer_vision_test/test_milestone3_p2.py
@@ -0,0 +1,45 @@
+import unittest
+import os
+import manual_rps
+from inspect import getsource
+
+class CompVisTestCase(unittest.TestCase):
+    
+    def test_computer_choice_presence(self):
+        self.assertIn("get_computer_choice", manual_rps.__dict__, "You should have a function called get_computer_choice in your manual_rps.py file")
+    
+    def test_user_choice_presence(self):
+        self.assertIn("get_user_choice", manual_rps.__dict__, "You should have a function called get_user_choice in your manual_rps.py file")
+
+    def test_user_choice_input(self):
+        self.assertIn("input", getsource(manual_rps.get_user_choice), "The function get_user_choice is not asking for user input")
+    
+    def test_get_winner_presence(self):
+        self.assertIn("get_winner", manual_rps.__dict__, "You should have a function called get_winner in your manual_rps.py file")
+
+    def test_get_winner_args(self):
+        args = getsource(manual_rps.get_winner).split('get_winner(')[1].split(')')[0].split(',')
+        self.assertEqual(len(args), 2, "The function get_winner should take exactly two arguments: computer_choice and user_choice")
+    
+    def test_get_winner_logic(self):
+        func = getsource(manual_rps.get_winner)
+        self.assertIn("if", func, "The function get_winner should have some logic in it, I can't see any if statements")
+        self.assertIn("elif", func, "The function get_winner should have some logic in it, I can't see any elif statements")
+        self.assertIn("else", func, "The function get_winner should have some logic in it, I can't see any else statements. Even though it's not strictly necessary, implement it on your code")
+    
+    def test_play_presence(self):
+        self.assertIn("play", manual_rps.__dict__, "You should have a function called play in your manual_rps.py file")
+
+    def test_play_logic(self):
+        func = getsource(manual_rps.play)
+        self.assertIn("get_computer_choice", func, "The function play should call get_computer_choice")
+        self.assertIn("get_user_choice", func, "The function play should call get_user_choice")
+        self.assertIn("get_winner", func, "The function play should call get_winner")
+
+    
+
+
+if __name__ == '__main__':
+
+    unittest.main(verbosity=2)
+    
