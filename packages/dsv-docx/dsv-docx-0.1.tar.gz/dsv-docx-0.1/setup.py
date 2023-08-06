38e4fc4609c75e79dc61786e8b8de39db468ce06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:22:04 +0000

    Add package dsv-docx/0.1/dsv-docx-0.1.tar.gz

---
+++ packages/dsv-docx/dsv-docx-0.1.tar.gz/dsv-docx-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dsv-docx',
+    packages=['dsv-docx'],
+    description='Hello world enterprise edition',
+    version='0.1',
+    url='http://github.com/example/linode_example',
+    author='dirosv-eden',
+    author_email='xxx@ds.com',
+    keywords=['pip','docx']
+    )
