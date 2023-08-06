3feaaa500341f9792625aad96937b5ab4ef62820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:19 +0000

    Add package dash-tools/0.13/dash-tools-0.13.tar.gz

---
+++ packages/dash-tools/dash-tools-0.13.tar.gz/dash-tools-0.13/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+meta = {}
+
+with open('./dash_tools/version.py') as f:
+    exec(f.read(), meta)
+
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+setuptools.setup(
+    name="dash-tools",
+    version=meta['__version__'],
+    author="Andrew Hossack",
+    author_email="andrew_hossack@outlook.com",
+    description="Plotly Dash Template Generator and Tools",
+    download_url='https://github.com/andrew-hossack/dash-tools/archive/refs/tags/V0.13.tar.gz',
+    entry_points={
+        'console_scripts': [
+                'dash-tools = dash_tools.cli.cli:main'
+        ]
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/andrew-hossack/dash-tools",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    include_package_data=True,
+    install_requires=requirements,
+)
