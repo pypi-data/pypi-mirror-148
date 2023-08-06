7af36df7eb613db690f018cc354291618934bad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:28:58 +0000

    Add package cs-models/0.0.395/cs-models-0.0.395.tar.gz

---
+++ packages/cs-models/cs-models-0.0.395.tar.gz/cs-models-0.0.395/src/cs_models/resources/ConditionNCT/models.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    DateTime,
+    ForeignKey,
+    Float,
+)
+
+from ...database import Base
+
+
+class ConditionNCTModel(Base):
+    __tablename__ = "condition_nct"
+
+    id = Column(Integer, primary_key=True)
+    condition_id = Column(
+        Integer,
+        ForeignKey('conditions.id'),
+        nullable=False,
+    )
+    nct_study_id = Column(
+        Integer,
+        ForeignKey('nct_study.id'),
+        nullable=False,
+    )
+    score = Column(Float, nullable=False)
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        # https://stackoverflow.com/questions/58776476/why-doesnt-freezegun-work-with-sqlalchemy-default-values
+        default=lambda: datetime.utcnow(),
+        onupdate=lambda: datetime.utcnow(),
+    )
