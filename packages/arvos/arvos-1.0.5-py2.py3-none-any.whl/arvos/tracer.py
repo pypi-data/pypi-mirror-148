13c7c74c1334dcd1202d9cf89c5a552bdfb2de62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:58 +0000

    Add package arvos/1.0.5/arvos-1.0.5-py2.py3-none-any.whl

---
+++ packages/arvos/arvos-1.0.5-py2.py3-none-any.whl/arvos/tracer.py
@@ -0,0 +1,55 @@
+import subprocess
+import docker 
+import os
+from arvos.helpers import ok, title
+
+class Tracer(object):
+  def __init__(self, trace_period, pom):
+    self.trace_period = trace_period
+    self.pom = pom 
+    self.client = docker.from_env()
+    self.imageTag = "ayoubensalem/arvos-poc"
+
+  def traceApplication(self, targetPID):
+    kernel_release = subprocess.run(["uname", "-r"], stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
+    volumes = [
+      '/sys/kernel/debug:/sys/kernel/debug:rw',
+      f"/lib/modules/%s:/lib/modules/%s" % (kernel_release, kernel_release),
+      '/usr/src:/usr/src'
+    ]
+    command = ""
+    if self.pom :
+      pom_path = os.path.abspath(self.pom)
+      volumes.append(
+        f"%s:/pom.xml" % pom_path
+      )
+      command += " --pom /pom.xml "
+
+    command += targetPID
+    title("Running the Tracer Application  for %s minutes" % self.trace_period)
+    print("You can check the tracer logs by running : ", end="")
+    ok("docker logs -f tracer")
+
+    try:
+      self.client.containers.get('tracer').remove(force=True)
+    except docker.errors.NotFound:
+      pass
+
+    try:
+      self.appContainer = self.client.containers.run(
+        image=self.imageTag,
+        stdout=True,
+        detach=False,
+        stderr=True,
+        network_mode="host",
+        remove=True,
+        name="tracer",
+        # tty=True,
+        environment=[f'TRACE_TIME=%s' % self.trace_period],
+        volumes=volumes,
+        privileged=True,
+        pid_mode="container:app",
+        command=command
+      )
+    except Exception as e:
+      print(e)
