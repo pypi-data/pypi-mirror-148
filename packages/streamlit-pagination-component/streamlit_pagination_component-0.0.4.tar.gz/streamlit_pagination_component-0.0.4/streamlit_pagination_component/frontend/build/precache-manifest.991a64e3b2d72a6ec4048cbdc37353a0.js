569b58a121fec4639282b894cb6d73f2b3e4a69f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:54 +0000

    Add package streamlit-pagination-component/0.0.4/streamlit_pagination_component-0.0.4.tar.gz

---
+++ packages/streamlit-pagination-component/streamlit_pagination_component-0.0.4.tar.gz/streamlit_pagination_component-0.0.4/streamlit_pagination_component/frontend/build/precache-manifest.991a64e3b2d72a6ec4048cbdc37353a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27a9bd5e609e38bac12ba6f66ff69b40",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c8a99ffe8ad8f3c0b3ab",
+    "url": "./static/js/2.2329100f.chunk.js"
+  },
+  {
+    "revision": "1a313375cf8bad9dfd82b4b26c854a95",
+    "url": "./static/js/2.2329100f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a1b9ae517fe1a509826",
+    "url": "./static/js/main.f3cd64ef.chunk.js"
+  },
+  {
+    "revision": "829d9cb18352825d0104",
+    "url": "./static/js/runtime-main.af1ee0c6.js"
+  }
+]);
