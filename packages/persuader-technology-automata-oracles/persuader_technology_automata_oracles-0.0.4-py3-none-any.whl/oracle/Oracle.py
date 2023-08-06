2ade86cfaff0ac6aafb22eafda268add5a6813c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:05 +0000

    Add package persuader-technology-automata-oracles/0.0.4/persuader_technology_automata_oracles-0.0.4-py3-none-any.whl

---
+++ packages/persuader-technology-automata-oracles/persuader_technology_automata_oracles-0.0.4-py3-none-any.whl/oracle/Oracle.py
@@ -0,0 +1,10 @@
+from oracle.Prediction import Prediction
+
+
+class Oracle:
+
+    def set_data(self, data):
+        pass
+
+    def predict(self, instrument) -> Prediction:
+        pass
---
+++ packages/persuader-technology-automata-oracles/persuader_technology_automata_oracles-0.0.4-py3-none-any.whl/oracle/resolve/PredictionResolver.py
@@ -0,0 +1,35 @@
+from typing import List
+
+from oracle.Oracle import Oracle
+from oracle.Prediction import Prediction
+
+
+class PredictionResolver:
+
+    def __init__(self, oracles):
+        self.oracles: List[Oracle] = oracles
+
+    def resolve(self, instrument, data) -> Prediction:
+        self.set_all_oracle_with_data(data)
+        predictions = self.collect_predictions_from_all_oracles(instrument)
+        best_prediction = self.determine_best_profitable_prediction(predictions)
+        return best_prediction
+
+    def set_all_oracle_with_data(self, data):
+        for oracle in self.oracles:
+            oracle.set_data(data)
+
+    def collect_predictions_from_all_oracles(self, instrument) -> List[Prediction]:
+        predictions = []
+        for oracle in self.oracles:
+            prediction = oracle.predict(instrument)
+            predictions.append(prediction)
+        valid_predictions = [p for p in predictions if p is not None]
+        return valid_predictions
+
+    @staticmethod
+    def determine_best_profitable_prediction(predictions: List[Prediction]):
+        if not predictions:
+            return None
+        sorted_predictions = sorted(predictions, key=lambda prediction: prediction.profit)
+        return sorted_predictions[0]
