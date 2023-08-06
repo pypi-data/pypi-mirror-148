7304671f8f9652efcbd4412bf7a10af0e82e6905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:45 +0000

    Add package PSHA/0.0.1.dev0/PSHA-0.0.1.dev0-py3-none-any.whl

---
+++ packages/PSHA/PSHA-0.0.1.dev0-py3-none-any.whl/PSHA/PSHA.py
@@ -0,0 +1,11 @@
+class A():
+    """docstring for A
+    """
+    def __init__(self,args):
+        self.args = args
+
+class B():
+    """docstring for A
+    """
+    def __init__(self,args):
+        self.args = args
