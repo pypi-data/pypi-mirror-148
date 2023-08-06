49a0e60b2cf234fac451a2bac2b26b3304042c96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:14 +0000

    Add package cmdcmd/3/cmdcmd-3-py3-none-any.whl

---
+++ packages/cmdcmd/cmdcmd-3-py3-none-any.whl/cmdcmd/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2010-2017, 2022 Adrian Perez <aperez@igalia.com>
+#
+# Distributed under terms of the MIT license.
+
+__version__ = 3
+
+from .cmd import CLI, Command, Option, main  # noqa
