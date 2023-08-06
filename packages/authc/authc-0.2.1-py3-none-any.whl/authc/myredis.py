3e3c3e95389642fe9e009b437b2dd25241ff0fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:29 +0000

    Add package authc/0.2.1/authc-0.2.1-py3-none-any.whl

---
+++ packages/authc/authc-0.2.1-py3-none-any.whl/authc/myredis.py
@@ -0,0 +1,17 @@
+from redis import StrictRedis
+from authc.core import authc
+
+
+def get_redis_cn():
+    a = authc()
+    host, port, password = a['ali_redis_host'], a['ali_redis_port'], a[
+        'ali_redis_pass']
+    return StrictRedis(host=host, port=port, password=password)
+
+
+def get_redis():
+    x = authc()
+    host, port, password = x['redis_host'], x['redis_port'], x['redis_pass']
+    return StrictRedis(host=host, port=port, password=password)
+    
+
