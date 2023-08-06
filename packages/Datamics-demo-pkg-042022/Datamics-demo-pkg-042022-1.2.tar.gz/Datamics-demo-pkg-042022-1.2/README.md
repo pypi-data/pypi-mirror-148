45ac8e34eee56462836efdb18dedc8ab9fc93b1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:32 +0000

    Add package Datamics-demo-pkg-042022/1.2/Datamics-demo-pkg-042022-1.2.tar.gz

---
+++ packages/Datamics-demo-pkg-042022/Datamics-demo-pkg-042022-1.2.tar.gz/Datamics-demo-pkg-042022-1.2/README.md
@@ -0,0 +1,2 @@
+# Datamics Package
+This is a simple example package for Datamics Open-source contribution demo
---
+++ packages/Datamics-demo-pkg-042022/Datamics-demo-pkg-042022-1.2.tar.gz/Datamics-demo-pkg-042022-1.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools 
+
+with open("README.md", "r", encoding="utf-8") as fh: 
+   long_description = fh.read()
+
+setuptools.setup(
+   name = "Datamics-demo-pkg-042022",
+   version = "1.2",
+   author = "Saumya",
+   author_email = "saumya.goyal@datamics.com",
+   description = "A small example package",
+   long_description = long_description,
+   long_description_content_type = "text/markdown",
+   url = "https://github.com/saumyagoyal95/OSC_PYPI",
+   project_urls = {
+      "Bug Tracker": "https://github.com/saumyagoyal95/OSC_PYPI/issues"
+   },
+   classifiers = [
+      "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+   ],
+   package_dir = {"": "src"},
+   packages = setuptools.find_packages(where = "src"),
+   python_requires = ">=3.6", 
+) 
