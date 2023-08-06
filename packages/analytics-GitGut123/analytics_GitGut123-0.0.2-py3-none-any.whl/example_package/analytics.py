0a54379555387b511b2f76ff61024f4409071b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:21:36 +0000

    Add package analytics-GitGut123/0.0.2/analytics_GitGut123-0.0.2-py3-none-any.whl

---
+++ packages/analytics-GitGut123/analytics_GitGut123-0.0.2-py3-none-any.whl/example_package/analytics.py
@@ -0,0 +1,38 @@
+"""Main file for the package."""
+import pandas as pd
+import numpy as np
+
+
+class Ops():
+    """Class for making operations on pd.DataFrames."""
+
+    @staticmethod
+    def remove_null_values(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Remove all rows containing null values in the dataframe.
+
+        Args:
+            df (pd.DataFrame): Dataframe to remove null rows from.
+
+        Returns:
+            pd.DataFrame: The input dataframe without any null values.
+        """
+        # Regex: (flags = case insensitive) matches "none" or "nan"
+        df = df.replace(to_replace="(?i)none|nan", value=np.nan, regex=True).dropna()
+
+        return df
+
+    @staticmethod
+    def rotate(self, df: pd.DataFrame, xx: str, yy: str, scalars: str) -> pd.DataFrame:
+        """Remove missing values and rotate the dataframe using the pivot function.
+
+        Args:
+            df (pd.DataFrame): Dataframe to rotate.
+            xx (str): Column to make frame's new index.
+            yy (str): Column to make frame's new columns.
+            scalars (str): Column to use for populating he new index.
+
+        Returns:
+            pd.DataFrame: reshaped dataframe.
+        """
+        df = self.remove_null_values(df=df)
+        return df.pivot(index=xx, columns=yy, values=scalars)
