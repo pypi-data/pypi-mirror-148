9c8f15fce87f8867d1fa8fb135e9941cca3d6151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:36 +0000

    Add package DMS-APP/0.0.3/DMS_APP-0.0.3.tar.gz

---
+++ packages/DMS-APP/DMS_APP-0.0.3.tar.gz/DMS_APP-0.0.3/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="DMS_APP",
+    version="0.0.3",
+    author="Datamoulds",
+    author_email="author@example.com",
+    description="A small example package",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.8.8",
+)
