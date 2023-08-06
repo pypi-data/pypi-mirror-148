db41313e8451e38ca5cc7c2e3f255e9a22abbab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:04 +0000

    Add package text-gists/0.0.1/text_gists-0.0.1-py3-none-any.whl

---
+++ packages/text-gists/text_gists-0.0.1-py3-none-any.whl/text_gists/__init__.py
@@ -0,0 +1,17 @@
+__version__ = "0.0.1"
+from .__main__ import (
+    construct_indicators,
+    cull_suffix,
+    get_first_matching_text_of_regex,
+    if_match_found_return_text,
+    remove_pattern_if_found_else_text,
+    text_in_pattern_count,
+    trim_text,
+)
+from .constructors import (
+    combine_regexes_as_options,
+    construct_acronyms,
+    construct_negative_lookbehinds,
+    construct_prefix_options,
+)
+from .dates import format_full_date, parse_date_if_exists
