76084a3bf403fee1a3e96905a83cbd304bab324e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:50 +0000

    Add package qiskit-qir/0.1.0b5/qiskit-qir-0.1.0b5.tar.gz

---
+++ packages/qiskit-qir/qiskit-qir-0.1.0b5.tar.gz/qiskit-qir-0.1.0b5/tests/test_circuits/__init__.py
@@ -0,0 +1,15 @@
+##
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+##
+
+from .random import *
+from .basic_circuits import *
+
+# Core test fixtures
+core_tests = [
+    "ghz",
+    "teleport",
+    "unroll",
+    "teleport_with_subroutine"
+] + random_fixtures
