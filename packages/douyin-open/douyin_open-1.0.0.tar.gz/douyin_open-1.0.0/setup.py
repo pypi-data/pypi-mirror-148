e9531f32c8c15e384a2f1138cb95b0aad31b239c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:53 +0000

    Add package douyin-open/1.0.0/douyin_open-1.0.0.tar.gz

---
+++ packages/douyin-open/douyin_open-1.0.0.tar.gz/douyin_open-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='douyin_open',
+    version='1.0.0',
+    py_modules=['douyin_open'],
+    author='douyin',
+    author_email='',
+    description='douyin_open sdk for python',
+)
