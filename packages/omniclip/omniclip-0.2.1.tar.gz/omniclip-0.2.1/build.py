9fed8251fd049eb9cb86da5b448222ac2046fc72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:20:17 +0000

    Add package omniclip/0.2.1/omniclip-0.2.1.tar.gz

---
+++ packages/omniclip/omniclip-0.2.1.tar.gz/omniclip-0.2.1/build.py
@@ -0,0 +1,28 @@
+from distutils.command.build_ext import build_ext
+
+import numpy as np
+from Cython.Build import cythonize
+from setuptools import Extension
+
+
+def build(setup_kwargs):
+    """Needed for the poetry building interface."""
+    extensions = [
+        Extension(
+            "omniCLIP.viterbi",
+            sources=["omniCLIP/omni_stat/viterbi.pyx"],
+            include_dirs=[np.get_include()],
+        ),
+    ]
+    extensions = cythonize(
+        extensions,
+        compiler_directives={"language_level": 3, "linetrace": True},
+    )
+    setup_kwargs.update(
+        {
+            "extmodules": extensions,
+            "cmdclass": {"build_ext": build_ext},
+            "include_dirs": [np.get_include()],
+        }
+    )
+    return setup_kwargs
