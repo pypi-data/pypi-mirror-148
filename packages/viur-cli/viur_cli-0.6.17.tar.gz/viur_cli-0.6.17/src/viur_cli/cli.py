14e594ff1498cc07cb34d33ed80c73d0f0fb4a15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:41 +0000

    Add package viur-cli/0.6.17/viur_cli-0.6.17.tar.gz

---
+++ packages/viur-cli/viur_cli-0.6.17.tar.gz/viur_cli-0.6.17/src/viur_cli/cli.py
@@ -0,0 +1,37 @@
+import click
+import os
+from .conf import *
+from .version import __version__
+
+
+@click.group(invoke_without_command=True, no_args_is_help=True)
+@click.version_option(__version__)
+def cli():
+    """Info output"""
+    load_config()
+    fetch_core_version()
+
+
+@cli.command()
+@click.argument("action", type=click.Choice(['add', 'remove', 'list', 'addFlare']))
+def project(action):
+    """manage project.json and generate if missing"""
+    projectConfig = get_config()
+    if action == "add":
+        add_to_config()
+    elif action == "addFlare":
+        _projectconf = add_to_flare_config(projectConfig)
+        write_config(_projectconf)
+    elif action == "remove":
+        remove_from_config()
+    elif action == "list":
+        click.echo(click.style(json.dumps(projectConfig, indent=4, sort_keys=True), fg="cyan"))
+
+
+@cli.command()
+@click.argument("action", type=click.Choice(['ssl']))
+def fix(action):
+    if action =="ssl":
+        os.system("chmod +x scripts/macos_certificate_fix.command && ./scripts/macos_certificate_fix.command")
+        click.echo()
+
