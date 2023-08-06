52d2c4a2f5cd05afb096cffd754d0c8a239a6b4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:17 +0000

    Add package ms1searchpy/2.3.10/ms1searchpy-2.3.10-py3-none-any.whl

---
+++ packages/ms1searchpy/ms1searchpy-2.3.10-py3-none-any.whl/ms1searchpy/combine.py
@@ -0,0 +1,81 @@
+from .main import final_iteration
+import pandas as pd
+from collections import defaultdict
+import argparse
+import logging
+
+logger = logging.getLogger(__name__)
+
+def run():
+    parser = argparse.ArgumentParser(
+        description='Combine DirectMS1 search results',
+        epilog='''
+
+    Example usage
+    -------------
+    $ ms1combine.py file1_PFMs_ML.tsv ... filen_PFMs_ML.tsv
+    -------------
+    ''',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('file', nargs='+', help='input tsv PFMs_ML files')
+    parser.add_argument('-out', help='prefix output file names', default='combined')
+    parser.add_argument('-prots_full', help='path to any of *_proteins_full.tsv file. By default this file will be searched in the folder with PFMs_ML files', default='')
+    parser.add_argument('-fdr', help='protein fdr filter in %%', default=1.0, type=float)
+    parser.add_argument('-prefix', help='decoy prefix', default='DECOY_')
+    parser.add_argument('-nproc', help='number of processes', default=1, type=int)
+    args = vars(parser.parse_args())
+    logging.basicConfig(format='%(levelname)9s: %(asctime)s %(message)s',
+            datefmt='[%H:%M:%S]', level=logging.INFO)
+
+    df1 = None
+    for idx, filen in enumerate(args['file']):
+        df3 = pd.read_csv(filen, sep='\t')
+        df3['ids'] = df3['ids'].apply(lambda x: '%d:%s' % (idx, str(x)))
+        if df1 is None:
+            df1 = df3
+            if args['prots_full']:
+                df2 = pd.read_csv(args['prots_full'], sep='\t')
+            else:
+                try:
+                    df2 = pd.read_csv(filen.replace('_PFMs_ML.tsv', '_proteins_full.tsv'), sep='\t')
+                except:
+                    logging.critical('Proteins_full file is missing!')
+                    break
+
+        else:
+            df1 = pd.concat([df1, df3], ignore_index=True)
+
+    pept_prot = defaultdict(set)
+    for seq, prots in df1[['seqs', 'proteins']].values:
+        for dbname in prots.split(';'):
+            pept_prot[seq].add(dbname)
+
+    protsN = dict()
+    for dbname, theorpept in df2[['dbname', 'theoretical peptides']].values:
+        protsN[dbname] = theorpept
+
+
+    prefix = args['prefix']
+    isdecoy = lambda x: x[0].startswith(prefix)
+    isdecoy_key = lambda x: x.startswith(prefix)
+    escore = lambda x: -x[1]
+    fdr = float(args['fdr']) / 100
+
+    resdict = dict()
+
+    resdict['qpreds'] = df1['qpreds'].values
+    resdict['preds'] = df1['preds'].values
+    resdict['seqs'] = df1['peptide'].values
+    resdict['ids'] = df1['ids'].values
+    resdict['iorig'] = df1['iorig'].values
+
+    mass_diff = resdict['qpreds']
+    rt_diff = resdict['qpreds']
+
+    base_out_name = args['out']
+    final_iteration(resdict, mass_diff, rt_diff, pept_prot, protsN, base_out_name, prefix, isdecoy, isdecoy_key, escore, fdr, args['nproc'])
+
+
+if __name__ == '__main__':
+    run()
