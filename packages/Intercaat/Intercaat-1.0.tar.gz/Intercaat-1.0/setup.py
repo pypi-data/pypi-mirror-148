8d2c5a92170000e0a0dc57bec33354058b41dc73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:05 +0000

    Add package Intercaat/1.0/Intercaat-1.0.tar.gz

---
+++ packages/Intercaat/Intercaat-1.0.tar.gz/Intercaat-1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.txt", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name='Intercaat',
+      version='1.0',
+      description='This program uses a PDB file to identify the residues present in the interface between a query chain and an interacting chain(s)',
+      url='https://gitlab.com/fiserlab.org/intercaat',
+      author='Steve Grudman',
+      author_email='steven.grudman@einsteinmed.edu',
+      license='MIT',
+      packages=['Intercaat'],
+      zip_safe=False, 
+      install_requires=[         
+        'scipy',         
+        'numpy',
+        'pyhull'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+      entry_points = {
+        'console_scripts': ['intercaat=Intercaat.intercaat:main'],
+    })
+
+#python setup.py sdist
+#python3 -m twine upload dist/*
