179adba330599682672afd7a376ba9d69c0d47e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:17 +0000

    Add package robotframework-archive/0.0.4/robotframework_archive-0.0.4-py3.7.egg

---
+++ packages/robotframework-archive/robotframework_archive-0.0.4-py3.7.egg/robotframework_archive/sp_historic/parserargs.py
@@ -0,0 +1,57 @@
+import os
+import argparse
+from .sphistoricparser import sphistoric_parser
+
+
+def parse_options():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    general = parser.add_argument_group("Parser")
+
+    general.add_argument(
+        '-s', '--host',
+        dest='host',
+        default='localhost',
+        help="MySQL hosted address"
+    )
+
+    general.add_argument(
+        '-u', '--username',
+        dest='username',
+        default='superuser',
+        help="MySQL db user name"
+    )
+
+    general.add_argument(
+        '-p', '--password',
+        dest='password',
+        default='passw0rd',
+        help="MySQL db password"
+    )
+
+    general.add_argument(
+        '-d', '--projectid',
+        dest='projectid',
+        help="project id"
+    )
+
+    general.add_argument(
+        '-o', '--output',
+        dest='output',
+        help="Name of csv file"
+    )
+
+    general.add_argument(
+        '-g', '--ignoreresult',
+        dest='ignoreresult',
+        default="False",
+        help="Flag to ignore execution result, by this flag we can restrict storing results into mysql"
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_options()
+    sphistoric_parser(args)
