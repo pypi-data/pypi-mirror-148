bfef967cfb2112d14d71ad79f05d99df176e69d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:37 +0000

    Add package tinycarlo/1.1/tinycarlo-1.1.tar.gz

---
+++ packages/tinycarlo/tinycarlo-1.1.tar.gz/tinycarlo-1.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='tinycarlo',  
+     version='1.1',
+     author="Daniel Riege, Markus Kasten",
+     description="2D car simulation",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/danielriege/tinycarlo",
+     packages=setuptools.find_packages(),
+     install_requires=[
+        'gym>=0.21.0',
+        'numpy>=1.22.0',
+        'opencv-python>=4.5.5.62',
+        'setuptools>=60.3.1',
+        'pyyaml>=6.0'
+     ]
+ )
