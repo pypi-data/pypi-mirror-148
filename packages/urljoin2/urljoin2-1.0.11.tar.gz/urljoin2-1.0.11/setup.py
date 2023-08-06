bb0834204d540aa6cac5275ac7778eac0f5f134a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:01 +0000

    Add package urljoin2/1.0.11/urljoin2-1.0.11.tar.gz

---
+++ packages/urljoin2/urljoin2-1.0.11.tar.gz/urljoin2-1.0.11/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+  name = 'urljoin2',
+  packages = ['urljoin2'],
+  version = '1.0.11',
+  license='MIT',
+  description = 'Module for joining strings to form a url.',
+  author = 'Nagy Zoltán',
+  author_email = 'zolika3400@gmail.com',
+)
