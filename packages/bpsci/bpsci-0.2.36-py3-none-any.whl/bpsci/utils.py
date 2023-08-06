60941f32e40dfeaad92ea27244a830ad4fd6467e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:21 +0000

    Add package bpsci/0.2.36/bpsci-0.2.36-py3-none-any.whl

---
+++ packages/bpsci/bpsci-0.2.36-py3-none-any.whl/bpsci/utils.py
@@ -0,0 +1,34 @@
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+import bpy
+import pandas as pd
+
+def erase_others(obj_name):
+    actual_obj = bpy.data.objects[obj_name]
+    name = actual_obj.name
+    
+    all_objs = list(bpy.data.objects)
+    all_objs_names = np.array([obj.name for obj in all_objs])
+    
+    inds = [all_obj_name.find(name) for all_obj_name in all_objs_names]
+    
+    family = []
+    relatives = []
+    
+    for i in range(len(inds)):
+        if inds[i] >= 0:
+            family.append(all_objs_names[i])
+    
+    for member in family:
+        if member != name:
+            relatives.append(member)
+            
+    [bpy.data.objects.remove(bpy.data.objects[obj_name]) for obj_name in relatives]
+
+def bpy_obj(obj_name):
+    return bpy.data.objects[obj_name]
+
+def euler2quat(angles1, angles2, angles3, euler_type):
+    all_angles = np.vstack([angles1, angles2, angles3]).transpose()
+    quat_out = R.from_euler(euler_type, all_angles).as_quat()
+    return quat_out
