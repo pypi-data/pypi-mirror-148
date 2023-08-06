646a119e0d5858410ae5befd05cf621567a21cb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:20:33 +0000

    Add package lzlla/0.0.1/lzlla-0.0.1.tar.gz

---
+++ packages/lzlla/lzlla-0.0.1.tar.gz/lzlla-0.0.1/lzlla/developer.py
@@ -0,0 +1,18 @@
+import time
+
+
+def developer():
+    print("Chinese:")
+    print('==================================================================')
+    print('开发者为：龙·玛卡巴卡的小推车·卓·奥儿’s_ruddy`·李')
+    print('国籍：中国')
+    print('年龄：14岁')
+    print('邮箱：liuniandexiaohuo@qq.com')
+    print('===================================================================')
+    time.sleep(2)
+    print('English:')
+    print('===================================================================')
+    print('developer: Long·Makabaka`s_bogey_dolly_trolley·Zhuo·Aoer`s_ruddy·Li')
+    print('Nationality:Chinese')
+    print('Age:14')
+    print('Email：liuniandexiaohuo@qq.com')
