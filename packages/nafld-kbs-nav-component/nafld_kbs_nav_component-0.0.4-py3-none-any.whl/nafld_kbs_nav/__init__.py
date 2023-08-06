6509fd526c077143b24d57ea5d33b39888595e35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:20 +0000

    Add package nafld-kbs-nav-component/0.0.4/nafld_kbs_nav_component-0.0.4-py3-none-any.whl

---
+++ packages/nafld-kbs-nav-component/nafld_kbs_nav_component-0.0.4-py3-none-any.whl/nafld_kbs_nav/__init__.py
@@ -0,0 +1,24 @@
+import os
+import streamlit.components.v1 as components
+
+_RELEASE = True
+
+if not _RELEASE:
+  _component_func = components.declare_component(
+    "nafld_kbs_nav",
+    url="http://localhost:3000", # vite dev server port
+  )
+else:
+  parent_dir = os.path.dirname(os.path.abspath(__file__))
+  build_dir = os.path.join(parent_dir, "frontend/dist")
+  _component_func = components.declare_component("nafld_kbs_nav", path=build_dir)
+
+def nafld_kbs_nav(name, key=None):
+  component_value = _component_func(name=name, key=key, default=0)
+  return component_value
+
+if not _RELEASE:
+  import streamlit as st
+  st.subheader("Component Test")
+  num_clicks = nafld_kbs_nav(name = "NameViteVue")
+  st.markdown("You've clicked %s times!" % int(num_clicks))
