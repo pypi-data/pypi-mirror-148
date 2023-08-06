c560acfdbcf7ebf55dfe4f9cccd99628bd29cc02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:21:37 +0000

    Add package FastDataFile/2.1.1/FastDataFile-2.1.1.tar.gz

---
+++ packages/FastDataFile/FastDataFile-2.1.1.tar.gz/FastDataFile-2.1.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="FastDataFile",
+    version="2.1.1",
+    license='MIT License',
+    author="Ashenguard",
+    author_email="Ashenguard@agmdev.com",
+    description="The ease of file datafiles",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ashenguard/fastdatafile",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    include_package_data=True,
+    install_requires=["pyyaml"],
+    dependency_links=[]
+)
