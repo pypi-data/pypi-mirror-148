2830b8c58e264ab0ed88055d1bfcba73b6685a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:46 +0000

    Add package raiden-services/2.0.0/raiden-services-2.0.0.tar.gz

---
+++ packages/raiden-services/raiden-services-2.0.0.tar.gz/raiden-services-2.0.0/src/raiden_libs/utils.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timezone
+from functools import lru_cache
+from typing import Union
+
+import eth_utils
+from coincurve import PrivateKey, PublicKey
+from eth_typing import AnyAddress, ChecksumAddress
+from eth_utils import keccak
+
+from raiden.utils.typing import Address, Timestamp
+from raiden_contracts.utils.type_aliases import PrivateKey as PrivateKeyType
+
+
+def camel_to_snake(input_str: str) -> str:
+    return "".join("_" + c.lower() if c.isupper() else c for c in input_str).lstrip("_")
+
+
+def public_key_to_address(public_key: PublicKey) -> Address:
+    """Converts a public key to an Ethereum address."""
+    key_bytes = public_key.format(compressed=False)
+    return Address(keccak(key_bytes[1:])[-20:])
+
+
+def private_key_to_address(private_key: PrivateKeyType) -> Address:
+    """Converts a private key to an Ethereum address."""
+    privkey = PrivateKey(private_key)
+    return public_key_to_address(privkey.public_key)
+
+
+@lru_cache(maxsize=5000)
+def to_checksum_address(value: Union[AnyAddress, str, bytes]) -> ChecksumAddress:
+    return eth_utils.to_checksum_address(value)
+
+
+def get_posix_utc_time_now() -> Timestamp:
+    datetime_utc_now = datetime.now(tz=timezone.utc)
+    return Timestamp(int(datetime_utc_now.timestamp()))
