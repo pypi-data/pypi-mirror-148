9931b706e2a466948302cd07bc6e9264979e04de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:15 +0000

    Add package graphtastic/0.12.0/graphtastic-0.12.0.tar.gz

---
+++ packages/graphtastic/graphtastic-0.12.0.tar.gz/graphtastic-0.12.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "graphtastic",
+    version = "0.12.0",
+    author = "Richard Tjörnhammar",
+    author_email = "richard.tjornhammar@gmail.com",
+    description = "Graphtastic, a Graph Learning library for Humans",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/richardtjornhammar/graphtastic",
+    packages = setuptools.find_packages('src'),
+    package_dir = {'graphtastic':'src/graphtastic','graphs':'src/graphs','convert':'src/convert','clustering':'src/clustering','fit':'src/fit','hierarchical':'src/hierarchical','utility':'src/utility','stats':'src/stats'},
+    classifiers = [
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
