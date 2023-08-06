9cac27f9bf29896c824c0ada460d923c2312b69f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:11 +0000

    Add package fairies/0.1.29/fairies-0.1.29.tar.gz

---
+++ packages/fairies/fairies-0.1.29.tar.gz/fairies-0.1.29/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='fairies',
+    version='0.1.29',
+    description=(
+        'utils for nlp'
+    ),
+    author='liyuhao',
+    author_email='1241225413@qq.com',
+    license='Apache License 2.0',
+    packages=find_packages(),
+    url='https://github.com/hgliyuhao/little_dinosaur',
+    install_requires=[
+        'xlrd==1.2.0',
+        'jieba',
+        'xlwt',
+        'xlutils',
+        'matplotlib',
+        'sklearn',
+        'pandas'
+    ],
+)
