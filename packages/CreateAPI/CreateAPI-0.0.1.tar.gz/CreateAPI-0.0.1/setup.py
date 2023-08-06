a565d6e1f08648f87fe5100761e1d9efefaa98a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:22 +0000

    Add package CreateAPI/0.0.1/CreateAPI-0.0.1.tar.gz

---
+++ packages/CreateAPI/CreateAPI-0.0.1.tar.gz/CreateAPI-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='CreateAPI',
+    version='0.0.1',
+    description='A very simple API creator',
+    url='',
+    author='oren',
+    author_email='orennadle@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='api-creator CreateAPI api-maker make-api api create-api create',
+    packages=find_packages(),
+    install_requires=['Flask']
+)
