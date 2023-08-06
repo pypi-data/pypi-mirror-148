4ad1f88d8e886a4cc96a2b7844ec316d5e3636e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:18:57 +0000

    Add package CloserLookFewShot/0.0.3/CloserLookFewShot-0.0.3.tar.gz

---
+++ packages/CloserLookFewShot/CloserLookFewShot-0.0.3.tar.gz/CloserLookFewShot-0.0.3/CloserLookFewShot/configs.py
@@ -0,0 +1,6 @@
+save_dir                    = '/home/ubuntu/vinc/mamlprefix/CloserLookFewShot/'
+data_dir = {}
+data_dir['CUB']             = './CloserLookFewShot/filelists/CUB/' 
+data_dir['miniImagenet']    = './CloserLookFewShot/filelists/miniImagenet/' 
+data_dir['omniglot']        = './CloserLookFewShot/filelists/omniglot/' 
+data_dir['emnist']          = './CloserLookFewShot/filelists/emnist/' 
