6362b28467692970fca24b6407769bb686dbbba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:04 +0000

    Add package stringmatch/0.6.2/stringmatch-0.6.2-py3-none-any.whl

---
+++ packages/stringmatch/stringmatch-0.6.2-py3-none-any.whl/tests/test_ratio.py
@@ -0,0 +1,36 @@
+import pytest
+
+from stringmatch.ratio import JaroWinklerScorer, LevenshteinScorer, Ratio, _Scorer
+
+
+def test_ratio():
+    assert Ratio().ratio("test", "test") == 100
+    assert Ratio().ratio("bla", "nope") == 0
+    assert Ratio().ratio("searchlib", "srechlib") == 82
+
+    assert Ratio(scorer=JaroWinklerScorer).ratio("searchlib", "srechlib") == 93
+    assert Ratio(scorer=LevenshteinScorer).ratio("test", "th test") == 73
+    assert Ratio(scorer=JaroWinklerScorer).ratio("test", "th test") == 60
+
+    with pytest.raises(TypeError):
+        assert Ratio(scorer="nope").ratio("searchlib", "srechlib") == 82  # type: ignore
+
+    with pytest.raises(NotImplementedError):
+        assert Ratio(scorer=_Scorer).ratio("searchlib", "srechlib") == 82
+
+    assert Ratio().ratio("", "f") == 0
+    assert Ratio(LevenshteinScorer).ratio_list("test", ["th test", "hwatever"]) == [
+        73,
+        33,
+    ]
+    assert Ratio(JaroWinklerScorer).ratio_list("test", ["th test", "hwatever"]) == [
+        60,
+        58,
+    ]
+
+
+def test_ratio_list():
+    assert Ratio().ratio_list("test", ["test", "nope"]) == [100, 25]
+    assert Ratio().ratio_list(
+        "srechlib", ["searchlib", "slib", "searching library", "spam"]
+    ) == [82, 67, 56, 17]
