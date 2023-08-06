1894ca4af84956eaf492400e05a645d4398fbf9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:20:23 +0000

    Add package mynum/0.0.1/mynum-0.0.1.tar.gz

---
+++ packages/mynum/mynum-0.0.1.tar.gz/mynum-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_desc = f.read()
+
+setup(
+    name='mynum',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://biningo.github.io/',
+    license='MIT',
+    author='Example Author',
+    author_email='icepan5@gmail.com',
+    description='test',
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
