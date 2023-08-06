bae2598ba6b43614d5473d4fd881db3136b1606d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:04 +0000

    Add package TF-package-management/0.0.0/TF_package_management-0.0.0.tar.gz

---
+++ packages/TF-package-management/TF_package_management-0.0.0.tar.gz/TF_package_management-0.0.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='TF_package_management',
+    #version='0.0.1',
+    auther='Jesper Thoft Illemann Jæger',
+    author_email='jesperjag86@gmail.com',
+    description='package manegement system on github',
+    url='https://github.com/JesperJager1986/testing_package_management',
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
---
+++ packages/TF-package-management/TF_package_management-0.0.0.tar.gz/TF_package_management-0.0.0/src/main.py
@@ -0,0 +1,9 @@
+
+import TF_package_management
+
+def testing():
+    print('from testing')
+
+
+if '__main__' == __name__:
+    print('hallo from main')
