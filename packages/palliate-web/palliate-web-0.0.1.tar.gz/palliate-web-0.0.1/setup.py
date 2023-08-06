85d2804f19dbdfbbdf6b7779ec95fa6ac69c53b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:21 +0000

    Add package palliate-web/0.0.1/palliate-web-0.0.1.tar.gz

---
+++ packages/palliate-web/palliate-web-0.0.1.tar.gz/palliate-web-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='palliate-web',
+    version='0.0.1',
+    description='palliate web GUI',
+    long_description='Web GUI to control palliate instances',
+    url='https://github.com/palliate/web',
+    author='Tsche',
+    author_email='contact@palliate.io',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Development Status :: 1 - Planning'
+    ],
+    project_urls={
+        "Bug Tracker": "https://github.com/palliate/web/issues",
+    },
+    packages=[],
+)
