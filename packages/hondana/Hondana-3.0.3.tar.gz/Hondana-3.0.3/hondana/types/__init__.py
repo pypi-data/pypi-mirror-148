da14f33d560f114c7bb653a3725e6439e7713c62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:53 +0000

    Add package hondana/3.0.3/Hondana-3.0.3.tar.gz

---
+++ packages/hondana/Hondana-3.0.3.tar.gz/Hondana-3.0.3/hondana/types/__init__.py
@@ -0,0 +1,46 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2021-Present AbstractUmbra
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+
+from __future__ import annotations
+
+from .account import *
+from .artist import *
+from .auth import *
+from .author import *
+from .chapter import *
+from .common import *
+from .cover import *
+from .custom_list import *
+from .errors import *
+from .legacy import *
+from .manga import *
+from .relationship import *
+from .report import *
+from .scanlator_group import *
+from .settings import *
+from .statistics import *
+from .tags import *
+from .token import *
+from .upload import *
+from .user import *
