e05461abf822bce334cafb668c330e9894194f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:21 +0000

    Add package tempor/0.5.0/tempor-0.5.0.tar.gz

---
+++ packages/tempor/tempor-0.5.0.tar.gz/tempor-0.5.0/tempor/providers/gcp/variables.tf
@@ -0,0 +1,13 @@
+variable "api_token" {}
+variable "image" {
+    default = "ubuntu-os-cloud/ubuntu-2004-lts"
+}
+variable "region" {
+    default = "us-east1"
+}
+variable "zone" {
+    default = "us-east1-b"
+}
+variable "num" {
+	default = 1
+}
---
+++ packages/tempor/tempor-0.5.0.tar.gz/tempor-0.5.0/tempor/providers/vultr/variables.tf
@@ -0,0 +1,13 @@
+variable "api_token" {}
+variable "image" {
+    default = "387"
+}
+variable "region" {
+    default = "ewr"
+}
+variable "num" {
+	default = 1
+}
+variable "username" {
+	default = "user"
+}
