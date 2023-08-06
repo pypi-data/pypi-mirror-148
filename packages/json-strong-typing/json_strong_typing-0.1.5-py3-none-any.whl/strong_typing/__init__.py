ec71462767497d6f33cd11b51f173f6454a2fa1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:47:37 +0000

    Add package json-strong-typing/0.1.5/json_strong_typing-0.1.5-py3-none-any.whl

---
+++ packages/json-strong-typing/json_strong_typing-0.1.5-py3-none-any.whl/strong_typing/__init__.py
@@ -0,0 +1,6 @@
+"""
+Type-safe data interchange for Python data classes.
+
+Provides auxiliary services for working with Python type annotations, converting typed data to and from JSON,
+and generating a JSON schema for a complex type.
+"""
