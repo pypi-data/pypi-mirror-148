fb56282efd497c81551dc12a26adb0c73262d967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:27 +0000

    Add package outlook/1.0.4/outlook-1.0.4.tar.gz

---
+++ packages/outlook/outlook-1.0.4.tar.gz/outlook-1.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='outlook',
+    version='1.0.4',
+    author='Tiancheng Jiao',
+    author_email='jtc1246@outlook.com',
+    url='https://github.com/jtc1246/outlook',
+    description='A simple email sender for Outlook',
+    packages=['outlook'],
+    install_requires=['myHttp','mySecrets','msal'],
+    python_requires='>=3',
+    platforms=["all"],
+    license='GPL-2.0 License'
+)
