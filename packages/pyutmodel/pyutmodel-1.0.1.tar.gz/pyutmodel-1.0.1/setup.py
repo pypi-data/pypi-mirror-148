f4b24c71ca39407fb1342d61d306acf55727ba15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:21 +0000

    Add package pyutmodel/1.0.1/pyutmodel-1.0.1.tar.gz

---
+++ packages/pyutmodel/pyutmodel-1.0.1.tar.gz/pyutmodel-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="pyutmodel",
+    version="1.0.1",
+    author_email='Humberto.A.Sanchez.II@gmail.com',
+    description='External Pyut Data Model',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hasii2011/pyutmodel",
+    packages=[
+        'pyutmodel',
+    ],
+    install_requires=['Deprecated'],
+)
