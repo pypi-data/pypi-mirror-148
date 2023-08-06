dc16472215f942b69449777c77fe1dd37f857853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:08 +0000

    Add package phanterpwa/13.10.7/phanterpwa-13.10.7.tar.gz

---
+++ packages/phanterpwa/phanterpwa-13.10.7.tar.gz/phanterpwa-13.10.7/phanterpwa/samples/project_config_sample.py
@@ -0,0 +1,34 @@
+project_config_sample = {
+    "CONFIG_INDENTIFY": "project_config",
+    "ENVIRONMENT": {
+        "path": "",
+        "python": ""
+    },
+    "PROJECT": {
+        "name": "PhanterPWA",
+        "title": "PhanterPWA",
+        "version": "0.0.1",
+        "compilation": 0,
+        "author": "PhanterJR<phanterjr@conexaodidata.com.br>",
+        "debug": True,
+        "packaged": True,
+        "minify": False,
+        "baseport": 10000,
+        "basehost": "127.0.0.1",
+        "path": ""
+    },
+    "FRONTEND": {
+        "sample_app": {
+            "title": "PhanterPWA Frontend"
+        }
+    },
+    "BACKEND": {
+        "sample_app": {
+            "title": "PhanterPWA Backend",
+            "build_folder": "",
+            "transcrypt_main_file": "application",
+            "styles_main_file": "application",
+            "views_main_file": "index"
+        }
+    }
+}
