8b3e2e96463041c858feaa4a3f96fd88c393734a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:59 +0000

    Add package antidote/1.3.0/antidote-1.3.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/antidote/antidote-1.3.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/antidote/_providers/__init__.py
@@ -0,0 +1,6 @@
+from .factory import FactoryProvider
+from .indirect import IndirectProvider
+from .lazy import Lazy, LazyProvider
+from .world_test import WorldTestProvider
+
+__all__ = ['FactoryProvider', 'IndirectProvider', 'Lazy', 'LazyProvider', 'WorldTestProvider']
