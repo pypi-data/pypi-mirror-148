d96409f102f0d519353337629776be7ef1f39349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:33 +0000

    Add package pi2143oop/0.5.8/pi2143oop-0.5.8.tar.gz

---
+++ packages/pi2143oop/pi2143oop-0.5.8.tar.gz/pi2143oop-0.5.8/pi2143oop/__init__.py
@@ -0,0 +1,5 @@
+from .ErrorClass import Error
+from .CanvasClass import Canvas
+from .DotClass import Dot, Rectangle, Triangle, Square, Rhomb, Circle
+from .LineClass import Line
+from .Settings import default_font_color, default_background_color, font_color, background_color
