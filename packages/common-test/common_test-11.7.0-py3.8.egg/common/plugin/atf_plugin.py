dc12f873d5a5a4006829c4605750b114819d24db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:47:44 +0000

    Add package common-test/11.7.0/common_test-11.7.0-py3.8.egg

---
+++ packages/common-test/common_test-11.7.0-py3.8.egg/common/plugin/atf_plugin.py
@@ -0,0 +1,39 @@
+from common.data.handle_common import get_system_key, set_system_key
+
+from common.plat.jira_platform import JiraPlatForm
+
+from common.plugin.data_bus import DataBus
+from common.common.constant import Constant
+from common.plat.ATF_platform import ATFPlatForm
+
+
+class ATFPlugin(object):
+
+    @classmethod
+    def db_ops(self,_key, _sql, env: str=Constant.ENV):
+        DataBus.save_init_data()
+        return ATFPlatForm.db_ops(_key, _sql, env)
+
+    @classmethod
+    def sendResult(self):
+        if get_system_key('runtype') is not None:
+            jirakey=get_system_key('issueKey')
+            prjectname=get_system_key('name')
+            joburl=get_system_key('JOB_URL')
+            buildurl=get_system_key('BUILD_URL')
+            jobName_url=joburl+"/allure/"
+            build_report=buildurl+"/allure/"
+            dict = {"issuekey": f"{jirakey}", "project": f"{prjectname}", "result": "success"}
+            ATFPlatForm.runDeploy("AutoTest-Result", dict)
+            _desc=f'总用例数：100 \n  失败用例数：10\n ' \
+                  f'通过用例数：90\n 构建详情： {buildurl}\n 执行结果：{build_report} '
+            JiraPlatForm.setJiraComment(jirakey, _desc)
+            JiraPlatForm.setJiraFlowStatus(jirakey, "2551")
+
+
+
+
+if __name__ == '__main__':
+    DataBus.set_key("env","test")
+    print(ATFPlugin.db_ops('CESNDS',"update tb_send_message s set s.receive_name = '0' , s.receive_telephone = '0' where s.sm_type = 'MajorBookingGroundMuc'").text)
+
