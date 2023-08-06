9971f04b103e23c259b9b273762b61c058eed30b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:33 +0000

    Add package pyecb/0.0.9/pyecb-0.0.9.tar.gz

---
+++ packages/pyecb/pyecb-0.0.9.tar.gz/pyecb-0.0.9/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pyecb']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['mkdocs>=1.3.0,<2.0.0', 'poetry-dynamic-versioning>=0.14.1,<0.15.0']
+
+setup_kwargs = {
+    'name': 'pyecb',
+    'version': '0.0.9',
+    'description': '',
+    'long_description': None,
+    'author': 'Matthew Macdonald-Wallace',
+    'author_email': 'matt@doics.co',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
