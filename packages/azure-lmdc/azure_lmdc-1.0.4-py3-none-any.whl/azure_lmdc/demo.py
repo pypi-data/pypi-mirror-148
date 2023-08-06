d78103f8ffca135f00a207211839eff1cdcfccba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:22 +0000

    Add package azure-lmdc/1.0.4/azure_lmdc-1.0.4-py3-none-any.whl

---
+++ packages/azure-lmdc/azure_lmdc-1.0.4-py3-none-any.whl/azure_lmdc/demo.py
@@ -0,0 +1,10 @@
+from azure_lmdc.azure_wrapper_class import Azure_wrapper
+
+
+def main():
+    azure_client = Azure_wrapper()
+    print("Atualice os credencias do Azure SDK no Environment ..")
+
+
+if __name__=="__main__":
+    main()
