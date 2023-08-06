3d3d5d6d3b654c5e8050d7ee367a6122aa234162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:04 +0000

    Add package qsemailer/0.3/qsemailer-0.3.tar.gz

---
+++ packages/qsemailer/qsemailer-0.3.tar.gz/qsemailer-0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qsemailer',
+    version='0.3',
+    license='MIT',
+    author="Fabian Rücker",
+    author_email='rueckerfab@aol.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/fruecker/emailer',
+    keywords='email',
+    install_requires=[
+          'email'
+      ],
+
+)
