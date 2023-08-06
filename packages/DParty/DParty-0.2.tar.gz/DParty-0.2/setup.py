0ae48e1f959b3022819790371981bb9b54f2270e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:46 +0000

    Add package DParty/0.2/DParty-0.2.tar.gz

---
+++ packages/DParty/DParty-0.2.tar.gz/DParty-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='DParty',
+      version='0.2',
+      descrption='Smart home SDK from DParty',
+      author='lolisky',
+      author_email='lorisky1214@gmail.com',
+      requires=['mediapipe', 'cv2'],
+      packages=find_packages(),
+      license="apache 3.0")
