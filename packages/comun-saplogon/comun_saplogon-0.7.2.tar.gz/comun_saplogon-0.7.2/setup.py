21e7023d7d7feb97e914606e62e0fe6d766cbd40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:39 +0000

    Add package comun-saplogon/0.7.2/comun_saplogon-0.7.2.tar.gz

---
+++ packages/comun-saplogon/comun_saplogon-0.7.2.tar.gz/comun_saplogon-0.7.2/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='comun_saplogon',
+      version='0.7.2',
+      py_modules=['comun_saplogon'],
+      install_requires=['pywin32', 'numpy', 'opencv-python', 'pyautogui'])
