9a5b615033d6ffdff959d1a21f56ba3558ac7eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:24:17 +0000

    Add package realcwpmat/1.0.4/realcwpmat-1.0.4-py3-none-any.whl

---
+++ packages/realcwpmat/realcwpmat-1.0.4-py3-none-any.whl/realcwpmat/__init__.py
@@ -0,0 +1,10 @@
+
+print("加载/realcwpmat/_init_.py文件,1.0.2")
+
+from  .app import cwp_add
+from  .app import Student
+
+
+
+# print(cwp_add(1,2))
+# Student.speak()
---
+++ packages/realcwpmat/realcwpmat-1.0.4-py3-none-any.whl/realcwpmat/app.py
@@ -0,0 +1,9 @@
+
+print("加载/realcwpmat/app.py文件")
+def  cwp_add(a,b):
+    return a+b+1000;
+
+class Student:
+    @classmethod
+    def speak(self):
+        print("我是1.02的学生类")
