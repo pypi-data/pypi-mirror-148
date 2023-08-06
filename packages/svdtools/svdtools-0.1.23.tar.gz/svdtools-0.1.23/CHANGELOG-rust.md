678ba45f1d7338ab45389ef47cb66a587d565af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:28:51 +0000

    Add package svdtools/0.1.23/svdtools-0.1.23.tar.gz

---
+++ packages/svdtools/svdtools-0.1.23.tar.gz/svdtools-0.1.23/CHANGELOG-rust.md
@@ -0,0 +1,40 @@
+# Changelog
+
+This changelog tracks the Rust `svdtools` project. See
+[CHANGELOG-python.md](CHANGELOG-python.md) for the Python `svdtools` project.
+
+## [Unreleased]
+
+## [v0.2.3] 2022-05-01
+
+* Add support for `modifiedWriteValues` & `readAction` for fields
+
+## [v0.2.2] 2022-04-23
+
+* Use `svd-encoder` 0.13.2
+* Support `expand` when processing SVD files (#104)
+* Sanitize enumeratedValues (#103)
+
+## [v0.2.1] 2022-02-12
+
+* Use `svd-encoder` 0.13.1
+* Remove register `access` if empty
+
+## [v0.2.0] 2022-01-15
+
+* Use `svd-parser` 0.13.1
+* Add `_clear_fields` in `Device` and `Peripheral` (#90)
+* Add new `convert` command to convert between SVD (XML), JSON, and YAML (#92)
+* Provide option to opt out of regex replace of 0's in description when
+  creating arrays by using a custom `description` attribute (#95)
+
+## [v0.1.0] 2021-12-09
+
+* Initial release with feature-parity with the Python project.
+
+[Unreleased]: https://github.com/stm32-rs/svdtools/compare/v0.2.3...HEAD
+[v0.2.3]: https://github.com/stm32-rs/svdtools/compare/v0.2.2...v0.2.3
+[v0.2.2]: https://github.com/stm32-rs/svdtools/compare/v0.2.1...v0.2.2
+[v0.2.1]: https://github.com/stm32-rs/svdtools/compare/v0.2.0...v0.2.1
+[v0.2.0]: https://github.com/stm32-rs/svdtools/compare/35c3a79...v0.2.0
+[v0.1.0]: https://github.com/stm32-rs/svdtools/pull/84
---
+++ packages/svdtools/svdtools-0.1.23.tar.gz/svdtools-0.1.23/src/cli.rs
@@ -0,0 +1,120 @@
+use anyhow::Result;
+use clap::Parser;
+use std::path::PathBuf;
+
+use svdtools::{
+    convert::convert_cli, interrupts::interrupts_cli, makedeps::makedeps_cli, mmap::mmap_cli,
+    patch::patch_cli,
+};
+
+#[derive(Parser, Debug)]
+#[clap(about, version, author)]
+enum Command {
+    /// Patches an SVD file as specified by a YAML file
+    Patch {
+        /// Path to input SVD file
+        #[clap(parse(from_os_str))]
+        svd_file: PathBuf,
+    },
+    /// Generate Make dependency file listing dependencies for a YAML file.
+    Makedeps {
+        /// Input yaml file
+        #[clap(parse(from_os_str))]
+        yaml_file: PathBuf,
+
+        /// Dependencies output file
+        #[clap(parse(from_os_str))]
+        deps_file: PathBuf,
+    },
+    /// Print list of all interrupts described by an SVD file
+    Interrupts {
+        /// Path to input SVD file
+        #[clap(parse(from_os_str))]
+        svd_file: PathBuf,
+
+        /// Whether to print gaps in interrupt number sequence
+        #[clap(long)]
+        no_gaps: bool,
+    },
+    /// Generate text-based memory map of an SVD file.
+    Mmap {
+        /// Path to input SVD file
+        #[clap(parse(from_os_str))]
+        svd_file: PathBuf,
+    },
+    /// Convert SVD representation between file formats
+    Convert {
+        /// Path to input file
+        #[clap(parse(from_os_str))]
+        in_path: PathBuf,
+
+        /// Path to output file
+        #[clap(parse(from_os_str))]
+        out_path: PathBuf,
+
+        /// Format of input file (XML, JSON or YAML)
+        #[clap(long = "input-format")]
+        input_format: Option<convert_cli::InputFormat>,
+
+        /// Format of output file (XML, JSON or YAML)
+        #[clap(long = "output-format")]
+        output_format: Option<convert_cli::OutputFormat>,
+
+        /// Expand arrays, clusters and derived values
+        #[clap(long)]
+        expand: bool,
+
+        /// Skip enumeratedValues and writeConstraints during parsing (XML input only)
+        #[clap(long)]
+        ignore_enums: bool,
+    },
+}
+
+impl Command {
+    pub fn run(&self) -> Result<()> {
+        match self {
+            Self::Interrupts { svd_file, no_gaps } => {
+                interrupts_cli::parse_device(svd_file, !no_gaps)?;
+            }
+            Self::Mmap { svd_file } => mmap_cli::parse_device(svd_file)?,
+            Self::Patch { svd_file } => patch_cli::patch(svd_file)?,
+            Self::Makedeps {
+                yaml_file,
+                deps_file,
+            } => makedeps_cli::makedeps(yaml_file, deps_file)?,
+            Self::Convert {
+                in_path,
+                out_path,
+                input_format,
+                output_format,
+                expand,
+                ignore_enums,
+            } => convert_cli::convert(
+                in_path,
+                out_path,
+                *input_format,
+                *output_format,
+                *expand,
+                *ignore_enums,
+            )?,
+        }
+        Ok(())
+    }
+}
+
+#[derive(Parser, Debug)]
+struct CliArgs {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+pub fn run() {
+    env_logger::init();
+
+    let args = CliArgs::parse();
+    if let Err(e) = args.command.run() {
+        log::error!("{:?}", e);
+
+        std::process::exit(1);
+    }
+}
