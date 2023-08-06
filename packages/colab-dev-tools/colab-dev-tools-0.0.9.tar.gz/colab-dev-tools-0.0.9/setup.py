919367c008f5e2417d0db5f2204e0d944332ee0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:17 +0000

    Add package colab-dev-tools/0.0.9/colab-dev-tools-0.0.9.tar.gz

---
+++ packages/colab-dev-tools/colab-dev-tools-0.0.9.tar.gz/colab-dev-tools-0.0.9/setup.py
@@ -0,0 +1,26 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+#this_dir = os.path.abspath(os.path.dirname(__file__))
+#sys.path.insert(0, this_dir)
+
+#requirements_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')
+
+#with open(requirements_path, 'r') as f:
+#    install_requires = f.read().split()
+
+setup(
+    name='colab-dev-tools',
+    version='0.0.9',
+    description='Python utils package for Google Colab',
+    author='Yousef Nami',
+    author_email='namiyousef@hotmail.com',
+    url='https://github.com/namiyousef/colab-utils',
+    #install_requires=install_requires,
+    install_requires=['torch', 'numpy', 'nvidia-ml-py3'],
+    #package_data={}
+    packages=find_packages(exclude=('tests*', 'experiments*')),
+    license='MIT',
+    #entry_points=(),
+)
