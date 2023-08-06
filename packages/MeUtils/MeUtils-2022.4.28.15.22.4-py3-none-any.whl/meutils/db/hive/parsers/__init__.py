95d8fbaf5b12220679cdd8236f9a3ac4a38f79a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:27 +0000

    Add package MeUtils/2022.4.28.15.22.4/MeUtils-2022.4.28.15.22.4-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2022.4.28.15.22.4-py3-none-any.whl/meutils/db/hive/parsers/__init__.py
@@ -0,0 +1,4 @@
+from meutils.db.hive.parsers.base_parser import BaseParser
+from meutils.db.hive.parsers.orc import ORCParser
+from meutils.db.hive.parsers.parquet import ParquetParser
+from meutils.db.hive.parsers.text import TextParser
