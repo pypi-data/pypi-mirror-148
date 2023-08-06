9c60a6288aa94be00c32b7d8776d3d4985b7924b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:08 +0000

    Add package pymuco/1.0a1/pymuco-1.0A1.tar.gz

---
+++ packages/pymuco/pymuco-1.0A1.tar.gz/pymuco-1.0A1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name='pymuco',
+    packages=["pymuco"],
+    version='1.0A1',
+    description='Sample',
+    author='arsnovo',
+    author_email='sample@gmail.com',
+    url='https://www.pymuco.org/',
+    license='MIT',
+)
