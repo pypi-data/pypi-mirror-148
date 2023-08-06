f7ef4d4b40bbac725cfc8eb8919307ff1dda0cb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:27 +0000

    Add package torchminer/0.4.14/TorchMiner-0.4.14-py3-none-any.whl

---
+++ packages/torchminer/TorchMiner-0.4.14-py3-none-any.whl/TorchMiner/plugins/Recorder/BasicSheet.py
@@ -0,0 +1,87 @@
+# -*- coding:utf-8 -*-
+import functools
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
+
+from TorchMiner.plugins import BasePlugin
+
+pool = ThreadPoolExecutor(1)
+
+
+# TODO:Find a better way to accomplish async function
+def _async(fn):
+    @functools.wraps(fn)
+    def _func(*args, **kwargs):
+        def _inner(*args, **kwargs):
+            try:
+                fn(*args, **kwargs)
+            except Exception as e:
+                print(e)
+                raise e
+
+        return pool.submit(_inner, *args, **kwargs)
+
+    return _func
+
+
+class ColumnNotExistsError(Exception):
+    pass
+
+
+class BaseSheet(BasePlugin):
+    def __init__(self):
+        super().__init__()
+        self.columns = []
+        self.cached_row_data = {}
+        self.last_flushed_at = None
+
+    def _create_experiment_row(self):
+        """Create a row for this experiment"""
+        raise NotImplementedError
+
+    def _create_end_column_divider(self):
+        raise NotImplementedError
+
+    @_async
+    def update(self, key, value):
+        """Update value for a column"""
+        if key not in self.columns:
+            raise ColumnNotExistsError
+        if not isinstance(value, dict):
+            value = {"raw": value}
+        self.cached_row_data[key] = value
+
+    def flush(self):
+        raise NotImplementedError
+
+    def periodly_flush(self, force=False):
+        now = int(datetime.now().timestamp())
+        # flush every 10 seconds
+        if not force and now - self.last_flushed_at < 10:
+            return
+        self.flush()
+        self.last_flushed_at = now
+
+    def onready(self):
+        """Called after all the columns are created"""
+        raise NotImplementedError
+
+    def create_column(self, key, title):
+        """Create a column on the sheet."""
+        self.columns.append(key)
+
+    @property
+    def experiment_row_name(self):
+        return f"_row_{self.code}"
+
+    @property
+    def title_row_name(self):
+        return "_row_title"
+
+    @property
+    def end_column_name(self):
+        return "_column_end_divider"
+
+    @property
+    def banner_row_name(self):
+        return "_row_banner"
---
+++ packages/torchminer/TorchMiner-0.4.14-py3-none-any.whl/TorchMiner/utils.py
@@ -0,0 +1,88 @@
+import io
+from pathlib import Path
+
+import numpy as np
+
+
+def seed_everything(seed):
+    """
+    Fix the seed for generating random numbers.
+    :param seed:
+    :return:
+    """
+    import torch
+    from numpy import random
+
+    torch.manual_seed(seed)
+    # may lead to bad performance
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    random.seed(seed)
+
+    if seed == 0:
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+
+def create_dir(directory, *args):
+    """Create directory"""
+    import os
+
+    current_dir = directory
+    for dir_name in args:
+        current_dir = os.path.join(current_dir, dir_name)
+    if not os.path.isdir(current_dir):
+        os.mkdir(current_dir)
+
+
+def figure2numpy(fig):
+    # Solution to store Matplotlib Figure in TensorBoardX
+    # https://stackoverflow.com/questions/7821518/matplotlib-save-plot-to-numpy-array
+    with io.BytesIO() as buff:
+        fig.savefig(buff, format='raw')
+        buff.seek(0)
+        data = np.frombuffer(buff.getvalue(), dtype=np.uint8)
+    w, h = fig.canvas.get_width_height()
+    im = data.reshape((int(h), int(w), -1))
+    im = im.transpose((2, 0, 1))  # Change to CHW
+    return im
+
+
+def find_resume_target(path: Path, index):
+    """
+
+    :param path: /
+        Miner experiment path
+    :param index: /
+        True: Accept resume auto find result (Promise,default) Only best or latest
+        string/Path: Will use the given checkpoint.
+        int: Choose this epoch in auto find path.
+    :return:
+    """
+    if index is True:
+        search_paths = [
+            path / "best.pth.tar",
+            path / "latest.pth.tar",
+        ]
+    else:
+        index = str(index)
+        if Path(index).is_file():
+            return Path(index)
+
+        if (path / Path(index)).is_file():
+            return path / Path(index)
+        # The checkpoint is not given
+
+        search_paths = [
+            path,
+            path / index,
+            path / f"epoch_{index}.pth.tar",
+            path / f"{index}.pth.tar",
+            *path.glob("*.pth.tar"),
+        ]
+
+    for path in search_paths:
+        if path.is_file():
+            return path
+    print(f"Tried to find Checkpoint in {search_paths} but failed.")
+    return None
