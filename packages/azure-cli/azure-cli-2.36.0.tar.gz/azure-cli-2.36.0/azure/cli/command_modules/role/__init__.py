44be9ae2b6dc174e446e520d5dba40e353979724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:38 +0000

    Add package azure-cli/2.36.0/azure-cli-2.36.0.tar.gz

---
+++ packages/azure-cli/azure-cli-2.36.0.tar.gz/azure-cli-2.36.0/azure/cli/command_modules/role/__init__.py
@@ -0,0 +1,39 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from azure.cli.core import AzCommandsLoader
+from azure.cli.core.profiles import ResourceType
+
+import azure.cli.command_modules.role._help  # pylint: disable=unused-import
+
+
+class RoleCommandsLoader(AzCommandsLoader):
+
+    def __init__(self, cli_ctx=None):
+        from azure.cli.core.commands import CliCommandType
+        role_custom = CliCommandType(operations_tmpl='azure.cli.command_modules.role.custom#{}')
+        super(RoleCommandsLoader, self).__init__(cli_ctx=cli_ctx,
+                                                 resource_type=ResourceType.MGMT_AUTHORIZATION,
+                                                 operation_group='role_assignments',
+                                                 custom_command_type=role_custom)
+
+    def load_command_table(self, args):
+        if args and args[0] in ('role', 'ad'):
+            from knack.log import get_logger
+            logger = get_logger(__name__)
+            logger.warning("The underlying Active Directory Graph API will be replaced by Microsoft Graph API in "
+                           "Azure CLI 2.37.0. "
+                           "Please carefully review all breaking changes introduced during this migration: "
+                           "https://docs.microsoft.com/cli/azure/microsoft-graph-migration")
+        from azure.cli.command_modules.role.commands import load_command_table
+        load_command_table(self, args)
+        return self.command_table
+
+    def load_arguments(self, command):
+        from azure.cli.command_modules.role._params import load_arguments
+        load_arguments(self, command)
+
+
+COMMAND_LOADER_CLS = RoleCommandsLoader
