7ab651c456615b3dd5ccf407e7b55f536bb03ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:43 +0000

    Add package urljoin2/1.0.7/urljoin2-1.0.7.tar.gz

---
+++ packages/urljoin2/urljoin2-1.0.7.tar.gz/urljoin2-1.0.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='urljoin2',
+    version='1.0.7',
+    license='MIT',
+    author="Nagy Zoltán",
+    author_email='zolika3400@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+)
