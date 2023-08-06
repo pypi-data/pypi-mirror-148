9c03470486f386eb4cd9b6e088dc10c5b9aadec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:36 +0000

    Add package nwon-django-toolbox/0.2.8/nwon_django_toolbox-0.2.8-py3-none-any.whl

---
+++ packages/nwon-django-toolbox/nwon_django_toolbox-0.2.8-py3-none-any.whl/nwon_django_toolbox/nwon_django_settings.py
@@ -0,0 +1,42 @@
+from typing import List, Optional
+
+from pydantic import Field
+
+from nwon_django_toolbox.typings.pydantic_base_django import PydanticBaseDjango
+
+
+class NWONDjangoSettings(PydanticBaseDjango):
+    """
+    Settings for the NWON-django-toolbox package.
+
+    These can be set in the Django configuration by using the key NWON_DJANGO and
+    providing a dictionary that resembles this schema.
+    """
+
+    authorization_prefix: str = Field(
+        default="Bearer",
+        description="Authorization prefix for API calls",
+    )
+
+    logger_name: str = Field(
+        default="nwon-django",
+        description="Logger that is used in the whole package",
+    )
+
+    file_encoding: str = Field(
+        default="utf-8",
+        description="Default File encoding used for all file operations",
+    )
+
+    keys_to_skip_on_api_test: List[str] = Field(
+        default=[],
+        description="On some API test helper we check the returned objects against the initial parameters. During this check the given keys are skipped",
+    )
+
+    application_name: Optional[str] = Field(
+        default=None,
+        description="Application name that is used whenever needed",
+    )
+
+
+__all__ = ["NWONDjangoSettings"]
