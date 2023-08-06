0870a6b409855dbd969eb83b1623d8d2aad68567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:12 +0000

    Add package django-plans-paypal/0.4.10/django-plans-paypal-0.4.10.tar.gz

---
+++ packages/django-plans-paypal/django-plans-paypal-0.4.10.tar.gz/django-plans-paypal-0.4.10/plans_paypal/hooks.py
@@ -0,0 +1,83 @@
+import ast
+
+from django.conf import settings
+from paypal.standard.ipn.signals import valid_ipn_received
+from paypal.standard.models import ST_PP_COMPLETED
+from plans.models import Order, Pricing, UserPlan
+
+from .models import PayPalPayment
+
+
+def parse_custom(custom):
+    custom = custom.replace("null", "None")
+    return ast.literal_eval(custom)
+
+
+def receive_ipn(sender, **kwargs):
+    print("paypal hook")
+    ipn_obj = sender
+    print("Payment status: ", ipn_obj.payment_status)
+    print(ipn_obj.receiver_email)
+    print(ipn_obj.txn_type)
+
+    if not (
+        ipn_obj.is_subscription_cancellation() or ipn_obj.is_subscription_payment()
+    ):
+        # Not a subscription
+        return None
+
+    custom = parse_custom(ipn_obj.custom)
+    order = Order.objects.get(pk=custom["first_order_id"])
+    print("Order: ", order.id)
+    user_plan = UserPlan.objects.get(pk=custom["user_plan_id"])
+
+    if ipn_obj.is_subscription_cancellation() and hasattr(user_plan, "recurring"):
+        user_plan.recurring.delete()
+    elif (
+        ipn_obj.is_subscription_payment() and ipn_obj.payment_status == ST_PP_COMPLETED
+    ):
+        # WARNING !
+        # Check that the receiver email is the same we previously
+        # set on the `business` field. (The user could tamper with
+        # that fields on the payment form before it goes to PayPal)
+        print(ipn_obj.receiver_email)
+        bussiness_email = settings.PAYPAL_BUSSINESS_EMAIL
+        if ipn_obj.test_ipn:
+            bussiness_email = settings.PAYPAL_TEST_BUSSINESS_EMAIL
+        if ipn_obj.receiver_email != bussiness_email:
+            # Not a valid payment
+            raise Exception(
+                f"Returned email doesn't match: '{ipn_obj.receiver_email}' != '{bussiness_email}'"
+            )
+
+        # ALSO: for the same reason, you need to check the amount
+        # received, `custom` etc. are all what you expect or what
+        # is allowed.
+
+        # Undertake some action depending upon `ipn_obj`.
+        pricing = Pricing.objects.get(pk=custom["pricing_id"])
+        if order.status == Order.STATUS.COMPLETED:
+            order = Order.objects.create(
+                user=user_plan.user,
+                plan=user_plan.plan,
+                pricing=pricing,
+                amount=ipn_obj.mc_gross,
+                currency=ipn_obj.mc_currency,
+            )
+        user_plan.set_plan_renewal(
+            order,
+            token=ipn_obj.subscr_id,
+            payment_provider="paypal-recurring"
+            + ("-sandbox" if ipn_obj.test_ipn else ""),
+            has_automatic_renewal=True,
+            token_verified=True,
+        )
+        paypal_payment = PayPalPayment.objects.create(
+            paypal_ipn=ipn_obj, user_plan=user_plan, order=order
+        )  # use the new order
+        order.complete_order()
+        return paypal_payment
+    raise Exception(f"IPN with unknown status: {ipn_obj}")
+
+
+valid_ipn_received.connect(receive_ipn)
---
+++ packages/django-plans-paypal/django-plans-paypal-0.4.10.tar.gz/django-plans-paypal-0.4.10/plans_paypal/tests/test_hooks.py
@@ -0,0 +1,80 @@
+from django.test import TestCase
+from model_bakery import baker
+from paypal.standard.models import ST_PP_COMPLETED
+
+from plans_paypal.hooks import parse_custom, receive_ipn
+
+
+class HooksTests(TestCase):
+    def test_receive_ipn_no_subscription(self):
+        ipn = baker.make("PayPalIPN")
+        paypal_payment = receive_ipn(ipn)
+        self.assertEqual(paypal_payment, None)
+
+    def test_parse_custom(self):
+        self.assertEqual(
+            parse_custom(
+                '{"user_plan_id": 250329, "plan_id": 1, '
+                '\'pricing_id\': 1, "first_order_id": 32782, "user_email": null}'
+            ),
+            {
+                "user_plan_id": 250329,
+                "plan_id": 1,
+                "pricing_id": 1,
+                "first_order_id": 32782,
+                "user_email": None,
+            },
+        )
+
+    def test_receive_ipn(self):
+        order = baker.make("Order")
+        user_plan = baker.make("UserPlan")
+        ipn = baker.make(
+            "PayPalIPN",
+            txn_type="subscr_payment",
+            custom="{"
+            f"'first_order_id': {order.id},"
+            f"'user_plan_id': {user_plan.id},"
+            "}",
+        )
+        paypal_payment = receive_ipn(ipn)
+        self.assertEqual(paypal_payment.paypal_ipn, ipn)
+        self.assertEqual(paypal_payment.order, order)
+
+    def test_receive_ipn_completed_email_does_not_match(self):
+        order = baker.make("Order")
+        user_plan = baker.make("UserPlan")
+        ipn = baker.make(
+            "PayPalIPN",
+            txn_type="subscr_payment",
+            payment_status=ST_PP_COMPLETED,
+            custom="{"
+            f"'first_order_id': {order.id},"
+            f"'user_plan_id': {user_plan.id},"
+            "}",
+        )
+        with self.assertRaisesRegex(
+            Exception, "Returned email doesn't match: '' != 'fake@email.com'"
+        ):
+            receive_ipn(ipn)
+
+    def test_receive_ipn_completed(self):
+        user = baker.make("User", username="foobar")
+        user_plan = baker.make("UserPlan", user=user)
+        order = baker.make("Order", user=user)
+        order.user.save()
+        pricing = baker.make("Pricing")
+        ipn = baker.make(
+            "PayPalIPN",
+            txn_type="subscr_payment",
+            payment_status=ST_PP_COMPLETED,
+            receiver_email="fake@email.com",
+            custom="{"
+            f"'first_order_id': {order.id},"
+            f"'user_plan_id': {user_plan.id},"
+            f"'pricing_id': {pricing.id},"
+            "}",
+        )
+        paypal_payment = receive_ipn(ipn)
+        self.assertEqual(paypal_payment.paypal_ipn, ipn)
+        self.assertEqual(paypal_payment.order, order)
